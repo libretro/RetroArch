fed9c1521bcfd8603f66354361c00056fee21e90
twinaphex <autechre1024@hotmail.com>
2006-01-17 04:51:24 +0100

    Use vsnprintf for MSVC 2003

--- libretro-common/compat/compat_snprintf.c
+++ libretro-common/compat/compat_snprintf.c
@@ -34,7 +34,11 @@ int c99_vsnprintf_retro__(char *outBuf, size_t size, const char *format, va_list
    int count = -1;
 
    if (size != 0)
+#if (_MSC_VER <= 1310)
+       count = _vsnprintf(outBuf, _TRUNCATE, format, ap);
+#else
        count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
+#endif
    if (count == -1)
        count = _vscprintf(format, ap);
 
