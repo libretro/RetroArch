b2a041d51e63a88c595dcc82dd24735b9b2bebe7
Themaister <maister@archlinux.us>
2010-05-28 18:21:33 +0200

    Updated some copyright stuff.

--- ssnes.c
+++ ssnes.c
@@ -1,3 +1,21 @@
+/*  SSNES - A Super Ninteno Entertainment System (SNES) Emulator frontend for libsnes.
+ *  Copyright (C) 2010 - Hans-Kristian Arntzen
+ *
+ *  Some code herein may be based on code found in BSNES.
+ * 
+ *  SSNES is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  SSNES is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with SSNES.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <stdbool.h>
 #include <GL/glfw.h>
 #include <samplerate.h>
@@ -24,13 +42,18 @@ extern const video_driver_t video_gl;
 static driver_t driver = {
 #if VIDEO_DRIVER == VIDEO_GL
    .video = &video_gl,
+#else
+#error "Define a video driver in config.h"
 #endif
+
 #if AUDIO_DRIVER == AUDIO_RSOUND
    .audio = &audio_rsound,
 #elif AUDIO_DRIVER == AUDIO_OSS
    .audio = &audio_oss,
 #elif AUDIO_DRIVER == AUDIO_ALSA
    .audio = &audio_alsa,
+#else
+#error "Define an audio driver in config.h"
 #endif
 };
 
