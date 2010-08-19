d4536d61a7287e6daadc0d07a033dab0fc0852a0
Themaister <maister@archlinux.us>
2010-08-19 16:03:53 +0200

    Fixes.

--- config.h
+++ config.h
@@ -53,8 +53,8 @@ static const float yscale = 4.0; // Real y res = 224 * yscale
 
 // Fullscreen
 static bool fullscreen = false; // To start in Fullscreen or not
-static const unsigned fullscreen_x = 1920;
-static const unsigned fullscreen_y = 1200;
+static const unsigned fullscreen_x = 1280;
+static const unsigned fullscreen_y = 720;
 
 // Video VSYNC (recommended)
 static const bool vsync = true;
@@ -84,7 +84,7 @@ static const bool force_aspect = true;
 static const bool audio_enable = true;
 
 // Output samplerate
-static const unsigned out_rate = 96000; 
+static const unsigned out_rate = 48000; 
 
 // Input samplerate from libSNES. 
 // Lower this (slightly) if you are experiencing frequent audio dropouts and vsync is enabled.
