5eccffced322e7912b6ec7db85dd80f9a97b42bb
Themaister <maister@archlinux.us>
2010-08-28 16:45:06 +0200

    :v

--- hqflt/ntsc.c
+++ hqflt/ntsc.c
@@ -3,11 +3,14 @@
 
 void ntsc_filter(uint16_t * restrict out, const uint16_t * restrict in, int width, int height)
 {
+   static snes_ntsc_t *ntsc = NULL;
    static int phase = 0;
-   snes_ntsc_t *ntsc = calloc(1, sizeof(snes_ntsc_t));
-   snes_ntsc_init(ntsc, &snes_ntsc_composite);
+   if (ntsc == NULL)
+   {
+      ntsc = calloc(1, sizeof(snes_ntsc_t));
+      snes_ntsc_init(ntsc, &snes_ntsc_composite);
+   }
 
    snes_ntsc_blit(ntsc, in, width, phase, width, height, out, SNES_NTSC_OUT_WIDTH(width) * sizeof(uint16_t));
-   free(ntsc);
    phase = (phase + 1) % 4;
 }
