d679f8b24f26c4e1aef4318a20e5c46f4d90e94b
Themaister <maister@archlinux.us>
2011-04-26 19:00:50 +0200

    v0.4.1 updates.

--- ssnes.c
+++ ssnes.c
@@ -249,7 +249,7 @@ static void fill_pathname_noext(char *out_path, const char *in_path, const char 
 #endif
 
 #ifdef _WIN32
-#define PACKAGE_VERSION "0.4.1-beta"
+#define PACKAGE_VERSION "0.4.1"
 #endif
 
 #include "config.features.h"
