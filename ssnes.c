74782a1bea6a17eb3d5d73b05430e97acb4e3d86
Themaister <maister@archlinux.us>
2011-09-01 10:06:18 +0200

    Clarify.

--- ssnes.c
+++ ssnes.c
@@ -401,7 +401,7 @@ static void print_help(void)
 #endif
 
 #ifdef HAVE_FFMPEG
-   puts("\t-r/--record: Path to record video file.\n\t\tSettings for video/audio codecs are found in config file.");
+   puts("\t-r/--record: Path to record video file.\n\t\tUsing .mkv extension is recommended, and codecs used are FFV1/FLAC.");
 #endif
    puts("\t-v/--verbose: Verbose logging.");
    puts("\t-U/--ups: Specifies path for UPS patch that will be applied to ROM.");
