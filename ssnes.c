a79a1b46f72f75719a1e06e3108b345d4d6ce276
Themaister <maister@archlinux.us>
2011-10-18 19:12:28 +0200

    Updates for 0.9-rc4.

--- ssnes.c
+++ ssnes.c
@@ -355,7 +355,7 @@ static int16_t input_state(bool port, unsigned device, unsigned index, unsigned 
 #endif
 
 #ifdef _WIN32
-#define PACKAGE_VERSION "0.9-rc3"
+#define PACKAGE_VERSION "0.9-rc4"
 #endif
 
 #include "config.features.h"
