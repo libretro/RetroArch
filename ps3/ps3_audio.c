a5079bdda1ba9df6b8da28700117a1a5b1c22eff
Themaister <maister@archlinux.us>
2011-12-02 15:50:51 +0100

    Should improve the state of audio.

--- ps3/ps3_audio.c
+++ ps3/ps3_audio.c
@@ -16,54 +16,30 @@
  */
 
 #include "../driver.h"
+#include "../general.h"
 #include <stdlib.h>
 #include <cell/audio.h>
 #include <sys/timer.h>
 #include <string.h>
 #include <pthread.h>
-#include "buffer.h"
-#include "resampler.h"
+#include "../fifo_buffer.h"
 #include <sys/event.h>
 
 #define AUDIO_BLOCKS 8 // 8 or 16. Guess what we choose? :)
 #define AUDIO_CHANNELS 2 // All hail glorious stereo!
-#define AUDIO_OUT_RATE (48000.0)
 
 typedef struct
 {
-   float tmp_data[CELL_AUDIO_BLOCK_SAMPLES * AUDIO_CHANNELS];
    uint32_t audio_port;
    bool nonblocking;
    volatile bool quit_thread;
    fifo_buffer_t *buffer;
-   uint64_t input_rate;
 
    pthread_t thread;
    pthread_mutex_t lock;
    pthread_mutex_t cond_lock;
    pthread_cond_t cond;
 } ps3_audio_t;
-
-static size_t drain_fifo(void *cb_data, float **data)
-{
-   ps3_audio_t *aud = cb_data;
-
-   int16_t tmp[CELL_AUDIO_BLOCK_SAMPLES * AUDIO_CHANNELS];
-
-   if (fifo_read_avail(aud->buffer) >= sizeof(tmp))
-   {
-      pthread_mutex_lock(&aud->lock);
-      fifo_read(aud->buffer, tmp, sizeof(tmp));
-      pthread_mutex_unlock(&aud->lock);
-      resampler_s16_to_float(aud->tmp_data, tmp, CELL_AUDIO_BLOCK_SAMPLES * AUDIO_CHANNELS);
-   }
-   else
-   {
-      memset(aud->tmp_data, 0, sizeof(aud->tmp_data));
-   }
-   *data = aud->tmp_data;
-   return CELL_AUDIO_BLOCK_SAMPLES;
-}
 
 static void *event_loop(void *data)
 {
@@ -75,37 +51,41 @@ static void *event_loop(void *data)
    cellAudioCreateNotifyEventQueue(&id, &key);
    cellAudioSetNotifyEventQueue(key);
 
-   resampler_t *resampler = resampler_new(drain_fifo, AUDIO_OUT_RATE/aud->input_rate, 2, data);
-
    float out_tmp[CELL_AUDIO_BLOCK_SAMPLES * AUDIO_CHANNELS] __attribute__((aligned(16)));
 
    while (!aud->quit_thread)
    {
       sys_event_queue_receive(id, &event, SYS_NO_TIMEOUT);
-      resampler_cb_read(resampler, CELL_AUDIO_BLOCK_SAMPLES, out_tmp);
+
+      pthread_mutex_lock(&aud->lock);
+      if (fifo_read_avail(aud->buffer) >= sizeof(out_tmp))
+         fifo_read(aud->buffer, out_tmp, sizeof(out_tmp));
+      else
+         memset(out_tmp, 0, sizeof(out_tmp));
+      pthread_mutex_unlock(&aud->lock);
+
       cellAudioAddData(aud->audio_port, out_tmp, CELL_AUDIO_BLOCK_SAMPLES, 1.0);
       pthread_cond_signal(&aud->cond);
    }
 
    cellAudioRemoveNotifyEventQueue(key);
-   resampler_free(resampler);
    pthread_exit(NULL);
    return NULL;
 }
 
-static void* __ps3_init(const char* device, unsigned rate, unsigned latency)
+static void *ps3_audio_init(const char *device, unsigned rate, unsigned latency)
 {
    (void)latency;
    (void)device;
+   (void)rate; // Always use 48kHz.
+   g_settings.audio.out_rate = 48000.0;
 
    ps3_audio_t *data = calloc(1, sizeof(*data));
-   if (data == NULL)
+   if (!data)
       return NULL;
 
    CellAudioPortParam params;
-
    cellAudioInit();
-
    params.nChannel = AUDIO_CHANNELS;
    params.nBlock = AUDIO_BLOCKS;
    params.attr = 0;
@@ -113,12 +93,11 @@ static void* __ps3_init(const char* device, unsigned rate, unsigned latency)
    if (cellAudioPortOpen(&params, &data->audio_port) != CELL_OK)
    {
       cellAudioQuit();
+      free(data);
       return NULL;
    }
 
-   // Create a small fifo buffer. :)
-   data->buffer = fifo_new(CELL_AUDIO_BLOCK_SAMPLES * AUDIO_CHANNELS * AUDIO_BLOCKS * sizeof(int16_t));
-   data->input_rate = rate;
+   data->buffer = fifo_new(CELL_AUDIO_BLOCK_SAMPLES * AUDIO_CHANNELS * AUDIO_BLOCKS * sizeof(float));
 
    pthread_mutex_init(&data->lock, NULL);
    pthread_mutex_init(&data->cond_lock, NULL);
@@ -129,12 +108,10 @@ static void* __ps3_init(const char* device, unsigned rate, unsigned latency)
    return data;
 }
 
-// Should make some noise at least. :)
-static ssize_t __ps3_write(void* data, const void* buf, size_t size) // Recieve exactly 1024 bytes at a time.
+static ssize_t ps3_audio_write(void *data, const void *buf, size_t size)
 {
    ps3_audio_t *aud = data;
 
-   // We will continuously write slightly more data than we should per second, and rely on blocking mechanisms to ensure we don't write too much. 
    if (aud->nonblocking)
    {
       if (fifo_write_avail(aud->buffer) < size)
@@ -156,31 +133,32 @@ static ssize_t __ps3_write(void* data, const void* buf, size_t size) // Recieve 
    return size;
 }
 
-static bool __ps3_stop(void *data)
+static bool ps3_audio_stop(void *data)
 {
-   //ps3_audio_t *aud = data;
-   //cellAudioPortStop(aud->audio_port);
+   ps3_audio_t *aud = data;
+   cellAudioPortStop(aud->audio_port);
    return true;
 }
 
-static bool __ps3_start(void *data)
+static bool ps3_audio_start(void *data)
 {
-   //ps3_audio_t *aud = data;
-   //cellAudioPortStart(aud->audio_port);
+   ps3_audio_t *aud = data;
+   cellAudioPortStart(aud->audio_port);
    return false;
 }
 
-static void __ps3_set_nonblock_state(void *data, bool state)
+static void ps3_audio_set_nonblock_state(void *data, bool state)
 {
    ps3_audio_t *aud = data;
    aud->nonblocking = state;
 }
 
-static void __ps3_free(void *data)
+static void ps3_audio_free(void *data)
 {
    ps3_audio_t *aud = data;
 
    aud->quit_thread = true;
+   cellAudioPortStart(aud->audio_port);
    pthread_join(aud->thread, NULL);
 
    cellAudioPortStop(aud->audio_port);
@@ -195,12 +173,20 @@ static void __ps3_free(void *data)
    free(data);
 }
 
+static bool ps3_audio_use_float(void *data)
+{
+   (void)data;
+   return true;
+}
+
 const audio_driver_t audio_ps3 = {
-   .init = __ps3_init,
-   .write = __ps3_write,
-   .stop = __ps3_stop,
-   .start = __ps3_start,
-   .set_nonblock_state = __ps3_set_nonblock_state,
-   .free = __ps3_free,
+   .init = ps3_audio_init,
+   .write = ps3_audio_write,
+   .stop = ps3_audio_stop,
+   .start = ps3_audio_start,
+   .set_nonblock_state = ps3_audio_set_nonblock_state,
+   .use_float = ps3_audio_use_float,
+   .free = ps3_audio_free,
    .ident = "ps3"
 };
+
