a207549f18144bbe482593d7fbefcfda27a960e9
Themaister <maister@archlinux.us>
2011-12-02 01:59:44 +0100

    fixup stub.

--- ps3/main.c
+++ ps3/main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
    char arg1[] = "ssnes";
    char arg2[] = "/dev_hdd0/game/SNES90000/USRDIR/main.sfc";
    char arg3[] = "-v";
-   char *argv_[] = { arg1, arg2, NULL };
-   return ssnes_main(2, argv_);
+   char *argv_[] = { arg1, arg2, arg3, NULL };
+   return ssnes_main(3, argv_);
 }
 
