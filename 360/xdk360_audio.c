a287ebe736a06f322566d610d33b4882098ac955
TwinAphex51224 <twinaphex1@gmail.com>
2012-01-05 14:17:35 +0100

    (360) New Xaudio2 driver

--- 360/xdk360_audio.c
+++ 360/xdk360_audio.c
@@ -1,107 +1,94 @@
-/*
-	Simple C interface for XAudio2
-	Author: Hans-Kristian Arntzen
-	License: Public Domain
-*/
+/*  SSNES - A Super Nintendo Entertainment System (SNES) Emulator frontend for libsnes.
+ *  Copyright (C) 2010-2011 - Hans-Kristian Arntzen
+ *
+ *  Some code herein may be based on code found in BSNES.
+ * 
+ *  SSNES is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  SSNES is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with SSNES.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include <stdint.h>
+#include "../driver.h"
+#include <stdlib.h>
 #include <xtl.h>
 #include <xaudio2.h>
-#include "xdk360_audio.h"
-#include "msvc_compat.h"
+#include "../general.h"
 
-#define MAX_BUFFERS 16
-#define MAX_BUFFERS_MASK (MAX_BUFFERS - 1)
-
-#undef min
-#undef max
-#define min(a, b) ((a) < (b) ? (a) : (b))
-#define max(a, b) ((a) > (b) ? (a) : (b))
-
-struct xaudio2
+typedef struct
 {
-   const IXAudio2VoiceCallbackVtbl *lpVtbl;
-
-   uint8_t *buf;
-   IXAudio2 *pXAudio2;
-   IXAudio2MasteringVoice *pMasterVoice;
+   IXAudio2 * pXAudio2;
+   IXAudio2MasteringVoice *pMasteringVoice;
    IXAudio2SourceVoice *pSourceVoice;
-   HANDLE hEvent;
-
-   volatile long buffers;
+   bool nonblock;
    unsigned bufsize;
-   unsigned bufptr;
-   unsigned write_buffer;
-};
+   volatile long buffers;
+} xa_t;
 
-static void WINAPI voice_on_buffer_end(void *handle_, void *data)
+static void *xa_init(const char *device, unsigned rate, unsigned latency)
 {
-   (void)data;
-   xaudio2_t *handle = (xaudio2_t*)handle_;
-   InterlockedDecrement(&handle->buffers);
-   SetEvent(handle->hEvent);
-}
+   HRESULT hr;
+   unsigned flags;
 
-static void WINAPI dummy_voidp(void *handle, void *data) { (void)handle; (void)data; }
-static void WINAPI dummy_nil(void *handle) { (void)handle; }
-static void WINAPI dummy_uint32(void *handle, UINT32 dummy) { (void)handle; (void)dummy; }
-static void WINAPI dummy_voidp_hresult(void *handle, void *data, HRESULT dummy) { (void)handle; (void)data; (void)dummy; }
-   
-xaudio2_t *xaudio2_new(unsigned samplerate, unsigned channels, size_t size)
-{
-   xaudio2_t *handle = (xaudio2_t*)calloc(1, sizeof(*handle));
-   if (!handle)
-      return NULL;
+   if (latency < 8)
+      latency = 8; // Do not allow shenanigans.
 
-   handle->lpVtbl = &voice_vtable;
-   WAVEFORMATEX wfx = {0};
+   xa_t *xa = (xa_t*)calloc(1, sizeof(*xa));
+   if (!xa)
+   	goto error;
 
-   if (FAILED(XAudio2Create(&handle->pXAudio2)))
+   size_t bufsize = latency * rate / 1000;
+   size_t size = busize * 2 * sizeof(float);
+
+   SSNES_LOG("XAudio2: Requesting %d ms latency, using %d ms latency.\n", latency, (int)bufsize * 1000 / rate);
+
+   if( FAILED( hr = XAudio2Create(&xa->pXAudio2, flags)))
+   	goto error;
+
+   if (FAILED(IXAudio2_CreateMasteringVoice(xa->pXAudio2,
+   &xa->pMasterVoice, 2, rate, 0, 0, 0)))
       goto error;
 
-   if (FAILED(IXAudio2_CreateMasteringVoice(handle->pXAudio2,
-               &handle->pMasterVoice, channels, samplerate, 0, 0, 0)))
-      goto error;
+   WAVEFORMATEXTENSIBLE wfx = {0};
 
    wfx.wFormatTag = WAVE_FORMAT_IEEE_FLOAT;
-   wfx.nChannels = channels;
-   wfx.nSamplesPerSec = samplerate;
+   wfx.nChannels = 2;
+   wfx.nSamplesPerSec = rate;
    wfx.nBlockAlign = channels * sizeof(float);
    wfx.wBitsPerSample = sizeof(float) * 8;
    wfx.nAvgBytesPerSec = wfx.nSamplesPerSec * wfx.nBlockAlign;
    wfx.cbSize = 0;
 
-   if (FAILED(IXAudio2_CreateSourceVoice(handle->pXAudio2,
-               &handle->pSourceVoice, &wfx,
-               XAUDIO2_VOICE_NOSRC, XAUDIO2_DEFAULT_FREQ_RATIO,
-               (IXAudio2VoiceCallback*)handle, 0, 0)))
-      goto error;
+   if( FAILED( hr = xa->pXAudio2->CreateSourceVoice( &xa->pSourceVoice, ( WAVEFORMATEX*)&wfx ) ) )
+   	goto error;
 
-   handle->hEvent = CreateEvent(0, FALSE, FALSE, 0);
-   if (!handle->hEvent)
-      goto error;
+   xa->bufsize = size / MAX_BUFFERS;
+   xa->buf = (uint8_t*)calloc(1, xa->bufsize * MAX_BUFFERS);
 
-   handle->bufsize = size / MAX_BUFFERS;
-   handle->buf = (uint8_t*)calloc(1, handle->bufsize * MAX_BUFFERS);
-   if (!handle->buf)
-      goto error;
+   if (!xa->buf)
+   	goto error;
 
-   if (FAILED(IXAudio2SourceVoice_Start(handle->pSourceVoice, 0, XAUDIO2_COMMIT_NOW)))
-      goto error;
+   if( FAILED(IXAUdio2SourceVoice_Start(xa->pSourceVoice, 0, XAUDIO2_COMMIT_NOW)))
+        goto error;
 
-   return handle;
+   if (!xa->xa)
+	   goto error;
+
+   return xa;
 
 error:
-   xaudio2_free(handle);
+   SSNES_ERR("Failed to init XAudio2.\n");
+   free(xa);
    return NULL;
 }
 
-size_t xaudio2_write_avail(xaudio2_t *handle)
-{
-   return handle->bufsize * (MAX_BUFFERS - handle->buffers - 1);
-}
-
-size_t xaudio2_write(xaudio2_t *handle, const void *buf, size_t bytes_)
+static size_t xaudio2_write(xa_t *handle, const void *buf, size_t bytes_)
 {
    unsigned bytes = bytes_;
    const uint8_t *buffer = (const uint8_t*)buf;
@@ -136,7 +123,49 @@ size_t xaudio2_write(xaudio2_t *handle, const void *buf, size_t bytes_)
    return bytes_;
 }
 
-void xaudio2_free(xaudio2_t *handle)
+static ssize_t xa_write(void *data, const void *buf, size_t size)
+{
+   xa_t *xa = (xa_t*)data;
+   if (xa->nonblock)
+   {
+      size_t avail = (xa->bufsize * (MAX_BUFFERS - xa->buffers - 1);
+      if (avail == 0)
+         return 0;
+      if (avail < size)
+         size = avail;
+   }
+
+   size_t ret = xaudio2_write(xa->pXAudio2, buf, size);
+   if (ret == 0)
+      return -1;
+   return ret;
+}
+
+static bool xa_stop(void *data)
+{
+   (void)data;
+   return true;
+}
+
+static void xa_set_nonblock_state(void *data, bool state)
+{
+   xa_t *xa = (xa_t*)data;
+   xa->nonblock = state;
+}
+
+static bool xa_start(void *data)
+{
+   (void)data;
+   return true;
+}
+
+static bool xa_use_float(void *data)
+{
+   (void)data;
+   return true;
+}
+
+static void xaudio2_free(xa_t *handle)
 {
    if (handle)
    {
@@ -160,3 +189,24 @@ void xaudio2_free(xaudio2_t *handle)
    }
 }
 
+static void xa_free(void *data)
+{
+   xa_t *xa = (xa_t*)data;
+   if (xa)
+   {
+      if (xa->xa)
+         xaudio2_free(xa->xa);
+      free(xa);
+   }
+}
+
+const audio_driver_t audio_xa = {
+   xa_init,
+   xa_write,
+   xa_stop,
+   xa_start,
+   xa_set_nonblock_state,
+   xa_free,
+   xa_use_float,
+   "xdk360_xaudio"
+};
