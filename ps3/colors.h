c4b24928720646cb5c6e9c739581264bda97844e
TwinAphex51224 <twinaphex1@gmail.com>
2012-01-16 04:09:28 +0100

    (PS3) Some cleanups

--- ps3/colors.h
+++ ps3/colors.h
@@ -1,10 +1,23 @@
-/******************************************************************************* 
- * colors.h - SNES9x PS3
+/*  SSNES - A Super Nintendo Entertainment System (SNES) Emulator frontend for libsnes.
+ *  Copyright (C) 2010-2012 - Hans-Kristian Arntzen
+ *  Copyright (C) 2011-2012 - Daniel De Matteis
  *
- *  Created on: Oct 20, 2010
-********************************************************************************/
+ *  Some code herein may be based on code found in BSNES.
+ * 
+ *  SSNES is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  SSNES is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with SSNES.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-// define some colours for dbgfont output (remember it uses ABGR colour format)
+/* ABGR color format */
+
 #define WHITE		0xffffffffu
 #define RED		0xff0000ffu
 #define GREEN		0xff00ff00u
