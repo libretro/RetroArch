98f2f1c23d2cb13b39ef73e8ddbd8f57458dc7e8
Themaister <maister@archlinux.us>
2012-01-16 22:06:30 +0100

    Updates for 0.9.4.

--- qb/config.params.sh
+++ qb/config.params.sh
@@ -1,7 +1,7 @@
 . qb/qb.params.sh
 
 PACKAGE_NAME=ssnes
-PACKAGE_VERSION=0.9.3
+PACKAGE_VERSION=0.9.4
 
 # Adds a command line opt to ./configure --help
 # $1: Variable (HAVE_ALSA, HAVE_OSS, etc)   
