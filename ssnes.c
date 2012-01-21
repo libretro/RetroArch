99f622de0e8cb284a0603120fafd87d7dec6f408
Themaister <maister@archlinux.us>
2012-01-21 12:12:44 +0100

    Just rename video_render_cached().

--- ssnes.c
+++ ssnes.c
@@ -95,7 +95,6 @@ static inline unsigned lines_to_pitch(unsigned height)
       return g_extern.system.pitch;
 }
 
-static void video_cached_frame(void);
 static void take_screenshot(void)
 {
    if (!(*g_settings.screenshot_directory))
@@ -130,7 +129,7 @@ static void take_screenshot(void)
    if (g_extern.is_paused)
    {
       msg_queue_push(g_extern.msg_queue, msg, 1, 1);
-      video_cached_frame();
+      ssnes_render_cached_frame();
    }
    else
       msg_queue_push(g_extern.msg_queue, msg, 1, 180);
@@ -222,7 +221,7 @@ static void video_frame(const uint16_t *data, unsigned width, unsigned height)
    g_extern.frame_cache.height = height;
 }
 
-static void video_cached_frame(void)
+void ssnes_render_cached_frame(void)
 {
 #ifdef HAVE_FFMPEG
    // Cannot allow FFmpeg recording when pushing duped frames.
@@ -235,8 +234,6 @@ static void video_cached_frame(void)
    // It would be really stupid at any rate ...
    if (g_extern.frame_cache.data)
    {
-      // Push the pipeline through. A hack sort of.
-      SSNES_LOG("Pushing cached frame.\n");
       video_frame(g_extern.frame_cache.data,
             g_extern.frame_cache.width,
             g_extern.frame_cache.height);
@@ -1973,11 +1970,6 @@ static void check_mute(void)
    old_pressed = pressed;
 }
 
-void ssnes_render_cached_frame(void)
-{
-   video_cached_frame();
-}
-
 static void do_state_checks(void)
 {
    check_screenshot();
@@ -1991,7 +1983,7 @@ static void do_state_checks(void)
       check_oneshot();
 
       if (check_fullscreen() && g_extern.is_paused)
-         video_cached_frame();
+         ssnes_render_cached_frame();
 
       if (g_extern.is_paused && !g_extern.is_oneshot)
          return;
