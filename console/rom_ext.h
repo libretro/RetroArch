dd1d83768b181f313ff46dc4e646a3919adcfce7
Themaister <maister@archlinux.us>
2012-01-25 01:10:53 +0100

    Add stubs for future rom_ext handling.

---
+++ console/rom_ext.h
@@ -0,0 +1,27 @@
+/*  SSNES - A Super Nintendo Entertainment System (SNES) Emulator frontend for libsnes.
+ *  Copyright (C) 2010-2012 - Hans-Kristian Arntzen
+ *
+ *  Some code herein may be based on code found in BSNES.
+ * 
+ *  SSNES is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  SSNES is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with SSNES.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ROM_EXT_H__
+#define ROM_EXT_H__
+
+// Get rom extensions for current library.
+// Infers info from snes_library_id().
+// Returns NULL if library doesn't have any preferences in particular.
+const char *ssnes_console_get_rom_ext(void);
+
+#endif
+
