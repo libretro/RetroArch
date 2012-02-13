77e55bb577e8cd9de7c2661a2b255d00dabc6903
Themaister <maister@archlinux.us>
2012-02-13 19:17:13 +0100

    Add some video callbacks for consoles.

--- driver.h
+++ driver.h
@@ -141,6 +141,12 @@ typedef struct video_driver
    bool (*xml_shader)(void *data, const char *path); // Sets XML-shader. Might not be implemented.
    void (*free)(void *data);
    const char *ident;
+
+   // Callbacks essentially useless on PC, but useful on consoles where the drivers are used for more stuff.
+#ifdef SSNES_CONSOLE
+   void (*set_swap_block_state)(void *data, bool toggle); // Block swapping from being called in ::frame().
+   void (*swap)(void *data) // Explicitly swap buffers. Only useful when set_swap_block_state() is set to true.
+#endif
 } video_driver_t;
 
 typedef struct driver
