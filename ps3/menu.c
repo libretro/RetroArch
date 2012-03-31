ed75cb80313d41387967782a4542ca692924213d
Twinaphex <autechre1024@hotmail.com>
2012-04-01 00:13:07 +0200

    (PS3) Fixed 'Set default Libsnes core' option

--- ps3/menu.c
+++ ps3/menu.c
@@ -940,8 +940,8 @@ static void select_file(uint32_t menu_id)
 					case BORDER_CHOICE:
 						break;
 					case LIBSNES_CHOICE:
-						strlcpy(g_console.launch_app_on_exit, path, 
-						sizeof(g_console.launch_app_on_exit));
+						strlcpy(g_settings.libsnes, path, 
+						sizeof(g_settings.libsnes));
 						g_console.return_to_launcher = true;
 						g_console.menu_enable = false;
 						g_console.mode_switch = MODE_EXIT;
