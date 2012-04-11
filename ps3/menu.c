61cea3f0270125a435b6d01996210a15f9ee3400
Twinaphex <autechre1024@hotmail.com>
2012-04-11 03:32:58 +0200

    (PS3) Some indenting nits

--- ps3/menu.c
+++ ps3/menu.c
@@ -53,118 +53,118 @@ uint32_t set_shader = 0;
 static uint32_t currently_selected_controller_menu = 0;
 
 static menu menu_filebrowser = {
-	"FILE BROWSER |",		/* title*/
-	FILE_BROWSER_MENU,		/* enum*/
-	0,				/* selected item*/
-	0,				/* page*/
-	1,				/* maxpages */
-	1,				/* refreshpage*/
-	NULL				/* items*/
+   "FILE BROWSER |",		/* title*/
+   FILE_BROWSER_MENU,		/* enum*/
+   0,				/* selected item*/
+   0,				/* page*/
+   1,				/* maxpages */
+   1,				/* refreshpage*/
+   NULL				/* items*/
 };
 
 static menu menu_generalvideosettings = {
-	"VIDEO |",			/* title*/
-	GENERAL_VIDEO_MENU,		/* enum*/
-	FIRST_VIDEO_SETTING,		/* selected item*/
-	0,				/* page*/
-	MAX_NO_OF_VIDEO_SETTINGS/NUM_ENTRY_PER_PAGE,	/* max pages */
-	1,				/* refreshpage*/
-	FIRST_VIDEO_SETTING,		/* first setting*/
-	MAX_NO_OF_VIDEO_SETTINGS,	/* max no of path settings*/
-	items_generalsettings		/* items*/
+   "VIDEO |",			/* title*/
+   GENERAL_VIDEO_MENU,		/* enum*/
+   FIRST_VIDEO_SETTING,		/* selected item*/
+   0,				/* page*/
+   MAX_NO_OF_VIDEO_SETTINGS/NUM_ENTRY_PER_PAGE,	/* max pages */
+   1,				/* refreshpage*/
+   FIRST_VIDEO_SETTING,		/* first setting*/
+   MAX_NO_OF_VIDEO_SETTINGS,	/* max no of path settings*/
+   items_generalsettings		/* items*/
 };
 
 static menu menu_generalaudiosettings = {
-	"AUDIO |",			/* title*/
-	GENERAL_AUDIO_MENU,		/* enum*/
-	FIRST_AUDIO_SETTING,		/* selected item*/
-	0,				/* page*/
-	MAX_NO_OF_AUDIO_SETTINGS/NUM_ENTRY_PER_PAGE,	/* max pages */
-	1,				/* refreshpage*/
-	FIRST_AUDIO_SETTING,		/* first setting*/
-	MAX_NO_OF_AUDIO_SETTINGS,	/* max no of path settings*/
-	items_generalsettings		/* items*/
+   "AUDIO |",			/* title*/
+   GENERAL_AUDIO_MENU,		/* enum*/
+   FIRST_AUDIO_SETTING,		/* selected item*/
+   0,				/* page*/
+   MAX_NO_OF_AUDIO_SETTINGS/NUM_ENTRY_PER_PAGE,	/* max pages */
+   1,				/* refreshpage*/
+   FIRST_AUDIO_SETTING,		/* first setting*/
+   MAX_NO_OF_AUDIO_SETTINGS,	/* max no of path settings*/
+   items_generalsettings		/* items*/
 };
 
 static menu menu_emu_settings = {
-	EMU_MENU_TITLE,						/* title*/
-	EMU_GENERAL_MENU,					/* enum*/
-	FIRST_EMU_SETTING,					/* selected item*/
-	0,							/* page*/
-	MAX_NO_OF_EMU_SETTINGS/NUM_ENTRY_PER_PAGE,		/* max pages*/
-	1,                      				/* refreshpage*/
-	FIRST_EMU_SETTING,					/* first setting*/
-	MAX_NO_OF_EMU_SETTINGS,					/* max no of path settings*/
-	items_generalsettings					/* items*/
+   EMU_MENU_TITLE,					/* title*/
+   EMU_GENERAL_MENU,					/* enum*/
+   FIRST_EMU_SETTING,					/* selected item*/
+   0,							/* page*/
+   MAX_NO_OF_EMU_SETTINGS/NUM_ENTRY_PER_PAGE,		/* max pages*/
+   1,                      				/* refreshpage*/
+   FIRST_EMU_SETTING,					/* first setting*/
+   MAX_NO_OF_EMU_SETTINGS,				/* max no of path settings*/
+   items_generalsettings				/* items*/
 };
 
 static menu menu_emu_videosettings = {
-	VIDEO_MENU_TITLE,					/* title*/
-	EMU_VIDEO_MENU,						/* enum */
-	FIRST_EMU_VIDEO_SETTING,				/* selected item*/
-	0,							/* page*/
-	MAX_NO_OF_EMU_VIDEO_SETTINGS/NUM_ENTRY_PER_PAGE,	/* max pages */
-	1,							/* refreshpage*/
-	FIRST_EMU_VIDEO_SETTING,				/* first setting*/
-	MAX_NO_OF_EMU_VIDEO_SETTINGS,				/* max no of settings*/
-	items_generalsettings					/* items*/
+   VIDEO_MENU_TITLE,					/* title*/
+   EMU_VIDEO_MENU,					/* enum */
+   FIRST_EMU_VIDEO_SETTING,				/* selected item*/
+   0,							/* page*/
+   MAX_NO_OF_EMU_VIDEO_SETTINGS/NUM_ENTRY_PER_PAGE,	/* max pages */
+   1,							/* refreshpage*/
+   FIRST_EMU_VIDEO_SETTING,				/* first setting*/
+   MAX_NO_OF_EMU_VIDEO_SETTINGS,			/* max no of settings*/
+   items_generalsettings				/* items*/
 };
 
 static menu menu_emu_audiosettings = {
-	AUDIO_MENU_TITLE,					/* title*/
-	EMU_AUDIO_MENU,						/* enum*/
-	FIRST_EMU_AUDIO_SETTING,				/* selected item*/
-	0,							/* page*/
-	MAX_NO_OF_EMU_AUDIO_SETTINGS/NUM_ENTRY_PER_PAGE,	/* max pages*/
-	1,							/* refreshpage*/
-	FIRST_EMU_AUDIO_SETTING,				/* first setting*/
-	MAX_NO_OF_EMU_AUDIO_SETTINGS,				/* max no of path settings*/
-	items_generalsettings					/* items*/
+   AUDIO_MENU_TITLE,					/* title*/
+   EMU_AUDIO_MENU,					/* enum*/
+   FIRST_EMU_AUDIO_SETTING,				/* selected item*/
+   0,							/* page*/
+   MAX_NO_OF_EMU_AUDIO_SETTINGS/NUM_ENTRY_PER_PAGE,	/* max pages*/
+   1,							/* refreshpage*/
+   FIRST_EMU_AUDIO_SETTING,				/* first setting*/
+   MAX_NO_OF_EMU_AUDIO_SETTINGS,			/* max no of path settings*/
+   items_generalsettings				/* items*/
 };
 
 static menu menu_pathsettings = {
-	"PATH |",						/* title*/
-	PATH_MENU,						/* enum*/
-	FIRST_PATH_SETTING,					/* selected item*/
-	0,							/* page*/
-	MAX_NO_OF_PATH_SETTINGS/NUM_ENTRY_PER_PAGE,		/* max pages*/
-	1,							/* refreshpage*/
-	FIRST_PATH_SETTING,					/* first setting*/
-	MAX_NO_OF_PATH_SETTINGS,				/* max no of path settings*/
-	items_generalsettings					/* items*/
+   "PATH |",						/* title*/
+   PATH_MENU,						/* enum*/
+   FIRST_PATH_SETTING,					/* selected item*/
+   0,							/* page*/
+   MAX_NO_OF_PATH_SETTINGS/NUM_ENTRY_PER_PAGE,		/* max pages*/
+   1,							/* refreshpage*/
+   FIRST_PATH_SETTING,					/* first setting*/
+   MAX_NO_OF_PATH_SETTINGS,				/* max no of path settings*/
+   items_generalsettings				/* items*/
 };
 
 static menu menu_controlssettings = {
-	"CONTROLS |",						/* title */
-	CONTROLS_MENU,						/* enum */
-	FIRST_CONTROLS_SETTING_PAGE_1,				/* selected item */
-	0,							/* page */
-	MAX_NO_OF_CONTROLS_SETTINGS/NUM_ENTRY_PER_PAGE,		/* max pages */
-	1,							/* refreshpage */
-	FIRST_CONTROLS_SETTING_PAGE_1,				/* first setting */
-	MAX_NO_OF_CONTROLS_SETTINGS,				/* max no of path settings*/
-	items_generalsettings					/* items */
+   "CONTROLS |",						/* title */
+   CONTROLS_MENU,						/* enum */
+   FIRST_CONTROLS_SETTING_PAGE_1,				/* selected item */
+   0,								/* page */
+   MAX_NO_OF_CONTROLS_SETTINGS/NUM_ENTRY_PER_PAGE,		/* max pages */
+   1,								/* refreshpage */
+   FIRST_CONTROLS_SETTING_PAGE_1,				/* first setting */
+   MAX_NO_OF_CONTROLS_SETTINGS,					/* max no of path settings*/
+   items_generalsettings					/* items */
 };
 
 static void display_menubar(uint32_t menu_enum)
 {
-	cellDbgFontPuts    (0.09f,  0.05f,  FONT_SIZE,  menu_enum == GENERAL_VIDEO_MENU ? RED : GREEN,   menu_generalvideosettings.title);
-	cellDbgFontPuts    (0.19f,  0.05f,  FONT_SIZE,  menu_enum == GENERAL_AUDIO_MENU ? RED : GREEN,  menu_generalaudiosettings.title);
-	cellDbgFontPuts    (0.29f,  0.05f,  FONT_SIZE,  menu_enum == EMU_GENERAL_MENU ? RED : GREEN,  menu_emu_settings.title);
-	cellDbgFontPuts    (0.39f,  0.05f,  FONT_SIZE,  menu_enum == EMU_VIDEO_MENU ? RED : GREEN,   menu_emu_videosettings.title);
-	cellDbgFontPuts    (0.57f,  0.05f,  FONT_SIZE,  menu_enum == EMU_AUDIO_MENU ? RED : GREEN,   menu_emu_audiosettings.title);
-	cellDbgFontPuts    (0.75f,  0.05f,  FONT_SIZE,  menu_enum == PATH_MENU ? RED : GREEN,  menu_pathsettings.title);
-	cellDbgFontPuts    (0.84f,  0.05f,  FONT_SIZE, menu_enum == CONTROLS_MENU ? RED : GREEN,  menu_controlssettings.title); 
-	cellDbgFontDraw();
+   cellDbgFontPuts    (0.09f,  0.05f,  FONT_SIZE,  menu_enum == GENERAL_VIDEO_MENU ? RED : GREEN,   menu_generalvideosettings.title);
+   cellDbgFontPuts    (0.19f,  0.05f,  FONT_SIZE,  menu_enum == GENERAL_AUDIO_MENU ? RED : GREEN,  menu_generalaudiosettings.title);
+   cellDbgFontPuts    (0.29f,  0.05f,  FONT_SIZE,  menu_enum == EMU_GENERAL_MENU ? RED : GREEN,  menu_emu_settings.title);
+   cellDbgFontPuts    (0.39f,  0.05f,  FONT_SIZE,  menu_enum == EMU_VIDEO_MENU ? RED : GREEN,   menu_emu_videosettings.title);
+   cellDbgFontPuts    (0.57f,  0.05f,  FONT_SIZE,  menu_enum == EMU_AUDIO_MENU ? RED : GREEN,   menu_emu_audiosettings.title);
+   cellDbgFontPuts    (0.75f,  0.05f,  FONT_SIZE,  menu_enum == PATH_MENU ? RED : GREEN,  menu_pathsettings.title);
+   cellDbgFontPuts    (0.84f,  0.05f,  FONT_SIZE, menu_enum == CONTROLS_MENU ? RED : GREEN,  menu_controlssettings.title); 
+   cellDbgFontDraw();
 }
 
 enum
 {
-	DELAY_NONE,
-	DELAY_SMALLEST,
-	DELAY_SMALL,
-	DELAY_MEDIUM,
-	DELAY_LONG
+   DELAY_NONE,
+   DELAY_SMALLEST,
+   DELAY_SMALL,
+   DELAY_MEDIUM,
+   DELAY_LONG
 };
 
 static uint32_t set_delay = DELAY_NONE;
--- ps3/menu.h
+++ ps3/menu.h
@@ -27,112 +27,110 @@
 
 typedef struct
 {
-	uint32_t enum_id;			/* enum ID of item				*/
-	char text[256];				/* item label					*/
-	char setting_text[256];			/* setting label				*/
-	float text_xpos;			/* text X position (upper left corner)		*/
-	float text_ypos;			/* text Y position (upper left corner)		*/
-	uint32_t text_color;			/* text color					*/
-	char comment[256];			/* item comment					*/
-	uint32_t item_color;			/* color of item 				*/
-	float comment_scalefont;		/* font scale of item comment			*/ 
-	float comment_xpos;			/* comment X position (upper left corner)	*/
-	float comment_ypos;			/* comment Y position (upper left corner)	*/
-	uint32_t default_value;			/* default value of item			*/
-	uint32_t page;				/* page						*/
+   uint32_t enum_id;			/* enum ID of item				*/
+   char text[256];			/* item label					*/
+   char setting_text[256];		/* setting label				*/
+   float text_xpos;			/* text X position (upper left corner)		*/
+   float text_ypos;			/* text Y position (upper left corner)		*/
+   uint32_t text_color;			/* text color					*/
+   char comment[256];			/* item comment					*/
+   uint32_t item_color;			/* color of item 				*/
+   float comment_scalefont;		/* font scale of item comment			*/ 
+   float comment_xpos;			/* comment X position (upper left corner)	*/
+   float comment_ypos;			/* comment Y position (upper left corner)	*/
+   uint32_t default_value;		/* default value of item			*/
+   uint32_t page;			/* page						*/
 } item;
 
 typedef struct
 {
-	char title[64];				/* menu title					*/
-	uint32_t enum_id;			/* enum ID of menu				*/
-	uint32_t selected;			/* index of selected item			*/
-	uint32_t page;				/* page						*/
-	uint32_t max_pages;			/* max pages					*/
-	uint32_t refreshpage;			/* bit whether or not to refresh page		*/
-	uint32_t first_setting;			/* first setting				*/
-	uint32_t max_settings;			/* max no of settings in menu			*/
-	item *items;				/* menu items					*/
+   char title[64];			/* menu title					*/
+   uint32_t enum_id;			/* enum ID of menu				*/
+   uint32_t selected;			/* index of selected item			*/
+   uint32_t page;			/* page						*/
+   uint32_t max_pages;			/* max pages					*/
+   uint32_t refreshpage;		/* bit whether or not to refresh page		*/
+   uint32_t first_setting;		/* first setting				*/
+   uint32_t max_settings;		/* max no of settings in menu			*/
+   item *items;				/* menu items					*/
 } menu;
 
 
 enum
 {
-	FILE_BROWSER_MENU,
-	GENERAL_VIDEO_MENU,
-	GENERAL_AUDIO_MENU,
-	EMU_GENERAL_MENU,
-	EMU_VIDEO_MENU,
-	EMU_AUDIO_MENU,
-	PATH_MENU,
-	CONTROLS_MENU,
-	SHADER_CHOICE,
-	PRESET_CHOICE,
-	BORDER_CHOICE,
-	LIBSNES_CHOICE,
-	PATH_SAVESTATES_DIR_CHOICE,
-	PATH_DEFAULT_ROM_DIR_CHOICE,
-	PATH_CHEATS_DIR_CHOICE,
-	PATH_SRAM_DIR_CHOICE,
-	INPUT_PRESET_CHOICE,
-	INGAME_MENU
+   FILE_BROWSER_MENU,
+   GENERAL_VIDEO_MENU,
+   GENERAL_AUDIO_MENU,
+   EMU_GENERAL_MENU,
+   EMU_VIDEO_MENU,
+   EMU_AUDIO_MENU,
+   PATH_MENU,
+   CONTROLS_MENU,
+   SHADER_CHOICE,
+   PRESET_CHOICE,
+   BORDER_CHOICE,
+   LIBSNES_CHOICE,
+   PATH_SAVESTATES_DIR_CHOICE,
+   PATH_DEFAULT_ROM_DIR_CHOICE,
+   PATH_CHEATS_DIR_CHOICE,
+   PATH_SRAM_DIR_CHOICE,
+   INPUT_PRESET_CHOICE,
+   INGAME_MENU
 };
 
 enum
 {
-	SETTING_CHANGE_RESOLUTION,
-	SETTING_SHADER_PRESETS,
-	SETTING_SHADER,
-	SETTING_SHADER_2,
-	SETTING_FONT_SIZE,
-	SETTING_KEEP_ASPECT_RATIO,
-	SETTING_HW_TEXTURE_FILTER,
-	SETTING_HW_TEXTURE_FILTER_2,
-	SETTING_SCALE_ENABLED,
-	SETTING_SCALE_FACTOR,
-	SETTING_HW_OVERSCAN_AMOUNT,
-	SETTING_THROTTLE_MODE,
-	SETTING_TRIPLE_BUFFERING,
-	SETTING_ENABLE_SCREENSHOTS,
-	SETTING_SAVE_SHADER_PRESET,
-	SETTING_APPLY_SHADER_PRESET_ON_STARTUP,
-	SETTING_DEFAULT_VIDEO_ALL,
-	SETTING_SOUND_MODE,
-	SETTING_RSOUND_SERVER_IP_ADDRESS,
-	SETTING_ENABLE_CUSTOM_BGM,
-	SETTING_DEFAULT_AUDIO_ALL,
-	/* port-specific */
-	SETTING_EMU_CURRENT_SAVE_STATE_SLOT,
-	SETTING_SSNES_DEFAULT_EMU,
-	SETTING_EMU_DEFAULT_ALL,
-	SETTING_EMU_REWIND_ENABLED,
-	SETTING_EMU_VIDEO_DEFAULT_ALL,
-	SETTING_EMU_AUDIO_MUTE,
-	SETTING_EMU_AUDIO_DEFAULT_ALL,
-	/* end of port-specific */
-	SETTING_PATH_DEFAULT_ROM_DIRECTORY,
-	SETTING_PATH_SAVESTATES_DIRECTORY,
-	SETTING_PATH_SRAM_DIRECTORY,
-	SETTING_PATH_CHEATS,
-	SETTING_ENABLE_SRAM_PATH,
-	SETTING_ENABLE_STATE_PATH,
-	SETTING_PATH_DEFAULT_ALL,
-	SETTING_CONTROLS_SCHEME,
-	SETTING_CONTROLS_NUMBER,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_B,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_Y,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_SELECT,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_START,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_UP,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_DOWN,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_LEFT,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_RIGHT,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_A,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_X,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_L,
-	SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_R,
-	SETTING_CONTROLS_SAVE_CUSTOM_CONTROLS,
-	SETTING_CONTROLS_DEFAULT_ALL
+   SETTING_CHANGE_RESOLUTION,
+   SETTING_SHADER_PRESETS,
+   SETTING_SHADER,
+   SETTING_SHADER_2,
+   SETTING_FONT_SIZE,
+   SETTING_KEEP_ASPECT_RATIO,
+   SETTING_HW_TEXTURE_FILTER,
+   SETTING_HW_TEXTURE_FILTER_2,
+   SETTING_SCALE_ENABLED,
+   SETTING_SCALE_FACTOR,
+   SETTING_HW_OVERSCAN_AMOUNT,
+   SETTING_THROTTLE_MODE,
+   SETTING_TRIPLE_BUFFERING,
+   SETTING_ENABLE_SCREENSHOTS,
+   SETTING_SAVE_SHADER_PRESET,
+   SETTING_APPLY_SHADER_PRESET_ON_STARTUP,
+   SETTING_DEFAULT_VIDEO_ALL,
+   SETTING_SOUND_MODE,
+   SETTING_RSOUND_SERVER_IP_ADDRESS,
+   SETTING_ENABLE_CUSTOM_BGM,
+   SETTING_DEFAULT_AUDIO_ALL,
+   SETTING_EMU_CURRENT_SAVE_STATE_SLOT,
+   SETTING_SSNES_DEFAULT_EMU,
+   SETTING_EMU_DEFAULT_ALL,
+   SETTING_EMU_REWIND_ENABLED,
+   SETTING_EMU_VIDEO_DEFAULT_ALL,
+   SETTING_EMU_AUDIO_MUTE,
+   SETTING_EMU_AUDIO_DEFAULT_ALL,
+   SETTING_PATH_DEFAULT_ROM_DIRECTORY,
+   SETTING_PATH_SAVESTATES_DIRECTORY,
+   SETTING_PATH_SRAM_DIRECTORY,
+   SETTING_PATH_CHEATS,
+   SETTING_ENABLE_SRAM_PATH,
+   SETTING_ENABLE_STATE_PATH,
+   SETTING_PATH_DEFAULT_ALL,
+   SETTING_CONTROLS_SCHEME,
+   SETTING_CONTROLS_NUMBER,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_B,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_Y,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_SELECT,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_START,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_UP,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_DOWN,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_LEFT,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_RIGHT,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_A,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_X,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_L,
+   SETTING_CONTROLS_RETRO_DEVICE_ID_JOYPAD_R,
+   SETTING_CONTROLS_SAVE_CUSTOM_CONTROLS,
+   SETTING_CONTROLS_DEFAULT_ALL
 };
 
 #define FIRST_VIDEO_SETTING				0
