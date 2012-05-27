f8ad3dcc9144633866cb6ce412f6f9d12d95356f
TwinAphex51224 <twinaphex1@gmail.com>
2012-05-28 00:45:30 +0200

    (360) Font subsystem works again - font crap needs to be badly refactored

--- 360/xdk360_video.cpp
+++ 360/xdk360_video.cpp
@@ -108,6 +108,7 @@ const DWORD g_MapLinearToSrgbGpuFormat[] =
 
 struct XPR_HEADER
 {
+   unsigned long dwMagic;
    unsigned long dwHeaderSize;
    unsigned long dwDataSize;
 };
@@ -162,6 +163,13 @@ HRESULT PackedResource::Create( const char * strFilename )
         RARCH_ERR( "Error reading XPR header in file <%s>.\n", strFilename );
         CloseHandle( hFile );
         return E_FAIL;
+    }
+
+    if( xprh.dwMagic != XPR2_MAGIC_VALUE )
+    {
+       RARCH_ERR( "Invalid Xbox Packed Resource (.xpr) file: Magic = 0x%08lx.\n", xprh.dwMagic );
+       CloseHandle( hFile );
+       return E_FAIL;
     }
 
     // Compute memory requirements
