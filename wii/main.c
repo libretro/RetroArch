49c9e766af88cf25f0473635fd5498c87efa6fd7
Toad King <toadking@toadking.com>
2012-06-25 15:27:41 -0400

    Wii changes

--- wii/main.c
+++ wii/main.c
@@ -158,6 +158,8 @@ int main(void)
       do{
          repeat = rarch_main_iterate();
       }while(repeat && !g_console.frame_advance_enable);
+
+      audio_stop_func();
    }
 
    if(g_console.emulator_initialized)
