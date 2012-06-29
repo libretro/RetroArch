81c35236fac9f51080ca6fc457fefdda0d9ad50b
Twinaphex <autechre1024@hotmail.com>
2012-06-29 21:39:28 +0200

    (Wii) Set audiorate control delta to 0.004

--- wii/frontend/main.c
+++ wii/frontend/main.c
@@ -142,7 +142,7 @@ int main(void)
    config_set_defaults();
 
    g_settings.audio.rate_control = true;
-   g_settings.audio.rate_control_delta = 0.005;
+   g_settings.audio.rate_control_delta = 0.004;
    g_console.block_config_read = true;
 
    wii_video_init();
