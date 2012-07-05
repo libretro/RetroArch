a8b31c90e6aa52fcebd225a3c4ef21eea28595d5
Darren Alton <dalton@stevens.edu>
2012-07-05 03:29:13 -0400

    (Pandora) forgot to preload pixelperfect SDL for phoenix as well.

--- pandora/scripts/retroarch-phoenix.sh
+++ pandora/scripts/retroarch-phoenix.sh
@@ -3,5 +3,6 @@
 source "/mnt/utmp/retroarch/scripts/env-vars.sh"
 
 # load the libstdc++ from gcc-4.7 because phoenix doesn't build in anything less
-exec env LD_PRELOAD=/mnt/utmp/retroarch/lib/libstdc++.so.6 retroarch-phoenix "${@}"
+# preload my modified SDL that knows what "pixelperfect" is
+exec env LD_PRELOAD=/mnt/utmp/retroarch/lib/libstdc++.so.6:/mnt/utmp/retroarch/lib/libSDL-1.2.so.0.11.3 retroarch-phoenix "${@}"
 
