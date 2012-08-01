5cbdc4109350973d31289756d4c4337a860d9e62
Twinaphex <autechre1024@hotmail.com>
2012-08-01 03:01:29 +0200

    (PS3) Enums should be included regardless of whether or not HAVE_OSKUTIL is defined

--- ps3/ps3_input.h
+++ ps3/ps3_input.h
@@ -44,25 +44,6 @@
 #define OSK_IS_RUNNING(object) object.is_running
 #define OUTPUT_TEXT_STRING(object) object.osk_text_buffer_char
 
-#ifdef HAVE_OSKUTIL
-
-typedef struct
-{
-   unsigned int osk_memorycontainer;
-   wchar_t init_message[CELL_OSKDIALOG_STRING_SIZE + 1];
-   wchar_t message[CELL_OSKDIALOG_STRING_SIZE + 1];
-   wchar_t osk_text_buffer[CELL_OSKDIALOG_STRING_SIZE + 1];
-   char osk_text_buffer_char[CELL_OSKDIALOG_STRING_SIZE + 1];
-   uint32_t flags;
-   bool is_running;
-   bool text_can_be_fetched;
-   sys_memory_container_t containerid;
-   CellOskDialogPoint pos;
-   CellOskDialogInputFieldInfo inputFieldInfo;
-   CellOskDialogCallbackReturnParam outputInfo;
-   CellOskDialogParam dialogParam;
-} oskutil_params;
-
 enum {
    PS3_GAMEPAD_CIRCLE =			1 << 0,
    PS3_GAMEPAD_CROSS =			1 << 1,
@@ -127,6 +108,25 @@ enum ps3_device_id
 
    RARCH_LAST_PLATFORM_KEY
 };
+
+#ifdef HAVE_OSKUTIL
+
+typedef struct
+{
+   unsigned int osk_memorycontainer;
+   wchar_t init_message[CELL_OSKDIALOG_STRING_SIZE + 1];
+   wchar_t message[CELL_OSKDIALOG_STRING_SIZE + 1];
+   wchar_t osk_text_buffer[CELL_OSKDIALOG_STRING_SIZE + 1];
+   char osk_text_buffer_char[CELL_OSKDIALOG_STRING_SIZE + 1];
+   uint32_t flags;
+   bool is_running;
+   bool text_can_be_fetched;
+   sys_memory_container_t containerid;
+   CellOskDialogPoint pos;
+   CellOskDialogInputFieldInfo inputFieldInfo;
+   CellOskDialogCallbackReturnParam outputInfo;
+   CellOskDialogParam dialogParam;
+} oskutil_params;
 
 void oskutil_write_message(oskutil_params *params, const wchar_t* msg);
 void oskutil_write_initial_message(oskutil_params *params, const wchar_t* msg);
