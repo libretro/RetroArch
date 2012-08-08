d7fd90883be253d996b72450b06c3b81b2755756
Twinaphex <autechre1024@hotmail.com>
2012-08-08 23:18:31 +0200

    (GX) Slim down vi_encoder.c

--- gx/vi_encoder.h
+++ gx/vi_encoder.h
@@ -38,42 +38,6 @@
  ****************************************************************************************/
 #ifdef HW_RVL
 
-typedef enum
-{
-    VI_GM_0_1=1,
-    VI_GM_0_2,
-    VI_GM_0_3,
-    VI_GM_0_4,
-    VI_GM_0_5,
-    VI_GM_0_6,
-    VI_GM_0_7,
-    VI_GM_0_8,
-    VI_GM_0_9,
-    VI_GM_1_0,
-    VI_GM_1_1,
-    VI_GM_1_2,
-    VI_GM_1_3,
-    VI_GM_1_4,
-    VI_GM_1_5,
-    VI_GM_1_6,
-    VI_GM_1_7,
-    VI_GM_1_8,
-    VI_GM_1_9,
-    VI_GM_2_0,
-    VI_GM_2_1,
-    VI_GM_2_2,
-    VI_GM_2_3,
-    VI_GM_2_4,
-    VI_GM_2_5,
-    VI_GM_2_6,
-    VI_GM_2_7,
-    VI_GM_2_8,
-    VI_GM_2_9,
-    VI_GM_3_0
-} VIGamma;
-
-extern void VIDEO_SetGamma(VIGamma gamma);
 extern void VIDEO_SetTrapFilter(bool enable);
-
 
 #endif
