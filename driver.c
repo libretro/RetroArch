0e5528796d220c3e487176ed8b1a7d0241585d6b
Twinaphex <autechre1024@hotmail.com>
2012-08-12 11:55:12 +0200

    Set nonblock_state_func argument for >60Hz emu ports/games to true

--- driver.c
+++ driver.c
@@ -209,7 +209,7 @@ static void adjust_system_rates(void)
          g_settings.video.vsync = false;
          RARCH_LOG("Game FPS > Monitor FPS. Cannot rely on VSync.\n");
 #ifdef RARCH_CONSOLE
-         video_set_nonblock_state_func(false);
+         video_set_nonblock_state_func(true);
 #endif
       }
 
