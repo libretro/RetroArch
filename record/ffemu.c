0f3c3ff06cdf56a54f88753bac6b6271a2a498f4
Hans-Kristian Arntzen <maister@archlinux.us>
2012-08-27 15:56:42 +0300

    Fix small derp.

--- record/ffemu.c
+++ record/ffemu.c
@@ -20,6 +20,8 @@ extern "C" {
 #endif
 #include <libavcodec/avcodec.h>
 #include <libavutil/mathematics.h>
+#include <libavutil/avutil.h>
+#include <libavutil/avstring.h>
 #include <libavutil/opt.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
