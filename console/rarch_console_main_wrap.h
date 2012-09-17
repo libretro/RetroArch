4d2cbd0095873433c5faec1846696960c32213cf
Twinaphex <autechre1024@hotmail.com>
2012-09-17 05:35:36 +0200

    (Android/HAVE_DYLIB) Add dynamic libretro path loading to rarch_startup

--- console/rarch_console_main_wrap.h
+++ console/rarch_console_main_wrap.h
@@ -23,6 +23,9 @@ struct rarch_main_wrap
    const char *sram_path;
    const char *state_path;
    const char *config_path;
+#ifdef HAVE_DYLIB
+   const char *libretro_path;
+#endif
    bool verbose;
 };
 
