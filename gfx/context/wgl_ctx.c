f605e8acf2c976dd7ad902eea95aef97ac873d31
Themaister <maister@archlinux.us>
2012-10-11 23:31:46 +0200

    Fix compiler warnings.

--- gfx/context/wgl_ctx.c
+++ gfx/context/wgl_ctx.c
@@ -238,11 +238,11 @@ static bool gfx_ctx_set_video_mode(
    (void)bits;
 
    DWORD style;
-   MONITORINFOEX current_mon = {0};
+   MONITORINFOEX current_mon = {{0}};
    current_mon.cbSize = sizeof(MONITORINFOEX);
    if (!g_last_hm)
       g_last_hm = MonitorFromWindow(GetDesktopWindow(), MONITOR_DEFAULTTONEAREST);
-   GetMonitorInfo(g_last_hm, (MONITORINFO *)&current_mon);
+   GetMonitorInfo(g_last_hm, (MONITORINFO*)&current_mon);
 
    g_resize_width  = width;
    g_resize_height = height;
@@ -264,7 +264,7 @@ static bool gfx_ctx_set_video_mode(
             goto error;
 
          // display settings might have changed, get new coordinates
-         GetMonitorInfo(g_last_hm, (MONITORINFO *)&current_mon);
+         GetMonitorInfo(g_last_hm, (MONITORINFO*)&current_mon);
          g_restore_desktop = true;
       }
    }
@@ -351,9 +351,9 @@ static void gfx_ctx_destroy(void)
 
    if (g_restore_desktop)
    {
-      MONITORINFOEX current_mon = {0};
+      MONITORINFOEX current_mon = {{0}};
       current_mon.cbSize = sizeof(MONITORINFOEX);
-      GetMonitorInfo(g_last_hm, (MONITORINFO *)&current_mon);
+      GetMonitorInfo(g_last_hm, (MONITORINFO*)&current_mon);
       ChangeDisplaySettingsEx(current_mon.szDevice, NULL, NULL, 0, NULL);
       g_restore_desktop = false;
    }
