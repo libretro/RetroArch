f5e9ac31d3375befc4ac657055d8bbf9d23e889f
twinaphex <autechre1024@hotmail.com>
2012-10-21 22:24:25 +0200

    (Android) Add Android makefiles for libretro-test

---
+++ libretro-test/jni/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+APP_DIR := ../../src
+
+LOCAL_MODULE    := retro
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CFLAGS += -DANDROID_ARM
+endif
+
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_CFLAGS +=  -DANDROID_X86
+endif
+
+ifeq ($(TARGET_ARCH),mips)
+LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
+endif
+
+LOCAL_SRC_FILES    += ../libretro-test.c
+LOCAL_CFLAGS += -O3 -std=gnu99 -ffast-math -funroll-loops
+
+
+include $(BUILD_SHARED_LIBRARY)
