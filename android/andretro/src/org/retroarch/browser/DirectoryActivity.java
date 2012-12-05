fca88c4bd0d87ca4df8eea15bc81c6c722a3ef73
meancoot <nowhere>
2012-12-05 11:17:57 -0500

    (Android) Add new frontend for easier install and use.

---
+++ android/andretro/src/org/retroarch/browser/DirectoryActivity.java
@@ -0,0 +1,153 @@
+package org.retroarch.browser;
+import org.retroarch.R;
+
+import java.util.*;
+import java.io.*;
+
+import android.content.*;
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+import android.view.*;
+import android.view.inputmethod.*;
+import android.graphics.drawable.*;
+
+class FileWrapper implements IconAdapterItem
+{
+    public final File file;
+    protected final int typeIndex;
+    protected final boolean enabled;
+
+    public FileWrapper(File aFile, boolean aIsEnabled)
+    {
+        file = aFile;
+        typeIndex = (file.isDirectory() ? 1 : 0) + (file.isFile() ? 2 : 0);
+        enabled = aIsEnabled;
+    }
+        
+    @Override public boolean isEnabled()
+    {
+    	return enabled;
+    }
+    
+    @Override public String getText()
+    {
+    	return file.getName();
+    }
+    
+    @Override public int getIconResourceId()
+    {
+    	return file.isFile() ? R.drawable.file : R.drawable.folder;
+    }
+    
+    @Override public Drawable getIconDrawable()
+    {
+    	return null;
+    }
+    
+    public int compareTo(FileWrapper aOther)
+    {	
+    	if(null != aOther)
+    	{
+    		// Who says ternary is hard to follow
+    		if(isEnabled() == aOther.isEnabled())
+    		{
+    			return (typeIndex == aOther.typeIndex) ? file.compareTo(aOther.file) : ((typeIndex < aOther.typeIndex) ? -1 : 1);
+    		}
+    		else
+    		{
+    			return isEnabled() ? -1 : 1;
+    		}
+    	}
+    	
+    	return -1;
+    }
+}
+
+public class DirectoryActivity extends Activity implements AdapterView.OnItemClickListener
+{
+    private IconAdapter<FileWrapper> adapter;
+    private String modulePath;
+    
+    @Override public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.line_list);
+        
+        modulePath = getIntent().getStringExtra("LIBRETRO");
+        
+        // Setup the list
+        adapter = new IconAdapter<FileWrapper>(this, R.layout.line_list_item);
+        ListView list = (ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(this);
+        
+        // Load Directory
+        String path = getIntent().getStringExtra("ROM");
+        if(path == null)
+        {
+            path = Environment.getExternalStorageDirectory().getPath();
+        }
+        
+        wrapFiles(new File(path));
+        setTitle(path);
+    }
+    
+	@Override public void onItemClick(AdapterView<?> aListView, View aView, int aPosition, long aID)
+	{
+		final File selected = adapter.getItem(aPosition).file;
+		
+		final Intent intent = new Intent(this, selected.isFile() ? NativeActivity.class : DirectoryActivity.class)
+				.putExtra("ROM", selected.getAbsolutePath())
+				.putExtra("LIBRETRO", modulePath);
+
+		startActivity(intent);
+	}
+	
+    @Override public boolean onCreateOptionsMenu(Menu aMenu)
+    {
+    	super.onCreateOptionsMenu(aMenu);
+		getMenuInflater().inflate(R.menu.directory_list, aMenu);
+    	    	
+    	return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem aItem)
+    {    	
+        if(R.id.input_method_select == aItem.getItemId())
+        {
+        	InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        	imm.showInputMethodPicker();
+        	return true;
+        }
+    	        
+        return super.onOptionsItemSelected(aItem);
+    }
+        
+    private void wrapFiles(File aDirectory)
+    {
+    	if(null == aDirectory || !aDirectory.isDirectory())
+    	{
+    		throw new IllegalArgumentException("Directory is not valid.");
+    	}
+
+        // Copy new items
+        for(File file: aDirectory.listFiles())
+        {
+        	adapter.add(new FileWrapper(file, file.isDirectory() || true));
+        }
+        
+        // Sort items
+        adapter.sort(new Comparator<FileWrapper>()
+        {
+            @Override public int compare(FileWrapper aLeft, FileWrapper aRight)
+            {
+                return aLeft.compareTo(aRight);
+            };
+        });
+
+        // Update
+        adapter.notifyDataSetChanged();    	
+    }
+}
---
+++ android/andretro/src/org/retroarch/browser/ModuleActivity.java
@@ -0,0 +1,106 @@
+package org.retroarch.browser;
+import org.retroarch.R;
+
+import java.io.*;
+
+import android.content.*;
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+import android.view.*;
+import android.view.inputmethod.*;
+import android.graphics.drawable.*;
+
+class ModuleWrapper implements IconAdapterItem
+{
+	public final File file;
+	
+    public ModuleWrapper(Context aContext, File aFile) throws IOException
+    {
+		file = aFile;
+    }
+    
+    @Override public boolean isEnabled()
+    {
+    	return true;
+    }
+    
+    @Override public String getText()
+    {
+    	return file.getName();
+    }
+    
+    @Override public int getIconResourceId()
+    {
+    	return 0;
+    }
+    
+    @Override public Drawable getIconDrawable()
+    {
+    	return null;
+    }
+}
+
+public class ModuleActivity extends Activity implements AdapterView.OnItemClickListener
+{
+	// HACK: Hard path
+	private static final String modulePath = "/data/data/org.retroarch/lib/";
+    private IconAdapter<ModuleWrapper> adapter;
+    
+    @Override public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.line_list);
+        
+        // Setup the list
+        adapter = new IconAdapter<ModuleWrapper>(this, R.layout.line_list_item);
+        ListView list = (ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(this);
+        
+        setTitle("Select Emulator");
+    	
+        for(final File lib: new File(modulePath).listFiles())
+        {
+        	if(lib.getName().startsWith("libretro_"))
+        	{
+        		try
+        		{
+        			adapter.add(new ModuleWrapper(this, lib));;
+        		}
+        		catch(Exception e)
+        		{
+        			//Logger.d("Couldn't add module: " + lib.getPath());
+        		}
+        	}
+        }
+    }
+    
+	@Override public void onItemClick(AdapterView<?> aListView, View aView, int aPosition, long aID)
+	{
+		final ModuleWrapper item = adapter.getItem(aPosition);
+
+		startActivity(new Intent(ModuleActivity.this, DirectoryActivity.class)
+			.putExtra("LIBRETRO", item.file.getAbsolutePath()));
+	}
+		
+    @Override public boolean onCreateOptionsMenu(Menu aMenu)
+    {
+    	super.onCreateOptionsMenu(aMenu);
+		getMenuInflater().inflate(R.menu.directory_list, aMenu);
+    	return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem aItem)
+    {
+        if(R.id.input_method_select == aItem.getItemId())
+        {
+        	InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        	imm.showInputMethodPicker();
+        	return true;
+        }
+    	
+        return super.onOptionsItemSelected(aItem);
+    }
+}
