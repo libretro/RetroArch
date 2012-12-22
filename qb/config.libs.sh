d032764e6ba6088a0b620f7a5f2497e9adcaeee8
sparklewind <swsparklewind611@gmail.com>
2012-12-22 17:16:35 +0100

    Update qb/config.libs.sh

--- qb/config.libs.sh
+++ qb/config.libs.sh
@@ -8,8 +8,7 @@ add_define_make NOUNUSED "$HAVE_NOUNUSED"
 
 [ -z "$CROSS_COMPILE" ] && [ -d /opt/local/lib ] && add_library_dirs /opt/local/lib
 
-if [ "$OS" = 'BSD' ]; then DYLIB=-lc; else DYLIB=-ldl; fi
-if [ "$OS" = 'Haiku' ]: then DYLIB=""; else DYLIB=""; fi
+if [ "$OS" = 'BSD' ]; then DYLIB=-lc; elif [ "$OS" = 'Haiku' ]: then DYLIB=""; else DYLIB=-ldl; fi
 [ "$OS" = 'Darwin' ] && HAVE_X11=no # X11 breaks on recent OSXes even if present.
 
 [ -d /opt/vc/lib ] && add_library_dirs /opt/vc/lib
