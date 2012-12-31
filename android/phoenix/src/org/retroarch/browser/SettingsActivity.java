ca2e6c3e241fef4e30c7daeaeb2d434327cd8c29
Themaister <maister@archlinux.us>
2012-12-31 18:52:35 +0100

    Fix crash when device is rotate in settings.

--- android/phoenix/src/org/retroarch/browser/SettingsActivity.java
+++ android/phoenix/src/org/retroarch/browser/SettingsActivity.java
@@ -7,17 +7,6 @@ import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
-class SettingsFragment extends PreferenceFragment {
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.prefs);
-    }
-}
-
 public class SettingsActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,5 +14,16 @@ public class SettingsActivity extends Activity {
 		getFragmentManager().beginTransaction().
 			replace(android.R.id.content, new SettingsFragment()).commit();
 		PreferenceManager.setDefaultValues(this, R.xml.prefs, false);
+	}
+	
+	public static class SettingsFragment extends PreferenceFragment {
+		
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+
+	        // Load the preferences from an XML resource
+	        addPreferencesFromResource(R.xml.prefs);
+	    }
 	}
 }
