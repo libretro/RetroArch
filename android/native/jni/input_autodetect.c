dd74683f26f20e190bcd542ea53faeb71e1e2ace
twinaphex <autechre1024@hotmail.com>
2013-01-05 18:33:26 +0100

    (Android) Take out earlier SixAxisIME code - we'll always use gamepad emulation with this IME app now

--- android/native/jni/input_autodetect.c
+++ android/native/jni/input_autodetect.c
@@ -431,29 +431,6 @@ void input_autodetect_setup (void *data, char *msg, size_t sizeof_msg, unsigned 
          keycode_lut[AKEYCODE_GRAVE] |= ((RETRO_DEVICE_ID_JOYPAD_L2+1) << shift);
          keycode_lut[AKEYCODE_MEDIA_PAUSE] |= ((RETRO_DEVICE_ID_JOYPAD_R2+1) << shift);
       }
-      else if (strstr(current_ime, "SixaxisIME"))
-      {
-         snprintf(msg, sizeof_msg, "RetroPad #%d is: SixAxis Bluetooth (IME).\n", port);
-         snprintf(name_buf, sizeof(name_buf), "dancingpixelstudios.SixAxis");
-         g_settings.input.dpad_emulation[port] = DPAD_EMULATION_NONE;
-         keycode_lut[AKEYCODE_DPAD_UP]   |= ((RETRO_DEVICE_ID_JOYPAD_UP+1)    << shift);
-         keycode_lut[AKEYCODE_DPAD_DOWN] |= ((RETRO_DEVICE_ID_JOYPAD_DOWN+1)    << shift);
-         keycode_lut[AKEYCODE_DPAD_LEFT] |= ((RETRO_DEVICE_ID_JOYPAD_LEFT+1)    << shift);
-         keycode_lut[AKEYCODE_DPAD_RIGHT]|= ((RETRO_DEVICE_ID_JOYPAD_RIGHT+1)    << shift);
-         keycode_lut[AKEYCODE_4] |= ((RETRO_DEVICE_ID_JOYPAD_L+1)      << shift);
-         keycode_lut[AKEYCODE_5]         |= ((RETRO_DEVICE_ID_JOYPAD_L2+1)      << shift);
-         keycode_lut[AKEYCODE_6]         |= ((RETRO_DEVICE_ID_JOYPAD_R+1)      << shift);
-         keycode_lut[AKEYCODE_7]         |= ((RETRO_DEVICE_ID_JOYPAD_R2+1)      << shift);
-         keycode_lut[AKEYCODE_BACK]      |= ((RETRO_DEVICE_ID_JOYPAD_SELECT+1)  << shift);
-         keycode_lut[AKEYCODE_DPAD_CENTER]      |= ((RETRO_DEVICE_ID_JOYPAD_START+1)  << shift);
-         keycode_lut[AKEYCODE_8]      |= ((RETRO_DEVICE_ID_JOYPAD_L3+1)  << shift);
-         keycode_lut[AKEYCODE_9]      |= ((RETRO_DEVICE_ID_JOYPAD_R3+1)  << shift);
-         keycode_lut[AKEYCODE_0]      |= ((RETRO_DEVICE_ID_JOYPAD_Y+1)  << shift);
-         keycode_lut[AKEYCODE_3]      |= ((RETRO_DEVICE_ID_JOYPAD_B+1)  << shift);
-         keycode_lut[AKEYCODE_2]      |= ((RETRO_DEVICE_ID_JOYPAD_A+1)  << shift);
-         keycode_lut[AKEYCODE_1]      |= ((RETRO_DEVICE_ID_JOYPAD_X+1)  << shift);
-         keycode_lut[AKEYCODE_HOME]      |= ((RARCH_RESET+1)  << shift);
-      }
       else if (source == AINPUT_SOURCE_TOUCHSCREEN)
       {
          snprintf(msg, sizeof_msg, "RetroPad #%d is: Touchscreen.\n", port);
