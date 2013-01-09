f5bbe7604671a677b2437b86a065e0666d1f3225
twinaphex <autechre1024@hotmail.com>
2013-01-09 03:34:34 +0100

    Add frontend_console header

---
+++ frontend/frontend_console.h
@@ -0,0 +1,38 @@
+/* RetroArch - A frontend for libretro.
+ * Copyright (C) 2010-2013 - Hans-Kristian Arntzen
+ * Copyright (C) 2011-2013 - Daniel De Matteis
+ *
+ * RetroArch is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Found-
+ * ation, either version 3 of the License, or (at your option) any later version.
+ *
+ * RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with RetroArch.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _FRONTEND_CONSOLE_H
+#define _FRONTEND_CONSOLE_H
+
+//optional RetroArch forward declarations
+static void rarch_console_exec(const char *path);
+
+#ifdef IS_SALAMANDER
+//optional Salamander forward declarations
+static void rarch_manage_libretro_set_first_file(char *first_file,
+   size_t size_of_first_file, const char *libretro_path,
+   const char * exe_ext);
+#endif
+
+#ifdef HAVE_LIBRETRO_MANAGEMENT
+
+static void get_libretro_core_name(char *name, size_t size);
+static bool install_libretro_core(const char *core_exe_path, const char *tmp_path,
+ const char *libretro_path, const char *config_path, const char *extension);
+
+#endif
+
+#endif
