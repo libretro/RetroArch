ce79b33feed538391560ec382aa34e5f255dc441
twinaphex <autechre1024@hotmail.com>
2013-01-24 20:28:22 +0100

    (Android) Add auto-load state option

--- android/phoenix/src/org/retroarch/browser/RetroArch.java
+++ android/phoenix/src/org/retroarch/browser/RetroArch.java
@@ -214,6 +214,7 @@ public class RetroArch extends Activity implements
 		config.setBoolean("audio_enable", prefs.getBoolean("audio_enable", true));
 		config.setBoolean("video_smooth", prefs.getBoolean("video_smooth", true));
 		config.setBoolean("video_allow_rotate", prefs.getBoolean("video_allow_rotate", true));
+		config.setBoolean("savestate_auto_load", prefs.getBoolean("savestate_auto_load", false));
 		config.setBoolean("savestate_auto_save", prefs.getBoolean("savestate_auto_save", false));
 		config.setBoolean("rewind_enable", prefs.getBoolean("rewind_enable", false));
 		config.setBoolean("video_vsync", prefs.getBoolean("video_vsync", true));
