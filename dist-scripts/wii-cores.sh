14ee3cf7c2371af9fbdf23558286ed00117361a5
ToadKing <toadking@toadking.com>
2013-02-01 17:29:04 -0500

    (Wii) fix build script

--- dist-scripts/wii-cores.sh
+++ dist-scripts/wii-cores.sh
@@ -4,7 +4,7 @@ make -C ../ -f Makefile.wii clean || exit 1
 
 for f in *_wii.a ; do
    name=`echo "$f" | sed 's/\(_libretro\|\)_wii.a$//'`
-   cp -f "$f" ../libretro.a
+   cp -f "$f" ../libretro_wii.a
    make -C ../ -f Makefile.wii -j3 || exit 1
    mv -f ../retroarch_wii.dol "$name.dol"
    rm -f ../retroarch_wii.dol ../retroarch_wii.elf ../retroarch_wii.elf.map
