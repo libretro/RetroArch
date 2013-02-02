f5f712f05055d2a3357eedd0de8dc99105eb02ca
twinaphex <autechre1024@hotmail.com>
2013-02-02 01:56:56 +0100

    (PS3) Fix CEX 3.55 build script

--- dist-scripts/ps3-cores-cex-355.sh
+++ dist-scripts/ps3-cores-cex-355.sh
@@ -13,14 +13,15 @@ for f in *_ps3.a ; do
    name=`echo "$f" | sed 's/\(_libretro\|\)_ps3.a$//'`
    cp -f "$f" ../libretro_ps3.a
    make -C ../ -f Makefile.ps3 -j3 || exit 1
-   make_self_wc retroarch_ps3.elf "$name.dol"
-   rm -f ../retroarch_ps3.elf ../retroarch_ps3.self
+   make_self_wc ../retroarch_ps3.elf ../CORE.SELF
+   mv -f ../CORE.SELF ../ps3/pkg/USRDIR/cores/"$name.SELF"
+   rm -f ../retroarch_ps3.elf ../retroarch_ps3.self ../CORE.SELF
 done
 
 cp -r ../media/rmenu/*.png ../ps3/pkg/USRDIR/cores/borders/Menu/
 
 make -C ../ -f Makefile.shaders deploy-ps3
 
-make_fself_wc ../retroarch-salamander_ps3.elf ../ps3/pkg/USRDIR/EBOOT.BIN
+make_self_wc ../retroarch-salamander_ps3.elf ../ps3/pkg/USRDIR/EBOOT.BIN
 rm -rf ../retroarch-salamander_ps3.elf
 python2 ../ps3/ps3py/pkg.py --contentid UP0001-SSNE10000_00-0000000000000001 ../ps3/pkg/ retroarch-ps3-cfw-0.9.8.1.pkg
