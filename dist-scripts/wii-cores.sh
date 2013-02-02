87812517507b1f5a65bbb98e7576f13f8a66ab01
twinaphex <autechre1024@hotmail.com>
2013-02-02 03:41:33 +0100

    (Wii) Improve build script

--- dist-scripts/wii-cores.sh
+++ dist-scripts/wii-cores.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+make -C ../ -f Makefile.wii.salamander clean || exit 1
 make -C ../ -f Makefile.wii clean || exit 1
+
+make -C ../ -f Makefile.wii.salamander || exit 1
+make -C ../ -f Makefile.wii.salamander pkg || exit 1
 
 for f in *_wii.a ; do
    name=`echo "$f" | sed 's/\(_libretro\|\)_wii.a$//'`
    cp -f "$f" ../libretro_wii.a
    make -C ../ -f Makefile.wii -j3 || exit 1
-   mv -f ../retroarch_wii.dol "$name.dol"
+   mv -f ../retroarch_wii.dol ../wii/pkg/$name.dol
    rm -f ../retroarch_wii.dol ../retroarch_wii.elf ../retroarch_wii.elf.map
 done
