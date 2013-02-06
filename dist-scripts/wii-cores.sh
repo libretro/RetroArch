94c2f13b3512bea1b775a2710733386700aaf6a8
ToadKing <toadking@toadking.com>
2013-02-05 19:29:27 -0500

    (Wii) link nxengine correctly in distscript

--- dist-scripts/wii-cores.sh
+++ dist-scripts/wii-cores.sh
@@ -8,8 +8,13 @@ make -C ../ -f Makefile.wii.salamander pkg || exit 1
 
 for f in *_wii.a ; do
    name=`echo "$f" | sed 's/\(_libretro\|\)_wii.a$//'`
+   whole_archive=
+   if [ $name = "nxengine" ] ; then
+      whole_archive="WHOLE_ARCHIVE_LINK=1"
+      echo $name yes
+   fi
    cp -f "$f" ../libretro_wii.a
-   make -C ../ -f Makefile.wii -j3 || exit 1
+   make -C ../ -f Makefile.wii $whole_archive -j3 || exit 1
    mv -f ../retroarch_wii.dol ../wii/pkg/$name.dol
    rm -f ../retroarch_wii.dol ../retroarch_wii.elf ../retroarch_wii.elf.map
 done
