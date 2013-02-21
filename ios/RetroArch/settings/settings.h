1edd531544ed7a7f22959fa889d94e2172ac8a77
meancoot <nowhere>
2013-02-20 19:52:52 -0500

    ios: Add proper license header to all files.

--- ios/RetroArch/settings/settings.h
+++ ios/RetroArch/settings/settings.h
@@ -1,10 +1,17 @@
-//
-//  settings_list.m
-//  RetroArch
-//
-//  Created by Jason Fetters on 2/8/13.
-//  Copyright (c) 2013 RetroArch. All rights reserved.
-//
+/*  RetroArch - A frontend for libretro.
+ *  Copyright (C) 2013 - Jason Fetters
+ * 
+ *  RetroArch is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with RetroArch.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "conf/config_file.h"
 
--- ios/RetroArch/views.h
+++ ios/RetroArch/views.h
@@ -1,3 +1,18 @@
+/*  RetroArch - A frontend for libretro.
+ *  Copyright (C) 2013 - Jason Fetters
+ * 
+ *  RetroArch is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with RetroArch.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
 
