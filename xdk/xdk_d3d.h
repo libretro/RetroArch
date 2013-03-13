2718847f11c571c5cc90b282a5f556cec5af44f7
twinaphex <autechre1024@hotmail.com>
2013-03-13 01:15:37 +0100

    (XDK) Clean up xdk_d3d.h header

--- xdk/xdk_d3d.h
+++ xdk/xdk_d3d.h
@@ -87,9 +87,4 @@ typedef struct xdk_d3d_video
 
 extern void xdk_d3d_generate_pp(D3DPRESENT_PARAMETERS *d3dpp, const video_info_t *video);
 
-#ifdef HAVE_FBO
-extern void xdk_d3d_deinit_fbo(void *data);
-extern void xdk_d3d_init_fbo(void *data);
-#endif
-
 #endif
