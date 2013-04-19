eb42492ab0c3ca16570da2d11fd11b711c910473
twinaphex <autechre1024@hotmail.com>
2013-04-19 16:23:09 +0200

    (GX) Use GX_InitTexObjFilterMode to set filtering modes

--- gx/gx_video.c
+++ gx/gx_video.c
@@ -321,9 +321,9 @@ static void init_texture(unsigned width, unsigned height)
    unsigned g_filter = g_settings.video.smooth ? GX_LINEAR : GX_NEAR;
 
    GX_InitTexObj(&g_tex.obj, g_tex.data, width, height, (gx->rgb32) ? GX_TF_RGBA8 : gx->rgui_texture_enable ? GX_TF_RGB5A3 : GX_TF_RGB565, GX_CLAMP, GX_CLAMP, GX_FALSE);
-   GX_InitTexObjLOD(&g_tex.obj, g_filter, g_filter, 0, 0, 0, GX_TRUE, GX_FALSE, GX_ANISO_1);
+   GX_InitTexObjFilterMode(&g_tex.obj, g_filter, g_filter);
    GX_InitTexObj(&menu_tex.obj, menu_tex.data, RGUI_WIDTH, RGUI_HEIGHT, GX_TF_RGB5A3, GX_CLAMP, GX_CLAMP, GX_FALSE);
-   GX_InitTexObjLOD(&menu_tex.obj, g_filter, g_filter, 0, 0, 0, GX_TRUE, GX_FALSE, GX_ANISO_1);
+   GX_InitTexObjFilterMode(&menu_tex.obj, g_filter, g_filter);
    GX_InvalidateTexAll();
 }
 
