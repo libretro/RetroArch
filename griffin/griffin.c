a521b9ff7594bcfa4c5b3a2ea7670ffb8731c6e0
twinaphex <autechre1024@hotmail.com>
2013-04-28 03:52:48 +0200

    (Griffin) Compile in history.c

--- griffin/griffin.c
+++ griffin/griffin.c
@@ -443,6 +443,7 @@ MENU
 ============================================================ */
 #ifdef HAVE_MENU
 #include "../frontend/menu/menu_common.c"
+#include "../frontend/menu/history.c"
 
 #if defined(HAVE_RMENU_GUI)
 #include "../frontend/menu/rmenu.c"
