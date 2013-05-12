b20c31dc3a8892433afea6a238c1e7086ea6db4e
Themaister <maister@archlinux.us>
2013-05-12 12:33:59 +0200

    Fix GL_ES #ifdef.

--- libretro-test-gl/libretro-test.c
+++ libretro-test-gl/libretro-test.c
@@ -119,6 +119,9 @@ static const char *vertex_shader[] = {
 };
 
 static const char *fragment_shader[] = {
+   "#ifdef GL_ES\n",
+   "precision mediump float;\n",
+   "#endif\n",
    "varying vec4 color;",
    "void main() {",
    "  gl_FragColor = color;",
