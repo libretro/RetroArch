f6c126ecaa64700ec120a4f7048f7be4960fb37a
Themaister <maister@archlinux.us>
2013-05-22 19:49:51 +0200

    Only save config if g_extern.config_path is set.

--- frontend/frontend.c
+++ frontend/frontend.c
@@ -99,7 +99,7 @@ int main(int argc, char *argv[])
 
    menu_free();
 
-   if (g_extern.config_save_on_exit)
+   if (g_extern.config_save_on_exit && *g_extern.config_path)
       config_save_file(g_extern.config_path);
 
    if (g_extern.main_is_init)
