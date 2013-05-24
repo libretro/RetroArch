c7108383c2951a236335e902b26712db78f8cb1b
Themaister <maister@archlinux.us>
2013-05-24 16:24:49 +0200

    Save integer scale option. Fix aspect_ratio save.
    Don't save g_extern aspect ratio as it doesn't correspond to
    video_aspect_ratio.

--- settings.c
+++ settings.c
@@ -932,7 +932,8 @@ bool config_save_file(const char *path)
    config_set_int(conf, "rewind_granularity", g_settings.rewind_granularity);
    config_set_string(conf, "video_shader", g_settings.video.shader_path);
    config_set_bool(conf, "video_shader_enable", g_settings.video.shader_enable);
-   config_set_float(conf, "video_aspect_ratio", g_extern.system.aspect_ratio);
+   config_set_float(conf, "video_aspect_ratio", g_settings.video.aspect_ratio);
+   config_set_bool(conf, "video_scale_integer", g_settings.video.scale_integer);
    config_set_bool(conf, "video_smooth", g_settings.video.smooth);
    config_set_bool(conf, "video_vsync", g_settings.video.vsync);
    config_set_int(conf, "aspect_ratio_index", g_settings.video.aspect_ratio_idx);
