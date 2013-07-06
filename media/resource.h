b9cbcd0b81f784d8fb6418fab88e42a39e8b45a3
OV2 <OV2@sessionclan.de>
2013-07-06 17:52:22 +0200

    win32: add simple menu to opengl context

---
+++ media/resource.h
@@ -0,0 +1,6 @@
+// Used by rarch.rc
+//
+#define IDR_MENU                              101
+#define ID_M_OPENROM                          40001
+#define ID_M_RESET                            40002
+#define ID_M_QUIT                             40003
