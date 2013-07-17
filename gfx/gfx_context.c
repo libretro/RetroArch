c54af1907643c3e9438bf879943e0b87e281acab
Themaister <maister@archlinux.us>
2013-07-17 22:38:25 +0200

    Purge SDL/GL context.
    Conflicts:
    	gfx/context/sdl_ctx.c

--- gfx/gfx_context.c
+++ gfx/gfx_context.c
@@ -51,9 +51,6 @@ static const gfx_ctx_driver_t *gfx_ctx_drivers[] = {
 #if defined(IOS) || defined(OSX) //< Don't use __APPLE__ as it breaks basic SDL builds
    &gfx_ctx_apple,
 #endif
-#if defined(HAVE_SDL) && defined(HAVE_OPENGL)
-   &gfx_ctx_sdl_gl,
-#endif
 };
 
 const gfx_ctx_driver_t *gfx_ctx_find_driver(const char *ident)
