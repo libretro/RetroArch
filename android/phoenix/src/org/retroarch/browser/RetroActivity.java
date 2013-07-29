fa5e58ffb844f4bdcd3c9e00286d3364e589d8f5
Themaister <maister@archlinux.us>
2013-07-29 22:53:34 +0200

    Fixup manifest.
    Crashed with full org.retroarch. path.

--- android/phoenix/src/org/retroarch/browser/RetroActivity.java
+++ android/phoenix/src/org/retroarch/browser/RetroActivity.java
@@ -3,25 +3,21 @@ package org.retroarch.browser;
 import android.app.NativeActivity;
 import android.os.Bundle;
 
-public class RetroActivity extends NativeActivity
-{
-	public RetroActivity()
-	{
+public class RetroActivity extends NativeActivity {
+	public RetroActivity() {
 		super();
 	}
-	
+
 	@Override
-	public void onCreate(Bundle savedInstance)
-	{
+	public void onCreate(Bundle savedInstance) {
 		super.onCreate(savedInstance);
 	}
-	
+
 	@Override
-	public void onLowMemory()
-	{
+	public void onLowMemory() {
 	}
-	
-    @Override
-    public void onTrimMemory(int level) {
-    }
+
+	@Override
+	public void onTrimMemory(int level) {
+	}
 }
