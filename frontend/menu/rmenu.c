ff3ce71b98faafb107d60261780ae0a1903ccf75
twinaphex <libretro@gmail.com>
2013-09-27 22:52:05 +0200

    (RMenu) select_directory - remove logic at RGUI_ACTION_CANCEL

--- frontend/menu/rmenu.c
+++ frontend/menu/rmenu.c
@@ -873,27 +873,6 @@ static int select_directory(void *data, uint64_t action)
          break;
 #endif
       case RGUI_ACTION_CANCEL:
-         strlcpy(path, default_paths.port_dir, sizeof(path));
-         switch(rgui->menu_type)
-         {
-            case PATH_SAVESTATES_DIR_CHOICE:
-               strlcpy(g_extern.savestate_dir, path, sizeof(g_extern.savestate_dir));
-               break;
-            case PATH_SRAM_DIR_CHOICE:
-               strlcpy(g_extern.savefile_dir, path, sizeof(g_extern.savefile_dir));
-               break;
-            case PATH_DEFAULT_ROM_DIR_CHOICE:
-               strlcpy(g_settings.rgui_browser_directory, path, sizeof(g_settings.rgui_browser_directory));
-               break;
-#ifdef HAVE_XML
-            case PATH_CHEATS_DIR_CHOICE:
-               strlcpy(g_settings.cheat_database, path, sizeof(g_settings.cheat_database));
-               break;
-#endif
-            case PATH_SYSTEM_DIR_CHOICE:
-               strlcpy(g_settings.system_directory, path, sizeof(g_settings.system_directory));
-               break;
-         }
          pop_menu_stack = true;
          break;
    }
