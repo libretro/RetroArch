6fe01e3fcfc9b32df1abd0c7a3ac5d795c65e73a
Themaister <maister@archlinux.us>
2013-09-30 18:45:04 +0200

    Remove an arbitrary hardcoded pad.
    Input config RGUI should avoid having to rely too much on autoconfigs.
    Keep Xinput pad autoconfig however, as its convenience is worth quite a lot.

--- input/autoconf/builtin_win.c
+++ input/autoconf/builtin_win.c
@@ -61,32 +61,6 @@ const char* const input_builtin_autoconfs[] =
    "input_driver = \"winxinput\"                    \n"
    XINPUT_DEFAULT_BINDS,
 
-   "input_device = \"Dual Trigger 3-in-1\" \n"
-   "input_driver = \"dinput\"              \n"
-   DECL_BTN(a, 2)
-   DECL_BTN(b, 1)
-   DECL_BTN(x, 3)
-   DECL_BTN(y, 0)
-   DECL_BTN(start, 9)
-   DECL_BTN(select, 8)
-   DECL_BTN(up, h0up)
-   DECL_BTN(down, h0down)
-   DECL_BTN(left, h0left)
-   DECL_BTN(right, h0right)
-   DECL_BTN(l, 4)
-   DECL_BTN(r, 5)
-   DECL_BTN(l2, 6)
-   DECL_BTN(r2, 7)
-   DECL_BTN(l3, 10)
-   DECL_BTN(r3, 11)
-   DECL_AXIS(l_x_plus,  +0)
-   DECL_AXIS(l_x_minus, -0)
-   DECL_AXIS(l_y_plus,  +1)
-   DECL_AXIS(l_y_minus, -1)
-   DECL_AXIS(r_x_plus,  +2)
-   DECL_AXIS(r_x_minus, -2)
-   DECL_AXIS(r_y_plus,  +5)
-   DECL_AXIS(r_y_minus, -5),
-
    NULL
 };
+
