a16be6af65b76d11ab1c39131cc85803d068bbed
Lioncash <mathew1800@gmail.com>
2013-10-08 23:52:10 -0400

    [Android] Remove a redundant context variable in ReportIME.java.

--- android/phoenix/src/org/retroarch/browser/ReportIME.java
+++ android/phoenix/src/org/retroarch/browser/ReportIME.java
@@ -15,21 +15,19 @@ public final class ReportIME extends Activity {
 		String current_ime = Settings.Secure.getString(getContentResolver(),
 				Settings.Secure.DEFAULT_INPUT_METHOD);
 
-		final Activity ctx = this;
 		AlertDialog.Builder dialog = new AlertDialog.Builder(this)
 				.setMessage(current_ime)
 				.setNeutralButton(R.string.close,
 						new DialogInterface.OnClickListener() {
 							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								ctx.finish();
+							public void onClick(DialogInterface dialog, int which) {
+								finish();
 							}
 						}).setCancelable(true)
 				.setOnCancelListener(new DialogInterface.OnCancelListener() {
 					@Override
 					public void onCancel(DialogInterface dialog) {
-						ctx.finish();
+						finish();
 					}
 				});
 
