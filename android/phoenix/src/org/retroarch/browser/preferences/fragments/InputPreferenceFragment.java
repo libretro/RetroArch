9121aea91e073c427f003d9e7bdcc653e87eb83e
Lioncash <mathew1800@gmail.com>
2013-10-15 10:28:02 -0400

    [Android] Eliminate the need for the ReportIME activity. We can just add a listener for the preference within InputPreferenceFragment.

--- android/phoenix/src/org/retroarch/browser/preferences/fragments/InputPreferenceFragment.java
+++ android/phoenix/src/org/retroarch/browser/preferences/fragments/InputPreferenceFragment.java
@@ -3,7 +3,11 @@ package org.retroarch.browser.preferences.fragments;
 import org.retroarch.R;
 import org.retroarch.browser.preferences.fragments.util.PreferenceListFragment;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.provider.Settings;
 
 /**
  * A {@link PreferenceListFragment} responsible for handling the input preferences.
@@ -17,5 +21,24 @@ public final class InputPreferenceFragment extends PreferenceListFragment
 
 		// Add input preferences from the XML.
 		addPreferencesFromResource(R.xml.input_preferences);
+
+		// Report IME preference
+		final Preference reportImePref = findPreference("report_ime_pref");
+		reportImePref.setOnPreferenceClickListener(new OnPreferenceClickListener()
+		{
+			@Override
+			public boolean onPreferenceClick(Preference preference)
+			{
+				final String currentIme = Settings.Secure.getString(getActivity().getContentResolver(),
+						Settings.Secure.DEFAULT_INPUT_METHOD);
+				
+				AlertDialog.Builder reportImeDialog = new AlertDialog.Builder(getActivity());
+				reportImeDialog.setTitle(R.string.current_ime);
+				reportImeDialog.setMessage(currentIme);
+				reportImeDialog.setNegativeButton(R.string.close, null);
+				reportImeDialog.show();
+				return true;
+			}
+		});
 	}
 }
