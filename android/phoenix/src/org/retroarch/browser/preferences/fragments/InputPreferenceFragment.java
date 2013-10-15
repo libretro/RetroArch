b1a48dea519461bab7486848951da843e4428495
Lioncash <mathew1800@gmail.com>
2013-10-15 10:40:15 -0400

    [Android] Remove the need for IMEActivity. We can also do this within InputPreferenceFragment.

--- android/phoenix/src/org/retroarch/browser/preferences/fragments/InputPreferenceFragment.java
+++ android/phoenix/src/org/retroarch/browser/preferences/fragments/InputPreferenceFragment.java
@@ -4,10 +4,12 @@ import org.retroarch.R;
 import org.retroarch.browser.preferences.fragments.util.PreferenceListFragment;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.provider.Settings;
+import android.view.inputmethod.InputMethodManager;
 
 /**
  * A {@link PreferenceListFragment} responsible for handling the input preferences.
@@ -21,6 +23,20 @@ public final class InputPreferenceFragment extends PreferenceListFragment
 
 		// Add input preferences from the XML.
 		addPreferencesFromResource(R.xml.input_preferences);
+
+		// Set Input Method preference
+		final Preference setImePref = findPreference("set_ime_pref");
+		setImePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
+		{
+			@Override
+			public boolean onPreferenceClick(Preference preference)
+			{
+				// Show an IME picker so the user can change their set IME.
+				InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.showInputMethodPicker();
+				return true;
+			}
+		});
 
 		// Report IME preference
 		final Preference reportImePref = findPreference("report_ime_pref");
