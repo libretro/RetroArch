57677f8d2ad47d1c5e41350f10e55e47ce1b925a
ToadKing <toadking@toadking.com>
2013-10-21 21:38:54 -0400

    [ANDROID] fix compile on windows broken make in android toolkit can't have paths with colons in LOCAL_SRC_FILES, just assume we're getting called from the right directory

--- android/phoenix/jni/Android.mk
+++ android/phoenix/jni/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE := retroarch-jni
-RARCH_DIR := $(LOCAL_PATH)/../../..
+RARCH_DIR := ../../..
 LOCAL_CFLAGS += -std=gnu99 -Wall -DHAVE_LOGGER -DRARCH_DUMMY_LOG -DHAVE_ZLIB -DHAVE_MMAP -I$(RARCH_DIR)
 LOCAL_LDLIBS := -llog -lz
 LOCAL_SRC_FILES := apk-extract/apk-extract.c $(RARCH_DIR)/file_extract.c $(RARCH_DIR)/file_path.c $(RARCH_DIR)/compat/compat.c
