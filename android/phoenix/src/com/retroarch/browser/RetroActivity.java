8f3a404ff6288231f9d09a3d167e69aec7202b0b
twinaphex <libretro@gmail.com>
2013-10-31 17:58:13 +0100

    (Android Phoenix) RetroActivity - get rid of unused showToastAlert

--- android/phoenix/src/com/retroarch/browser/RetroActivity.java
+++ android/phoenix/src/com/retroarch/browser/RetroActivity.java
@@ -2,7 +2,6 @@ package com.retroarch.browser;
 
 import android.app.NativeActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public final class RetroActivity extends NativeActivity {
 
@@ -17,21 +16,5 @@ public final class RetroActivity extends NativeActivity {
 
 	@Override
 	public void onTrimMemory(int level) {
-	}
-
-	// We call this function from native to display a toast string
-	public void showToastAlert(String text)
-	{
-		// We need to use a runnable here to ensure that when the spawned
-		// native_app_glue thread calls, we actually post the work to the UI
-		// thread.  Otherwise, we'll likely get exceptions because there's no
-		// prepared Looper on the native_app_glue main thread.
-		final String finalText = text;
-		runOnUiThread(new Runnable() {
-			public void run()
-			{
-				Toast.makeText(getApplicationContext(), finalText, Toast.LENGTH_SHORT).show();
-			}
-		});
 	}
 }
