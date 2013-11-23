17a6444574ea05e249d3e2d2f18156c4248aca25
Lioncash <mathew1800@gmail.com>
2013-11-23 12:58:42 -0500

    [Android] Move FileWrapper into the dirfragment package. It's only used by it. Also remove it from the AndroidManifest. This isn't necessary, since it's not an Activity (RetroTV is removed since this Activity isn't used anymore).
    Also added Javadoc to the FileWrapper class.

--- android/phoenix/src/com/retroarch/browser/dirfragment/FileWrapper.java
+++ android/phoenix/src/com/retroarch/browser/dirfragment/FileWrapper.java
@@ -1,13 +1,17 @@
-package com.retroarch.browser;
+package com.retroarch.browser.dirfragment;
 
 import java.io.File;
 
 import com.retroarch.R;
+import com.retroarch.browser.IconAdapterItem;
 
 import android.graphics.drawable.Drawable;
 
-public final class FileWrapper implements IconAdapterItem, Comparable<FileWrapper> {
-
+/**
+ * Wrapper for files within the {@link DirectoryFragment}.
+ */
+final class FileWrapper implements IconAdapterItem, Comparable<FileWrapper>
+{
 	public static final int DIRSELECT = 0;
 	public static final int PARENT = 1;
 	public static final int FILE = 2;
@@ -18,23 +22,33 @@ public final class FileWrapper implements IconAdapterItem, Comparable<FileWrappe
 	private final boolean enabled;
 	private final int typeIndex;
 
-	public FileWrapper(File file, int type, boolean isEnabled) {
+	/**
+	 * Constructor
+	 * 
+	 * @param file      The {@link File} to wrap.
+	 * @param type      The type of file being wrapped.
+	 * @param isEnabled Whether or not the item is selectable.
+	 */
+	public FileWrapper(File file, int type, boolean isEnabled)
+	{
 		this.file = file;
 
 		this.parentItem    = (type == PARENT);
-		this.dirSelectItem = (type == DIRSELECT);		
+		this.dirSelectItem = (type == DIRSELECT);
 		this.typeIndex     = (type == FILE) ? (FILE + (file.isDirectory() ? 0 : 1)) : type;	
 
 		this.enabled = parentItem || dirSelectItem || isEnabled;
 	}
 
 	@Override
-	public boolean isEnabled() {
+	public boolean isEnabled()
+	{
 		return enabled;
 	}
 
 	@Override
-	public String getText() {
+	public String getText()
+	{
 		if (dirSelectItem)
 			return "[[Use this directory]]";
 		else if (parentItem)
@@ -44,21 +58,27 @@ public final class FileWrapper implements IconAdapterItem, Comparable<FileWrappe
 	}
 
 	@Override
-	public String getSubText() {
+	public String getSubText()
+	{
 		return null;
 	}
 
 	@Override
-	public int getIconResourceId() {
-		if (!parentItem && !dirSelectItem) {
+	public int getIconResourceId()
+	{
+		if (!parentItem && !dirSelectItem)
+		{
 			return file.isFile() ? R.drawable.ic_file : R.drawable.ic_dir;
-		} else {
+		}
+		else
+		{
 			return R.drawable.ic_dir;
 		}
 	}
 
 	@Override
-	public Drawable getIconDrawable() {
+	public Drawable getIconDrawable()
+	{
 		return null;
 	}
 
@@ -69,7 +89,8 @@ public final class FileWrapper implements IconAdapterItem, Comparable<FileWrappe
 	 * @return true if the wrapped {@link File} is the "Parent Directory"
 	 *         item in the file browser; false otherwise.
 	 */
-	public boolean isParentItem() {
+	public boolean isParentItem()
+	{
 		return parentItem;
 	}
 
@@ -80,7 +101,8 @@ public final class FileWrapper implements IconAdapterItem, Comparable<FileWrappe
 	 * @return true if the wrapped {@link File} is the "Use this directory"
 	 *         item in the file browser; false otherwise.
 	 */
-	public boolean isDirSelectItem() {
+	public boolean isDirSelectItem()
+	{
 		return dirSelectItem;
 	}
 	
@@ -89,19 +111,25 @@ public final class FileWrapper implements IconAdapterItem, Comparable<FileWrappe
 	 * 
 	 * @return the file wrapped by this FileWrapper.
 	 */
-	public File getFile() {
+	public File getFile()
+	{
 		return file;
 	}
 
 	@Override
-	public int compareTo(FileWrapper other) {
-		if (other != null) {
+	public int compareTo(FileWrapper other)
+	{
+		if (other != null)
+		{
 			// Who says ternary is hard to follow
-			if (isEnabled() == other.isEnabled()) {
+			if (isEnabled() == other.isEnabled())
+			{
 				return (typeIndex == other.typeIndex) ? file
 						.compareTo(other.file)
 						: ((typeIndex < other.typeIndex) ? -1 : 1);
-			} else {
+			}
+			else
+			{
 				return isEnabled() ? -1 : 1;
 			}
 		}
