6458ab09f4b204a5160f593830e799f7317e7fc2
AreaScout <d.mehrwald@gmail.com>
2013-12-04 14:14:00 +0100

    Fix wrong handle of merge conflict + Cosmetics

--- qb/config.params.sh
+++ qb/config.params.sh
@@ -38,4 +38,4 @@ HAVE_NEON=no            # Forcefully enable ARM NEON optimizations
 HAVE_SSE=no             # Forcefully enable x86 SSE optimizations (SSE, SSE2)
 HAVE_FLOATHARD=no       # Force hard float ABI (for ARM)
 HAVE_FLOATSOFTFP=no     # Force soft float ABI (for ARM)
-HAVE_LIMA=no            # Enable limadriver support
+HAVE_LIMA=no            # Enable Limadriver video support
