7fe9ec647cf46ce47d0fc5215197b2678f453018
twinaphex <libretro@gmail.com>
2013-12-26 18:07:09 +0100

    (PS3) Refactor PS3 input driver - no button input right now in RGUI though

--- ps3/ps3_input.c
+++ ps3/ps3_input.c
@@ -69,9 +69,12 @@ const struct platform_bind platform_keys[] = {
    { (1ULL << RARCH_TURBO_ENABLE), "Turbo button (unmapped)" },
 };
 
+extern const rarch_joypad_driver_t ps3_joypad;
+
 typedef struct ps3_input
 {
-   uint64_t state[MAX_PADS];
+   uint64_t pad_state[MAX_PADS];
+   int16_t analog_state[MAX_PADS][2][2];
    unsigned pads_connected;
 #ifdef HAVE_MOUSE
    unsigned mice_connected;
@@ -79,7 +82,6 @@ typedef struct ps3_input
    sensor_t accelerometer_state[MAX_PADS];
 } ps3_input_t;
 
-uint8_t analog_state[MAX_PADS][2][2];
 
 static void ps3_input_poll(void *data)
 {
@@ -93,12 +95,12 @@ static void ps3_input_poll(void *data)
 
       if (state_tmp.len != 0)
       {
-         uint64_t *state_cur = &ps3->state[port];
+         uint64_t *state_cur = &ps3->pad_state[port];
          *state_cur = 0;
-         analog_state[0][0][0] = analog_state[0][0][1] = analog_state[0][1][0] = analog_state[0][1][1] = 0;
-         analog_state[1][0][0] = analog_state[1][0][1] = analog_state[1][1][0] = analog_state[1][1][1] = 0;
-         analog_state[2][0][0] = analog_state[2][0][1] = analog_state[2][1][0] = analog_state[2][1][1] = 0;
-         analog_state[3][0][0] = analog_state[3][0][1] = analog_state[3][1][0] = analog_state[3][1][1] = 0;
+         ps3->analog_state[0][0][0] = ps3->analog_state[0][0][1] = ps3->analog_state[0][1][0] = ps3->analog_state[0][1][1] = 0;
+         ps3->analog_state[1][0][0] = ps3->analog_state[1][0][1] = ps3->analog_state[1][1][0] = ps3->analog_state[1][1][1] = 0;
+         ps3->analog_state[2][0][0] = ps3->analog_state[2][0][1] = ps3->analog_state[2][1][0] = ps3->analog_state[2][1][1] = 0;
+         ps3->analog_state[3][0][0] = ps3->analog_state[3][0][1] = ps3->analog_state[3][1][0] = ps3->analog_state[3][1][1] = 0;
 #ifdef __PSL1GHT__
          *state_cur |= (state_tmp.BTN_LEFT)     ? (1ULL << RETRO_DEVICE_ID_JOYPAD_LEFT) : 0;
          *state_cur |= (state_tmp.BTN_DOWN)     ? (1ULL << RETRO_DEVICE_ID_JOYPAD_DOWN) : 0;
@@ -134,10 +136,10 @@ static void ps3_input_poll(void *data)
          *state_cur |= (state_tmp.button[CELL_PAD_BTN_OFFSET_DIGITAL2] & CELL_PAD_CTRL_R2) ? (1ULL << RETRO_DEVICE_ID_JOYPAD_R2) : 0;
          *state_cur |= (state_tmp.button[CELL_PAD_BTN_OFFSET_DIGITAL2] & CELL_PAD_CTRL_L2) ? (1ULL << RETRO_DEVICE_ID_JOYPAD_L2) : 0;
          *state_cur |= (state_tmp.button[CELL_PAD_BTN_OFFSET_DIGITAL2] & CELL_PAD_CTRL_L2) ? (1ULL << RETRO_DEVICE_ID_JOYPAD_L2) : 0;
-         analog_state[port][RETRO_DEVICE_INDEX_ANALOG_LEFT ][RETRO_DEVICE_ID_ANALOG_X] = state_tmp.button[CELL_PAD_BTN_OFFSET_ANALOG_LEFT_X];
-         analog_state[port][RETRO_DEVICE_INDEX_ANALOG_LEFT ][RETRO_DEVICE_ID_ANALOG_Y] = state_tmp.button[CELL_PAD_BTN_OFFSET_ANALOG_LEFT_Y];
-         analog_state[port][RETRO_DEVICE_INDEX_ANALOG_RIGHT][RETRO_DEVICE_ID_ANALOG_X] = state_tmp.button[CELL_PAD_BTN_OFFSET_ANALOG_RIGHT_X];
-         analog_state[port][RETRO_DEVICE_INDEX_ANALOG_RIGHT][RETRO_DEVICE_ID_ANALOG_Y] = state_tmp.button[CELL_PAD_BTN_OFFSET_ANALOG_RIGHT_Y];
+         ps3->analog_state[port][RETRO_DEVICE_INDEX_ANALOG_LEFT ][RETRO_DEVICE_ID_ANALOG_X] = state_tmp.button[CELL_PAD_BTN_OFFSET_ANALOG_LEFT_X];
+         ps3->analog_state[port][RETRO_DEVICE_INDEX_ANALOG_LEFT ][RETRO_DEVICE_ID_ANALOG_Y] = state_tmp.button[CELL_PAD_BTN_OFFSET_ANALOG_LEFT_Y];
+         ps3->analog_state[port][RETRO_DEVICE_INDEX_ANALOG_RIGHT][RETRO_DEVICE_ID_ANALOG_X] = state_tmp.button[CELL_PAD_BTN_OFFSET_ANALOG_RIGHT_X];
+         ps3->analog_state[port][RETRO_DEVICE_INDEX_ANALOG_RIGHT][RETRO_DEVICE_ID_ANALOG_Y] = state_tmp.button[CELL_PAD_BTN_OFFSET_ANALOG_RIGHT_Y];
          ps3->accelerometer_state[port].x = state_tmp.button[CELL_PAD_BTN_OFFSET_SENSOR_X];
          ps3->accelerometer_state[port].y = state_tmp.button[CELL_PAD_BTN_OFFSET_SENSOR_Y];
          ps3->accelerometer_state[port].z = state_tmp.button[CELL_PAD_BTN_OFFSET_SENSOR_Z];
@@ -145,7 +147,7 @@ static void ps3_input_poll(void *data)
       }
    }
 
-   uint64_t *state_p1 = &ps3->state[0];
+   uint64_t *state_p1 = &ps3->pad_state[0];
    uint64_t *lifecycle_state = &g_extern.lifecycle_state;
 
    *lifecycle_state &= ~((1ULL << RARCH_MENU_TOGGLE));
@@ -193,7 +195,6 @@ static int16_t ps3_input_state(void *data, const struct retro_keybind **binds,
       unsigned index, unsigned id)
 {
    ps3_input_t *ps3 = (ps3_input_t*)data;
-   uint64_t button;
    int16_t retval = 0;
 
    if (port < ps3->pads_connected)
@@ -201,17 +202,9 @@ static int16_t ps3_input_state(void *data, const struct retro_keybind **binds,
       switch (device)
       {
          case RETRO_DEVICE_JOYPAD:
-            button = binds[port][id].joykey;
-            retval = (ps3->state[port] & button) ? 1 : 0;
-            break;
+            return input_joypad_pressed(&ps3_joypad, port, binds[port], id);
          case RETRO_DEVICE_ANALOG:
-            {
-               int analog = (analog_state[port][index][id] - 128) * 0x0101;
-               if (analog < -0x7fff)
-                  analog = -0x7fff;
-               retval = analog;
-            }
-            break;
+            return input_joypad_analog(&ps3_joypad, port, index, id, binds[port]);
          case RETRO_DEVICE_SENSOR_ACCELEROMETER:
             switch (id)
             {
@@ -269,11 +262,61 @@ static void ps3_input_set_keybinds(void *data, unsigned device,
 
    if (keybind_action & (1ULL << KEYBINDS_ACTION_SET_DEFAULT_BINDS))
    {
+      strlcpy(g_settings.input.device_names[port], "DualShock3/Sixaxis", sizeof(g_settings.input.device_names[port]));
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_B].def_joykey       = platform_keys[RETRO_DEVICE_ID_JOYPAD_B].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_Y].def_joykey       = platform_keys[RETRO_DEVICE_ID_JOYPAD_Y].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_SELECT].def_joykey  = platform_keys[RETRO_DEVICE_ID_JOYPAD_SELECT].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_START].def_joykey   = platform_keys[RETRO_DEVICE_ID_JOYPAD_START].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_UP].def_joykey      = platform_keys[RETRO_DEVICE_ID_JOYPAD_UP].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_DOWN].def_joykey    = platform_keys[RETRO_DEVICE_ID_JOYPAD_DOWN].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_LEFT].def_joykey    = platform_keys[RETRO_DEVICE_ID_JOYPAD_LEFT].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_RIGHT].def_joykey   = platform_keys[RETRO_DEVICE_ID_JOYPAD_RIGHT].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_A].def_joykey       = platform_keys[RETRO_DEVICE_ID_JOYPAD_A].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_X].def_joykey       = platform_keys[RETRO_DEVICE_ID_JOYPAD_X].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_L].def_joykey       = platform_keys[RETRO_DEVICE_ID_JOYPAD_L].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_R].def_joykey       = platform_keys[RETRO_DEVICE_ID_JOYPAD_R].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_L2].def_joykey      = platform_keys[RETRO_DEVICE_ID_JOYPAD_L2].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_R2].def_joykey      = platform_keys[RETRO_DEVICE_ID_JOYPAD_R2].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_L3].def_joykey      = platform_keys[RETRO_DEVICE_ID_JOYPAD_L3].joykey;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_R3].def_joykey      = platform_keys[RETRO_DEVICE_ID_JOYPAD_L3].joykey;
+      g_settings.input.binds[port][RARCH_ANALOG_LEFT_X_PLUS].def_joykey       = NO_BTN;
+      g_settings.input.binds[port][RARCH_ANALOG_LEFT_X_MINUS].def_joykey      = NO_BTN;
+      g_settings.input.binds[port][RARCH_ANALOG_LEFT_Y_PLUS].def_joykey       = NO_BTN;
+      g_settings.input.binds[port][RARCH_ANALOG_LEFT_Y_MINUS].def_joykey      = NO_BTN;
+      g_settings.input.binds[port][RARCH_ANALOG_RIGHT_X_PLUS].def_joykey      = NO_BTN;
+      g_settings.input.binds[port][RARCH_ANALOG_RIGHT_X_MINUS].def_joykey     = NO_BTN;
+      g_settings.input.binds[port][RARCH_ANALOG_RIGHT_Y_PLUS].def_joykey      = NO_BTN;
+      g_settings.input.binds[port][RARCH_ANALOG_RIGHT_Y_MINUS].def_joykey     = NO_BTN;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_B].def_joyaxis      = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_Y].def_joyaxis      = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_SELECT].def_joyaxis = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_START].def_joyaxis  = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_UP].def_joyaxis     = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_DOWN].def_joyaxis   = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_LEFT].def_joyaxis   = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_RIGHT].def_joyaxis  = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_A].def_joyaxis      = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_X].def_joyaxis      = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_L].def_joyaxis      = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_R].def_joyaxis      = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_L2].def_joyaxis     = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_R2].def_joyaxis     = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_L3].def_joyaxis     = AXIS_NONE;
+      g_settings.input.binds[port][RETRO_DEVICE_ID_JOYPAD_R3].def_joyaxis     = AXIS_NONE;
+      g_settings.input.binds[port][RARCH_ANALOG_LEFT_X_PLUS].def_joyaxis      = AXIS_POS(0);
+      g_settings.input.binds[port][RARCH_ANALOG_LEFT_X_MINUS].def_joyaxis     = AXIS_NEG(0);
+      g_settings.input.binds[port][RARCH_ANALOG_LEFT_Y_PLUS].def_joyaxis      = AXIS_POS(1);
+      g_settings.input.binds[port][RARCH_ANALOG_LEFT_Y_MINUS].def_joyaxis     = AXIS_NEG(1);
+      g_settings.input.binds[port][RARCH_ANALOG_RIGHT_X_PLUS].def_joyaxis     = AXIS_POS(2);
+      g_settings.input.binds[port][RARCH_ANALOG_RIGHT_X_MINUS].def_joyaxis    = AXIS_NEG(2);
+      g_settings.input.binds[port][RARCH_ANALOG_RIGHT_Y_PLUS].def_joyaxis     = AXIS_POS(3);
+      g_settings.input.binds[port][RARCH_ANALOG_RIGHT_Y_MINUS].def_joyaxis    = AXIS_NEG(3);
+
       for (int i = 0; i < RARCH_CUSTOM_BIND_LIST_END; i++)
       {
          g_settings.input.binds[port][i].id = i;
-         g_settings.input.binds[port][i].def_joykey = platform_keys[i].joykey;
          g_settings.input.binds[port][i].joykey = g_settings.input.binds[port][i].def_joykey;
+         g_settings.input.binds[port][i].joyaxis = g_settings.input.binds[port][i].def_joyaxis;
       }
    }
    
@@ -376,6 +419,11 @@ static bool ps3_input_set_rumble(void *data, unsigned port, enum retro_rumble_ef
    return true;
 }
 
+static const rarch_joypad_driver_t *ps3_input_get_joypad_driver(void *data)
+{
+   return &ps3_joypad;
+}
+
 const input_driver_t input_ps3 = {
    ps3_input_init,
    ps3_input_poll,
@@ -389,5 +437,90 @@ const input_driver_t input_ps3 = {
 
    NULL,
    ps3_input_set_rumble,
+   ps3_input_get_joypad_driver,
+};
+
+static bool ps3_joypad_init(void)
+{
+   return true;
+}
+
+static bool ps3_joypad_button(unsigned port_num, uint16_t joykey)
+{
+   ps3_input_t *ps3 = (ps3_input_t*)driver.input_data;
+
+   if (port_num >= MAX_PADS)
+      return false;
+
+   return ps3->pad_state[port_num] & (1ULL << joykey);
+}
+
+static int16_t ps3_joypad_axis(unsigned port_num, uint32_t joyaxis)
+{
+   ps3_input_t *ps3 = (ps3_input_t*)driver.input_data;
+   if (joyaxis == AXIS_NONE || port_num >= MAX_PADS)
+      return 0;
+
+   int val = 0;
+
+   int axis    = -1;
+   bool is_neg = false;
+   bool is_pos = false;
+
+   if (AXIS_NEG_GET(joyaxis) < 4)
+   {
+      axis = AXIS_NEG_GET(joyaxis);
+      is_neg = true;
+   }
+   else if (AXIS_POS_GET(joyaxis) < 4)
+   {
+      axis = AXIS_POS_GET(joyaxis);
+      is_pos = true;
+   }
+
+   switch (axis)
+   {
+      case 0: val = ps3->analog_state[port_num][0][0]; break;
+      case 1: val = ps3->analog_state[port_num][0][1]; break;
+      case 2: val = ps3->analog_state[port_num][1][0]; break;
+      case 3: val = ps3->analog_state[port_num][1][1]; break;
+   }
+
+   if (is_neg && val > 0)
+      val = 0;
+   else if (is_pos && val < 0)
+      val = 0;
+
+   return val;
+}
+
+static void ps3_joypad_poll(void)
+{
+}
+
+static bool ps3_joypad_query_pad(unsigned pad)
+{
+   ps3_input_t *ps3 = (ps3_input_t*)driver.input_data;
+   return pad < MAX_PLAYERS && ps3->pad_state[pad];
+}
+
+static const char *ps3_joypad_name(unsigned pad)
+{
+   return NULL;
+}
+
+static void ps3_joypad_destroy(void)
+{
+}
+
+const rarch_joypad_driver_t ps3_joypad = {
+   ps3_joypad_init,
+   ps3_joypad_query_pad,
+   ps3_joypad_destroy,
+   ps3_joypad_button,
+   ps3_joypad_axis,
+   ps3_joypad_poll,
    NULL,
+   ps3_joypad_name,
+   "ps3",
 };
