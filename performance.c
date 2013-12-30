2ace9a81a8507d64d9570a94af2045a6941dc5e5
Themaister <maister@archlinux.us>
2013-12-30 10:19:32 +0100

    Comment arm_enable_runfast_mode().

--- performance.c
+++ performance.c
@@ -262,14 +262,15 @@ static uint64_t xgetbv_x86(uint32_t index)
 #ifdef HAVE_NEON
 static void arm_enable_runfast_mode(void)
 {
-   static const unsigned int x = 0x04086060;
-   static const unsigned int y = 0x03000000;
+   // RunFast mode. Enables flush-to-zero and some floating point optimizations.
+   static const unsigned x = 0x04086060;
+   static const unsigned y = 0x03000000;
    int r;
    asm volatile(
-         "fmrx	%0, fpscr			\n\t"	//r0 = FPSCR
-         "and	%0, %0, %1			\n\t"	//r0 = r0 & 0x04086060
-         "orr	%0, %0, %2			\n\t"	//r0 = r0 | 0x03000000
-         "fmxr	fpscr, %0			\n\t"	//FPSCR = r0
+         "fmrx	%0, fpscr   \n\t" // r0 = FPSCR
+         "and	%0, %0, %1  \n\t" // r0 = r0 & 0x04086060
+         "orr	%0, %0, %2  \n\t" // r0 = r0 | 0x03000000
+         "fmxr	fpscr, %0   \n\t" // FPSCR = r0
          : "=r"(r)
          : "r"(x), "r"(y)
         );
