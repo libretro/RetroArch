dc734aea557622076040d2d0a8c3ba790819dc07
twinaphex <autechre1024@hotmail.com>
2013-12-31 23:26:12 +0100

    (D3D9) Get rid of C++ memory header include

--- gfx/d3d9/d3d9.hpp
+++ gfx/d3d9/d3d9.hpp
@@ -1,5 +1,6 @@
 /*  RetroArch - A frontend for libretro.
- *  Copyright (C) 2010-2012 - Hans-Kristian Arntzen
+ *  Copyright (C) 2010-2013 - Hans-Kristian Arntzen
+ *  Copyright (C) 2011-2013 - Daniel De Matteis
  * 
  *  RetroArch is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU General Public License as published by the Free Software Found-
@@ -32,7 +33,6 @@
 #endif
 #include <string>
 #include <vector>
-#include <memory>
 
 class RenderChain;
 
