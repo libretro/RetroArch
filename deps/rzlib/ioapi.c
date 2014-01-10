e9bf4f04937ca3bcdd921d59d6df3ce278307d68
twinaphex <autechre1024@hotmail.com>
2014-01-10 20:38:00 +0100

    (360) Build changes

--- deps/rzlib/ioapi.c
+++ deps/rzlib/ioapi.c
@@ -10,8 +10,10 @@
 
 */
 
-#if (defined(_WIN32))
-        #define _CRT_SECURE_NO_WARNINGS
+#ifdef _WIN32
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
 #endif
 
 #include "ioapi.h"
