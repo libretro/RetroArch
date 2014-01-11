252efff8daec5b40e2cacdfbece1bf7b83c1e43f
Themaister <maister@archlinux.us>
2014-01-11 18:51:42 +0100

    Log Git version in verbose logs.
    Makes it much easier to figure out which builds users are running when
    pasting logs.

---
+++ git_version.h
@@ -0,0 +1,23 @@
+/*  RetroArch - A frontend for libretro.
+ *  Copyright (C) 2010-2014 - Hans-Kristian Arntzen
+ * 
+ *  RetroArch is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with RetroArch.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef RARCH_VERSION_H__
+#define RARCH_VERSION_H__
+
+// Put this in a separate file so we don't have to rebuilt retroarch.c every single build.
+extern const char rarch_git_version[];
+
+#endif
+
