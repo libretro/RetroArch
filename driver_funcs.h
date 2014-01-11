b74aaa25cef397d923d515e083db7463c5f0325e
Themaister <maister@archlinux.us>
2014-01-11 12:13:41 +0100

    Only block input driver hotkey handling.
    Don't block overlay and command input when input_hotkey_enable is set.

--- driver_funcs.h
+++ driver_funcs.h
@@ -56,13 +56,13 @@
 
 static inline bool input_key_pressed_func(int key)
 {
-   if (driver.block_hotkey)
-      return false;
+   bool ret = false;
 
-   bool ret = driver.input->key_pressed(driver.input_data, key);
+   if (!driver.block_hotkey)
+      ret = ret || driver.input->key_pressed(driver.input_data, key);
 
 #ifdef HAVE_OVERLAY
-   ret |= driver.overlay_state.buttons & (UINT64_C(1) << key);
+   ret = ret || (driver.overlay_state.buttons & (1ULL << key));
 #endif
 
 #ifdef HAVE_COMMAND
