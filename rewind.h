c0a6f52ea942052aec69a833e010126e03d4fb6d
Alcaro <floating@muncher.se>
2014-02-21 19:35:55 +0100

    Missed yet another spot...

--- rewind.h
+++ rewind.h
@@ -28,6 +28,6 @@ void state_manager_free(state_manager_t *state);
 bool state_manager_pop(state_manager_t *state, void **data);
 void *state_manager_push_where(state_manager_t *state);
 bool state_manager_push_do(state_manager_t *state);
-void state_manager_capacity(state_manager_t *state, unsigned int * entries, size_t * bytes, bool * full);
+void state_manager_capacity(state_manager_t *state, unsigned int *entries, size_t *bytes, bool *full);
 
 #endif
