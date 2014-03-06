3d2c4171cd2686ca5f7832d0c5f77239c6f14761
twinaphex <autechre1024@hotmail.com>
2014-03-06 01:02:00 +0100

    (core_info) Add back hack for RARCH_CONSOLE

--- frontend/info/core_info.c
+++ frontend/info/core_info.c
@@ -121,7 +121,7 @@ core_info_list_t *core_info_list_new(const char *modules_path)
       fill_pathname_base(info_path_base, contents->elems[i].data, sizeof(info_path_base));
       path_remove_extension(info_path_base);
 
-#if defined(RARCH_MOBILE)
+#if defined(RARCH_MOBILE) || defined(RARCH_CONSOLE)
       char *substr = strrchr(info_path_base, '_');
       if (substr)
          *substr = '\0';
