5e317202e1ef907be3f5e1c6b483fa5e9ceca0d8
twinaphex <libretro@gmail.com>
2014-05-01 05:27:49 +0200

    (Filter) Blargg NTSC SNES composite should now be fixed - RGB565 only right now

--- gfx/filters/blargg_ntsc_snes_composite.c
+++ gfx/filters/blargg_ntsc_snes_composite.c
@@ -113,9 +113,6 @@ static void blargg_ntsc_snes_composite_render_rgb565(int width, int height,
    if(!ntsc)
       return;
 
-   pitch >>= 1;
-   outpitch >>= 1;
-
    if(width <= 256)
       snes_ntsc_blit(ntsc, input, pitch, burst, width, height, output, outpitch * 2, first, last);
    else
