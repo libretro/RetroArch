b162329eab4d282f1dfa389c72fff1a66ea8c8b6
twinaphex <libretro@gmail.com>
2014-05-22 21:26:57 +0200

    (GL) Hoist some more identical lines out of if (stencil) else block

--- gfx/gl.c
+++ gfx/gl.c
@@ -729,10 +729,12 @@ static bool gl_init_hw_render(gl_t *gl, unsigned width, unsigned height)
       if (depth)
       {
          glBindRenderbuffer(RARCH_GL_RENDERBUFFER, gl->hw_render_depth[i]);
+         glRenderbufferStorage(RARCH_GL_RENDERBUFFER, stencil ? RARCH_GL_DEPTH24_STENCIL8 : GL_DEPTH_COMPONENT16,
+               width, height);
+         glBindRenderbuffer(RARCH_GL_RENDERBUFFER, 0);
+
          if (stencil)
          {
-            glRenderbufferStorage(RARCH_GL_RENDERBUFFER, RARCH_GL_DEPTH24_STENCIL8, width, height);
-            glBindRenderbuffer(RARCH_GL_RENDERBUFFER, 0);
 #if defined(HAVE_OPENGLES2) || defined(OSX_PPC)
             // GLES2 is a bit weird, as always. :P
             // There's no GL_DEPTH_STENCIL_ATTACHMENT like in desktop GL.
@@ -748,9 +750,6 @@ static bool gl_init_hw_render(gl_t *gl, unsigned width, unsigned height)
          }
          else
          {
-            glRenderbufferStorage(RARCH_GL_RENDERBUFFER, GL_DEPTH_COMPONENT16,
-                  width, height);
-            glBindRenderbuffer(RARCH_GL_RENDERBUFFER, 0);
             glFramebufferRenderbuffer(RARCH_GL_FRAMEBUFFER, RARCH_GL_DEPTH_ATTACHMENT,
                   RARCH_GL_RENDERBUFFER, gl->hw_render_depth[i]);
          }
