9e844b06b341fb4377eb3ce57d177c9c5299018a
twinaphex <libretro@gmail.com>
2014-06-11 23:13:43 +0200

    (Android) Input cleanup - don't need all these keycode enums anymore

--- input/android_input.c
+++ input/android_input.c
@@ -26,80 +26,13 @@
 #include "../driver.h"
 
 #define MAX_TOUCH 16
-#define PRESSED_UP(x, y)    ((y <= (-g_settings.input.axis_threshold)))
-#define PRESSED_DOWN(x, y)  ((y >= (g_settings.input.axis_threshold)))
-#define PRESSED_LEFT(x, y)  ((x <= (-g_settings.input.axis_threshold)))
-#define PRESSED_RIGHT(x, y) ((x >= (g_settings.input.axis_threshold)))
-
 #define MAX_PADS 8
 
 #define AKEY_EVENT_NO_ACTION 255
 
-enum {
-   AKEYCODE_META_FUNCTION_ON = 8,
-   AKEYCODE_NUMPAD_LCK_0    = 96,
-   AKEYCODE_NUMPAD_LCK_1    = 97,
-   AKEYCODE_NUMPAD_LCK_2    = 98,
-   AKEYCODE_NUMPAD_LCK_3    = 99,
-   AKEYCODE_NUMPAD_LCK_4    = 100,
-   AKEYCODE_NUMPAD_LCK_5    = 101,
-   AKEYCODE_NUMPAD_LCK_6    = 102,
-   AKEYCODE_NUMPAD_LCK_7    = 103,
-   AKEYCODE_NUMPAD_LCK_8    = 104,
-   AKEYCODE_NUMPAD_LCK_9    = 105,
-   AKEYCODE_OTHR_108        = 108,
-   AKEYCODE_NUMPAD_SUB      = 109,
-   AKEYCODE_ESCAPE          = 111,
-   AKEYCODE_FORWARD_DEL     = 112,
-   AKEYCODE_CTRL_LEFT       = 113,
-   AKEYCODE_CTRL_RIGHT      = 114,
-   AKEYCODE_CAPS_LOCK       = 115,
-   AKEYCODE_SCROLL_LOCK     = 116,
-   AKEYCODE_SYSRQ           = 120,
-   AKEYCODE_BREAK           = 121,
-   AKEYCODE_MOVE_HOME       = 122,
-   AKEYCODE_MOVE_END        = 123,
-   AKEYCODE_INSERT          = 124,
-   AKEYCODE_FORWARD         = 125,
-   AKEYCODE_MEDIA_PLAY      = 126,
-   AKEYCODE_MEDIA_PAUSE     = 127,
-   AKEYCODE_F1              = 131,
-   AKEYCODE_F2              = 132,
-   AKEYCODE_F3              = 133,
-   AKEYCODE_F4              = 134,
-   AKEYCODE_F5              = 135,
-   AKEYCODE_F6              = 136,
-   AKEYCODE_F7              = 137,
-   AKEYCODE_F8              = 138,
-   AKEYCODE_F9              = 139,
-   AKEYCODE_NUMPAD_1        = 145,
-   AKEYCODE_NUMPAD_2        = 146,
-   AKEYCODE_NUMPAD_3        = 147,
-   AKEYCODE_NUMPAD_4        = 148,
-   AKEYCODE_NUMPAD_5        = 149,
-   AKEYCODE_NUMPAD_6        = 150,
-   AKEYCODE_NUMPAD_7        = 151,
-   AKEYCODE_NUMPAD_8        = 152,
-   AKEYCODE_NUMPAD_9        = 153,
-   AKEYCODE_WINDOW          = 171,
-   AKEYCODE_BUTTON_1        = 188,
-   AKEYCODE_BUTTON_2        = 189,
-   AKEYCODE_BUTTON_3        = 190,
-   AKEYCODE_BUTTON_4        = 191,
-   AKEYCODE_BUTTON_5        = 192,
-   AKEYCODE_BUTTON_6        = 193,
-   AKEYCODE_BUTTON_7        = 194,
-   AKEYCODE_BUTTON_8        = 195,
-   AKEYCODE_BUTTON_9        = 196,
-   AKEYCODE_BUTTON_10       = 197,
-   AKEYCODE_BUTTON_11       = 198,
-   AKEYCODE_BUTTON_12       = 199,
-   AKEYCODE_BUTTON_13       = 200,
-   AKEYCODE_BUTTON_14       = 201,
-   AKEYCODE_BUTTON_15       = 202,
-   AKEYCODE_BUTTON_16       = 203,
-   AKEYCODE_ASSIST          = 219,
-};
+#ifndef AKEYCODE_ASSIST
+#define AKEYCODE_ASSIST 219
+#endif
 
 #define LAST_KEYCODE AKEYCODE_ASSIST
 
