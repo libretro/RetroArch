61ba7ca2a691606e0bffe646f7f2b26991b837f0
vikbez <v@42.am>
2014-06-13 22:30:45 +0200

    map xbox360 pad center button to menu_toggle_btn

--- input/autoconf/builtin_android.c
+++ input/autoconf/builtin_android.c
@@ -366,7 +366,8 @@ DECL_AXIS(l_y_minus, -1) \
 DECL_AXIS(r_x_plus,  +2) \
 DECL_AXIS(r_x_minus, -2) \
 DECL_AXIS(r_y_plus, +3) \
-DECL_AXIS(r_y_minus, -3)
+DECL_AXIS(r_y_minus, -3) \
+"input_menu_toggle_btn = 82\n"
 
 #define HUIJIA_DEFAULT_BINDS \
 DECL_BTN(a, 189) \
