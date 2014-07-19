34002c0e10835d4b5a3e6eaf6ac96e3b4d62a17d
Twinaphex <libretro@gmail.com>
2014-07-19 17:28:06 +0200

    (Apple) Don't have ifdef IOS conditional in apple_run_core

--- apple/common/main.m
+++ apple/common/main.m
@@ -42,8 +42,6 @@ void apple_run_core(NSString* core, const char* file)
    char **argv;
    int argc;
     
-   (void)config_path;
-    
     [apple_platform loadingCore:core withFile:file];
     
     apple_core = core;
@@ -54,7 +52,6 @@ void apple_run_core(NSString* core, const char* file)
       strlcpy(file_path, file, sizeof(file_path));
    }
    
-#ifdef IOS
     if (core_info_has_custom_config(apple_core.UTF8String))
         core_info_get_custom_config(apple_core.UTF8String, config_path, sizeof(config_path));
     else
@@ -64,12 +61,6 @@ void apple_run_core(NSString* core, const char* file)
     static const char* const argv_menu[] = { "retroarch", "-c", config_path, "--menu", 0 };
     
     argc = (file && core) ? 6 : 4;
-#else
-   static const char* const argv_game[] = { "retroarch", "-L", core_path, file_path, 0 };
-   static const char* const argv_menu[] = { "retroarch", "--menu", 0 };
-
-   argc = (file && core) ? 4 : 2;
-#endif
     argv = (char**)((file && core) ? argv_game : argv_menu);
 
     if (apple_rarch_load_content(&argc, argv))
