08d95723a9d4c3f5e0c218c8d8e37881577d5cf3
SuperrSonic <Diego_Arroyo@live.com>
2014-08-10 16:44:17 -0400

    added viwidth stuff

--- driver.c
+++ driver.c
@@ -1093,6 +1093,7 @@ void init_video_input(void)
    video.fullscreen = g_settings.video.fullscreen;
    video.vsync = g_settings.video.vsync && !g_extern.system.force_nonblock;
    video.force_aspect = g_settings.video.force_aspect;
+   video.viwidth = g_settings.video.viwidth;
    video.smooth = g_settings.video.smooth;
    video.input_scale = scale;
    video.rgb32 = g_extern.filter.filter ? g_extern.filter.out_rgb32 : (g_extern.system.pix_fmt == RETRO_PIXEL_FORMAT_XRGB8888);
