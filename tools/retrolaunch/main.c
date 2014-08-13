dd72443211d83e1b9b13d6fccd69f767184e6355
twinaphex <libretro@gmail.com>
2014-08-13 02:51:49 +0200

    (retrolaunch) cleanups

--- tools/retrolaunch/main.c
+++ tools/retrolaunch/main.c
@@ -26,124 +26,117 @@
 static int find_hash(int fd, const char *hash, char *game_name, size_t max_len)
 {
 	char token[MAX_TOKEN_LEN] = {0};
-	while (1) {
-		if (find_token(fd, "game") < 0) {
-			return -1;
-		}
+	while (1)
+   {
+      if (find_token(fd, "game") < 0)
+         return -1;
 
-		if (find_token(fd, "name") < 0) {
-			return -1;
-		}
+      if (find_token(fd, "name") < 0)
+         return -1;
 
-		if (get_token(fd, game_name, max_len) < 0) {
-			return -1;
-		}
+      if (get_token(fd, game_name, max_len) < 0)
+         return -1;
 
-		if (find_token(fd, "sha1") < 0) {
-			return -1;
-		}
+      if (find_token(fd, "sha1") < 0)
+         return -1;
 
-		if (get_token(fd, token, MAX_TOKEN_LEN) < 0) {
-			return -1;
-		}
+      if (get_token(fd, token, MAX_TOKEN_LEN) < 0)
+         return -1;
 
-		if (strcasecmp(hash, token) == 0) {
-			return 0;
-		}
-	}
+      if (strcasecmp(hash, token) == 0)
+         return 0;
+   }
 }
 
 static int
 find_rom_canonical_name(const char *hash, char *game_name, size_t max_len)
 {
-	// TODO: Error handling
-	size_t i;
-	int rv;
-	int fd;
-	int offs;
-	char *dat_path;
-	const char *dat_name;
-	const char *dat_name_dot;
-	struct string_list *files;
+   // TODO: Error handling
+   size_t i;
+   int rv, fd, offs;
+   char *dat_path;
+   const char *dat_name, *dat_name_dot;
+   struct string_list *files = dir_list_new("db", "dat", false);
 
-	files = dir_list_new("db", "dat", false);
-	if (!files) {
-		return -1;
-	}
+   if (!files)
+      return -1;
 
-	for (i = 0; i < files->size; i++) {
-		dat_path = files->elems[i].data;
-		dat_name = path_basename(dat_path);
+   for (i = 0; i < files->size; i++)
+   {
+      dat_path = files->elems[i].data;
+      dat_name = path_basename(dat_path);
 
-		dat_name_dot = strchr(dat_name, '.');
-		if (!dat_name_dot) {
-			continue;
-		}
+      dat_name_dot = strchr(dat_name, '.');
+      if (!dat_name_dot)
+         continue;
 
-		offs = dat_name_dot - dat_name + 1;
-		memcpy(game_name, dat_name, offs);
+      offs = dat_name_dot - dat_name + 1;
+      memcpy(game_name, dat_name, offs);
 
-		fd = open(dat_path, O_RDONLY);
-		if (fd < 0) {
-			continue;
-		}
+      fd = open(dat_path, O_RDONLY);
+      if (fd < 0)
+         continue;
 
-		if (find_hash(fd, hash, game_name + offs, max_len - offs) == 0) {
-			rv = 0;
-			close(fd);
-			goto clean;
-		}
-		close(fd);
-	}
-	rv = -1;
+      if (find_hash(fd, hash, game_name + offs, max_len - offs) == 0)
+      {
+         rv = 0;
+         close(fd);
+         goto clean;
+      }
+      close(fd);
+   }
+   rv = -1;
 clean:
-	dir_list_free(files);
-	return rv;
+   dir_list_free(files);
+   return rv;
 }
 
 static int get_sha1(const char *path, char *result)
 {
-	int fd;
-	int rv;
-	unsigned char buff[4096];
-	SHA1Context sha;
+   int fd;
+   int rv;
+   unsigned char buff[4096];
+   SHA1Context sha;
 
-	fd = open(path, O_RDONLY);
-	if (fd < 0) {
-		return -errno;
-	}
+   fd = open(path, O_RDONLY);
+   if (fd < 0)
+      return -errno;
 
-	SHA1Reset(&sha);
-	rv = 1;
-	while (rv > 0) {
-		rv = read(fd, buff, 4096);
-		if (rv < 0) {
-			close(fd);
-			return -errno;
-		}
+   SHA1Reset(&sha);
+   rv = 1;
+   while (rv > 0)
+   {
+      rv = read(fd, buff, 4096);
+      if (rv < 0)
+      {
+         close(fd);
+         return -errno;
+      }
 
-		SHA1Input(&sha, buff, rv);
-	}
+      SHA1Input(&sha, buff, rv);
+   }
 
-	if (!SHA1Result(&sha)) {
-		close(fd);
-		return -1;
-	}
+   if (!SHA1Result(&sha))
+   {
+      close(fd);
+      return -1;
+   }
 
-	sprintf(result, "%08X%08X%08X%08X%08X",
-		sha.Message_Digest[0],
-		sha.Message_Digest[1],
-		sha.Message_Digest[2],
-		sha.Message_Digest[3], sha.Message_Digest[4]);
-	close(fd);
-	return 0;
+   sprintf(result, "%08X%08X%08X%08X%08X",
+         sha.Message_Digest[0],
+         sha.Message_Digest[1],
+         sha.Message_Digest[2],
+         sha.Message_Digest[3], sha.Message_Digest[4]);
+   close(fd);
+   return 0;
 }
 
-struct RunInfo {
-	char broken_cores[PATH_MAX];
-	int multitap;
-	int dualanalog;
-	char system[10];
+struct RunInfo
+{
+   char broken_cores[PATH_MAX];
+   int multitap;
+   int dualanalog;
+   char system[10];
 };
 
 static int read_launch_conf(struct RunInfo *info, const char *game_name)
@@ -152,19 +145,18 @@ static int read_launch_conf(struct RunInfo *info, const char *game_name)
 	int rv;
 	int bci = 0;
 	char token[MAX_TOKEN_LEN];
-	if (fd < 0) {
+	if (fd < 0)
 		return -errno;
-	}
 
-	while (1) {
-		if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0) {
+	while (1)
+   {
+		if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0)
 			goto clean;
-		}
 
-		if (rl_fnmatch(token, game_name, 0) != 0) {
-			if ((rv = find_token(fd, ";")) < 0) {
+		if (rl_fnmatch(token, game_name, 0) != 0)
+      {
+			if ((rv = find_token(fd, ";")) < 0)
 				goto clean;
-			}
 			continue;
 		}
 
@@ -172,47 +164,48 @@ static int read_launch_conf(struct RunInfo *info, const char *game_name)
 		break;
 	}
 
-	if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0) {
+	if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0)
 		goto clean;
-	}
 
-	while (strcmp(token, ";") != 0) {
-		if (strcmp(token, "multitap") == 0) {
-			info->multitap = 1;
-		} else if (strcmp(token, "dualanalog") == 0) {
-			info->dualanalog = 1;
-		} else if (token[0] == '!') {
-			strncpy(&info->broken_cores[bci], &token[1], PATH_MAX - bci);
-			bci += strnlen(&token[1], PATH_MAX) + 1;
-		}
+	while (strcmp(token, ";") != 0)
+   {
+      if (strcmp(token, "multitap") == 0)
+         info->multitap = 1;
+      else if (strcmp(token, "dualanalog") == 0)
+         info->dualanalog = 1;
+      else if (token[0] == '!')
+      {
+         strncpy(&info->broken_cores[bci], &token[1], PATH_MAX - bci);
+         bci += strnlen(&token[1], PATH_MAX) + 1;
+      }
 
-		if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0) {
-			goto clean;
-		}
-
-	}
+      if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0)
+         goto clean;
+   }
 	rv = 0;
  clean:
 	close(fd);
 	return rv;
 }
 
-static int get_run_info(struct RunInfo *info, const char *game_name) {
-	memset(info, 0, sizeof(struct RunInfo));
-	int i;
+static int get_run_info(struct RunInfo *info, const char *game_name)
+{
+   int i;
 
-	for (i = 0; i < 9; i++) {
-		if (game_name[i] == '.') {
-			break;
-		}
-		info->system[i] = game_name[i];
-	}
-	info->system[i] = '\0';
-	info->multitap = 0;
-	info->dualanalog = 0;
+   memset(info, 0, sizeof(struct RunInfo));
 
-	read_launch_conf(info, game_name);
-	return 0;
+   for (i = 0; i < 9; i++)
+   {
+      if (game_name[i] == '.')
+         break;
+      info->system[i] = game_name[i];
+   }
+   info->system[i] = '\0';
+   info->multitap = 0;
+   info->dualanalog = 0;
+
+   read_launch_conf(info, game_name);
+   return 0;
 }
 
 
@@ -240,32 +233,34 @@ static int detect_rom_game(const char *path, char *game_name, size_t max_len)
 {
 	char hash[HASH_LEN + 1];
 	int rv;
-   const char *suffix;
 	const char **tmp_suffix;
 
-	suffix = strrchr(path, '.');
-	if (!suffix) {
+	const char *suffix = strrchr(path, '.');
+	if (!suffix)
+   {
 		LOG_WARN("Could not find extension for: %s", path);
 		return -EINVAL;
 	}
 
 	memset(hash, 0, sizeof(hash));
 
-	if ((rv = get_sha1(path, hash)) < 0) {
+	if ((rv = get_sha1(path, hash)) < 0)
 		LOG_WARN("Could not calculate hash: %s", strerror(-rv));
-	}
 
-	if (find_rom_canonical_name(hash, game_name, max_len) < 0) {
+	if (find_rom_canonical_name(hash, game_name, max_len) < 0)
+   {
 		LOG_DEBUG("Could not detect rom with hash `%s` guessing", hash);
 
 		for (tmp_suffix = SUFFIX_MATCH; *tmp_suffix != NULL;
-		     tmp_suffix += 2) {
-			if (strcasecmp(suffix, *tmp_suffix) == 0) {
-				snprintf(game_name, max_len, "%s.<unknown>",
-					 *(tmp_suffix + 1));
-				return 0;
-			}
-		}
+		     tmp_suffix += 2)
+      {
+         if (strcasecmp(suffix, *tmp_suffix) == 0)
+         {
+            snprintf(game_name, max_len, "%s.<unknown>",
+                  *(tmp_suffix + 1));
+            return 0;
+         }
+      }
 		return -EINVAL;
 	}
 
@@ -275,81 +270,83 @@ static int detect_rom_game(const char *path, char *game_name, size_t max_len)
 static int detect_game(const char *path, char *game_name, size_t max_len)
 {
 	if ((strcasecmp(path + strlen(path) - 4, ".cue") == 0) ||
-	    (strcasecmp(path + strlen(path) - 4, ".m3u") == 0)) {
+	    (strcasecmp(path + strlen(path) - 4, ".m3u") == 0))
+   {
 		LOG_INFO("Starting CD game detection...");
 		return detect_cd_game(path, game_name, max_len);
-	} else {
+	}
+   else
+   {
 		LOG_INFO("Starting rom game detection...");
 		return detect_rom_game(path, game_name, max_len);
 	}
 }
 
 static int select_core(char *core_path, size_t max_len,
-		const struct RunInfo *info) {
-	int fd = open("./cores.conf", O_RDONLY);
-	int rv;
-	int bci = 0;
-	char token[MAX_TOKEN_LEN];
-	int broken = 0;
-	if (fd < 0) {
-		return -errno;
-	}
+		const struct RunInfo *info)
+{
+   int fd = open("./cores.conf", O_RDONLY);
+   int rv;
+   int bci = 0;
+   char token[MAX_TOKEN_LEN];
+   int broken = 0;
+   if (fd < 0)
+      return -errno;
 
-	LOG_INFO("Selecting core for system '%s'", info->system);
-	while (1) {
-		if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0) {
-			goto clean;
-		}
+   LOG_INFO("Selecting core for system '%s'", info->system);
+   while (1)
+   {
+      if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0)
+         goto clean;
 
-		if (rl_fnmatch(token, info->system, 0) != 0) {
-			if ((rv = find_token(fd, ";")) < 0) {
-				goto clean;
-			}
-			continue;
-		}
+      if (rl_fnmatch(token, info->system, 0) != 0)
+      {
+         if ((rv = find_token(fd, ";")) < 0)
+            goto clean;
+         continue;
+      }
 
-		LOG_INFO("Matched system '%s'", token);
+      LOG_INFO("Matched system '%s'", token);
 
-		break;
-	}
+      break;
+   }
 
-	if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0) {
-		goto clean;
-	}
+   if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0)
+      goto clean;
 
-	while (strcmp(token, ";") != 0) {
-		broken = 0;
-		for (bci = 0; info->broken_cores[bci] != '\0';
-				bci += strlen(&info->broken_cores[bci]) + 1) {
+   while (strcmp(token, ";") != 0)
+   {
+      broken = 0;
+      for (bci = 0; info->broken_cores[bci] != '\0';
+            bci += strlen(&info->broken_cores[bci]) + 1)
+      {
 
-			LOG_DEBUG("%s, %s", &info->broken_cores[bci], token);
-			if (strcmp(&info->broken_cores[bci], token) == 0) {
-				broken = 1;
-				LOG_DEBUG("Not using core %s because it is "
-						"marked broken for this game",
-						&info->broken_cores[bci]);
-				break;
-			}
-		}
+         LOG_DEBUG("%s, %s", &info->broken_cores[bci], token);
+         if (strcmp(&info->broken_cores[bci], token) == 0)
+         {
+            broken = 1;
+            LOG_DEBUG("Not using core %s because it is "
+                  "marked broken for this game",
+                  &info->broken_cores[bci]);
+            break;
+         }
+      }
 
-		if (!broken) {
-			goto success;
-		}
+            if (!broken)
+               goto success;
 
-		if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0) {
-			goto clean;
-		}
-
-	}
-	rv = -EINVAL;
-	goto clean;
+            if ((rv = get_token(fd, token, MAX_TOKEN_LEN)) < 0)
+               goto clean;
+   }
+   rv = -EINVAL;
+   goto clean;
 
 success:
-	snprintf(core_path, max_len, "./cores/%s.so", token);
-	rv = 0;
+   snprintf(core_path, max_len, "./cores/%s.so", token);
+   rv = 0;
 clean:
-	close(fd);
-	return rv;
+   close(fd);
+   return rv;
 }
 
 #ifndef RARCH_CONSOLE
@@ -363,20 +360,24 @@ static int run_retroarch(const char *path, const struct RunInfo *info)
 	};
 	int argi = 3;
 
-	if ((rv = select_core(core_path, PATH_MAX, info)) < 0) {
+	if ((rv = select_core(core_path, PATH_MAX, info)) < 0)
+   {
 		LOG_WARN("Could not find suitable core");
 		return rv;
 	}
 
 	LOG_INFO("Using core at '%s'", core_path);
-	if (info->multitap) {
+	if (info->multitap)
+   {
 		retro_argv[argi] = "-4";
 		argi++;
 		LOG_INFO("Game supports multitap");
 	}
 
-	if (info->dualanalog) {
-		for (i = 0; i < 8; i++) {
+	if (info->dualanalog)
+   {
+		for (i = 0; i < 8; i++)
+      {
 			retro_argv[argi] = "-A";
 			argi++;
 			retro_argv[argi] = "1";
@@ -394,33 +395,36 @@ static int run_retroarch(const char *path, const struct RunInfo *info)
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		printf("usage: retrolaunch <ROM>\n");
-		return -1;
-	}
+   if (argc < 2)
+   {
+      printf("usage: retrolaunch <ROM>\n");
+      return -1;
+   }
 
-	char game_name[MAX_TOKEN_LEN];
-	char *path = argv[1];
-	struct RunInfo info;
-	int rv;
+   char game_name[MAX_TOKEN_LEN];
+   char *path = argv[1];
+   struct RunInfo info;
+   int rv;
 
-	LOG_INFO("Analyzing '%s'", path);
-	if ((rv = detect_game(path, game_name, MAX_TOKEN_LEN)) < 0) {
-		LOG_WARN("Could not detect game: %s", strerror(-rv));
-		return -rv;
-	}
+   LOG_INFO("Analyzing '%s'", path);
+   if ((rv = detect_game(path, game_name, MAX_TOKEN_LEN)) < 0)
+   {
+      LOG_WARN("Could not detect game: %s", strerror(-rv));
+      return -rv;
+   }
 
-	LOG_INFO("Game is `%s`", game_name);
-	if ((rv = get_run_info(&info, game_name)) < 0) {
-		LOG_WARN("Could not detect run info: %s", strerror(-rv));
-		return -1;
-	}
+   LOG_INFO("Game is `%s`", game_name);
+   if ((rv = get_run_info(&info, game_name)) < 0)
+   {
+      LOG_WARN("Could not detect run info: %s", strerror(-rv));
+      return -1;
+   }
 
-	LOG_INFO("Launching '%s'", path);
+   LOG_INFO("Launching '%s'", path);
 
-	rv = run_retroarch(path, &info);
-	LOG_WARN("Could not launch retroarch: %s", strerror(-rv));
-	return -rv;
+   rv = run_retroarch(path, &info);
+   LOG_WARN("Could not launch retroarch: %s", strerror(-rv));
+   return -rv;
 }
 
 // Stub just so that it compiles
--- tools/retrolaunch/rl_fnmatch.c
+++ tools/retrolaunch/rl_fnmatch.c
@@ -6,127 +6,121 @@
 
 // Implemnentation of fnmatch(3) so it can be distributed to non *nix platforms
 // No flags are implemented ATM. We don't use them. Add flags as needed.
-int rl_fnmatch(const char *pattern, const char *string, int flags) {
-	const char *c;
-	int charmatch = 0;
-	int rv;
-	for (c = pattern; *c != '\0'; c++) {
-		// String ended before pattern
-		if ((*c != '*') && (*string == '\0')) {
-			return FNM_NOMATCH;
-		}
+int rl_fnmatch(const char *pattern, const char *string, int flags)
+{
+   const char *c;
+   int charmatch = 0;
+   int rv;
+   for (c = pattern; *c != '\0'; c++)
+   {
+      // String ended before pattern
+      if ((*c != '*') && (*string == '\0'))
+         return FNM_NOMATCH;
 
-		switch (*c) {
-		// Match any number of unknown chars
-		case '*':
-			// Find next node in the pattern ignoring multiple
-			// asterixes
-			do {
-				c++;
-				if (*c == '\0') {
-					return 0;
-				}
-			} while (*c == '*');
+      switch (*c)
+      {
+         // Match any number of unknown chars
+         case '*':
+            // Find next node in the pattern ignoring multiple
+            // asterixes
+            do {
+               c++;
+               if (*c == '\0')
+                  return 0;
+            } while (*c == '*');
 
-			// Match the remaining pattern ingnoring more and more
-			// chars.
-			do {
-				// We reached the end of the string without a
-				// match. There is a way to optimize this by
-				// calculating the minimum chars needed to
-				// match the remaining pattern but I don't
-				// think it is worth the work ATM.
-				if (*string == '\0') {
-					return FNM_NOMATCH;
-				}
+            // Match the remaining pattern ingnoring more and more
+            // chars.
+            do {
+               // We reached the end of the string without a
+               // match. There is a way to optimize this by
+               // calculating the minimum chars needed to
+               // match the remaining pattern but I don't
+               // think it is worth the work ATM.
+               if (*string == '\0')
+                  return FNM_NOMATCH;
 
-				rv = rl_fnmatch(c, string, flags);
-				string++;
-			} while (rv != 0);
+               rv = rl_fnmatch(c, string, flags);
+               string++;
+            } while (rv != 0);
 
-			return 0;
-		// Match char from list
-		case '[':
-			charmatch = 0;
-			for (c++; *c != ']'; c++) {
-				// Bad formath
-				if (*c == '\0') {
-					return FNM_NOMATCH;
-				}
+            return 0;
+            // Match char from list
+         case '[':
+            charmatch = 0;
+            for (c++; *c != ']'; c++)
+            {
+               // Bad formath
+               if (*c == '\0')
+                  return FNM_NOMATCH;
 
-				// Match already found
-				if (charmatch) {
-					continue;
-				}
+               // Match already found
+               if (charmatch)
+                  continue;
 
-				if (*c == *string) {
-					charmatch = 1;
-				}
-			}
+               if (*c == *string)
+                  charmatch = 1;
+            }
 
-			// No match in list
-			if (!charmatch) {
-				return FNM_NOMATCH;
-			}
+            // No match in list
+            if (!charmatch)
+               return FNM_NOMATCH;
 
-			string++;
-			break;
-		// Has any char
-		case '?':
-			string++;
-			break;
-		// Match following char verbatim
-		case '\\':
-			c++;
-			// Dangling escape at end of pattern
-			if (*c == '\0') { // FIXME: Was c == '\0' (makes no sense). Not sure if c == NULL or *c == '\0' is intended. Assuming *c due to c++ right before.
-				return FNM_NOMATCH;
-			}
-		default:
-			if (*c != *string) {
-				return FNM_NOMATCH;
-			}
-			string++;
-		}
-	}
+            string++;
+            break;
+            // Has any char
+         case '?':
+            string++;
+            break;
+            // Match following char verbatim
+         case '\\':
+            c++;
+            // Dangling escape at end of pattern
+            if (*c == '\0') // FIXME: Was c == '\0' (makes no sense). Not sure if c == NULL or *c == '\0' is intended. Assuming *c due to c++ right before.
+               return FNM_NOMATCH;
+         default:
+            if (*c != *string)
+               return FNM_NOMATCH;
+            string++;
+      }
+   }
 
-	// End of string and end of pattend
-	if (*string == '\0') {
-		return 0;
-	} else {
-		return FNM_NOMATCH;
-	}
+   // End of string and end of pattend
+   if (*string == '\0')
+      return 0;
+   return FNM_NOMATCH;
 }
 
 #if __TEST_FNMATCH__
-int main() {
-	assert(rl_fnmatch("TEST", "TEST", 0) == 0);
-	assert(rl_fnmatch("TE?T", "TEST", 0) == 0);
-	assert(rl_fnmatch("TE[Ssa]T", "TEST", 0) == 0);
-	assert(rl_fnmatch("TE[Ssda]T", "TEsT", 0) == 0);
-	assert(rl_fnmatch("TE[Ssda]T", "TEdT", 0) == 0);
-	assert(rl_fnmatch("TE[Ssda]T", "TEaT", 0) == 0);
-	assert(rl_fnmatch("TEST*", "TEST", 0) == 0);
-	assert(rl_fnmatch("TEST**", "TEST", 0) == 0);
-	assert(rl_fnmatch("TE*ST*", "TEST", 0) == 0);
-	assert(rl_fnmatch("TE**ST*", "TEST", 0) == 0);
-	assert(rl_fnmatch("TE**ST*", "TExST", 0) == 0);
-	assert(rl_fnmatch("TE**ST", "TEST", 0) == 0);
-	assert(rl_fnmatch("TE**ST", "TExST", 0) == 0);
-	assert(rl_fnmatch("TE\\**ST", "TE*xST", 0) == 0);
-	assert(rl_fnmatch("*.*", "test.jpg", 0) == 0);
-	assert(rl_fnmatch("*.jpg", "test.jpg", 0) == 0);
-	assert(rl_fnmatch("*.[Jj][Pp][Gg]", "test.jPg", 0) == 0);
-	assert(rl_fnmatch("*.[Jj]*[Gg]", "test.jPg", 0) == 0);
-	assert(rl_fnmatch("TEST?", "TEST", 0) == FNM_NOMATCH);
-	assert(rl_fnmatch("TES[asd", "TEST", 0) == FNM_NOMATCH);
-	assert(rl_fnmatch("TEST\\", "TEST", 0) == FNM_NOMATCH);
-	assert(rl_fnmatch("TEST*S", "TEST", 0) == FNM_NOMATCH);
-	assert(rl_fnmatch("TE**ST", "TExT", 0) == FNM_NOMATCH);
-	assert(rl_fnmatch("TE\\*T", "TExT", 0) == FNM_NOMATCH);
-	assert(rl_fnmatch("TES?", "TES", 0) == FNM_NOMATCH);
-	assert(rl_fnmatch("TE", "TEST", 0) == FNM_NOMATCH);
-	assert(rl_fnmatch("TEST!", "TEST", 0) == FNM_NOMATCH);
-	assert(rl_fnmatch("DSAD", "TEST", 0) == FNM_NOMATCH);
+int main(void)
+{
+   assert(rl_fnmatch("TEST", "TEST", 0) == 0);
+   assert(rl_fnmatch("TE?T", "TEST", 0) == 0);
+   assert(rl_fnmatch("TE[Ssa]T", "TEST", 0) == 0);
+   assert(rl_fnmatch("TE[Ssda]T", "TEsT", 0) == 0);
+   assert(rl_fnmatch("TE[Ssda]T", "TEdT", 0) == 0);
+   assert(rl_fnmatch("TE[Ssda]T", "TEaT", 0) == 0);
+   assert(rl_fnmatch("TEST*", "TEST", 0) == 0);
+   assert(rl_fnmatch("TEST**", "TEST", 0) == 0);
+   assert(rl_fnmatch("TE*ST*", "TEST", 0) == 0);
+   assert(rl_fnmatch("TE**ST*", "TEST", 0) == 0);
+   assert(rl_fnmatch("TE**ST*", "TExST", 0) == 0);
+   assert(rl_fnmatch("TE**ST", "TEST", 0) == 0);
+   assert(rl_fnmatch("TE**ST", "TExST", 0) == 0);
+   assert(rl_fnmatch("TE\\**ST", "TE*xST", 0) == 0);
+   assert(rl_fnmatch("*.*", "test.jpg", 0) == 0);
+   assert(rl_fnmatch("*.jpg", "test.jpg", 0) == 0);
+   assert(rl_fnmatch("*.[Jj][Pp][Gg]", "test.jPg", 0) == 0);
+   assert(rl_fnmatch("*.[Jj]*[Gg]", "test.jPg", 0) == 0);
+   assert(rl_fnmatch("TEST?", "TEST", 0) == FNM_NOMATCH);
+   assert(rl_fnmatch("TES[asd", "TEST", 0) == FNM_NOMATCH);
+   assert(rl_fnmatch("TEST\\", "TEST", 0) == FNM_NOMATCH);
+   assert(rl_fnmatch("TEST*S", "TEST", 0) == FNM_NOMATCH);
+   assert(rl_fnmatch("TE**ST", "TExT", 0) == FNM_NOMATCH);
+   assert(rl_fnmatch("TE\\*T", "TExT", 0) == FNM_NOMATCH);
+   assert(rl_fnmatch("TES?", "TES", 0) == FNM_NOMATCH);
+   assert(rl_fnmatch("TE", "TEST", 0) == FNM_NOMATCH);
+   assert(rl_fnmatch("TEST!", "TEST", 0) == FNM_NOMATCH);
+   assert(rl_fnmatch("DSAD", "TEST", 0) == FNM_NOMATCH);
 }
 #endif
