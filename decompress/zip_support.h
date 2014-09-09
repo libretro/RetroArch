189af65f71bf766db61ea604d3f8617aa0f24241
Timo Strunk <Timo.Strunk@nanomatch.com>
2014-09-09 15:25:32 +0200

    Added zip support skeletons

---
+++ decompress/zip_support.h
@@ -0,0 +1,33 @@
+/*  RetroArch - A frontend for libretro.
+ *  Copyright (C) 2010-2014 - Hans-Kristian Arntzen
+ * 
+ *  RetroArch is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with RetroArch.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __RARCH_ZIP_SUPPORT_H
+#define __RARCH_ZIP_SUPPORT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int read_zip_file(const char * archive_path,
+      const char *relative_path, void **buf);
+
+struct string_list *compressed_zip_file_list_new(const char *path,
+      const char* ext);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
