5b476460d869e7544e8e3df76e28f9cdff2ddc2b
twinaphex <libretro@gmail.com>
2014-09-09 18:31:44 +0200

    Style / indent nits

--- driver_menu.h
+++ driver_menu.h
@@ -134,13 +134,16 @@ typedef struct menu_ctx_driver_backend
 {
    int      (*iterate)(unsigned);
    void     (*shader_manager_init)(menu_handle_t *);
-   void     (*shader_manager_get_str)(struct gfx_shader *, char *, size_t, const char *, const char *, unsigned);
-   void     (*shader_manager_set_preset)(struct gfx_shader *, unsigned, const char*);
+   void     (*shader_manager_get_str)(struct gfx_shader *, char *,
+         size_t, const char *, const char *, unsigned);
+   void     (*shader_manager_set_preset)(struct gfx_shader *,
+         unsigned, const char*);
    void     (*shader_manager_save_preset)(const char *, bool);
    unsigned (*shader_manager_get_type)(const struct gfx_shader *);
    int      (*shader_manager_setting_toggle)(unsigned, const char *, unsigned);
    unsigned (*type_is)(const char *, unsigned);
-   void     (*setting_set_label)(char *, size_t, unsigned *, unsigned, const char *, const char *, unsigned);
+   void     (*setting_set_label)(char *, size_t, unsigned *,
+         unsigned, const char *, const char *, unsigned);
    const char *ident;
 } menu_ctx_driver_backend_t;
 
