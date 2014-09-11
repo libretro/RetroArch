93ebbc2cbc1bf800ab5ad9bbc9f3a414cc0d46c4
twinaphex <libretro@gmail.com>
2014-09-11 18:41:04 +0200

    (D3D) Build fixes

--- gfx/d3d9/d3d_shared.h
+++ gfx/d3d9/d3d_shared.h
@@ -15,8 +15,8 @@ static bool d3d_init_chain(d3d_video_t *d3d,
       const video_info_t *video_info);
 
 static void renderchain_free(void *data);
-static void d3d_deinit_shader(d3d_video_t *d3d);
-static bool d3d_init_shader(d3d_video_t *d3d);
+void d3d_deinit_shader(void *data);
+bool d3d_init_shader(void *data);
 
 void d3d_make_d3dpp(void *data, const video_info_t *info,
 	D3DPRESENT_PARAMETERS *d3dpp);
