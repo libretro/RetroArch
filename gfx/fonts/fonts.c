6200641ccfd8b89ce7b32b7231b2d939e1106322
twinaphex <libretro@gmail.com>
2014-09-25 06:54:40 +0200

    Take out DONT_HAVE_BITMAPFONTS

--- gfx/fonts/fonts.c
+++ gfx/fonts/fonts.c
@@ -24,9 +24,7 @@ static const font_renderer_driver_t *font_backends[] = {
 #ifdef HAVE_FREETYPE
    &ft_font_renderer,
 #endif
-#if !defined(DONT_HAVE_BITMAPFONTS)
    &bitmap_font_renderer,
-#endif
    NULL
 };
 
