715eeb0062eb7bf11a39dd4b43c93b81d1e6d230
twinaphex <libretro@gmail.com>
2014-09-25 06:49:55 +0200

    (Android) Fix build

--- input/android_input.c
+++ input/android_input.c
@@ -519,7 +519,7 @@ static int android_input_get_id(android_input_t *android, AInputEvent *event)
 static bool pause_key_pressed(void)
 {
    retro_input_t old_input = 0;
-   retro_input_t input = input_keys_pressed_func(RARCH_PAUSE_TOGGLE, RARCH_PAUSE_TOGGLE+1, &old_input);
+   retro_input_t input = meta_input_keys_pressed(RARCH_PAUSE_TOGGLE, RARCH_PAUSE_TOGGLE+1, &old_input);
    return BIND_PRESSED(input, RARCH_PAUSE_TOGGLE);
 }
 
