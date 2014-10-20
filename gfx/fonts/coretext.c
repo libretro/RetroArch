564467abe9c064a34ad452c5394837c4afa26aec
Twinaphex <libretro@gmail.com>
2014-10-20 19:41:39 +0200

    (CoreText) Add to AUTHORS

--- gfx/fonts/coretext.c
+++ gfx/fonts/coretext.c
@@ -1,5 +1,5 @@
 /*  RetroArch - A frontend for libretro.
- *  Copyright (C) 2010-2014 - Hans-Kristian Arntzen
+ *  Copyright (C) 2014 - Jay McCarthy
  *
  *  RetroArch is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU General Public License as published by the Free Software Found-
