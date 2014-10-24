6289abc1e0dd277ca71df1e62a9c6301f28a63bc
aliaspider <aliaspider@gmail.com>
2014-10-24 02:24:38 +0100

    (menu) fixes menu not being properly hidden after loading/saving a state

--- frontend/menu/menu_common.c
+++ frontend/menu/menu_common.c
@@ -443,7 +443,8 @@ int menu_iterate(retro_input_t input,
          && driver.menu_ctx->backend->iterate) 
       ret = driver.menu_ctx->backend->iterate(action);
 
-   draw_frame();
+   if (g_extern.is_menu)
+      draw_frame();
 
    if (driver.menu_ctx && driver.menu_ctx->input_postprocess)
       driver.menu_ctx->input_postprocess(input, old_input);
