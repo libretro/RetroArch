58b98aca5caf9ae2613a9f72fed411007885407d
twinaphex <libretro@gmail.com>
2014-10-27 02:28:25 +0100

    Take out one rule from menu_common_type_is

--- frontend/menu/menu_common.c
+++ frontend/menu/menu_common.c
@@ -466,7 +466,6 @@ unsigned menu_common_type_is(const char *label, unsigned type)
          type == MENU_SETTINGS ||
          type == MENU_FILE_CATEGORY ||
          !strcmp(label, "Shader Options") ||
-         !strcmp(label, "Input Options") ||
          !strcmp(label, "core_options") ||
          !strcmp(label, "core_information") ||
          !strcmp(label, "video_shader_parameters") ||
