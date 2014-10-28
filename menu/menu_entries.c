7f3c1a9f333ad3789983e8d74404570e6f242b3d
twinaphex <libretro@gmail.com>
2014-10-28 21:17:01 +0100

    Refactor setting_set_flags

--- menu/menu_entries.c
+++ menu/menu_entries.c
@@ -107,14 +107,25 @@ int setting_set_flags(rarch_setting_t *setting)
    if (!setting)
       return 0;
 
-   if (setting->type == ST_ACTION)
-      return MENU_SETTING_ACTION;
    if (setting->flags & SD_FLAG_IS_DRIVER)
       return MENU_SETTING_DRIVER;
-   if (setting->type == ST_PATH)
-      return MENU_FILE_PATH;
-   if (setting->type == ST_GROUP)
-      return MENU_SETTING_GROUP;
+   else
+   {
+      switch (setting->type)
+      {
+         case ST_ACTION:
+            return MENU_SETTING_ACTION;
+         case ST_PATH:
+            return MENU_FILE_PATH;
+         case ST_GROUP:
+            return MENU_SETTING_GROUP;
+         case ST_SUB_GROUP:
+            return MENU_SETTING_SUBGROUP;
+         default:
+            break;
+      }
+   }
+
    return 0;
 }
 
