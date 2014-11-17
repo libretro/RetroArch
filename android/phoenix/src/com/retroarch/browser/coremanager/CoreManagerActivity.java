b5b7b584f3caf021796882b25dfd5b11e92d89fc
Lioncash <mathew1800@gmail.com>
2014-11-16 21:36:43 -0500

    Android: Make UI more compliant with 5.0
    On 5.0 the ActionBar method of doing tabs are deprecated. So we use the PagerTabStrip instead.
    Should get rid of deprecation warnings when building for Android 5.0.

--- android/phoenix/src/com/retroarch/browser/coremanager/CoreManagerActivity.java
+++ android/phoenix/src/com/retroarch/browser/coremanager/CoreManagerActivity.java
@@ -10,22 +10,15 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBar.Tab;
-import android.support.v7.app.ActionBar.TabListener;
 import android.support.v7.app.ActionBarActivity;
 
 /**
  * Activity which provides the base for viewing installed cores,
  * as well as the ability to download other cores.
  */
-public final class CoreManagerActivity extends ActionBarActivity implements TabListener
+public final class CoreManagerActivity extends ActionBarActivity
 {
-	// ViewPager for the fragments
-	private ViewPager viewPager;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -33,48 +26,8 @@ public final class CoreManagerActivity extends ActionBarActivity implements TabL
 
 		// Set the ViewPager
 		setContentView(R.layout.coremanager_viewpager);
-		viewPager = (ViewPager) findViewById(R.id.coreviewer_viewPager);
-
-		// Set the ViewPager adapter.
-		final ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-		viewPager.setAdapter(adapter);
-
-		// Initialize the ActionBar.
-		final ActionBar actionBar = getSupportActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		actionBar.addTab(actionBar.newTab().setText(R.string.installed_cores).setTabListener(this));
-		actionBar.addTab(actionBar.newTab().setText(R.string.downloadable_cores).setTabListener(this));
-
-		// When swiping between different sections, select the corresponding
-		// tab. We can also use ActionBar.Tab#select() to do this if we have
-		// a reference to the Tab.
-		viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
-		{
-			@Override
-			public void onPageSelected(int position)
-			{
-				actionBar.setSelectedNavigationItem(position);
-			}
-		});
-	}
-	
-	@Override
-	public void onTabSelected(Tab tab, FragmentTransaction ft)
-	{
-		// Switch to the fragment indicated by the tab's position.
-		viewPager.setCurrentItem(tab.getPosition());
-	}
-
-	@Override
-	public void onTabReselected(Tab tab, FragmentTransaction ft)
-	{
-		// Do nothing. Not used.
-	}
-
-	@Override
-	public void onTabUnselected(Tab tab, FragmentTransaction ft)
-	{
-		// Do nothing. Not used.
+		final ViewPager viewPager = (ViewPager) findViewById(R.id.coreviewer_viewPager);
+		viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
 	}
 
 	@Override
@@ -126,6 +79,11 @@ public final class CoreManagerActivity extends ActionBarActivity implements TabL
 	// Adapter for the core manager ViewPager.
 	private final class ViewPagerAdapter extends FragmentPagerAdapter
 	{
+		private final String[] pageTitles = {
+			getString(R.string.installed_cores),
+			getString(R.string.downloadable_cores)
+		};
+		
 		/**
 		 * Constructor
 		 * 
@@ -161,17 +119,7 @@ public final class CoreManagerActivity extends ActionBarActivity implements TabL
 		@Override
 		public CharSequence getPageTitle(int position)
 		{
-			switch (position)
-			{
-				case 0:
-					return getString(R.string.installed_cores);
-
-				case 1:
-					return getString(R.string.downloadable_cores);
-
-				default: // Should never happen.
-					return null;
-			}
+			return pageTitles[position];
 		}
 	}
 }
