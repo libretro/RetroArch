c518d35f18206d39d3b2c8621647233bb94916ae
twinaphex <libretro@gmail.com>
2015-01-05 02:43:44 +0100

    (iOS) Add ifdef for menu_ctx_ios

--- frontend/platform/platform_apple.c
+++ frontend/platform/platform_apple.c
@@ -184,10 +184,13 @@ extern void apple_rarch_exited(void);
 
 static void frontend_apple_load_content(void)
 {
-  if ( driver.menu_ctx && driver.menu_ctx == &menu_ctx_ios && driver.menu && driver.menu->userdata ) {
-     ios_handle_t *ih = (ios_handle_t*)driver.menu->userdata;
-     ih->notify_content_loaded();
+#ifdef IOS
+   if ( driver.menu_ctx && driver.menu_ctx == &menu_ctx_ios && driver.menu && driver.menu->userdata )
+   {
+      ios_handle_t *ih = (ios_handle_t*)driver.menu->userdata;
+      ih->notify_content_loaded();
    }
+#endif
 }
 
 static void frontend_apple_shutdown(bool unused)
