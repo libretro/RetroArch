b2e413f0f371333f2768ef98bc5011473c5dd590
twinaphex <libretro@gmail.com>
2015-01-14 21:54:32 +0100

    (rarchdb) Fix two warnings

--- rarchdb/rmsgpack.c
+++ rarchdb/rmsgpack.c
@@ -519,7 +519,7 @@ int rmsgpack_read(int fd, struct rmsgpack_read_callbacks *callbacks, void *data)
       case 0xcd:
       case 0xce:
       case 0xcf:
-         tmp_len = 1 << (type - 0xcc);
+         tmp_len = 1ULL << (type - 0xcc);
          tmp_uint = 0;
          if(read_uint(fd, &tmp_uint, tmp_len) == -1)
             return -errno;
@@ -531,7 +531,7 @@ int rmsgpack_read(int fd, struct rmsgpack_read_callbacks *callbacks, void *data)
       case 0xd1:
       case 0xd2:
       case 0xd3:
-         tmp_len = 1 << (type - 0xd0);
+         tmp_len = 1ULL << (type - 0xd0);
          tmp_int = 0;
          if(read_int(fd, &tmp_int, tmp_len) == -1)
             return -errno;
