1413470313d121ae31b088ba27a12b0fc79a716f
twinaphex <libretro@gmail.com>
2015-01-16 03:33:10 +0100

    (2xBR Filter) Fix warnings with Theos iOS on Linux

--- gfx/video_filters/2xbr.c
+++ gfx/video_filters/2xbr.c
@@ -432,9 +432,9 @@ float df8(uint32_t A, uint32_t B,
    r = abs((int)(((A & pg_red_mask        ) -  (B & pg_red_mask         ))));
 #endif
 
-   y = abs(0.299*r + 0.587*g + 0.114*b);
-   u = abs(-0.169*r - 0.331*g + 0.500*b);
-   v = abs(0.500*r - 0.419*g - 0.081*b);
+   y = fabs(0.299*r + 0.587*g + 0.114*b);
+   u = fabs(-0.169*r - 0.331*g + 0.500*b);
+   v = fabs(0.500*r - 0.419*g - 0.081*b);
 
    return 48*y + 7*u + 6*v;
 }
@@ -455,9 +455,9 @@ int eq8(uint32_t A, uint32_t B,
    r = abs((int)(((A & pg_red_mask        ) -  (B & pg_red_mask         ))));
 #endif
    
-    y = abs(0.299*r + 0.587*g + 0.114*b);
-    u = abs(-0.169*r - 0.331*g + 0.500*b);
-    v = abs(0.500*r - 0.419*g - 0.081*b);
+    y = fabs(0.299*r + 0.587*g + 0.114*b);
+    u = fabs(-0.169*r - 0.331*g + 0.500*b);
+    v = fabs(0.500*r - 0.419*g - 0.081*b);
 
     return ((48 >= y) && (7 >= u) && (6 >= v)) ? 1 : 0;
 }
