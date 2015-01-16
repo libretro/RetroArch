4b8f6ce6faadbcd334fb0141d6f40f11e790bf20
twinaphex <libretro@gmail.com>
2015-01-16 16:17:58 +0100

    (Menu) Add labels for some categories

--- menu/drivers_display/shared.h
+++ menu/drivers_display/shared.h
@@ -39,6 +39,12 @@ static void get_title(const char *label, const char *dir,
       strlcpy(title, "INPUT OPTIONS", sizeof_title);
    else if (!strcmp(label, "Overlay Options"))
       strlcpy(title, "OVERLAY OPTIONS", sizeof_title);
+   else if (!strcmp(label, "Menu Options"))
+      strlcpy(title, "MENU OPTIONS", sizeof_title);
+   else if (!strcmp(label, "Patch Options"))
+      strlcpy(title, "PATCH OPTIONS", sizeof_title);
+   else if (!strcmp(label, "UI Options"))
+      strlcpy(title, "UI OPTIONS", sizeof_title);
    else if (!strcmp(label, "Playlist Options"))
       strlcpy(title, "PLAYLIST OPTIONS", sizeof_title);
    else if (!strcmp(label, "Network Options"))
