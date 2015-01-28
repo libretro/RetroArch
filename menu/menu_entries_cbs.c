d603c24fb2ca7e0956dae733670ba7b9ea75d9c6
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-01-28 19:43:41 +0100

    (XMB) Add input remapping options to the contextual menu

--- menu/menu_entries_cbs.c
+++ menu/menu_entries_cbs.c
@@ -3091,6 +3091,8 @@ static int deferred_push_content_actions(void *data, void *userdata,
       menu_list_push(list, "Resume", "file_load_or_resume", MENU_SETTING_ACTION_RUN, 0);
       menu_list_push(list, "Core Informations", "core_information", MENU_SETTING_ACTION_CORE_INFORMATION, 0);
       menu_list_push(list, "Core Options", "core_options", MENU_SETTING_ACTION_CORE_OPTIONS, 0);
+      if (g_extern.has_set_input_descriptors)
+         menu_list_push(list, "Core Input Remapping Options", "core_input_remapping_options", MENU_SETTING_ACTION_CORE_INPUT_REMAPPING_OPTIONS, 0);
       menu_list_push(list, "Core Cheat Options", "core_cheat_options", MENU_SETTING_ACTION_CORE_CHEAT_OPTIONS, 0);
       if ( !g_extern.libretro_dummy && g_extern.system.disk_control.get_num_images)
          menu_list_push(list, "Core Disk Options", "disk_options", MENU_SETTING_ACTION_CORE_DISK_OPTIONS, 0);
