362ab0e53af4c866969d5bf1005a59b7ede98a53
twinaphex <libretro@gmail.com>
2015-01-28 09:00:53 +0100

    Define HAVE_NETPLAY on when HAVE_NETWORKING is enabled in qb/config.libs.sh

--- qb/config.libs.sh
+++ qb/config.libs.sh
@@ -146,6 +146,7 @@ if [ "$HAVE_NETWORKING" = 'yes' ]; then
       HAVE_SOCKET_LEGACY='yes'
    fi
    HAVE_NETWORK_CMD='yes'
+   HAVE_NETPLAY='yes'
 else
    echo "Warning: All networking features have been disabled."
    HAVE_NETWORK_CMD='no'
