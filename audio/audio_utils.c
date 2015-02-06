7bc3a2892d37f9b42b54f1a9451e3a318e4d8a5b
twinaphex <libretro@gmail.com>
2015-02-06 17:26:01 +0100

    Update

--- audio/audio_utils.c
+++ audio/audio_utils.c
@@ -24,6 +24,7 @@
 #endif
 
 #ifdef RARCH_INTERNAL
+#include "../performance.h"
 #include "../libretro.h"
 #else
 #include "../libretro/libretro.h"
@@ -398,6 +399,15 @@ void audio_convert_float_to_s16_ALLEGREX(int16_t *out,
 }
 #endif
 
+static unsigned audio_convert_get_cpu_features(void)
+{
+#ifdef RARCH_INTERNAL
+   return rarch_get_cpu_features();
+#else
+   return perf_get_cpu_features_cb();
+#endif
+}
+
 /**
  * audio_convert_init_simd:
  *
@@ -406,8 +416,10 @@ void audio_convert_float_to_s16_ALLEGREX(int16_t *out,
  **/
 void audio_convert_init_simd(void)
 {
+   unsigned cpu = audio_convert_get_cpu_features();
+
+   (void)cpu;
 #if defined(__ARM_NEON__) 
-   unsigned cpu = rarch_get_cpu_features();
    audio_convert_s16_to_float_arm = cpu & RETRO_SIMD_NEON ?
       audio_convert_s16_to_float_neon : audio_convert_s16_to_float_C;
    audio_convert_float_to_s16_arm = cpu & RETRO_SIMD_NEON ?
