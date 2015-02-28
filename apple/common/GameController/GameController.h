eb608fc09fd88cfcd34f73c880bc801589b4ddf2
twinaphex <libretro@gmail.com>
2015-02-28 05:55:08 +0100

    (iOS) Make GAMECONTROLLER_EXPORT blank when trying to compile on systems with iOS 6 SDK

--- apple/common/GameController/GameController.h
+++ apple/common/GameController/GameController.h
@@ -6,6 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Availability.h>
 
 #ifdef __cplusplus
 #define GAMECONTROLLER_EXTERN		extern "C" __attribute__((visibility ("default")))
@@ -13,7 +14,11 @@
 #define GAMECONTROLLER_EXTERN	        extern __attribute__((visibility ("default")))
 #endif
 
+#ifndef __IPHONE_7_0
+#define GAMECONTROLLER_EXPORT
+#else
 #define GAMECONTROLLER_EXPORT NS_CLASS_AVAILABLE(10_9, 7_0)
+#endif
 
 #import "GCControllerElement.h"
 
