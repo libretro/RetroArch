6e437c449b485265d0466ef2708540ca73a37be3
Geoffrey Plitt <geoff@gweb.org>
2015-03-03 16:07:04 -0800

    Mac OS X / CircleCI fixes
    adding artifact step
    
    installing Cg
    
    curl instead of wget
    
    specifying the xcodeproject path
    
    fixing path
    
    troubleshooting
    
    adding configure step
    
    fixing configure step
    
    adding submodule fetch step
    
    disabling artifact step
    
    using zip to upload just one file to artifacts
    
    fine tuning build.zip
    
    bugfix
    
    undoing whitespace changes
    
    fixing zip step

--- qb/qb.libs.sh
+++ qb/qb.libs.sh
@@ -45,7 +45,7 @@ check_lib()	#$1 = HAVE_$1	$2 = lib	$3 = function in lib	$4 = extralibs $5 = head
 		exit 1
 	}
 
-	/bin/true
+	true
 }
 
 check_lib_cxx()	#$1 = HAVE_$1	$2 = lib	$3 = function in lib	$4 = extralibs	$5 = critical error message [checked only if non-empty]
@@ -73,7 +73,7 @@ check_lib_cxx()	#$1 = HAVE_$1	$2 = lib	$3 = function in lib	$4 = extralibs	$5 = 
 	
 	}
 
-	/bin/true
+	true
 }
 
 check_code_c()
