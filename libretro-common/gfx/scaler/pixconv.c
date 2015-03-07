7f5a08e2cb0c14df53ad1278e2a6d33f247a22f2
notaz <notasas@gmail.com>
2015-03-07 02:34:47 +0200

    implement conv_rgba4444_rgb565

--- libretro-common/gfx/scaler/pixconv.c
+++ libretro-common/gfx/scaler/pixconv.c
@@ -359,6 +359,29 @@ void conv_rgba4444_argb8888(void *output_, const void *input_,
    }
 }
 
+void conv_rgba4444_rgb565(void *output_, const void *input_,
+      int width, int height,
+      int out_stride, int in_stride)
+{
+   int h, w;
+   const uint16_t *input = (const uint16_t*)input_;
+   uint16_t *output      = (uint16_t*)output_;
+
+   for (h = 0; h < height;
+         h++, output += out_stride >> 1, input += in_stride >> 1)
+   {
+      for (w = 0; w < width; w++)
+      {
+         uint32_t col = input[w];
+         uint32_t r = (col >> 12) & 0xf;
+         uint32_t g = (col >>  8) & 0xf;
+         uint32_t b = (col >>  4) & 0xf;
+
+         output[w] = (r << 12) | (g << 7) | (b << 1);
+      }
+   }
+}
+
 #if defined(__SSE2__)
 /* :( TODO: Make this saner. */
 static inline void store_bgr24_sse2(void *output, __m128i a,
