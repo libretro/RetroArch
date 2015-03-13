e4cfbcf93a09b5dffc0b7efdc50e68f0aa80bfe5
twinaphex <libretro@gmail.com>
2015-03-13 15:43:52 +0100

    (Griffin) Add stdstring.c to Griffin

--- griffin/griffin.c
+++ griffin/griffin.c
@@ -559,6 +559,7 @@ FILE
 #include "../file_path_special.c"
 #include "../libretro-common/file/dir_list.c"
 #include "../libretro-common/string/string_list.c"
+#include "../libretro-common/string/stdstring.c"
 #include "../file_ops.c"
 #include "../libretro-common/file/nbio/nbio_stdio.c"
 #include "../libretro-common/file/file_list.c"
