38204f5c26dd2b5fff48261be0f440696a3acd26
Twinaphex <libretro@gmail.com>
2015-04-02 19:38:37 +0200

    (BTStack) Cleanup

--- apple/iOS/bluetooth/btstack/linked_list.h
+++ apple/iOS/bluetooth/btstack/linked_list.h
@@ -35,14 +35,14 @@
 #if defined __cplusplus
 extern "C" {
 #endif
-
-typedef linked_item_t * linked_list_t;
 	
 typedef struct linked_item
 {
    struct linked_item *next; /* <-- next element in list, or NULL */
    void *user_data;          /* <-- pointer to struct base */
 } linked_item_t;
+    
+typedef linked_item_t *linked_list_t;
 
 void linked_item_set_user(linked_item_t *item, void *user_data);        /* <-- set user data */
 
