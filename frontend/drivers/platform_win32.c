20fbe6876ebaf157a6d302fd9dff89967458b349
twinaphex <libretro@gmail.com>
2015-04-07 21:52:26 +0200

    (platform_win32.c) Update header

--- frontend/drivers/platform_win32.c
+++ frontend/drivers/platform_win32.c
@@ -1,8 +1,5 @@
 /* RetroArch - A frontend for libretro.
- * Copyright (C) 2010-2014 - Hans-Kristian Arntzen
  * Copyright (C) 2011-2015 - Daniel De Matteis
- * Copyright (C) 2012-2014 - Jason Fetters
- * Copyright (C) 2014-2015 - Jay McCarthy
  *
  * RetroArch is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Found-
