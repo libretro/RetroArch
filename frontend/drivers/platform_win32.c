656d49c12d90374e066bba6a709abfc532dc7aec
twinaphex <libretro@gmail.com>
2015-04-08 02:11:23 +0200

    (Frontend Win32) Update

--- frontend/drivers/platform_win32.c
+++ frontend/drivers/platform_win32.c
@@ -12,6 +12,12 @@
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifdef _XBOX
+#include <xtl.h>
+#else
+#include <windows.h>
+#endif
+#include <retro_miscellaneous.h>
 #include "../frontend_driver.h"
 
 #include <stdint.h>
@@ -19,24 +25,29 @@
 #include <stddef.h>
 #include <string.h>
 
-#include "../general.h"
-
 static void frontend_win32_get_os(char *name, size_t sizeof_name, int *major, int *minor)
 {
-	DWORD version = GetVersion();
+	uint32_t version = GetVersion();
 
 	*major   = (DWORD)(LOBYTE(LOWORD(version)));
 	*minor   = (DWORD)(HIBYTE(LOWORD(version)));
 }
 
-static void frontend_win32_environment_get(int *argc, char *argv[],
-	void *args, void *params_data)
+static void frontend_win32_init(void *data)
 {
+	char os_version[PATH_MAX_LENGTH];
+	int major, minor;
+
+	(void)data;
+
+	frontend_win32_get_os(os_version, sizeof(os_version), &major, &minor);
+
+
 }
 
 const frontend_ctx_driver_t frontend_ctx_win32 = {
-   frontend_win32_environment_get, /* environment_get */
-   NULL,                           /* init */
+   NULL,						   /* environment_get */
+   frontend_win32_init,            /* init */
    NULL,                           /* deinit */
    NULL,                           /* exitspawn */
    NULL,                           /* process_args */
