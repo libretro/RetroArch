04fbedfcddb5b41d1556bbd534ac8a04345a215b
twinaphex <libretro@gmail.com>
2015-04-19 20:19:49 +0200

    Remove more header files - include more of it inside RetroArch_Apple.h

--- apple/common/RetroArch_Apple.h
+++ apple/common/RetroArch_Apple.h
@@ -69,6 +69,7 @@ void CFSearchPathForDirectoriesInDomains(unsigned flags,
 void CFTemporaryDirectory(char *buf, size_t sizeof_buf);
 
 #include "../../core_info.h"
+#include "../../playlist.h"
 #include "../../settings.h"
 #include "../../menu/menu.h"
 
@@ -82,7 +83,100 @@ void CFTemporaryDirectory(char *buf, size_t sizeof_buf);
 
 #include <CoreLocation/CoreLocation.h>
 #import <AVFoundation/AVCaptureOutput.h>
-#include "../iOS/menu.h"
+
+@protocol RAMenuItemBase
+- (UITableViewCell*)cellForTableView:(UITableView*)tableView;
+- (void)wasSelectedOnTableView:(UITableView*)tableView ofController:(UIViewController*)controller;
+@end
+
+/*********************************************/
+/* RAMenuBase                                */
+/* A menu class that displays RAMenuItemBase */
+/* objects.                                  */
+/*********************************************/
+@interface RAMenuBase : UITableViewController
+@property (nonatomic) NSMutableArray* sections;
+@property (nonatomic) BOOL hidesHeaders;
+
+- (id)initWithStyle:(UITableViewStyle)style;
+- (id)itemForIndexPath:(NSIndexPath*)indexPath;
+
+@end
+
+/*********************************************/
+/* RAMenuItemBasic                           */
+/* A simple menu item that displays a text   */
+/* description and calls a block object when */
+/* selected.                                 */
+/*********************************************/
+@interface RAMenuItemBasic : NSObject<RAMenuItemBase>
+@property (nonatomic) NSString* description;
+@property (nonatomic) id userdata;
+@property (copy) void (^action)(id userdata);
+@property (copy) NSString* (^detail)(id userdata);
+
++ (RAMenuItemBasic*)itemWithDescription:(NSString*)description action:(void (^)())action;
++ (RAMenuItemBasic*)itemWithDescription:(NSString*)description action:(void (^)())action detail:(NSString* (^)())detail;
++ (RAMenuItemBasic*)itemWithDescription:(NSString*)description association:(id)userdata action:(void (^)())action detail:(NSString* (^)())detail;
+
+@end
+
+/*********************************************/
+/* RAMenuItemGeneralSetting                  */
+/* A simple menu item that displays the      */
+/* state, and allows editing, of a string or */
+/* numeric setting.                          */
+/*********************************************/
+@interface RAMenuItemGeneralSetting : NSObject<RAMenuItemBase>
+@property (nonatomic) rarch_setting_t* setting;
+@property (copy) void (^action)();
+@property (nonatomic, weak) UITableView* parentTable;
++ (id)itemForSetting:(rarch_setting_t*)setting action:(void (^)())action;
+- (id)initWithSetting:(rarch_setting_t*)setting action:(void (^)())action;
+@end
+
+/*********************************************/
+/* RAMenuItemBooleanSetting                  */
+/* A simple menu item that displays the      */
+/* state, and allows editing, of a boolean   */
+/* setting.                                  */
+/*********************************************/
+@interface RAMenuItemBooleanSetting : NSObject<RAMenuItemBase>
+@property (nonatomic) rarch_setting_t* setting;
+@property (copy) void (^action)();
+- (id)initWithSetting:(rarch_setting_t*)setting action:(void (^)())action;
+@end
+
+/*********************************************/
+/* RAMenuItemPathSetting                     */
+/* A menu item that displays and allows      */
+/* browsing for a path setting.              */
+/*********************************************/
+@interface RAMenuItemPathSetting : RAMenuItemGeneralSetting<RAMenuItemBase> @end
+
+/*********************************************/
+/* RAMenuItemEnumSetting                     */
+/* A menu item that displays and allows      */
+/* a setting to be set from a list of        */
+/* allowed choices.                          */
+/*********************************************/
+@interface RAMenuItemEnumSetting : RAMenuItemGeneralSetting<RAMenuItemBase> @end
+
+/*********************************************/
+/* RAMenuItemBindSetting                     */
+/* A menu item that displays and allows      */
+/* mapping of a keybinding.                  */
+/*********************************************/
+@interface RAMenuItemBindSetting : RAMenuItemGeneralSetting<RAMenuItemBase> @end
+
+/*********************************************/
+/* RAMainMenu                                */
+/* Menu object that is displayed immediately */
+/* after startup.                            */
+/*********************************************/
+@interface RAMainMenu : RAMenuBase
+@property (nonatomic) NSString* core;
+@end
 
 @interface RADirectoryItem : NSObject<RAMenuItemBase>
 @property (nonatomic) NSString* path;
@@ -103,7 +197,6 @@ void CFTemporaryDirectory(char *buf, size_t sizeof_buf);
 - (void)browseTo:(NSString*)path;
 @end
 
-// browser.m
 @interface RAFoldersList : RAMenuBase
 - (id) initWithFilePath:(NSString*)path;
 @end
