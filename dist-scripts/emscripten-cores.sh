b378212866ccb2c1c0a06e2684680c021b5a636a
fr500 <radius@maildrop.cc>
2015-04-19 13:58:44 -0500

    emscripten build fixes

--- dist-scripts/emscripten-cores.sh
+++ dist-scripts/emscripten-cores.sh
@@ -9,13 +9,13 @@ make -C ../ -f Makefile.emscripten clean || exit 1
 
 for f in *_emscripten.bc ; do
    name=`echo "$f" | sed 's/\(_libretro_emscripten\|\).bc$//'`
-   lto=1
+   lto=0
    echo "building $name"
    if [ $name = "tyrquake" ] ; then
       lto=0
    fi
    cp -f "$f" ../libretro_emscripten.bc
    make -C ../ -f Makefile.emscripten LTO=$lto -j7 || exit 1
-   mv -f ../retroarch.js ../emscripten/$name.js
+   cp -fv ../retroarch.js ../emscripten/$name.js
    rm -f ../retroarch.js
 done
