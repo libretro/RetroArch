a9aab621694c183fdb8450e0726bbea66468a59b
twinaphex <libretro@gmail.com>
2015-05-02 00:27:33 +0200

    (Android) Create prototypes for functions

--- frontend/drivers/android_native_app_glue.h
+++ frontend/drivers/android_native_app_glue.h
@@ -334,7 +334,31 @@ enum
 
 void android_app_write_cmd(struct android_app *android_app, int8_t cmd);
 
+/**
+ * Call when ALooper_pollAll() returns LOOPER_ID_MAIN, reading the next
+ * app command message.
+ */
 int8_t android_app_read_cmd(struct android_app *android_app);
+
+/**
+ * Call with the command returned by android_app_read_cmd() to do the
+ * initial pre-processing of the given command.  You can perform your own
+ * actions for the command after calling this function.
+ */
+void android_app_pre_exec_cmd(struct android_app* android_app, int8_t cmd);
+
+/**
+ * Call with the command returned by android_app_read_cmd() to do the
+ * final post-processing of the given command.  You must have done your own
+ * actions for the command before calling this function.
+ */
+void android_app_post_exec_cmd(struct android_app* android_app, int8_t cmd);
+
+/**
+ * This is the function that application code must implement, representing
+ * the main entry to the app.
+ */
+void android_main(struct android_app* app);
 
 #ifdef __cplusplus
 }
