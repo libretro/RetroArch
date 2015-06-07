ce79c4a4a02ffb1eebfaa53fd006b06c9d918281
twinaphex <libretro@gmail.com>
2015-06-07 15:49:58 +0200

    (Menu) Buildfix

--- menu/menu_driver.c
+++ menu/menu_driver.c
@@ -264,7 +264,7 @@ size_t menu_driver_list_get_size(menu_list_type_t type)
    menu_handle_t *menu             = menu_driver_get_ptr();
    const menu_ctx_driver_t *driver = menu_ctx_driver_get_ptr();
 
-   if (driver->list_get_size)
+   if (driver && driver->list_get_size)
       return driver->list_get_size(menu, type);
    return 0;
 }
