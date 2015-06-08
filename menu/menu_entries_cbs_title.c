446a1756ff21b5ccff28a52df60a0e41dd59b387
twinaphex <libretro@gmail.com>
2015-06-08 11:40:20 +0200

    (menu_entries_cbs_title.c) Cleanup

--- menu/menu_entries_cbs_title.c
+++ menu/menu_entries_cbs_title.c
@@ -17,10 +17,10 @@
 #include <string/stdstring.h>
 #include <file/file_path.h>
 
+#include <rhash.h>
+
 #include "menu.h"
 #include "menu_entries_cbs.h"
-
-#include <rhash.h>
 
 static INLINE void replace_chars(char *str, char c1, char c2)
 {
