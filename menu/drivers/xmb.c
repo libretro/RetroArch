cee0f2bf46696f2a0d4f79c847932e21b63a0297
Higor Eurípedes <heuripedes@gmail.com>
2015-06-10 08:48:40 -0300

    (XMB) Display all system icons

--- menu/drivers/xmb.c
+++ menu/drivers/xmb.c
@@ -1260,7 +1260,7 @@ static void xmb_frame_horizontal_list(xmb_handle_t *xmb, menu_handle_t *menu, gl
    unsigned i;
    size_t list_size = xmb_list_get_size(menu, MENU_LIST_HORIZONTAL);
 
-   for (i = 0; i < list_size; i++)
+   for (i = 0; i <= list_size; i++)
    {
       xmb_node_t *node = &xmb->settings_node;
 
