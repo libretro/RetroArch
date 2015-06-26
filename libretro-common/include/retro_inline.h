c3e111605be7e07f8b9538a7293b3c9cc9a6e472
Alcaro <floating@muncher.se>
2015-06-26 15:53:13 +0200

    Fix retro_inline.h under C89.

--- libretro-common/include/retro_inline.h
+++ libretro-common/include/retro_inline.h
@@ -23,18 +23,17 @@
 #ifndef __LIBRETRO_SDK_INLINE_H
 #define __LIBRETRO_SDK_INLINE_H
 
+#ifndef INLINE
+
 #if !defined(__cplusplus) && defined(_WIN32)
-
-#ifndef INLINE
 #define INLINE _inline
-#endif
-
-#else
-
-#ifndef INLINE
+#elif defined(__STDC_VERSION__) && __STDC_VERSION__>=199901L
 #define INLINE inline
+#elif defined(__GNUC__)
+#define INLINE __inline__
+#else
+#define INLINE
 #endif
 
 #endif
-
 #endif
