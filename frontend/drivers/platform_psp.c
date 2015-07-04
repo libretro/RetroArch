394cdf9c7c18ff5132f52f13525e4b51d1e747a3
radius <andres.430@gmail.com>
2015-07-04 18:11:48 -0500

    psp buildfix

--- frontend/drivers/platform_psp.c
+++ frontend/drivers/platform_psp.c
@@ -87,9 +87,9 @@ static void frontend_psp_get_environment_settings(int *argc, char *argv[],
          "playlists", sizeof(g_defaults.playlist_dir));
    fill_pathname_join(g_defaults.config_path, g_defaults.port_dir,
          "retroarch.cfg", sizeof(g_defaults.config_path));
-   fill_pathname_join(g_defaults.cheats_dir, path,
+   fill_pathname_join(g_defaults.cheats_dir, g_defaults.cheats_dir,
          "cheats", sizeof(g_defaults.cheats_dir));
-   fill_pathname_join(g_defaults.remap_dir, path,
+   fill_pathname_join(g_defaults.remap_dir, g_defaults.remap_dir,
          "remaps", sizeof(g_defaults.remap_dir));
 
 #ifndef IS_SALAMANDER
