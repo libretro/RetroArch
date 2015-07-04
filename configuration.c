e4ad131a2e7dfd50c6f67761b3ce5cbb101993b8
twinaphex <libretro@gmail.com>
2015-07-04 19:14:04 +0200

    Set max_users correctly

--- configuration.c
+++ configuration.c
@@ -586,7 +586,7 @@ static void config_set_defaults(void)
    settings->input.input_descriptor_label_show      = input_descriptor_label_show;
    settings->input.input_descriptor_hide_unbound    = input_descriptor_hide_unbound;
    settings->input.remap_binds_enable               = true;
-   settings->input.max_users                        = MAX_USERS;
+   settings->input.max_users                        = input_max_users;
 
    rarch_assert(sizeof(settings->input.binds[0]) >= sizeof(retro_keybinds_1));
    rarch_assert(sizeof(settings->input.binds[1]) >= sizeof(retro_keybinds_rest));
