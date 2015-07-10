25ffeb1ed95f3901e506c25230b8e46c2ac0bbd4
twinaphex <libretro@gmail.com>
2015-07-10 18:45:22 +0200

    (Xenon) Don't depend on global state in input driver

--- input/drivers/xenon360_input.c
+++ input/drivers/xenon360_input.c
@@ -27,6 +27,7 @@
 #define MAX_PADS 4
 
 static uint64_t state[MAX_PADS];
+static uint64_t lifecycle_state;
 
 static void xenon360_input_poll(void *data)
 {
@@ -89,8 +90,7 @@ static void* xenon360_input_init(void)
 
 static bool xenon360_input_key_pressed(void *data, int key)
 {
-   global_t *global = global_get_ptr();
-   return (global->lifecycle_state & (1ULL << key));
+   return (lifecycle_state & (1ULL << key));
 }
 
 static uint64_t xenon360_get_capabilities(void *data)
