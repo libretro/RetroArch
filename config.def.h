d9a72d9b5c95f1f806b6f8ccd282e7745c74759d
twinaphex <libretro@gmail.com>
2015-07-18 16:15:54 +0200

    (OSX) Set XMB driver as menu default driver from OSX 10.6 and up

--- config.def.h
+++ config.def.h
@@ -317,6 +317,8 @@ enum
 #define MENU_DEFAULT_DRIVER MENU_RMENU
 #elif defined(HAVE_RMENU_XUI)
 #define MENU_DEFAULT_DRIVER MENU_RMENU_XUI
+#elif defined(MAC_OS_X_VERSION_10_6)
+#define MENU_DEFAULT_DRIVER MENU_XMB
 #elif defined(IOS) || defined(ANDROID) || defined(__CELLOS_LV2__)
 #define MENU_DEFAULT_DRIVER MENU_GLUI
 #else
