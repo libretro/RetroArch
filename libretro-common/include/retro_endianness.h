99e1a9b297aa7f141496ccc3b4462398d7790c34
twinaphex <libretro@gmail.com>
2015-07-18 20:56:47 +0200

    Get rid of builtin_bswap16/bswap32 - breaks on OSX 10.8

--- libretro-common/include/retro_endianness.h
+++ libretro-common/include/retro_endianness.h
@@ -27,10 +27,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#if defined(__llvm__) || (defined(__GNUC__) && (__GNUC__ * 100 + __GNUC_MINOR__) >= 408)
-#define SWAP16 __builtin_bswap16
-#define SWAP32 __builtin_bswap32
-#elif defined(_MSC_VER)
+#if defined(_MSC_VER)
 #define SWAP16 _byteswap_ushort
 #define SWAP32 _byteswap_ulong
 #else
