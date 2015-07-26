44feeabace6b24f3a1fc7c30d99ff772a7187090
radius <andres.430@gmail.com>
2015-07-26 18:35:13 -0500

    this should fix controller indexes changing after binding

--- menu/menu_input.c
+++ menu/menu_input.c
@@ -371,7 +371,7 @@ static bool menu_input_poll_find_trigger(struct menu_bind_state *state,
 
       /* Update the joypad mapping automatically.
        * More friendly that way. */
-      settings->input.joypad_map[state->user] = i;
+      //settings->input.joypad_map[state->user] = i;
       return true;
    }
    return false;
