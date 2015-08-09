12dcc5ae0acf04528b3fe1989745c4b7b2f047d3
radius <andres.430@gmail.com>
2015-08-09 11:51:18 -0500

    fix typo

--- input/input_autodetect.c
+++ input/input_autodetect.c
@@ -227,7 +227,7 @@ static bool input_autoconfigure_joypad_from_conf_dir(
    else
    {
       RARCH_LOG("Autoconf: no profiles found for %s (%d/%d)", params->name, params->vid, params->pid);
-      snprintf(msg, sizeof(msg), "%s (%d/%d) not configured", parms->name, params->vid, params->pid);
+      snprintf(msg, sizeof(msg), "%s (%d/%d) not configured", params->name, params->vid, params->pid);
       rarch_main_msg_queue_push(msg, 0, 60, false);
       ret = 0;
    }
