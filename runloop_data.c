0b33c8f2b17bd7b3de344af0e302714fb808bf8f
twinaphex <libretro@gmail.com>
2015-08-22 18:26:14 +0200

    (runloop_data.c) Use fill_pathname_join_delim

--- runloop_data.c
+++ runloop_data.c
@@ -17,6 +17,7 @@
 #ifdef HAVE_THREADS
 #include <rthreads/rthreads.h>
 #endif
+#include <file/file_path.h>
 
 #include "general.h"
 
@@ -324,27 +325,27 @@ void rarch_main_data_msg_queue_push(unsigned type,
          break;
       case DATA_TYPE_FILE:
          queue = rarch_main_data_nbio_get_msg_queue_ptr();
-         snprintf(new_msg, sizeof(new_msg), "%s|%s", msg, msg2);
+         fill_pathname_join_delim(new_msg, msg, msg2, '|', sizeof(new_msg));
          break;
       case DATA_TYPE_IMAGE:
          queue = rarch_main_data_nbio_image_get_msg_queue_ptr();
-         snprintf(new_msg, sizeof(new_msg), "%s|%s", msg, msg2);
+         fill_pathname_join_delim(new_msg, msg, msg2, '|', sizeof(new_msg));
          break;
 #ifdef HAVE_NETWORKING
       case DATA_TYPE_HTTP:
          queue = rarch_main_data_http_get_msg_queue_ptr();
-         snprintf(new_msg, sizeof(new_msg), "%s|%s", msg, msg2);
+         fill_pathname_join_delim(new_msg, msg, msg2, '|', sizeof(new_msg));
          break;
 #endif
 #ifdef HAVE_OVERLAY
       case DATA_TYPE_OVERLAY:
-         snprintf(new_msg, sizeof(new_msg), "%s|%s", msg, msg2);
+         fill_pathname_join_delim(new_msg, msg, msg2, '|', sizeof(new_msg));
          break;
 #endif
 #ifdef HAVE_LIBRETRODB
       case DATA_TYPE_DB:
          queue = rarch_main_data_db_get_msg_queue_ptr();
-         snprintf(new_msg, sizeof(new_msg), "%s|%s", msg, msg2);
+         fill_pathname_join_delim(new_msg, msg, msg2, '|', sizeof(new_msg));
          break;
 #endif
    }
