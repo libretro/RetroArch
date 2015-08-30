9f3f0c1f1a1f75d63cc3ad1f797e6202c9879724
CautiousAlbino <cautiousalbino@none>
2015-08-30 17:02:52 +0200

    Typo fix.

--- libretro-common/file/dir_list.c
+++ libretro-common/file/dir_list.c
@@ -196,10 +196,10 @@ static int parse_dir_entry(const char *name, char *file_path,
 
 /**
  * dir_list_new:
- * @dir          : directory path.
- * @ext          : allowed extensions of file directory entries to include.
- * @include_dirs : include directories as part of the finished directory listing?
- * @strict_ext   : Only include files which match ext. Do not try to match compressed files, etc.
+ * @dir                : directory path.
+ * @ext                : allowed extensions of file directory entries to include.
+ * @include_dirs       : include directories as part of the finished directory listing?
+ * @include_compressed : Only include files which match ext. Do not try to match compressed files, etc.
  *
  * Create a directory listing.
  *
