11d574bc30a627b36c26fb88642629023ebb387e
twinaphex <libretro@gmail.com>
2015-09-01 16:40:09 +0200

    Fix up some dist-scripts

--- dist-scripts/ps3-cores-dex.sh
+++ dist-scripts/ps3-cores-dex.sh
@@ -1,37 +1,39 @@
 #!/bin/sh
 
-make -C ../ -f Makefile.ps3.salamander clean || exit 1
-make -C ../ -f Makefile.ps3 clean || exit 1
+PLATFORM=ps3
 
-make -C ../ -f Makefile.ps3.salamander || exit 1
+make -C ../ -f Makefile.${PLATFORM}.salamander clean || exit 1
+make -C ../ -f Makefile.${PLATFORM} clean || exit 1
+
+make -C ../ -f Makefile.${PLATFORM}.salamander || exit 1
 
 EXE_PATH=/usr/local/cell/host-win32/bin
 MAKE_FSELF_NPDRM=$EXE_PATH/make_fself_npdrm.exe
 MAKE_PACKAGE_NPDRM=$EXE_PATH/make_package_npdrm.exe
 
-for f in *_ps3.a ; do
-   name=`echo "$f" | sed 's/\(_libretro_ps3\|\).a$//'`
+for f in *_${PLATFORM}.a ; do
+   name=`echo "$f" | sed "s/\(_libretro_${PLATFORM}\|\).a$//"`
    whole_archive=
    if [ $name = "nxengine" ] ; then
-      echo "NXEngine found, applying whole archive linking..."
+      echo "Applying whole archive linking..."
       whole_archive="WHOLE_ARCHIVE_LINK=1"
-      echo $name yes
    fi
    if [ $name = "tyrquake" ] ; then
-      echo "Tyrquake found, applying big stack..."
+      echo "Applying big stack..."
       big_stack="BIG_STACK=1"
    fi
-   cp -f "$f" ../libretro_ps3.a
-   make -C ../ -f Makefile.ps3 $whole_archive $big_stack -j3 || exit 1
-   $MAKE_FSELF_NPDRM ../retroarch_ps3.elf ../CORE.SELF
-   mv -f ../CORE.SELF ../ps3/pkg/USRDIR/cores/"${name}_libretro_ps3.SELF"
-   rm -f ../retroarch_ps3.elf ../retroarch_ps3.self ../CORE.SELF
+   echo "-- Building core: $name --"
+   cp -f "$f" ../libretro_${PLATFORM}.a
+   make -C ../ -f Makefile.${PLATFORM} $whole_archive $big_stack -j3 || exit 1
+   $MAKE_FSELF_NPDRM ../retroarch_${PLATFORM}.elf ../CORE.SELF
+   mv -f ../CORE.SELF ../pkg/${PLATFORM}/USRDIR/cores/"${name}_libretro_${PLATFORM}.SELF"
+   rm -f ../retroarch_${PLATFORM}.elf ../retroarch_${PLATFORM}.self ../CORE.SELF
 done
 
-cp -r ../media/rmenu/*.png ../ps3/pkg/USRDIR/cores/borders/Menu/
+cp -r ../media/rmenu/*.png ../pkg/${PLATFORM}/USRDIR/cores/borders/Menu/
 
-make -C ../ -f Makefile.griffin platform=ps3 shaders-deploy
+make -C ../ -f Makefile.griffin platform=${PLATFORM} shaders-deploy
 
-$MAKE_FSELF_NPDRM ../retroarch-salamander_ps3.elf ../ps3/pkg/USRDIR/EBOOT.BIN
-rm -rf ../retroarch-salamander_ps3.elf
-$MAKE_PACKAGE_NPDRM ../ps3/pkg/package.conf ../ps3/pkg
+$MAKE_FSELF_NPDRM ../retroarch-salamander_${PLATFORM}.elf ../pkg/${PLATFORM}/USRDIR/EBOOT.BIN
+rm -rf ../retroarch-salamander_${PLATFORM}.elf
+$MAKE_PACKAGE_NPDRM ../pkg/${PLATFORM}/package.conf ../pkg/${PLATFORM}
--- dist-scripts/wii-cores.sh
+++ dist-scripts/wii-cores.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-make -C ../ -f Makefile.wii.salamander clean || exit 1
-make -C ../ -f Makefile.griffin platform=wii clean || exit 1
+PLATFORM=wii
 
-make -C ../ -f Makefile.wii.salamander || exit 1
-make -C ../ -f Makefile.wii.salamander pkg || exit 1
+make -C ../ -f Makefile.${PLATFORM}.salamander clean || exit 1
+make -C ../ -f Makefile.griffin PLATFORM=${PLATFORM} clean || exit 1
 
-for f in *_wii.a ; do
-   name=`echo "$f" | sed 's/\(_libretro_wii\|\).a$//'`
+make -C ../ -f Makefile.${PLATFORM}.salamander || exit 1
+make -C ../ -f Makefile.${PLATFORM}.salamander pkg || exit 1
+
+for f in *_${PLATFORM}.a ; do
+   name=`echo "$f" | sed "s/\(_libretro_${PLATFORM}\|\).a$//"`
    whole_archive=
    big_stack=
    if [ $name = "nxengine" ] ; then
@@ -18,8 +20,8 @@ for f in *_wii.a ; do
       echo "Tyrquake found, applying big stack..."
       big_stack="BIG_STACK=1"
    fi
-   cp -f "$f" ../libretro_wii.a
-   make -C ../ -f Makefile.griffin platform=wii $whole_archive $big_stack -j3 || exit 1
-   mv -f ../retroarch_wii.dol ../wii/pkg/${name}_libretro_wii.dol
-   rm -f ../retroarch_wii.dol ../retroarch_wii.elf ../retroarch_wii.elf.map
+   cp -f "$f" ../libretro_${PLATFORM}.a
+   make -C ../ -f Makefile.griffin platform=${PLATFORM} $whole_archive $big_stack -j3 || exit 1
+   mv -f ../retroarch_${PLATFORM}.dol ../pkg/${PLATFORM}/${name}_libretro_${PLATFORM}.dol
+   rm -f ../retroarch_${PLATFORM}.dol ../retroarch_${PLATFORM}.elf ../retroarch_${PLATFORM}.elf.map
 done
