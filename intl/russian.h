f672742f7b69b2e1f7351fc40584d5381a2243a8
6lackmag3 <alexeibox@mail.ru>
2015-09-04 21:10:15 +0400

    Create russian.h

---
+++ intl/russian.h
@@ -0,0 +1,82 @@
+/*  RetroArch - A frontend for libretro.
+ *  Copyright (C) 2010-2014 - Hans-Kristian Arntzen
+ *  Copyright (C) 2011-2015 - Daniel De Matteis
+ *
+ *  RetroArch is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with RetroArch.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __INTL_ENGLISH_H
+#define __INTL_ENGLISH_H
+
+#define RETRO_LBL_JOYPAD_B "RetroPad, кнопка B"
+#define RETRO_LBL_JOYPAD_Y "RetroPad, кнопка Y"
+#define RETRO_LBL_JOYPAD_SELECT "RetroPad, кнопка Select"
+#define RETRO_LBL_JOYPAD_START "RetroPad, кнопка Start"
+#define RETRO_LBL_JOYPAD_UP "RetroPad, D-Pad вверх"
+#define RETRO_LBL_JOYPAD_DOWN "RetroPad, D-Pad вниз"
+#define RETRO_LBL_JOYPAD_LEFT "RetroPad, D-Pad влево"
+#define RETRO_LBL_JOYPAD_RIGHT "RetroPad, D-Pad вправо"
+#define RETRO_LBL_JOYPAD_A "RetroPad, кнопка A"
+#define RETRO_LBL_JOYPAD_X "RetroPad, кнопка X"
+#define RETRO_LBL_JOYPAD_L "RetroPad, кнопка L1"
+#define RETRO_LBL_JOYPAD_R "RetroPad, кнопка R1"
+#define RETRO_LBL_JOYPAD_L2 "RetroPad, кнопка L2"
+#define RETRO_LBL_JOYPAD_R2 "RetroPad, кнопка R2"
+#define RETRO_LBL_JOYPAD_L3 "RetroPad, кнопка L3"
+#define RETRO_LBL_JOYPAD_R3 "RetroPad, кнопка R3"
+#define RETRO_LBL_TURBO_ENABLE "Активация турбо"
+#define RETRO_LBL_ANALOG_LEFT_X "Левый джойстик, ось X"
+#define RETRO_LBL_ANALOG_LEFT_Y "Левый джойстик, ось Y"
+#define RETRO_LBL_ANALOG_RIGHT_X "Правый джойстик, ось X"
+#define RETRO_LBL_ANALOG_RIGHT_Y "Правый джойстик, ось Y"
+#define RETRO_LBL_ANALOG_LEFT_X_PLUS "Левый джойстик, ось X +"
+#define RETRO_LBL_ANALOG_LEFT_X_MINUS "Левый джойстик, ось X -"
+#define RETRO_LBL_ANALOG_LEFT_Y_PLUS "Левый джойстик, ось Y +"
+#define RETRO_LBL_ANALOG_LEFT_Y_MINUS "Левый джойстик, ось Y -"
+#define RETRO_LBL_ANALOG_RIGHT_X_PLUS "Правый джойстик, ось X +"
+#define RETRO_LBL_ANALOG_RIGHT_X_MINUS "Правый джойстик, ось X -"
+#define RETRO_LBL_ANALOG_RIGHT_Y_PLUS "Правый джойстик, ось Y +"
+#define RETRO_LBL_ANALOG_RIGHT_Y_MINUS "Правый джойстик, ось Y -"
+#define RETRO_LBL_FAST_FORWARD_KEY "Ускорение"
+#define RETRO_LBL_FAST_FORWARD_HOLD_KEY "Удерживать ускорение"
+#define RETRO_LBL_LOAD_STATE_KEY "Загрузка"
+#define RETRO_LBL_SAVE_STATE_KEY "Сохранение"
+#define RETRO_LBL_FULLSCREEN_TOGGLE_KEY "Полноэкранный режим"
+#define RETRO_LBL_QUIT_KEY "Выход из RetroArch"
+#define RETRO_LBL_STATE_SLOT_PLUS "Повысить слот сохранения"
+#define RETRO_LBL_STATE_SLOT_MINUS "Понизить слот сохранения"
+#define RETRO_LBL_REWIND "Перемотка"
+#define RETRO_LBL_MOVIE_RECORD_TOGGLE "Вкл./выкл. запись"
+#define RETRO_LBL_PAUSE_TOGGLE "Пауза"
+#define RETRO_LBL_FRAMEADVANCE "Покадровая перемотка"
+#define RETRO_LBL_RESET "Сброс"
+#define RETRO_LBL_SHADER_NEXT "Загрузить след. шейдер"
+#define RETRO_LBL_SHADER_PREV "Загрузить пред. шейдер"
+#define RETRO_LBL_CHEAT_INDEX_PLUS "Следующий чит-код"
+#define RETRO_LBL_CHEAT_INDEX_MINUS "Предыдущий чит-код"
+#define RETRO_LBL_CHEAT_TOGGLE "Вкл./откл. чит-код"
+#define RETRO_LBL_SCREENSHOT "Скриншот"
+#define RETRO_LBL_MUTE "Откл./вкл. звук"
+#define RETRO_LBL_OSK "Вызов оверлея клавиатуры"
+#define RETRO_LBL_NETPLAY_FLIP "Смена игроков в сетевой игре"
+#define RETRO_LBL_SLOWMOTION "Замедление"
+#define RETRO_LBL_ENABLE_HOTKEY "Активация горячей клавиши"
+#define RETRO_LBL_VOLUME_UP "Увеличить громкость"
+#define RETRO_LBL_VOLUME_DOWN "Уменьшить громкость "
+#define RETRO_LBL_OVERLAY_NEXT "Загрузить след. оверлей"
+#define RETRO_LBL_DISK_EJECT_TOGGLE "Откр./закр. лоток cd-привода"
+#define RETRO_LBL_DISK_NEXT "Загрузить след. диск"
+#define RETRO_LBL_DISK_PREV "Загрузить пред. диск"
+#define RETRO_LBL_GRAB_MOUSE_TOGGLE "Перехват мыши"
+#define RETRO_LBL_MENU_TOGGLE "Вызов меню"
+
+#endif
