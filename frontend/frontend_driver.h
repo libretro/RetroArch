95a7331d01809bfefbe4f35141b79209b0f4e48a
twinaphex <libretro@gmail.com>
2015-09-15 21:07:27 +0200

    Remove reference to frontend_ctx_android

--- frontend/frontend_driver.h
+++ frontend/frontend_driver.h
@@ -81,7 +81,6 @@ extern frontend_ctx_driver_t frontend_ctx_ps3;
 extern frontend_ctx_driver_t frontend_ctx_xdk;
 extern frontend_ctx_driver_t frontend_ctx_qnx;
 extern frontend_ctx_driver_t frontend_ctx_darwin;
-extern frontend_ctx_driver_t frontend_ctx_android;
 extern frontend_ctx_driver_t frontend_ctx_linux;
 extern frontend_ctx_driver_t frontend_ctx_psp;
 extern frontend_ctx_driver_t frontend_ctx_ctr;
