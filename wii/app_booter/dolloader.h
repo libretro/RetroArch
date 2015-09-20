10ec315a43e06cdadab0e637a4c1a94d4ecaf076
twinaphex <libretro@gmail.com>
2015-09-20 15:17:12 +0200

    Downsize wii/app_booter

--- wii/app_booter/dolloader.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DOLLOADER_H_
-#define _DOLLOADER_H_
-
-typedef void (*entrypoint) (void);
-
-u32 load_dol_image(const void *dolstart);
-
-#endif
--- wii/app_booter/elfloader.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ELFLOADER_H_
-#define _ELFLOADER_H_
-
-s32 valid_elf_image (void *addr);
-u32 load_elf_image (void *addr);
-
-#endif
