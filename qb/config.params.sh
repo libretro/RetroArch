b748c4d27f8d555ad5205de687794d2ac7e8ed34
twinaphex <libretro@gmail.com>
2015-09-22 16:47:56 +0200

    Blacklist imageviewer for C89

--- qb/config.params.sh
+++ qb/config.params.sh
@@ -61,5 +61,6 @@ HAVE_7ZIP=yes           # Compile in 7z support
 HAVE_PRESERVE_DYLIB=no  # Disable dlclose() for Valgrind support
 HAVE_PARPORT=auto       # Parallel port joypad support
 HAVE_IMAGEVIEWER=yes    # Built-in image viewer support.
+C89_IMAGEVIEWER=no
 HAVE_MMAP=auto          # MMAP support
 HAVE_QT=no              # QT companion support
