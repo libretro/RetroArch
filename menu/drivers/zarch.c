201faccff9ae10e4a45d24d5671b7ebab2c1cb88
Twinaphex <libretro@gmail.com>
2015-10-03 02:37:17 +0200

    Silence clang warnings

--- menu/drivers/zarch.c
+++ menu/drivers/zarch.c
@@ -551,7 +551,7 @@ void render_lay_root(zui_t *zui)
       if (!zui->load_cwd)
       {
          zui->load_cwd = strdup(zui->set->menu_content_directory);
-         if ((zui->load_cwd[strlen(zui->load_cwd)-1] == '/')
+         if (zui->load_cwd[strlen(zui->load_cwd)-1] == '/'
 #ifdef _WIN32
             || (zui->load_cwd[strlen(zui->load_cwd)-1] == '\\')
 #endif
@@ -588,7 +588,7 @@ void render_lay_root(zui_t *zui)
          if (zui_list_item(zui, 0, tabbed.tabline_size + 40, "^ .."))
          {
             path_basedir(zui->load_cwd);
-            if ((zui->load_cwd[strlen(zui->load_cwd)-1] == '/')
+            if (zui->load_cwd[strlen(zui->load_cwd)-1] == '/'
 #ifdef _WIN32
                   || (zui->load_cwd[strlen(zui->load_cwd)-1] == '\\')
 #endif
