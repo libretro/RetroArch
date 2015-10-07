1b403105e45fbee80df1a3d130de37f25791d7f9
Andre Leiradella <andre@leiradella.com>
2015-10-07 19:28:38 -0300

    fixed plain converter; added lua converter back until we're sure the plain converter is working

---
+++ libretro-db/dat_converter.lua
@@ -0,0 +1,216 @@
+local dat_obj = {}
+local match_key = nil
+
+local function dat_lexer(f, fname)
+    local line, err = f:read("*l")
+    local location = {line_no = 1, column = 1, fname = fname}
+    return function()
+        local tok = nil
+        while not tok do
+            if not line then
+                return nil
+            end
+            pre_space, tok, line = string.match(line, "^(%s*)(..-)([()]*%s.*)")
+            if tok and string.match(tok, "^\"") then
+                tok, line = string.match(tok..line, "^\"([^\"]-)\"(.*)")
+            elseif tok and string.match(tok, "^[()]") then
+                line = tok:sub(2) .. line
+                tok = tok:sub(1,1)
+            end
+            location.column = location.column  + #(pre_space or "")
+            tok_loc = {
+                line_no = location.line_no,
+                column = location.column,
+                fname = location.fname
+            }
+            if not line then
+                line = f:read("*l")
+                location.line_no = location.line_no + 1
+                location.column = 1
+            else
+                location.column = location.column + #tok
+            end
+        end
+        -- print(tok)
+        return tok, tok_loc
+    end
+end
+
+local function dat_parse_table(lexer, start_loc)
+    local res = {}
+    local state = "key"
+    local key = nil
+    for tok, loc in lexer do
+        if state == "key" then
+            if tok == ")" then
+                return res
+            elseif tok == "(" then
+                error(string.format(
+                    "%s:%d:%d: fatal error: Unexpected '(' instead of key",
+                    loc.fname,
+                    loc.line_no,
+                    loc.column
+                ))
+            else
+                key = tok
+                state = "value"
+            end
+        else
+            if tok == "(" then
+                res[key] = dat_parse_table(lexer, loc)
+            elseif tok == ")" then
+                error(string.format(
+                    "%s:%d:%d: fatal error: Unexpected ')' instead of value",
+                    loc.fname,
+                    loc.line_no,
+                    loc.column
+                ))
+            else
+                res[key] = tok
+            end
+            state = "key"
+        end
+    end
+    error(string.format(
+        "%s:%d:%d: fatal error: Missing ')' for '('",
+        start_loc.fname,
+        start_loc.line_no,
+        start_loc.column
+    ))
+end
+
+local function dat_parser(lexer)
+    local res = {}
+    local state = "key"
+    local key = nil
+    local skip = true
+    for tok, loc in lexer do
+        if state == "key" then
+            if tok == "game" then
+                skip = false
+            end
+            state = "value"
+        else
+            if tok == "(" then
+                local v = dat_parse_table(lexer, loc)
+                if not skip then
+                    table.insert(res, v)
+                    skip = true
+                end
+            else
+                error(string.format(
+                    "%s:%d:%d: fatal error: Expected '(' found '%s'",
+                    loc.fname,
+                    loc.line_no,
+                    loc.column,
+                    tok
+                ))
+            end
+            state = "key"
+        end
+    end
+    return res
+end
+
+local function unhex(s)
+    if not s then return nil end
+    return (s:gsub('..', function (c)
+        return string.char(tonumber(c, 16))
+    end))
+end
+
+local function get_match_key(mk, t)
+    for p in string.gmatch(mk, "(%w+)[.]?") do
+        if p == nil or t == nil then
+            error("Invalid match key '"..mk.."'")
+        end
+        t = t[p]
+    end
+    return t
+end
+
+table.update = function(a, b)
+    for k,v in pairs(b) do
+        a[k] = v
+    end
+end
+
+function init(...)
+    local args = {...}
+    table.remove(args, 1)
+    if #args == 0 then
+        assert(dat_path, "dat file argument is missing")
+    end
+
+    if #args > 1 then
+        match_key = table.remove(args, 1)
+    end
+
+    local dat_hash = {}
+    for _, dat_path in ipairs(args) do
+        local dat_file, err = io.open(dat_path, "r")
+        if err then
+            error("could not open dat file '" .. dat_path .. "':" .. err)
+        end
+
+        print("Parsing dat file '" .. dat_path .. "'...")
+        local objs = dat_parser(dat_lexer(dat_file, dat_path))
+        dat_file:close()
+        for _, obj in pairs(objs) do
+            if match_key then
+                local mk = get_match_key(match_key, obj)
+                if mk == nil then
+                    error("missing match key '" .. match_key .. "' in one of the entries")
+                end
+                if dat_hash[mk] == nil then
+                    dat_hash[mk] = {}
+                    table.insert(dat_obj, dat_hash[mk])
+                end
+                table.update(dat_hash[mk], obj)
+            else
+                table.insert(dat_obj, obj)
+            end
+        end
+    end
+end
+
+function get_value()
+    local t = table.remove(dat_obj)
+    if not t then
+        return
+    else
+        return {
+            name = t.name,
+            description = t.description,
+            rom_name = t.rom.name,
+            size = uint(tonumber(t.rom.size)),
+            users = uint(tonumber(t.users)),
+            releasemonth = uint(tonumber(t.releasemonth)),
+            releaseyear = uint(tonumber(t.releaseyear)),
+            rumble = uint(tonumber(t.rumble)),
+            analog = uint(tonumber(t.analog)),
+
+            famitsu_rating = uint(tonumber(t.famitsu_rating)),
+            edge_rating = uint(tonumber(t.edge_rating)),
+            edge_issue = uint(tonumber(t.edge_issue)),
+            edge_review = t.edge_review,
+
+            enhancement_hw = t.enhancement_hw,
+            barcode = t.barcode,
+            esrb_rating = t.esrb_rating,
+            elspa_rating = t.elspa_rating,
+            pegi_rating = t.pegi_rating,
+            cero_rating = t.cero_rating,
+            franchise   = t.franchise,
+
+            developer = t.developer,
+            publisher = t.publisher,
+            origin = t.origin,
+
+            crc = binary(unhex(t.rom.crc)),
+            md5 = binary(unhex(t.rom.md5)),
+            sha1 = binary(unhex(t.rom.sha1)),
+            serial = binary(t.serial or t.rom.serial),
+        }
+    end
+end
--- libretro-db/parser.c
+++ libretro-db/parser.c
@@ -1,155 +1,179 @@
 #include <malloc.h>
 #include <string.h>
 
-#include <rhash.h>
-
 #include "parser.h"
 
 static void match_any( pr_state_t* parser )
 {
-   switch ( lx_next( &parser->lexer ) )
-   {
-      case LX_UNTERMINATED_STRING:
-         longjmp( parser->env, PR_UNTERMINATED_STRING );
-      case LX_INVALID_CHARACTER:
-         longjmp( parser->env, PR_INVALID_CHARACTER );
-   }
+  switch ( lx_next( &parser->lexer ) )
+  {
+  case LX_UNTERMINATED_STRING: longjmp( parser->env, PR_UNTERMINATED_STRING );
+  case LX_INVALID_CHARACTER:   longjmp( parser->env, PR_INVALID_CHARACTER );
+  }
 }
 
 static void match( pr_state_t* parser, int token )
 {
-   if ( parser->lexer.token != token )
-      longjmp( parser->env, PR_UNEXPECTED_TOKEN );
-
-   match_any( parser );
+  if ( parser->lexer.token != token )
+  {
+    longjmp( parser->env, PR_UNEXPECTED_TOKEN );
+  }
+  
+  match_any( parser );
 }
 
 static void match_tag( pr_state_t* parser, const char* tag )
 {
-   if ( parser->lexer.token != LX_TAG || strncmp( parser->lexer.start, tag, strlen( tag ) ) )
-      longjmp( parser->env, PR_UNEXPECTED_TOKEN );
-
-   match_any( parser );
+  if ( parser->lexer.token != LX_TAG || strncmp( parser->lexer.lexeme.str, tag, strlen( tag ) ) )
+  {
+    longjmp( parser->env, PR_UNEXPECTED_TOKEN );
+  }
+  
+  match_any( parser );
 }
 
-static void parse_value( pr_state_t* parser, const char* key, unsigned keylen, pr_node_t* node, int isrom )
+static int cmp_keys( const pr_key_t* key1, const pr_key_t* key2 )
 {
-   unsigned i;
-
-   if ( isrom && keylen == 4 && !strncmp( key, "name", 4 ) )
-   {
-      key = "rom_name";
-      keylen = 8;
-   }
-
-   for ( i = 0; i < node->count; i++ )
-   {
-      if ( keylen == node->pairs[ i ].key_len && !strncmp( key, node->pairs[ i ].key, keylen ) )
-         break;
-   }
-
-   if ( i == node->count )
-      node->count++;
-
-   node->pairs[ i ].key = key;
-   node->pairs[ i ].key_len = keylen;
-
-   node->pairs[ i ].value = parser->lexer.start;
-   node->pairs[ i ].value_len = parser->lexer.len;
-
-   if ( parser->lexer.token == LX_STRING || parser->lexer.token == LX_NUMBER || parser->lexer.token == LX_TAG )
-      match_any( parser );
-   else
-      longjmp( parser->env, PR_UNEXPECTED_TOKEN );
+  if ( !key1 && !key2 )
+  {
+    return 1;
+  }
+  
+  if ( ( key1 && !key2 ) || ( !key1 && key2 ) )
+  {
+    return 0;
+  }
+  
+  if ( key1->key.len != key2->key.len || strncmp( key1->key.str, key2->key.str, key1->key.len ) )
+  {
+    return 0;
+  }
+  
+  return cmp_keys( key1->prev, key2->prev );
 }
 
-static void parse_map( pr_state_t* parser, int skip, int isrom )
+static pr_pair_t* find_key( pr_node_t* node, const pr_key_t* key )
 {
-   pr_node_t   dummy;
-   pr_node_t*  node;
+  int i;
+  
+  for ( i = 0; i < node->count; i++ )
+  {
+    const pr_key_t* other = &node->pairs[ i ].key;
+    
+    if ( cmp_keys( key, other ) )
+    {
+      return node->pairs + i;
+    }
+  }
+  
+  return node->pairs + node->count++;
+}
 
-   if ( skip )
-   {
-      node = &dummy;
-      dummy.count = 0;
-   }
-   else
-      node = parser->node;
+static void parse_value( pr_state_t* parser, const pr_key_t* key, pr_node_t* node )
+{
+  pr_pair_t* pair = find_key( node, key );
+  int token = parser->lexer.token;
+  
+  pair->key = *key;
+  pair->value = parser->lexer.lexeme;
+  
+  if ( token == LX_STRING || token == LX_NUMBER || token == LX_VERSION || token == LX_TAG )
+  {
+    match_any( parser );
+  }
+  else
+  {
+    longjmp( parser->env, PR_UNEXPECTED_TOKEN );
+  }
+}
 
-   match( parser, LX_LPAREN );
-
-   while ( parser->lexer.token != LX_RPAREN )
-   {
-      unsigned    hash;
-      const char* key;
-      unsigned    keylen;
-
-      if ( parser->lexer.token != LX_TAG )
-         longjmp( parser->env, PR_UNEXPECTED_TOKEN );
-
-      key = parser->lexer.start;
-      keylen = parser->lexer.len;
-
-      hash = djb2_calculate(key);
-
-      match_any( parser );
-
-      switch ( hash )
-      {
-         case 0x0b88a693U: /* rom */
-            parse_map( parser, skip, 1 );
-            break;
-
-         default:
-            parse_value( parser, key, keylen, node, isrom );
-            break;
-      }
-   }
-
-   match_any( parser );
+static void parse_map( pr_state_t* parser, const pr_key_t* prev, int skip )
+{
+  pr_node_t  dummy;
+  pr_node_t* node;
+  pr_key_t   key;
+  
+  if ( skip )
+  {
+    dummy.count = 0;
+    node = &dummy;
+  }
+  else
+  {
+    node = parser->node;
+  }
+  
+  match( parser, LX_LPAREN );
+  
+  while ( parser->lexer.token != LX_RPAREN )
+  {
+    key.key = parser->lexer.lexeme;
+    key.prev = prev;
+    match( parser, LX_TAG );
+    
+    if ( parser->lexer.token == LX_LPAREN )
+    {
+      parse_map( parser, &key, skip );
+    }
+    else
+    {
+      parse_value( parser, &key, node );
+    }
+  }
+  
+  match_any( parser );
 }
 
 static void parse_clrmamepro( pr_state_t* parser )
 {
-   match_tag( parser, "clrmamepro" );
-   parse_map( parser, 1, 0 );
+  static const pr_key_t clrmamepro = { { "clrmamepro", 10 }, NULL };
+  
+  match_tag( parser, clrmamepro.key.str );
+  parse_map( parser, &clrmamepro, 1 );
 }
 
 static void parse_game( pr_state_t* parser )
 {
-   match_tag( parser, "game" );
-
-   pr_node_t* node = (pr_node_t*)malloc( sizeof( pr_node_t ) );
-
-   if ( node == NULL )
-      longjmp( parser->env, PR_OUT_OF_MEMORY );
-
-   node->count   = 0;
-   parser->node  = node;
-   *parser->prev = node;
-   parser->prev  = &node->next;
-   parse_map( parser, 0, 0 );
+  static const pr_key_t game = { { "game", 4 }, NULL };
+  
+  match_tag( parser, game.key.str );
+  
+  pr_node_t* node = (pr_node_t*)malloc( sizeof( pr_node_t ) );
+  
+  if ( node == NULL )
+  {
+    longjmp( parser->env, PR_OUT_OF_MEMORY );
+  }
+  
+  node->count = 0;
+  parser->node = node;
+  *parser->prev = node;
+  parser->prev = &node->next;
+  parse_map( parser, NULL, 0 );
 }
 
 void pr_new( pr_state_t* parser, const char* source, unsigned srclen )
 {
-   lx_new( &parser->lexer, source, srclen );
-   parser->prev  = &parser->first;
+  lx_new( &parser->lexer, source, srclen );
+  parser->prev = &parser->first;
 }
 
 int pr_parse( pr_state_t* parser )
 {
-   int res;
-
-   if ( ( res = setjmp( parser->env ) ) == 0 )
-   {
-      match_any( parser );
-      parse_clrmamepro( parser );
-
-      while ( parser->lexer.token != LX_EOF )
-         parse_game( parser );
-   }
-
-   *parser->prev = NULL;
-   return res;
+  int res;
+  
+  if ( ( res = setjmp( parser->env ) ) == 0 )
+  {
+    match_any( parser );
+    parse_clrmamepro( parser );
+    
+    while ( parser->lexer.token != LX_EOF )
+    {
+      parse_game( parser );
+    }
+  }
+  
+  *parser->prev = NULL;
+  return res;
 }
+
