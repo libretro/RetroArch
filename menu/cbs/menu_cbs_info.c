e1a1f961bef6b850e5b8802974c8f008d2bc1a13
twinaphex <libretro@gmail.com>
2015-10-17 18:09:05 +0200

    (menu_cbs_info.c) Don't use menu_list_get_ptr

--- menu/cbs/menu_cbs_info.c
+++ menu/cbs/menu_cbs_info.c
@@ -29,14 +29,12 @@ static int action_info_default(unsigned type, const char *label)
 {
    size_t selection             = 0;
    menu_displaylist_info_t info = {0};
-   menu_list_t *menu_list       = menu_list_get_ptr();
+   file_list_t *menu_stack      = menu_entries_get_menu_stack_ptr();
 
-   if (!menu_list)
-      return 0;
    if (!menu_navigation_ctl(MENU_NAVIGATION_CTL_GET_SELECTION, &selection))
       return 0;
 
-   info.list          = menu_list->menu_stack;
+   info.list          = menu_stack;
    info.directory_ptr = selection;
    strlcpy(info.label,
          menu_hash_to_str(MENU_LABEL_INFO_SCREEN),
