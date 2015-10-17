b7098c0846e94e0af11d9990bc792a0f461a5028
aliaspider <aliaspider@gmail.com>
2015-10-17 09:56:23 +0100

    (griffin) build fix for !defined(HAVE_CHEEVOS)

--- griffin/griffin.c
+++ griffin/griffin.c
@@ -79,13 +79,15 @@ CHEATS
 /*============================================================
 ACHIEVEMENTS
 ============================================================ */
-#if defined(HAVE_CHEEVOS) && !defined(HAVE_NETPLAY)
+#if defined(HAVE_CHEEVOS)
+#if !defined(HAVE_NETPLAY)
 #include "../libretro-common/net/net_http.c"
 #endif
 
 #include "../libretro-common/formats/json/jsonsax.c"
 #include "../libretro-common/utils/md5.c"
 #include "../cheevos.c"
+#endif
 
 /*============================================================
 UI COMMON CONTEXT
