83cf945016666f818364a059a2359e7ea0305306
twinaphex <libretro@gmail.com>
2015-10-26 06:41:22 +0100

    Add DIR_LIST_COLLECTIONS to dir_list_new_special

--- dir_list_special.c
+++ dir_list_special.c
@@ -44,6 +44,10 @@ struct string_list *dir_list_new_special(const char *input_dir, enum dir_list_ty
          dir  = settings->video.shader_dir;
          exts = "cg|cgp|glsl|glslp";
          break;
+      case DIR_LIST_COLLECTIONS:
+         dir  = settings->playlist_directory;
+         exts = "lpl";
+         break;
       case DIR_LIST_DATABASES:
          dir  = settings->content_database;
          exts = "rdb";
