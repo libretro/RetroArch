f59df8b9df56886ab991540ae2d796f3f3f2e452
twinaphex <libretro@gmail.com>
2015-11-03 11:32:32 +0100

    Revert "(XMB) Fix a display bug on 16:9 displays caused by an optimization"
    This reverts commit a467af48cfea20e7b046a8a78d01280f4d259c9c.

--- menu/drivers/xmb.c
+++ menu/drivers/xmb.c
@@ -559,7 +559,7 @@ static void xmb_selection_pointer_changed(bool allow_animations)
 
    end       = menu_entries_get_end();
    tag       = (uintptr_t)menu_list;
-   threshold = xmb->icon.size * 16;
+   threshold = xmb->icon.size*10;
 
    video_driver_get_size(NULL, &height);
 
@@ -614,7 +614,7 @@ static void xmb_list_open_old(xmb_handle_t *xmb,
       file_list_t *list, int dir, size_t current)
 {
    unsigned i, height = 0;
-   int        threshold = xmb->icon.size * 16;
+   int        threshold = xmb->icon.size * 10;
    size_t           end = 0;
 
    end = file_list_get_size(list);
