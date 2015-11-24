libretro/RetroArch

commit c342bb437ab7db2c70936e7d953579a9643a059c

Fix warnings

tasks/task_overlay.c
@@ -622,16 +622,19 @@ static void rarch_task_overlay_handler(rarch_task_t *task)
          rarch_task_overlay_resolve_iterate(loader);
          break;
       case OVERLAY_STATUS_DEFERRED_DONE:
-//         input_overlay_new_done();
-//         break;
+#if 0
+         input_overlay_new_done();
+         break;
+#endif
       case OVERLAY_STATUS_DEFERRED_ERROR:
-//         input_overlay_free();
-//         break;
-
+#if 0
+         input_overlay_free();
+         break;
+#endif
+         break;
       default:
       case OVERLAY_STATUS_NONE:
          goto task_finished;
-         break;
    }
 
    return;

tasks/task_database.c
@@ -532,7 +532,6 @@ static void rarch_dbscan_task_handler(rarch_task_t *task)
       case DATABASE_STATUS_FREE:
       case DATABASE_STATUS_NONE:
          goto task_finished;
-         break;
    }
 
    return;

tasks/task_http.c
@@ -153,7 +153,6 @@ static void rarch_task_http_transfer_handler(rarch_task_t *task)
          break;
       case HTTP_STATUS_TRANSFER_PARSE:
          goto task_finished;
-         break;
       case HTTP_STATUS_TRANSFER:
          if (!rarch_main_data_http_iterate_transfer(http))
             http->status = HTTP_STATUS_TRANSFER_PARSE;

commit 6e43bd4893b83efe415ba9fe1a7992069b46e6ec

(task_overlay) Fix bug introduced by c342bb4

tasks/task_overlay.c
@@ -621,17 +621,9 @@ static void rarch_task_overlay_handler(rarch_task_t *task)
       case OVERLAY_STATUS_DEFERRED_LOADING_RESOLVE:
          rarch_task_overlay_resolve_iterate(loader);
          break;
-      case OVERLAY_STATUS_DEFERRED_DONE:
-#if 0
-         input_overlay_new_done();
-         break;
-#endif
       case OVERLAY_STATUS_DEFERRED_ERROR:
-#if 0
-         input_overlay_free();
-         break;
-#endif
-         break;
+         task->error = strdup("Failed to load the overlay.");
+      case OVERLAY_STATUS_DEFERRED_DONE:
       default:
       case OVERLAY_STATUS_NONE:
          goto task_finished;