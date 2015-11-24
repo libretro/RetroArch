38579e56ff023ab4857a9cf80c97811de10a3b6c
aliaspider <aliaspider@gmail.com>
2015-11-24 11:25:28 +0100

    (3DS) gfx driver: add missing d-cache flush.

--- gfx/drivers/ctr_gfx.c
+++ gfx/drivers/ctr_gfx.c
@@ -540,7 +540,9 @@ static bool ctr_frame(void* data, const void* frame,
          && (pitch > 0x40))
       {
          /* can copy the buffer directly with the GPU */
-         ctrGuCopyImage(false, frame, pitch / (ctr->rgb32? 4: 2), height, ctr->rgb32 ? CTRGU_RGBA8: CTRGU_RGB565, false,
+//         GSPGPU_FlushDataCache(frame, pitch * height);
+         ctrGuSetCommandList_First(true,frame, pitch * height,0,0,0,0);
+         ctrGuCopyImage(true, frame, pitch / (ctr->rgb32? 4: 2), height, ctr->rgb32 ? CTRGU_RGBA8: CTRGU_RGB565, false,
                         ctr->texture_swizzled, ctr->texture_width, ctr->rgb32 ? CTRGU_RGBA8: CTRGU_RGB565,  true);
       }
       else
