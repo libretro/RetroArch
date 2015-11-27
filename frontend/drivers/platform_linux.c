99ca95a1000b5fc8f2b5d7b63bec5bcbace90a75
twinaphex <libretro@gmail.com>
2015-11-28 00:16:31 +0100

    (Android) Buildfix

--- frontend/drivers/platform_linux.c
+++ frontend/drivers/platform_linux.c
@@ -733,12 +733,16 @@ static void android_app_entry(void *data)
 #ifndef HAVE_MAIN
    do
    {
+      bool sleeping     = false;
       unsigned sleep_ms = 0;
       ret = rarch_main_iterate(&sleep_ms);
 
       if (ret == 1 && sleep_ms > 0)
+      {
+         sleeping = true;
          retro_sleep(sleep_ms);
-      rarch_main_data_iterate();
+      }
+      rarch_main_data_iterate(sleeping);
    }while (ret != -1);
 
    main_exit(data);
