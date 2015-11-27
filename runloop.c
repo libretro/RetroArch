libretro/RetroArch

commit bcb54b2eec135ae9c318eb313f2a010b1dfa7aae

(rarch_main_iterate) Another refactor

runloop.c
@@ -993,7 +993,6 @@ int rarch_main_iterate(void)
    retro_input_t trigger_input;
    event_cmd_state_t    cmd;
    bool do_quit                    = false;
-   int ret                         = 0;
    static retro_input_t last_input = 0;
    driver_t *driver                = driver_get_ptr();
    settings_t *settings            = config_get_ptr();
@@ -1044,7 +1043,7 @@ int rarch_main_iterate(void)
             rarch_main_set_state(RARCH_ACTION_STATE_MENU_RUNNING_FINISHED);
 
       if (!input && settings->menu.pause_libretro)
-        ret = 1;
+        return 1;
       goto success;
    }
 #endif
@@ -1146,5 +1145,5 @@ success:
    if (settings->fastforward_ratio_throttle_enable)
       rarch_limit_frame_time(settings->fastforward_ratio);
 
-   return ret;
+   return 0;
 }

commit 63d977d674372e6c7cf5fef42e1291cc40b1b5fb

(Menu) Fixes #2479 that was caused by bcb54b2eec135ae9c318eb313f2a010b1dfa7aae and resulted in 2000fps on some nvidia cards

runloop.c
@@ -938,7 +938,7 @@ int rarch_main_iterate(unsigned *sleep_ms)
       }
 
       if (!input && settings->menu.pause_libretro)
-         return 1;
+         ret = 1;
       goto end;
    }
 #endif