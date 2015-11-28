d810ba607b75d218352cb279e833592abbff2a69
twinaphex <libretro@gmail.com>
2015-11-28 21:55:46 +0100

    Get rid of extern "C" for WndProc

--- gfx/common/win32_common.cpp
+++ gfx/common/win32_common.cpp
@@ -172,10 +172,6 @@ static const char *win32_video_get_ident(void)
    return video_driver_get_ident();
 }
 
-#ifndef _MSC_VER
-extern "C" {
-#endif
-
 LRESULT CALLBACK WndProcD3D(HWND hwnd, UINT message,
       WPARAM wparam, LPARAM lparam)
 {
@@ -300,10 +296,6 @@ LRESULT CALLBACK WndProcGL(HWND hwnd, UINT message,
       return 0;
    return DefWindowProc(hwnd, message, wparam, lparam);
 }
-
-#ifndef _MSC_VER
-}
-#endif
 
 bool win32_window_create(void *data, unsigned style,
       RECT *mon_rect, unsigned width,
