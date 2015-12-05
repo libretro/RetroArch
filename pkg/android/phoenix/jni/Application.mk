d62839516e9e89d16f8237be5030121ef3289bbd
Higor Eurípedes <heuripedes@gmail.com>
2015-12-05 09:59:03 -0300

    (phoenix) Add optional clang sanitizer support
    Set the SANITIZER=address environment variable. Debug mode isn't
    required but helps.

--- pkg/android/phoenix/jni/Application.mk
+++ pkg/android/phoenix/jni/Application.mk
@@ -17,3 +17,24 @@ ifndef TARGET_ABIS
 else
    APP_ABI := $(TARGET_ABIS)
 endif
+
+# Run $NDK/toolchains/llvm-3.6/prebuilt/linux-x86_64/bin/asan_device_setup once
+# for each AVD you use, replacing llvm-3.6 with the latest you have and
+# linux-x86_64 with your host ABI.
+ifneq ($(SANITIZER),)
+   # AddressSanitizer doesn't run on mips
+   ifndef TARGET_ABIS
+      APP_ABI   := armeabi-v7a x86
+   else
+      ifneq ($(findstring mips,$(TARGET_ABIS)),)
+         $(error "AddressSanitizer does not support mips.")
+      endif
+   endif
+   USE_CLANG := 1
+endif
+
+ifeq ($(USE_CLANG),1)
+   NDK_TOOLCHAIN_VERSION := clang
+   APP_CFLAGS   := -Wno-invalid-source-encoding
+   APP_CPPFLAGS := -Wno-invalid-source-encoding
+endif
