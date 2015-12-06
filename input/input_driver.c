015986fea4e5b5e10ca256af25297e3296107dbc
twinaphex <libretro@gmail.com>
2015-12-06 17:19:12 +0100

    RARCH_INPUT_CTL - Make sure all static local variables are set back to default state

--- input/input_driver.c
+++ input/input_driver.c
@@ -716,11 +716,14 @@ bool input_driver_ctl(enum rarch_input_ctl_state state, void *data)
          current_input_data = NULL;
          break;
       case RARCH_INPUT_CTL_DESTROY:
-         input_driver_flushing_input = false;
-         input_driver_block_hotkey   = false;
-         input_driver_nonblock_state = false;
+         input_driver_block_hotkey             = false;
+         input_driver_block_libretro_input     = false;
+         input_driver_keyboard_linefeed_enable = false;
+         input_driver_nonblock_state           = false;
+         input_driver_flushing_input           = false;
+         input_driver_data_own                 = false;
          memset(&input_driver_turbo_btns, 0, sizeof(turbo_buttons_t));
-         current_input      = NULL;
+         current_input                         = NULL;
          return true;
       case RARCH_INPUT_CTL_GRAB_STDIN:
          if (current_input->grab_stdin)
