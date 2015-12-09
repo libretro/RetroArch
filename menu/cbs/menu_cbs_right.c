86c68482817820d6118d51e60fa99d8fc4f4a513
twinaphex <libretro@gmail.com>
2015-12-09 09:31:22 +0100

    Fix CORE_OPTION_NEXT

--- menu/cbs/menu_cbs_right.c
+++ menu/cbs/menu_cbs_right.c
@@ -354,7 +354,7 @@ int core_setting_right(unsigned type, const char *label,
 {
    unsigned idx     = type - MENU_SETTINGS_CORE_OPTION_START;
 
-   runloop_ctl(RUNLOOP_CTL_CORE_OPTION_NEXT, NULL);
+   runloop_ctl(RUNLOOP_CTL_CORE_OPTION_NEXT, &idx);
 
    return 0;
 }
