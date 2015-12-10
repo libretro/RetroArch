42643088f4604ab4124e20cb4ed6d27fcf599a05
twinaphex <libretro@gmail.com>
2015-12-10 17:25:40 +0100

    Refine earlier commit

--- menu/menu_driver.c
+++ menu/menu_driver.c
@@ -368,11 +368,8 @@ void menu_driver_navigation_clear(bool pending_push)
 {
    const menu_ctx_driver_t *driver = menu_ctx_driver_get_ptr();
 
-   if (!menu_driver_data)
-      return;
-
    if (driver->navigation_clear)
-      driver->navigation_clear(menu_driver_data->userdata, pending_push);
+      driver->navigation_clear(menu_driver_data ? menu_driver_data->userdata : NULL, pending_push);
 }
 
 static void menu_environment_get(int *argc, char *argv[],
