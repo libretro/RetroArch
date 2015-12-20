fd644cb43449829aaad77bbe5248a9a9c1b44ab6
twinaphex <libretro@gmail.com>
2015-12-20 23:52:50 +0100

    Revert "(video_driver.c) Ensure input is initialized in init_video_input"
    This reverts commit d191e29d1b0ccb2218c34b6348755d3fc9e89c08.

--- gfx/video_driver.c
+++ gfx/video_driver.c
@@ -423,12 +423,7 @@ static void init_video_input(const input_driver_t *tmp)
 {
    const input_driver_t **input = input_get_double_ptr();
    if (*input)
-   {
-      if (!input_driver_get_data() && !input_driver_ctl(RARCH_INPUT_CTL_INIT, NULL))
-         goto error;
-
       return;
-   }
 
    /* Reset video frame count */
    video_driver_frame_count = 0;
