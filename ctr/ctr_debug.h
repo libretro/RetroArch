fdad936fab4018f6fec26f2c8434722e0e7b1da6
aliaspider <aliaspider@gmail.com>
2015-12-24 23:16:40 +0100

    (3DS) update makefile.

--- ctr/ctr_debug.h
+++ ctr/ctr_debug.h
@@ -16,6 +16,7 @@ void dump_result_value(Result val);
 #define DEBUG_LINE() do{printf("%s:%d.\n",__FUNCTION__, __LINE__);fflush(stdout);}while(0)
 #define DEBUG_STR(X) printf( "%s: %s\n", #X, (char*)(X))
 #define DEBUG_VAR(X) printf( "%-20s: 0x%08X\n", #X, (u32)(X))
+#define DEBUG_INT(X) printf( "%-20s: %10i\n", #X, (s32)(X))
 #define DEBUG_VAR64(X) printf( #X"\r\t\t\t\t : 0x%016llX\n", (u64)(X))
 #define DEBUG_ERROR(X) do{if(X)dump_result_value(X)}while(0)
 #define PRINTFPOS(X,Y) "\x1b["#X";"#Y"H"
