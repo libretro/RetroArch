f76b7f2b609204b4a25e9e4369c0ff05657b8dc5
twinaphex <libretro@gmail.com>
2016-01-09 03:00:33 +0100

    Don't attempt to use std::string anymore in gfx/d3d/d3d.cpp

--- gfx/d3d/d3d.h
+++ gfx/d3d/d3d.h
@@ -17,8 +17,9 @@
 #ifndef __D3DVIDEO_INTF_H__
 #define __D3DVIDEO_INTF_H__
 
-#include <string>
 #include <vector>
+
+#include <retro_miscellaneous.h>
 
 #ifdef HAVE_CONFIG_H
 #include "../../config.h"
@@ -99,7 +100,7 @@ typedef struct d3d_video
    unsigned dev_rotation;
    D3DVIEWPORT final_viewport;
 
-   std::string shader_path;
+   char shader_path[PATH_MAX_LENGTH];
 
    struct video_shader shader;
    video_info_t video_info;
