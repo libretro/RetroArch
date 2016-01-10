fdb5f10c71ff5135e4b50569d3c2f1e4a6dbf079
twinaphex <libretro@gmail.com>
2016-01-10 01:48:41 +0100

    Check for assets.zip inside bundle

--- frontend/drivers/platform_darwin.m
+++ frontend/drivers/platform_darwin.m
@@ -382,6 +382,10 @@ static void frontend_darwin_get_environment_settings(int *argc, char *argv[],
     get_ios_version(&major, &minor);
     if (major > 8)
        strlcpy(g_defaults.path.buildbot_server_url, "http://buildbot.libretro.com/nightly/apple/ios9/latest/", sizeof(g_defaults.path.buildbot_server_url));
+
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"assets" ofType:@"zip"];
+    NSURL    *url  = [NSURL fileURLWithPath:path];
+    NSLog(@"url from bundle: %@", url);
 #endif
 
    CFTemporaryDirectory(temp_dir, sizeof(temp_dir));
