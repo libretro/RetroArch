0696e0ca468e488409d12bf336d4f5cdd4a6ab59
twinaphex <libretro@gmail.com>
2016-01-14 07:14:27 +0100

    Change title

--- runloop.c
+++ runloop.c
@@ -505,7 +505,7 @@ bool runloop_ctl(enum runloop_ctl_state state, void *data)
 #ifndef RARCH_CONSOLE
          strlcpy(runloop_system.title_buf, 
                msg_hash_to_str(MSG_PROGRAM), sizeof(runloop_system.title_buf));
-         strlcat(runloop_system.title_buf, " : ", sizeof(runloop_system.title_buf));
+         strlcat(runloop_system.title_buf, " ", sizeof(runloop_system.title_buf));
 #endif
          strlcat(runloop_system.title_buf, runloop_system.info.library_name, sizeof(runloop_system.title_buf));
          strlcat(runloop_system.title_buf, " ", sizeof(runloop_system.title_buf));
