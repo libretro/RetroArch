212337c7c158c7778d91c4b8fb98b144b8e111d7
pponso1 <pponso1@gmail.com>
2016-01-16 11:57:00 -0600

    iOS audio support

--- gfx/drivers_context/cocoa_gl_ctx.m
+++ gfx/drivers_context/cocoa_gl_ctx.m
@@ -26,6 +26,7 @@
 #include <AppKit/NSOpenGL.h>
 #elif defined(HAVE_COCOATOUCH)
 #include <GLKit/GLKit.h>
+#import <AVFoundation/AVFoundation.h>
 #endif
 
 #include <retro_assert.h>
@@ -119,6 +120,10 @@ void *glkitview_init(void)
 #if defined(HAVE_COCOATOUCH)
 void cocoagl_bind_game_view_fbo(void)
 {
+   // implicitly initializes your audio session
+   [AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+   [audioSession setCategory: AVAudioSessionCategoryAmbient error: nil];
+   [audioSession setActive:YES error:nil];
    if (g_context)
       [g_view bindDrawable];
 }
