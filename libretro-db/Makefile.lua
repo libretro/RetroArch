8527742b73a5e87565ca80df8d34d6fd16ef1c14
twinaphex <libretro@gmail.com>
2016-01-26 09:34:43 +0100

    Create Makefile.lua

--- libretro-db/Makefile.lua
+++ libretro-db/Makefile.lua
@@ -2,7 +2,7 @@ DEBUG                = 0
 LIBRETRO_COMMON_DIR := ../libretro-common
 INCFLAGS             = -I. -I$(LIBRETRO_COMMON_DIR)/include
 
-TARGETS              = rmsgpack_test libretrodb_tool c_converter
+TARGETS              = rmsgpack_test libretrodb_tool lua_converter
 
 ifeq ($(DEBUG), 1)
 CFLAGS               = -g -O0 -Wall
@@ -10,19 +10,19 @@ else
 CFLAGS               = -g -O2 -Wall -DNDEBUG
 endif
 
-C_CONVERTER_C = \
+LUA_CONVERTER_C = \
 			 rmsgpack.c \
 			 rmsgpack_dom.c \
+			 lua/lua_common.c \
 			 libretrodb.c \
 			 bintree.c \
 			 query.c \
-			 c_converter.c \
-			 $(LIBRETRO_COMMON_DIR)/hash/rhash.c \
+			 lua/lua_converter.c \
 			 $(LIBRETRO_COMMON_DIR)/compat/compat_fnmatch.c \
 			 $(LIBRETRO_COMMON_DIR)/file/retro_file.c \
 			 $(LIBRETRO_COMMON_DIR)/compat/compat_strl.c
 
-C_CONVERTER_OBJS := $(C_CONVERTER_C:.c=.o)
+LUA_CONVERTER_OBJS := $(LUA_CONVERTER_C:.c=.o)
 
 RARCHDB_TOOL_C = \
 			 rmsgpack.c \
@@ -37,6 +37,19 @@ RARCHDB_TOOL_C = \
 
 RARCHDB_TOOL_OBJS := $(RARCHDB_TOOL_C:.c=.o)
 
+TESTLIB_C = \
+			 lua/testlib.c \
+			 query.c \
+			 libretrodb.c \
+			 bintree.c \
+			 rmsgpack.c \
+			 rmsgpack_dom.c \
+			 $(LIBRETRO_COMMON_DIR)/compat/compat_fnmatch.c \
+			 $(LIBRETRO_COMMON_DIR)/file/retro_file.c \
+			 $(LIBRETRO_COMMON_DIR)/compat/compat_strl.c
+
+TESTLIB_OBJS := $(TESTLIB_C:.c=.o)
+
 RMSGPACK_C = \
 			rmsgpack.c \
 			rmsgpack_test.c \
@@ -44,17 +57,18 @@ RMSGPACK_C = \
 
 RMSGPACK_OBJS := $(RMSGPACK_C:.c=.o)
 
-TESTLIB_FLAGS = $(CFLAGS) -shared -fpic
+LUA_FLAGS     = `pkg-config lua --libs`
+TESTLIB_FLAGS = $(CFLAGS) $(LUA_FLAGS) -shared -fpic
 
-.PHONY: all clean
+.PHONY: all clean check
 
 all: $(TARGETS)
 
 %.o: %.c
 	$(CC) $(INCFLAGS) $< -c $(CFLAGS) -o $@
 
-c_converter: $(C_CONVERTER_OBJS)
-	$(CC) $(INCFLAGS) $(C_CONVERTER_OBJS) $(CFLAGS) -o $@
+lua_converter: $(LUA_CONVERTER_OBJS)
+	$(CC) $(INCFLAGS) $(LUA_CONVERTER_OBJS) $(LUA_FLAGS) -o $@
 
 libretrodb_tool: $(RARCHDB_TOOL_OBJS)
 	$(CC) $(INCFLAGS) $(RARCHDB_TOOL_OBJS) -o $@
@@ -66,5 +80,8 @@ testlib.so: CFLAGS += -fPIC
 testlib.so: clean $(TESTLIB_OBJS)
 	$(CC) $(INCFLAGS) $(TESTLIB_FLAGS) $(TESTLIB_OBJS) -o $@
 
+check: testlib.so lua/tests.lua
+	lua ./lua/tests.lua
+
 clean:
-	rm -rf $(TARGETS) $(C_CONVERTER_OBJS) $(RARCHDB_TOOL_OBJS) $(RMSGPACK_OBJS) $(TESTLIB_OBJS) testlib.so
+	rm -rf $(TARGETS) $(LUA_CONVERTER_OBJS) $(C_CONVERTER_OBJS) $(RARCHDB_TOOL_OBJS) $(RMSGPACK_OBJS) $(TESTLIB_OBJS) testlib.so
