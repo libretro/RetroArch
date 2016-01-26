f6ca836e251593d6d3be9ed0cb48acf00c9e35d9
twinaphex <libretro@gmail.com>
2016-01-26 05:07:37 +0100

    Cleanups - respect 80-char width

--- dynamic.c
+++ dynamic.c
@@ -266,15 +266,18 @@ static void load_dynamic_core(void)
    {
       /* Try to verify that -lretro was not linked in from other modules
        * since loading it dynamically and with -l will fail hard. */
-      RARCH_ERR("Serious problem. RetroArch wants to load libretro cores dyamically, but it is already linked.\n");
-      RARCH_ERR("This could happen if other modules RetroArch depends on link against libretro directly.\n");
+      RARCH_ERR("Serious problem. RetroArch wants to load libretro cores"
+            "dyamically, but it is already linked.\n");
+      RARCH_ERR("This could happen if other modules RetroArch depends on "
+            "link against libretro directly.\n");
       RARCH_ERR("Proceeding could cause a crash. Aborting ...\n");
       retro_fail(1, "init_libretro_sym()");
    }
 
    if (!*settings->libretro)
    {
-      RARCH_ERR("RetroArch is built for dynamic libretro cores, but libretro_path is not set. Cannot continue.\n");
+      RARCH_ERR("RetroArch is built for dynamic libretro cores, but "
+            "libretro_path is not set. Cannot continue.\n");
       retro_fail(1, "init_libretro_sym()");
    }
 
