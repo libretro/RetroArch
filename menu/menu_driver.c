4f94e2b5d88a9761ea5fd1e2f786f1110b64f081
twinaphex <libretro@gmail.com>
2016-01-29 09:42:15 +0100

    (Android) Fix APK assets extraction

--- menu/menu_driver.c
+++ menu/menu_driver.c
@@ -245,7 +245,7 @@ static void *menu_init(const void *data)
       menu->push_help_screen           = true;
 #ifdef HAVE_ZLIB
       rarch_task_push_decompress(settings->bundle_assets_src_path, settings->bundle_assets_dst_path,
-         settings->bundle_assets_dst_path_subdir, NULL, NULL, bundle_decompressed, NULL);
+         NULL, settings->bundle_assets_dst_path_subdir, NULL, bundle_decompressed, NULL);
 #endif
    }
 
