05d982783012fd689a283d1a1f9aab9ea4024eab
twinaphex <libretro@gmail.com>
2016-02-06 21:33:45 +0100

    Revert "Get rid of SL_FLAG_ALLOW_EMPTY_LIST"
    This reverts commit bb0b8764c5fce1d385abd315532fe3c4d9550e2c.

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -2261,9 +2261,12 @@ static int menu_displaylist_parse_generic(menu_displaylist_info_t *info, bool ho
 
    if (list_size == 0)
    {
-      menu_entries_push(info->list,
-            menu_hash_to_str(MENU_LABEL_VALUE_NO_ITEMS),
-            "", MENU_SETTING_NO_ITEM, 0, 0);
+      if (!(info->flags & SL_FLAG_ALLOW_EMPTY_LIST))
+      {
+         menu_entries_push(info->list,
+               menu_hash_to_str(MENU_LABEL_VALUE_NO_ITEMS),
+               "", MENU_SETTING_NO_ITEM, 0, 0);
+      }
 
       string_list_free(str_list);
 
@@ -2395,9 +2398,12 @@ static int menu_displaylist_parse_generic(menu_displaylist_info_t *info, bool ho
       case MENU_LABEL_CORE_LIST:
          if (items_found == 0)
          {
-            menu_entries_push(info->list,
-                  menu_hash_to_str(MENU_LABEL_VALUE_NO_ITEMS),
-                  "", MENU_SETTING_NO_ITEM, 0, 0);
+            if (!(info->flags & SL_FLAG_ALLOW_EMPTY_LIST))
+            {
+               menu_entries_push(info->list,
+                     menu_hash_to_str(MENU_LABEL_VALUE_NO_ITEMS),
+                     "", MENU_SETTING_NO_ITEM, 0, 0);
+            }
 
             return 0;
          }
--- menu/menu_setting.h
+++ menu/menu_setting.h
@@ -95,7 +95,8 @@ enum setting_list_flags
    SL_FLAG_SETTINGS_LOGGING_OPTIONS                 =  (1 << 27),
    SL_FLAG_SETTINGS_SAVING_OPTIONS                  =  (1 << 28),
    SL_FLAG_SETTINGS_SUB_ACCOUNTS_OPTIONS            =  (1 << 29),
-   SL_FLAG_SETTINGS_ALL                             =  (1 << 30)
+   SL_FLAG_SETTINGS_ALL                             =  (1 << 30),
+   SL_FLAG_ALLOW_EMPTY_LIST                         =  (1 << 31)
 };
 
 typedef struct rarch_setting_group_info rarch_setting_group_info_t;
