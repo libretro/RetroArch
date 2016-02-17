207c87e551953539eaf1c65dd5727bea6f92a8f7
twinaphex <libretro@gmail.com>
2016-02-17 07:23:32 +0100

    Add glslang files to griffin_cpp

--- griffin/griffin_cpp.cpp
+++ griffin/griffin_cpp.cpp
@@ -25,6 +25,52 @@
 #include <compat/posix_string.h>
 #endif
 
+#ifdef HAVE_VULKAN
+#include "../deps/glslang/glslang.cpp"
+#include "../deps/glslang/glslang_tab.cpp"
+#include "../deps/glslang/glslang/SPIRV/SpvBuilder.cpp"
+#include "../deps/glslang/glslang/SPIRV/SPVRemapper.cpp"
+#include "../deps/glslang/glslang/SPIRV/InReadableOrder.cpp"
+#include "../deps/glslang/glslang/SPIRV/doc.cpp"
+#include "../deps/glslang/glslang/SPIRV/GlslangToSpv.cpp"
+#include "../deps/glslang/glslang/SPIRV/disassemble.cpp"
+#include "../deps/glslang/glslang/glslang/GenericCodeGen/Link.cpp"
+#include "../deps/glslang/glslang/glslang/GenericCodeGen/CodeGen.cpp"
+#include "../deps/glslang/glslang/OGLCompilersDLL/InitializeDll.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/Intermediate.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/Versions.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/RemoveTree.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/limits.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/intermOut.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/Initialize.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/SymbolTable.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/parseConst.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/ParseHelper.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/ShaderLang.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/IntermTraverse.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/InfoSink.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/Constant.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/Scan.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/reflection.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/linkValidate.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/PoolAlloc.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/preprocessor/PpAtom.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/preprocessor/PpContext.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/preprocessor/PpMemory.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/preprocessor/PpTokens.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/preprocessor/PpScanner.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/preprocessor/PpSymbols.cpp"
+#include "../deps/glslang/glslang/glslang/MachineIndependent/preprocessor/Pp.cpp"
+
+#ifdef _WIN32
+#include "../deps/glslang/glslang/glslang/OSDependent/Windows/ossource.cpp"
+#endif
+
+#if defined(__linux__) && !defined(ANDROID)
+#include "../deps/glslang/glslang/glslang/OSDependent/Unix/ossource.cpp"
+#endif
+#endif
+
 /*============================================================
 AUDIO
 ============================================================ */
