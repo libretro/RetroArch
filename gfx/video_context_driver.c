9bc889b908854a6fa59bb03fba52f3dfbc7665c3
twinaphex <libretro@gmail.com>
2016-02-20 05:50:37 +0100

    Add improved preprocessor conditionals for x context

--- gfx/video_context_driver.c
+++ gfx/video_context_driver.c
@@ -46,8 +46,10 @@ static const gfx_ctx_driver_t *gfx_ctx_drivers[] = {
 #if defined(_WIN32) && defined(HAVE_OPENGL)
    &gfx_ctx_wgl,
 #endif
-#if defined(HAVE_X11) && defined(HAVE_OPENGL) && !defined(HAVE_OPENGLES)
+#if defined(HAVE_X11) && !defined(HAVE_OPENGLES)
+#if defined(HAVE_OPENGL) || defined(HAVE_VULKAN)
    &gfx_ctx_x,
+#endif
 #endif
 #if defined(HAVE_WAYLAND)
    &gfx_ctx_wayland,
