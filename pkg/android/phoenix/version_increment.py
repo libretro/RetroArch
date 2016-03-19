52b6d73c5a923f3a29d5ed128a0c1d53fc166d85
radius <andres.430@gmail.com>
2016-03-19 13:18:06 -0500

    update version script

--- pkg/android/phoenix/version_increment.py
+++ pkg/android/phoenix/version_increment.py
@@ -7,8 +7,12 @@ dom1 = parse("AndroidManifest.xml")
 oldVersion = dom1.documentElement.getAttribute("android:versionCode")
 versionNumbers = oldVersion.split('.')
 
+versionName = dom1.documentElement.getAttribute("android:versionName")
+versionName = versionName + "+"
+
 versionNumbers[-1] = unicode(int(time.time()))
 dom1.documentElement.setAttribute("android:versionCode", u'.'.join(versionNumbers))
+dom1.documentElement.setAttribute("android:versionName", versionName)
 
 with open("AndroidManifest.xml", 'wb') as f:
     for line in dom1.toxml("utf-8"):
