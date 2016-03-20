0955fac1951f2ed644f1fc7c81a309ed997ae447
twinaphex <libretro@gmail.com>
2016-03-20 16:59:09 +0100

    (ffmpeg core) Avoid some deprecation warnings

--- cores/libretro-ffmpeg/ffmpeg_core.c
+++ cores/libretro-ffmpeg/ffmpeg_core.c
@@ -18,6 +18,7 @@ extern "C" {
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
 #include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
 #include <libavutil/time.h>
 #include <libavutil/opt.h>
 #include <libavdevice/avdevice.h>
@@ -1165,11 +1166,24 @@ static void decode_thread(void *data)
 
    if (video_stream >= 0)
    {
+      AVPicture *converted_frame = (AVPicture*)conv_frame;
+
+#if FF_API_AVPICTURE
+      frame_size = av_image_get_buffer_size(PIX_FMT_RGB32, media.width, media.height, 1);
+#else
       frame_size = avpicture_get_size(PIX_FMT_RGB32, media.width, media.height);
+#endif
       conv_frame = av_frame_alloc();
       conv_frame_buf = av_malloc(frame_size);
-      avpicture_fill((AVPicture*)conv_frame, (const uint8_t*)conv_frame_buf,
+#if FF_API_AVPICTURE
+      av_image_fill_arrays(converted_frame->data,
+            converted_frame->linesize,
+            (const uint8_t*)conv_frame_buf,
+            PIX_FMT_RGB32, media.width, media.height, 1);
+#else
+      avpicture_fill(converted_frame, (const uint8_t*)conv_frame_buf,
             PIX_FMT_RGB32, media.width, media.height);
+#endif
    }
 
    while (!decode_thread_dead)
@@ -1310,7 +1324,11 @@ static void decode_thread(void *data)
          avsubtitle_free(&sub);
       }
 
+#if FF_API_AVPICTURE
+      av_packet_unref(&pkt);
+#else
       av_free_packet(&pkt);
+#endif
    }
 
    if (sws)
