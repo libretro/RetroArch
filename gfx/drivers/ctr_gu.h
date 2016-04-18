0f8895bcc3801657d967897002e8533f795e34ab
aliaspider <aliaspider@gmail.com>
2016-04-18 15:22:41 +0100

    (3DS) the parallax layer register can be written to from userland with GSPGPU_WriteHWRegs.

--- gfx/drivers/ctr_gu.h
+++ gfx/drivers/ctr_gu.h
@@ -67,7 +67,13 @@ extern u32 gpuCmdBufOffset;
 extern u32 __linear_heap_size;
 extern u32 __linear_heap;
 
-Result ctr_set_parallax_layer(bool state);
+__attribute__((always_inline))
+static INLINE Result ctr_set_parallax_layer(bool state)
+{
+   u32 reg_state = state? 0x00010001: 0x0;
+   return GSPGPU_WriteHWRegs(0x202000, &reg_state, 4);
+}
+
 
 __attribute__((always_inline))
 static INLINE void ctrGuSetTexture(GPU_TEXUNIT unit, u32* data,
