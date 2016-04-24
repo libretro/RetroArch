b7e6161146cd7f1d16c05fca88aef91dd1f97e48
twinaphex <libretro@gmail.com>
2016-04-24 20:25:56 +0200

    Add old shader versions of XMB ribbon

---
+++ gfx/drivers/gl_shaders/pipeline_xmb_ribbon_simple.glsl.frag.h
@@ -0,0 +1,9 @@
+static const char *stock_fragment_xmb_simple =
+   "#ifdef GL_ES\n"
+   "precision mediump float;\n"
+   "#endif\n"
+   "uniform float time;\n"
+   "void main()\n"
+   "{\n"
+   "  gl_FragColor = vec4(1.0, 1.0, 1.0, 0.05);\n"
+   "}\n";
--- gfx/drivers_shader/shader_glsl.c
+++ gfx/drivers_shader/shader_glsl.c
@@ -124,6 +124,8 @@ static const char *glsl_prefixes[] = {
 #include "../drivers/gl_shaders/core_alpha_blend.glsl.vert.h"
 #include "../drivers/gl_shaders/core_alpha_blend.glsl.frag.h"
 
+#include "../drivers/gl_shaders/pipeline_xmb_ribbon_simple.glsl.vert.h"
+#include "../drivers/gl_shaders/pipeline_xmb_ribbon_simple.glsl.frag.h"
 #include "../drivers/gl_shaders/pipeline_xmb_ribbon.glsl.vert.h"
 #include "../drivers/gl_shaders/pipeline_xmb_ribbon.glsl.frag.h"
 
