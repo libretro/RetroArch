4372d55325a3a24248928a0ee057154cbd004f88
twinaphex <libretro@gmail.com>
2016-05-01 16:01:03 +0200

    Change ifdef to ifndef RARCH_CONSOLE

--- configuration.c
+++ configuration.c
@@ -1039,7 +1039,7 @@ static config_file_t *open_default_config_file(void)
 
       RARCH_WARN("Created new config file in: \"%s\".\n", conf_path);
    }
-#elif !defined(__CELLOS_LV2__) && !defined(_XBOX)
+#elif !defined(RARCH_CONSOLE)
    bool has_application_data = fill_pathname_application_data(application_data,
             sizeof(application_data));
 
