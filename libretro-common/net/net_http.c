8dd4333e16b5cf4e6ee745b86ac0ec9fecf29d41
twinaphex <libretro@gmail.com>
2016-05-01 14:38:10 +0200

    (net_http) Comment out debug logging

--- libretro-common/net/net_http.c
+++ libretro-common/net/net_http.c
@@ -124,6 +124,7 @@ error:
    return -1;
 }
 
+#if 0
 static void net_http_send(int fd, bool * error,
       const char * data, size_t len)
 {
@@ -147,11 +148,18 @@ static void net_http_send(int fd, bool * error,
       len  -= thislen;
    }
 }
+#endif
 
 static void net_http_send_str(int fd, bool *error, const char *text)
 {
-printf("%s",text);
+#if 0
+   printf("%s",text);
    net_http_send(fd, error, text, strlen(text));
+#else
+   (void)fd;
+   (void)error;
+   (void)text;
+#endif
 }
 
 static ssize_t net_http_recv(int fd, bool *error,
