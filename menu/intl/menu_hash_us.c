c3e9f64bc2ff8074a13831ee168c49ef72aa0dbc
twinaphex <libretro@gmail.com>
2016-05-02 04:24:26 +0200

    Rename some settings

--- menu/intl/menu_hash_us.c
+++ menu/intl/menu_hash_us.c
@@ -1069,9 +1069,9 @@ const char *menu_hash_to_str_us(uint32_t hash)
       case MENU_LABEL_VALUE_XMB_THEME:
          return "XMB Theme";
       case MENU_LABEL_VALUE_XMB_GRADIENT:
-         return "XMB Gradient";
+         return "Background Gradient";
       case MENU_LABEL_VALUE_XMB_SHADOWS_ENABLE:
-         return "XMB Shadows Enable";
+         return "Icon Shadows Enable";
       case MENU_LABEL_VALUE_XMB_RIBBON_ENABLE:
          return "Menu Shader Pipeline";
       case MENU_LABEL_VALUE_SUSPEND_SCREENSAVER_ENABLE:
