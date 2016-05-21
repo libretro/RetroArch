b7e15c2fe1b1891af2a14e639529cda994ce625d
Twinaphex <libretro@gmail.com>
2016-05-21 19:31:39 +0200

    free user_data here too

--- menu/menu_display.c
+++ menu/menu_display.c
@@ -550,6 +550,7 @@ void menu_display_handle_wallpaper_upload(void *task_data,
    menu_driver_ctl(RARCH_MENU_CTL_LOAD_IMAGE, &load_image_info);
    image_texture_free(img);
    free(img);
+   free(user_data);
 }
 
 void menu_display_allocate_white_texture(void)
