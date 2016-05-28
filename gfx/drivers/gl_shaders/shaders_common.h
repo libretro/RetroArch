2056d25e3e8acf9d84cc8c9e92d1b166d4b06606
Twinaphex <libretro@gmail.com>
2016-05-28 20:56:37 +0200

    Update GLSL_330_ES/GLSL_330_CORE

--- gfx/drivers/gl_shaders/shaders_common.h
+++ gfx/drivers/gl_shaders/shaders_common.h
@@ -4,10 +4,10 @@
 #if defined(HAVE_OPENGLES)
 #define GLSL(src) "precision mediump float;\n" #src
 #define GLSL_330_ES(src)   "#version 330 es\nprecision mediump float;\n"   #src
-#define GLSL_330_CORE(src) "#version 330 core\nprecision mediump float;\n" #src
+#define GLSL_330_CORE(src) "#version 330 es\nprecision mediump float;\n" #src
 #else
 #define GLSL(src) "" #src
-#define GLSL_330_ES(src)   "#version 330 es\n"   #src
+#define GLSL_330_ES(src)   "#version 330 core\n"   #src
 #define GLSL_330_CORE(src) "#version 330 core\n" #src
 #endif
 
