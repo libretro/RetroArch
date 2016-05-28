e331281d6aec9ca57b07c10a1543986d8ec1e695
Twinaphex <libretro@gmail.com>
2016-05-28 20:56:02 +0200

    (shaders_common.h) Fix GLES

--- gfx/drivers/gl_shaders/shaders_common.h
+++ gfx/drivers/gl_shaders/shaders_common.h
@@ -1,9 +1,14 @@
 #ifndef _SHADERS_COMMON
 #define _SHADERS_COMMON
 
+#if defined(HAVE_OPENGLES)
+#define GLSL(src) "precision mediump float;\n" #src
+#define GLSL_330_ES(src)   "#version 330 es\nprecision mediump float;\n"   #src
+#define GLSL_330_CORE(src) "#version 330 core\nprecision mediump float;\n" #src
+#else
 #define GLSL(src) "" #src
-
 #define GLSL_330_ES(src)   "#version 330 es\n"   #src
 #define GLSL_330_CORE(src) "#version 330 core\n" #src
+#endif
 
 #endif
