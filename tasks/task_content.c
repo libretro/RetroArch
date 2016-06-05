693ce8f6e5e5cf26e072bcdf007560b656b74b52
twinaphex <libretro@gmail.com>
2016-06-05 19:03:23 +0200

    (task_content.c) Fix explicit null dereferenced

--- tasks/task_content.c
+++ tasks/task_content.c
@@ -1422,7 +1422,7 @@ static bool content_file_init(struct string_list *temporary_content)
    if (!content)
       goto error;
 
-   if (!init_content_file_set_attribs(temporary_content,
+   if (special && !init_content_file_set_attribs(temporary_content,
             content, system, special))
       goto error;
 
