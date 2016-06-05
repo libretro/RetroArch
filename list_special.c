6d11f4ab9b3af9e4b4150d8688d9abf61564f63c
twinaphex <libretro@gmail.com>
2016-06-05 19:09:54 +0200

    Prevent 'logically dead code' warning

--- list_special.c
+++ list_special.c
@@ -266,7 +266,7 @@ struct string_list *string_list_new_special(enum string_list_type type,
          for (i = 0; i < *list_size; i++)
          {
             const char          *opt = NULL;
-            const core_info_t *info  = (const core_info_t*)&core_info[i];
+            core_info_t *info        = (core_info_t*)&core_info[i];
             
             if (!info)
                goto error;
