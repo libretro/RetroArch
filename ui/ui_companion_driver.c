3c7fafe9a36476240413591583139c1c14581ad1
twinaphex <libretro@gmail.com>
2016-06-08 07:02:38 +0200

    Create ui_companion_driver_get_msg_window_ptr

--- ui/ui_companion_driver.c
+++ ui/ui_companion_driver.c
@@ -170,6 +170,14 @@ void ui_companion_driver_free(void)
    ui_companion = NULL;
 }
 
+const ui_msg_window_t *ui_companion_driver_get_msg_window_ptr(void)
+{
+   const ui_companion_driver_t *ui = ui_companion_get_ptr();
+   if (!ui)
+      return NULL;
+   return ui->msg_window;
+}
+
 const ui_window_t *ui_companion_driver_get_window_ptr(void)
 {
    const ui_companion_driver_t *ui = ui_companion_get_ptr();
--- ui/ui_companion_driver.h
+++ ui/ui_companion_driver.h
@@ -166,6 +166,8 @@ void ui_companion_driver_toggle(void);
 
 void ui_companion_driver_free(void);
 
+const ui_msg_window_t *ui_companion_driver_get_msg_window_ptr(void);
+
 const ui_window_t *ui_companion_driver_get_window_ptr(void);
 
 const ui_application_t *ui_companion_driver_get_application_ptr(void);
