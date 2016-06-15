8720048b4e2a1bf6556a0c8f2857746b841b92a7
twinaphex <libretro@gmail.com>
2016-06-15 16:45:27 +0200

    menu_entries_prepend - use menu_setting_find_enum if enum_idx is set

--- menu/menu_entries.c
+++ menu/menu_entries.c
@@ -603,7 +603,10 @@ void menu_entries_prepend(file_list_t *list, const char *path, const char *label
 
    file_list_set_actiondata(list, idx, cbs);
 
-   cbs->setting = menu_setting_find(label);
+   if (cbs->enum_idx == MENU_ENUM_LABEL_UNKNOWN)
+      cbs->setting = menu_setting_find(label);
+   else
+      cbs->setting = menu_setting_find_enum(cbs->enum_idx);
 
    menu_cbs_init(list, cbs, path, label, type, idx);
 }
