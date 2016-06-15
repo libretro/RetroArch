d5c2d85fbb5fd4366cbcfcd8989c42527b321558
twinaphex <libretro@gmail.com>
2016-06-16 00:10:10 +0200

    Use menu_hash_to_str_enum

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -970,49 +970,49 @@ static int menu_displaylist_parse_system_info(menu_displaylist_info_t *info)
 #endif
 
    strlcpy(feat_str,
-         menu_hash_to_str(
-            MENU_LABEL_VALUE_SYSTEM_INFO_LIBRETRODB_SUPPORT),
+         menu_hash_to_str_enum(
+            MENU_ENUM_LABEL_VALUE_SYSTEM_INFO_LIBRETRODB_SUPPORT),
          sizeof(feat_str));
    strlcat(feat_str, ": ", sizeof(feat_str));
    strlcat(feat_str,
          _libretrodb_supp ? 
-         menu_hash_to_str(MENU_LABEL_VALUE_YES) : 
-         menu_hash_to_str(MENU_LABEL_VALUE_NO),
+         menu_hash_to_str_enum(MENU_ENUM_LABEL_VALUE_YES) : 
+         menu_hash_to_str_enum(MENU_ENUM_LABEL_VALUE_NO),
          sizeof(feat_str));
    menu_entries_add(info->list, feat_str, "",
          MENU_SETTINGS_CORE_INFO_NONE, 0, 0);
 
    strlcpy(feat_str,
-         menu_hash_to_str(
-            MENU_LABEL_VALUE_SYSTEM_INFO_OVERLAY_SUPPORT),
+         menu_hash_to_str_enum(
+            MENU_ENUM_LABEL_VALUE_SYSTEM_INFO_OVERLAY_SUPPORT),
          sizeof(feat_str));
    strlcat(feat_str, ": ", sizeof(feat_str));
    strlcat(feat_str, _overlay_supp ? 
-         menu_hash_to_str(MENU_LABEL_VALUE_YES) : 
-         menu_hash_to_str(MENU_LABEL_VALUE_NO),
+         menu_hash_to_str_enum(MENU_ENUM_LABEL_VALUE_YES) : 
+         menu_hash_to_str_enum(MENU_ENUM_LABEL_VALUE_NO),
          sizeof(feat_str));
    menu_entries_add(info->list, feat_str, "",
          MENU_SETTINGS_CORE_INFO_NONE, 0, 0);
 
    strlcpy(feat_str,
-         menu_hash_to_str(
-            MENU_LABEL_VALUE_SYSTEM_INFO_COMMAND_IFACE_SUPPORT),
+         menu_hash_to_str_enum(
+            MENU_ENUM_LABEL_VALUE_SYSTEM_INFO_COMMAND_IFACE_SUPPORT),
          sizeof(feat_str));
    strlcat(feat_str, ": ", sizeof(feat_str));
    strlcat(feat_str, _command_supp 
-         ? menu_hash_to_str(MENU_LABEL_VALUE_YES) 
-         : menu_hash_to_str(MENU_LABEL_VALUE_NO),
+         ? menu_hash_to_str_enum(MENU_ENUM_LABEL_VALUE_YES) 
+         : menu_hash_to_str_enum(MENU_ENUM_LABEL_VALUE_NO),
          sizeof(feat_str));
    menu_entries_add(info->list, feat_str, "",
          MENU_SETTINGS_CORE_INFO_NONE, 0, 0);
 
    snprintf(feat_str, sizeof(feat_str),
          "%s : %s",
-         menu_hash_to_str(
-            MENU_LABEL_VALUE_SYSTEM_INFO_NETWORK_COMMAND_IFACE_SUPPORT),
+         menu_hash_to_str_enum(
+            MENU_ENUM_LABEL_VALUE_SYSTEM_INFO_NETWORK_COMMAND_IFACE_SUPPORT),
          _network_command_supp 
-         ? menu_hash_to_str(MENU_LABEL_VALUE_YES) 
-         : menu_hash_to_str(MENU_LABEL_VALUE_NO));
+         ? menu_hash_to_str_enum(MENU_ENUM_LABEL_VALUE_YES) 
+         : menu_hash_to_str_enum(MENU_ENUM_LABEL_VALUE_NO));
    menu_entries_add(info->list, feat_str, "",
          MENU_SETTINGS_CORE_INFO_NONE, 0, 0);
 
