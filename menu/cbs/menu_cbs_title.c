10157c302b93227e03c3ad5865790d54dba68ce3
twinaphex <libretro@gmail.com>
2016-06-20 21:29:27 +0200

    Dehardcode more strings

--- menu/cbs/menu_cbs_title.c
+++ menu/cbs/menu_cbs_title.c
@@ -92,7 +92,8 @@ static int action_get_title_video_filter(const char *path, const char *label,
 static int action_get_title_cheat_directory(const char *path, const char *label, 
       unsigned menu_type, char *s, size_t len)
 {
-   return fill_title(s, "Cheat Dir", path, len);
+   return fill_title(s,
+         msg_hash_to_str(MENU_ENUM_LABEL_VALUE_CHEAT_DATABASE_PATH), path, len);
 }
 
 static int action_get_title_core_directory(const char *path, const char *label, 
@@ -705,7 +706,8 @@ static int action_get_title_extraction_directory(const char *path, const char *l
 static int action_get_title_menu(const char *path, const char *label, 
       unsigned menu_type, char *s, size_t len)
 {
-   return fill_title(s, "Menu", path, len);
+   return fill_title(s,
+         msg_hash_to_str(MENU_ENUM_LABEL_VALUE_MENU_SETTINGS), path, len);
 }
 
 static int action_get_title_input_settings(const char *path, const char *label, 
