d9651023c4dfa1ef355a70421a142056a779090e
twinaphex <libretro@gmail.com>
2016-06-20 21:17:28 +0200

    Fix action_get_title_input_binds_list

--- menu/cbs/menu_cbs_title.c
+++ menu/cbs/menu_cbs_title.c
@@ -718,7 +718,8 @@ static int action_get_title_input_settings(const char *path, const char *label,
 static int action_get_title_input_binds_list(const char *path, const char *label, 
       unsigned menu_type, char *s, size_t len)
 {
-   snprintf(s, len, msg_hash_to_str(MENU_ENUM_LABEL_VALUE_INPUT_USER_BINDS), path[0]);
+   unsigned val = (((unsigned)path[0]) - 49) + 1;
+   snprintf(s, len, msg_hash_to_str(MENU_ENUM_LABEL_VALUE_INPUT_USER_BINDS), val);
    return 0;
 }
 
