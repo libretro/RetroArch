2bd3e94e5e0adf9fe52463a7f5d1b45702484193
Alcaro <floating@muncher.se>
2016-06-29 18:01:07 +0200

    Apparently Coverity doesn't like this either.

--- cores/libretro-ffmpeg/fft/fft.cpp
+++ cores/libretro-ffmpeg/fft/fft.cpp
@@ -305,7 +305,7 @@ static void fft_step(glfft_t *fft,
 
 static inline unsigned log2i(unsigned x)
 {
-   if (x==0) return 0xBADF00D4U; // shouldn't happen
+   if (x==0) return 0; // shouldn't happen
    
    unsigned res;
    for (res = 0; x; x >>= 1)
