d4b3fa30df8fa084ede14a134e861fec867519eb
twinaphex <libretro@gmail.com>
2016-07-12 23:24:21 +0200

    Fix 'Cancel' for FILE_TYPE_DOWNLOAD_CORE

--- menu/cbs/menu_cbs_cancel.c
+++ menu/cbs/menu_cbs_cancel.c
@@ -60,6 +60,7 @@ static int menu_cbs_init_bind_cancel_compare_type(
    {
       case FILE_TYPE_DOWNLOAD_CORE_CONTENT:
       case FILE_TYPE_DOWNLOAD_URL:
+      case FILE_TYPE_DOWNLOAD_CORE:
          BIND_ACTION_CANCEL(cbs, action_cancel_core_content);
          return 0;
    }
