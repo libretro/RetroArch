5c55c098f6a8ce51aeeaeb430e38030fcec74fec
twinaphex <libretro@gmail.com>
2016-07-14 21:14:59 +0200

    Privacy Settings - should push an empty entry in case nothing was found

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -4583,7 +4583,7 @@ bool menu_displaylist_ctl(enum menu_displaylist_ctl_state type, void *data)
                PARSE_ONLY_BOOL, false);
          menu_displaylist_parse_settings_enum(menu, info,
                MENU_ENUM_LABEL_LOCATION_ALLOW,
-               PARSE_ONLY_BOOL, false);
+               PARSE_ONLY_BOOL, true);
 
          info->need_refresh = true;
          info->need_push    = true;
