67c0bc25f404032b5f3a6e62ff95c35afca5f6e0
twinaphex <libretro@gmail.com>
2016-07-14 23:29:15 +0200

    Hide away Retropad remote core for now

--- menu/drivers/materialui.c
+++ menu/drivers/materialui.c
@@ -1615,8 +1615,10 @@ static int mui_list_push(void *data, void *userdata,
             menu_displaylist_ctl(DISPLAYLIST_SETTING_ENUM, &entry);
          }
 
+#if 0
          entry.enum_idx      = MENU_ENUM_LABEL_START_NET_RETROPAD;
          menu_displaylist_ctl(DISPLAYLIST_SETTING_ENUM, &entry);
+#endif
 
 #ifndef HAVE_DYNAMIC
          if (frontend_driver_has_fork())
