7f3e63a18df462552018b8393ca50cb95b008033
twinaphex <libretro@gmail.com>
2016-08-20 21:20:52 +0200

    config_set_string - prevent potential crash

--- libretro-common/file/config_file.c
+++ libretro-common/file/config_file.c
@@ -771,8 +771,12 @@ void config_set_string(config_file_t *conf, const char *key, const char *val)
 
    entry = (struct config_entry_list*)calloc(1, sizeof(*entry));
 
-   if (!entry)
+   if (!entry || !val)
+   {
+      if (entry)
+         free(entry);
       return;
+   }
 
    entry->key   = strdup(key);
    entry->value = strdup(val);
