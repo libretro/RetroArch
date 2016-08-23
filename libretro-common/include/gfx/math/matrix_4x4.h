18f999d8dea67da4f36d4ec2b7d3ec753793458b
twinaphex <libretro@gmail.com>
2016-08-24 01:22:29 +0200

    (libretro-common) Header updates

--- libretro-common/include/gfx/math/matrix_4x4.h
+++ libretro-common/include/gfx/math/matrix_4x4.h
@@ -1,7 +1,7 @@
 /* Copyright  (C) 2010-2016 The RetroArch team
  *
  * ---------------------------------------------------------------------------------------
- * The following license statement only applies to this file (matrix.h).
+ * The following license statement only applies to this file (matrix_4x4.h).
  * ---------------------------------------------------------------------------------------
  *
  * Permission is hereby granted, free of charge,
