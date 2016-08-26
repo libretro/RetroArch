4daecbf04d3bd096a8f312f9601f7a52a4f661cc
Brad Parker <bparker@reach3d.net>
2016-08-25 21:46:59 -0400

    false is still not 1

--- libretro-common/include/file/file_path.h
+++ libretro-common/include/file/file_path.h
@@ -151,7 +151,7 @@ void path_resolve_realpath(char *buf, size_t size);
  *
  * Checks if @path is an absolute path or a relative path.
  *
- * Returns: true (1) if path is absolute, false (1) if path is relative.
+ * Returns: true if path is absolute, false if path is relative.
  **/
 bool path_is_absolute(const char *path);
 
