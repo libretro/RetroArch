78175e0e9c9d3c28b0ac8adf279d0f16919714f9
twinaphex <libretro@gmail.com>
2016-08-30 04:55:04 +0200

    Cleanup

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -2808,7 +2808,7 @@ static bool setting_append_list(
                &settings->video.shared_context,
                msg_hash_to_str(MENU_ENUM_LABEL_VIDEO_SHARED_CONTEXT),
                msg_hash_to_str(MENU_ENUM_LABEL_VALUE_VIDEO_SHARED_CONTEXT),
-               false,
+               video_shared_context,
                msg_hash_to_str(MENU_ENUM_LABEL_VALUE_OFF),
                msg_hash_to_str(MENU_ENUM_LABEL_VALUE_ON),
                &group_info,
