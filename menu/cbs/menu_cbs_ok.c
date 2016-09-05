e785c8ac55e773cea55c93c4337ba11139152e48
twinaphex <libretro@gmail.com>
2016-09-05 08:00:04 +0200

    Silence warning

--- menu/cbs/menu_cbs_ok.c
+++ menu/cbs/menu_cbs_ok.c
@@ -241,7 +241,7 @@ int generic_action_ok_displaylist_push(const char *path,
          break;
       case ACTION_OK_DL_HELP:
          info_label             = label;
-         menu_popup_push_pending(true, type);
+         menu_popup_push_pending(true, (enum menu_popup_type)type);
          dl_type                = DISPLAYLIST_HELP;
          break;
       case ACTION_OK_DL_RPL_ENTRY:
