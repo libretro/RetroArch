c3bf05de9685f58d2b9c4d8da03d1c31d7659e63
frangarcj <frangarcj@gmail.com>
2016-09-08 16:11:16 +0200

    (VITA) Let's use SDK pthreads

--- libretro-common/rthreads/rthreads.c
+++ libretro-common/rthreads/rthreads.c
@@ -41,7 +41,7 @@
 #endif
 #elif defined(GEKKO)
 #include "gx_pthread.h"
-#elif defined(PSP) || defined(VITA)
+#elif defined(PSP)
 #include "psp_pthread.h"
 #elif defined(__CELLOS_LV2__)
 #include <pthread.h>
@@ -154,11 +154,11 @@ error:
 
 /**
  * sthread_detach:
- * @thread                  : pointer to thread object 
+ * @thread                  : pointer to thread object
  *
  * Detach a thread. When a detached thread terminates, its
  * resource sare automatically released back to the system
- * without the need for another thread to join with the 
+ * without the need for another thread to join with the
  * terminated thread.
  *
  * Returns: 0 on success, otherwise it returns a non-zero error number.
@@ -176,13 +176,13 @@ int sthread_detach(sthread_t *thread)
 
 /**
  * sthread_join:
- * @thread                  : pointer to thread object 
+ * @thread                  : pointer to thread object
  *
  * Join with a terminated thread. Waits for the thread specified by
  * @thread to terminate. If that thread has already terminated, then
  * it will return immediately. The thread specified by @thread must
  * be joinable.
- * 
+ *
  * Returns: 0 on success, otherwise it returns a non-zero error number.
  */
 void sthread_join(sthread_t *thread)
@@ -198,13 +198,13 @@ void sthread_join(sthread_t *thread)
 
 /**
  * sthread_isself:
- * @thread                  : pointer to thread object 
+ * @thread                  : pointer to thread object
  *
  * Join with a terminated thread. Waits for the thread specified by
  * @thread to terminate. If that thread has already terminated, then
  * it will return immediately. The thread specified by @thread must
  * be joinable.
- * 
+ *
  * Returns: true (1) if calling thread is the specified thread
  */
 bool sthread_isself(sthread_t *thread)
@@ -248,7 +248,7 @@ error:
 
 /**
  * slock_free:
- * @lock                    : pointer to mutex object 
+ * @lock                    : pointer to mutex object
  *
  * Frees a mutex.
  **/
@@ -267,7 +267,7 @@ void slock_free(slock_t *lock)
 
 /**
  * slock_lock:
- * @lock                    : pointer to mutex object 
+ * @lock                    : pointer to mutex object
  *
  * Locks a mutex. If a mutex is already locked by
  * another thread, the calling thread shall block until
@@ -284,7 +284,7 @@ void slock_lock(slock_t *lock)
 
 /**
  * slock_unlock:
- * @lock                    : pointer to mutex object 
+ * @lock                    : pointer to mutex object
  *
  * Unlocks a mutex.
  **/
@@ -333,7 +333,7 @@ error:
 
 /**
  * scond_free:
- * @cond                    : pointer to condition variable object 
+ * @cond                    : pointer to condition variable object
  *
  * Frees a condition variable.
 **/
@@ -352,16 +352,16 @@ void scond_free(scond_t *cond)
 
 /**
  * scond_wait:
- * @cond                    : pointer to condition variable object 
- * @lock                    : pointer to mutex object 
+ * @cond                    : pointer to condition variable object
+ * @lock                    : pointer to mutex object
  *
- * Block on a condition variable (i.e. wait on a condition). 
+ * Block on a condition variable (i.e. wait on a condition).
  **/
 void scond_wait(scond_t *cond, slock_t *lock)
 {
 #ifdef USE_WIN32_THREADS
    WaitForSingleObject(cond->event, 0);
-   
+
    SignalObjectAndWait(lock->lock, cond->event, INFINITE, FALSE);
    slock_lock(lock);
 #else
@@ -371,15 +371,15 @@ void scond_wait(scond_t *cond, slock_t *lock)
 
 /**
  * scond_broadcast:
- * @cond                    : pointer to condition variable object 
+ * @cond                    : pointer to condition variable object
  *
  * Broadcast a condition. Unblocks all threads currently blocked
- * on the specified condition variable @cond. 
+ * on the specified condition variable @cond.
  **/
 int scond_broadcast(scond_t *cond)
 {
 #ifdef USE_WIN32_THREADS
-   /* FIXME _- check how this function should differ 
+   /* FIXME _- check how this function should differ
     * from scond_signal implementation. */
    SetEvent(cond->event);
    return 0;
@@ -390,10 +390,10 @@ int scond_broadcast(scond_t *cond)
 
 /**
  * scond_signal:
- * @cond                    : pointer to condition variable object 
+ * @cond                    : pointer to condition variable object
  *
  * Signal a condition. Unblocks at least one of the threads currently blocked
- * on the specified condition variable @cond. 
+ * on the specified condition variable @cond.
  **/
 void scond_signal(scond_t *cond)
 {
@@ -406,8 +406,8 @@ void scond_signal(scond_t *cond)
 
 /**
  * scond_wait_timeout:
- * @cond                    : pointer to condition variable object 
- * @lock                    : pointer to mutex object 
+ * @cond                    : pointer to condition variable object
+ * @lock                    : pointer to mutex object
  * @timeout_us              : timeout (in microseconds)
  *
  * Try to block on a condition variable (i.e. wait on a condition) until
