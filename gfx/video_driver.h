d4be224ea63ef66fd70e81c41a8b12ba65ac81b6
twinaphex <libretro@gmail.com>
2016-09-12 18:34:57 +0200

    Header include cleanups

--- gfx/video_driver.h
+++ gfx/video_driver.h
@@ -24,15 +24,19 @@
 #include <boolean.h>
 #include <retro_common_api.h>
 
+#ifdef HAVE_CONFIG_H
+#include "../config.h"
+#endif
+
+#ifdef HAVE_OVERLAY
+#include "../input/input_overlay.h"
+#endif
+
 #include "font_driver.h"
 #include "video_filter.h"
 #include "video_shader_parse.h"
 
 #include "../input/input_driver.h"
-
-#ifdef HAVE_OVERLAY
-#include "../input/input_overlay.h"
-#endif
 
 RETRO_BEGIN_DECLS
 
