82dd2bc64c767b9173072c4fe83e7f15cee2c0ca
twinaphex <libretro@gmail.com>
2016-09-13 22:37:27 +0200

    Should add default 'no settings' entry if no settings can be pushed for updater settings list

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -2422,12 +2422,16 @@ loop:
       menu_settings_list_increment(&setting);
    }
 
-   if (count == 0 && add_empty_entry)
-      menu_entries_append_enum(info->list,
-            msg_hash_to_str(MENU_ENUM_LABEL_VALUE_NO_SETTINGS_FOUND),
-            msg_hash_to_str(MENU_ENUM_LABEL_NO_SETTINGS_FOUND),
-            MENU_ENUM_LABEL_NO_SETTINGS_FOUND,
-            0, 0, 0);
+   if (count == 0)
+   {
+      if (add_empty_entry)
+         menu_entries_append_enum(info->list,
+               msg_hash_to_str(MENU_ENUM_LABEL_VALUE_NO_SETTINGS_FOUND),
+               msg_hash_to_str(MENU_ENUM_LABEL_NO_SETTINGS_FOUND),
+               MENU_ENUM_LABEL_NO_SETTINGS_FOUND,
+               0, 0, 0);
+      return -1;
+   }
 
    return 0;
 }
@@ -4624,15 +4628,28 @@ bool menu_displaylist_ctl(enum menu_displaylist_ctl_state type, void *data)
          info->need_push    = true;
          break;
       case DISPLAYLIST_UPDATER_SETTINGS_LIST:
-         menu_displaylist_parse_settings_enum(menu, info,
-               MENU_ENUM_LABEL_CORE_UPDATER_BUILDBOT_URL,
-               PARSE_ONLY_STRING, false);
-         menu_displaylist_parse_settings_enum(menu, info,
-               MENU_ENUM_LABEL_BUILDBOT_ASSETS_URL,
-               PARSE_ONLY_STRING, false);
-         menu_displaylist_parse_settings_enum(menu, info,
-               MENU_ENUM_LABEL_CORE_UPDATER_AUTO_EXTRACT_ARCHIVE,
-               PARSE_ONLY_BOOL, false);
+         {
+            unsigned count = 0;
+            if (menu_displaylist_parse_settings_enum(menu, info,
+                     MENU_ENUM_LABEL_CORE_UPDATER_BUILDBOT_URL,
+                     PARSE_ONLY_STRING, false) != -1)
+               count++;
+            if (menu_displaylist_parse_settings_enum(menu, info,
+                  MENU_ENUM_LABEL_BUILDBOT_ASSETS_URL,
+                  PARSE_ONLY_STRING, false) != -1)
+               count++;
+            if (menu_displaylist_parse_settings_enum(menu, info,
+                  MENU_ENUM_LABEL_CORE_UPDATER_AUTO_EXTRACT_ARCHIVE,
+                  PARSE_ONLY_BOOL, false) != -1)
+               count++;
+
+            if (count == 0)
+               menu_entries_append_enum(info->list,
+                     msg_hash_to_str(MENU_ENUM_LABEL_VALUE_NO_SETTINGS_FOUND),
+                     msg_hash_to_str(MENU_ENUM_LABEL_NO_SETTINGS_FOUND),
+                     MENU_ENUM_LABEL_NO_SETTINGS_FOUND,
+                     0, 0, 0);
+         }
 
          info->need_refresh = true;
          info->need_push    = true;
