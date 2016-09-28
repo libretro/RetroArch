0104302e1828daeff59a85d97f975978e7eb0cd6
twinaphex <libretro@gmail.com>
2016-09-28 07:23:14 +0200

    Add new RARCH_CTL calls

--- retroarch.c
+++ retroarch.c
@@ -1085,6 +1085,8 @@ error:
 
 bool rarch_ctl(enum rarch_ctl_state state, void *data)
 {
+   static bool rarch_is_sram_load_disabled = false;
+   static bool rarch_is_sram_save_disabled = false;
    static bool rarch_use_sram              = false;
    settings_t *settings                    = config_get_ptr();
 
@@ -1181,6 +1183,22 @@ bool rarch_ctl(enum rarch_ctl_state state, void *data)
          if (content_does_not_need_content())
             return false;
          path_set_redirect();
+         break;
+      case RARCH_CTL_IS_SRAM_LOAD_DISABLED:
+         return rarch_is_sram_load_disabled;
+      case RARCH_CTL_SET_SRAM_LOAD_DISABLED:
+         rarch_is_sram_load_disabled = true;
+         break;
+      case RARCH_CTL_UNSET_SRAM_LOAD_DISABLED:
+         rarch_is_sram_load_disabled = false;
+         break;
+      case RARCH_CTL_IS_SRAM_SAVE_DISABLED:
+         return rarch_is_sram_save_disabled;
+      case RARCH_CTL_SET_SRAM_SAVE_DISABLED:
+         rarch_is_sram_save_disabled = true;
+         break;
+      case RARCH_CTL_UNSET_SRAM_SAVE_DISABLED:
+         rarch_is_sram_save_disabled = false;
          break;
       case RARCH_CTL_IS_SRAM_USED:
          return rarch_use_sram;
