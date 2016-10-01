9050f75fc89405b95932beb69b66e3c736cdb647
twinaphex <libretro@gmail.com>
2016-10-01 23:30:09 +0200

    Create dir_is_empty

--- tasks/task_content.c
+++ tasks/task_content.c
@@ -847,9 +847,9 @@ static void menu_content_environment_get(int *argc, char *argv[],
 
    if (!path_is_empty(RARCH_PATH_CONFIG))
       wrap_args->config_path   = path_get(RARCH_PATH_CONFIG);
-   if (!dir_is_savefile_empty())
+   if (!dir_is_empty(RARCH_DIR_SAVEFILE))
       wrap_args->sram_path     = dir_get_savefile();
-   if (!dir_is_savestate_empty())
+   if (!dir_is_empty(RARCH_DIR_SAVESTATE))
       wrap_args->state_path    = dir_get_savestate();
    if (fullpath && *fullpath)
       wrap_args->content_path  = fullpath;
