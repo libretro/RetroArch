2109a13ce21c8bdf19b45b8ddf5bc3970c158dc2
Higor Eurípedes <heuripedes@gmail.com>
2016-10-17 19:14:31 -0300

    (libretro-common) Fix sthread_isself() documentation

--- libretro-common/include/rthreads/rthreads.h
+++ libretro-common/include/rthreads/rthreads.h
@@ -76,13 +76,8 @@ void sthread_join(sthread_t *thread);
 
 /**
  * sthread_isself:
- * @thread                  : pointer to thread object 
+ * @thread                  : pointer to thread object
  *
- * Join with a terminated thread. Waits for the thread specified by
- * @thread to terminate. If that thread has already terminated, then
- * it will return immediately. The thread specified by @thread must
- * be joinable.
- * 
  * Returns: true (1) if calling thread is the specified thread
  */
 bool sthread_isself(sthread_t *thread);
