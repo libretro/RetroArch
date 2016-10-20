fbf74565bdb6655a73388e1d831c664eb16ba5df
twinaphex <libretro@gmail.com>
2016-10-20 19:52:32 +0200

    Fix string

--- intl/msg_hash_us.c
+++ intl/msg_hash_us.c
@@ -3024,7 +3024,7 @@ const char *msg_hash_to_str_us(enum msg_hash_enums msg)
    switch (msg)
    {
       case MENU_ENUM_SUBLABEL_NETPLAY:
-         return "Join and/or host a netplay session.";
+         return "Join or host a netplay session.";
       case MENU_ENUM_SUBLABEL_FPS_SHOW:
          return "Displays the current framerate per second onscreen.";
       case MENU_ENUM_SUBLABEL_VIDEO_SETTINGS:
