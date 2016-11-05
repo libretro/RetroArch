c0b0cfbcbf1ef4a50b8d227bf7cfd9b488a19c6b
lasers <lasers@users.noreply.github.com>
2016-11-05 15:56:49 -0500

    netplay improvs

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -1540,6 +1540,11 @@ static int menu_displaylist_parse_netplay(
 {
 #ifdef HAVE_NETWORKING
    menu_entries_append_enum(info->list,
+         msg_hash_to_str(MENU_ENUM_LABEL_VALUE_NETPLAY_SETTINGS),
+         msg_hash_to_str(MENU_ENUM_LABEL_NETPLAY_SETTINGS),
+         MENU_ENUM_LABEL_NETWORK_SETTINGS, MENU_SETTING_GROUP, 0, 0);
+
+   menu_entries_append_enum(info->list,
          msg_hash_to_str(MENU_ENUM_LABEL_VALUE_NETPLAY_ENABLE_HOST),
          msg_hash_to_str(MENU_ENUM_LABEL_NETPLAY_ENABLE_HOST),
          MENU_ENUM_LABEL_NETPLAY_ENABLE_HOST,
@@ -1556,11 +1561,6 @@ static int menu_displaylist_parse_netplay(
          msg_hash_to_str(MENU_ENUM_LABEL_NETPLAY_DISCONNECT),
          MENU_ENUM_LABEL_NETPLAY_DISCONNECT,
          MENU_SETTING_ACTION, 0, 0);
-
-   menu_entries_append_enum(info->list,
-         msg_hash_to_str(MENU_ENUM_LABEL_VALUE_NETPLAY_SETTINGS),
-         msg_hash_to_str(MENU_ENUM_LABEL_NETPLAY_SETTINGS),
-         MENU_ENUM_LABEL_NETWORK_SETTINGS, MENU_SETTING_GROUP, 0, 0);
 
 #else
    menu_entries_append_enum(info->list,
