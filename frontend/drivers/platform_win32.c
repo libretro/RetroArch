fb2dddd73e3cc6e4d1371b76d68ccc6eeb6d4a02
Alcaro <Alcaro@users.noreply.github.com>
2016-11-07 00:36:58 +0100

    use linebreaks instead

--- frontend/drivers/platform_win32.c
+++ frontend/drivers/platform_win32.c
@@ -8,7 +8,8 @@
  * RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE. See the GNU General Public License for more details.
- * * You should have received a copy of the GNU General Public License along with RetroArch.
+ *
+ * You should have received a copy of the GNU General Public License along with RetroArch.
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
