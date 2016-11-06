a72c5dbdf2f61ebd60a3378013c48d92edfa98af
Alcaro <Alcaro@users.noreply.github.com>
2016-11-07 00:32:42 +0100

    use frontslashes instead

--- frontend/drivers/platform_win32.c
+++ frontend/drivers/platform_win32.c
@@ -9,7 +9,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE. See the GNU General Public License for more details.
  * * You should have received a copy of the GNU General Public License along with RetroArch.
- * If not, see <http:\\/www.gnu.org/licenses/>.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdint.h>
