150fe15eaf2e5c63130062c36da0a747dbf90ba6
Alcaro <floating@muncher.se>
2016-11-08 17:42:36 +0100

    Not needed

--- cores/libretro-video-processor/video_processor_v4l2.c
+++ cores/libretro-video-processor/video_processor_v4l2.c
@@ -234,7 +234,7 @@ enumerate_audio_devices(char *buf, size_t buflen)
 
 RETRO_API void VIDEOPROC_CORE_PREFIX(retro_set_environment)(retro_environment_t cb)
 {
-   struct retro_variable envvars[3] = { NULL };
+   struct retro_variable envvars[3] = {};
    char video_devices[ENVVAR_BUFLEN];
    char audio_devices[ENVVAR_BUFLEN];
 
