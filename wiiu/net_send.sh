deb9ea44537c7aa29c0b2df0d1edfea46ab36100
aliaspider <aliaspider@gmail.com>
2016-11-09 14:28:39 +0100

    (WiiU) netlog scripts.

---
+++ wiiu/net_send.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [$1 -eq ''] ; then
+   echo
+   echo "usage: $0 <WiiU-ip> <elf>"
+   echo
+   exit 0
+fi
+
+export WIILOAD=tcp:$1
+rm $2.stripped -rf
+powerpc-eabi-strip $2 -o $2.stripped
+wiiload $2.stripped
