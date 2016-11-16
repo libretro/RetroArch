72eb212decae3c72f4473365292a7605badef52e
twinaphex <libretro@gmail.com>
2016-11-16 07:35:58 +0100

    Remove this option - should be no longer relevant

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -5227,9 +5227,6 @@ bool menu_displaylist_ctl(enum menu_displaylist_ctl_state type, void *data)
          menu_displaylist_parse_settings_enum(menu, info,
                MENU_ENUM_LABEL_DUMMY_ON_CORE_SHUTDOWN,
                PARSE_ONLY_BOOL, false);
-         menu_displaylist_parse_settings_enum(menu, info,
-               MENU_ENUM_LABEL_CORE_SET_SUPPORTS_NO_CONTENT_ENABLE,
-               PARSE_ONLY_BOOL, false);
 
          info->need_refresh = true;
          info->need_push    = true;
