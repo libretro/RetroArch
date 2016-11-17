df7ffe1822d357d95f7632e66f1e397a90c74e50
twinaphex <libretro@gmail.com>
2016-11-17 21:25:02 +0100

    Don't use legacy vertex shader for snow for OpenGLES

--- gfx/drivers_shader/shader_glsl.c
+++ gfx/drivers_shader/shader_glsl.c
@@ -965,7 +965,11 @@ static void *gl_glsl_init(void *data, const char *path)
    gl_glsl_find_uniforms(glsl, 0, glsl->prg[VIDEO_SHADER_MENU_SEC].id,
          &glsl->uniforms[VIDEO_SHADER_MENU_SEC]);
 
+#if defined(HAVE_OPENGLES)
+   shader_prog_info.vertex   = stock_vertex_xmb_snow;
+#else
    shader_prog_info.vertex   = glsl_core ? stock_vertex_xmb_snow : stock_vertex_xmb_snow_legacy;
+#endif
    shader_prog_info.fragment = stock_fragment_xmb_snow;
 
    gl_glsl_compile_program(
