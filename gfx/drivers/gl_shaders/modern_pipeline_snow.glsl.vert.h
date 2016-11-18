4e74375356dcf1b936ac560c9620ac1762690a70
Jean-André Santoni <jean.andre.santoni@gmail.com>
2016-11-19 00:08:59 +0100

    Replace the modern snow glsl vertex shader

--- gfx/drivers/gl_shaders/modern_pipeline_snow.glsl.vert.h
+++ gfx/drivers/gl_shaders/modern_pipeline_snow.glsl.vert.h
@@ -1,12 +1,16 @@
 #include "shaders_common.h"
 
+/* Need to duplicate these to work around broken stuff on Android.
+ * Must enforce alpha = 1.0 or 32-bit games can potentially go black. */
 static const char *stock_vertex_xmb_snow = GLSL(
-   in vec3 VertexCoord;
-   uniform float time;
+   attribute vec2 TexCoord;
+   attribute vec2 VertexCoord;
+   attribute vec4 Color;
+   uniform mat4 MVPMatrix;
+   varying vec2 tex_coord;
 
-   void main()
-   {
-     vec3 v = vec3(VertexCoord.x, 0.0, VertexCoord.y);
-     gl_Position = vec4(v, 1.0);
+   void main() {
+      gl_Position = MVPMatrix * vec4(VertexCoord, 0.0, 1.0);
+      tex_coord = TexCoord;
    }
 );
