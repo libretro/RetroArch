2be4ca1755ec67d26d8d697fa6343b974e80707d
twinaphex <libretro@gmail.com>
2016-11-18 09:54:19 +0100

    Simplify vertex snow shaders

--- gfx/drivers/gl_shaders/legacy_pipeline_snow.glsl.vert.h
+++ gfx/drivers/gl_shaders/legacy_pipeline_snow.glsl.vert.h
@@ -3,23 +3,6 @@
 static const char *stock_vertex_xmb_snow_legacy = GLSL(
    attribute vec3 VertexCoord;
    uniform float time;
-   float iqhash( float n )
-   {
-     return fract(sin(n)*43758.5453);
-   }
-
-   float noise( vec3 x )
-   {
-     vec3 p = floor(x);
-     vec3 f = fract(x);
-     f = f*f*(3.0-2.0*f);
-     float n = p.x + p.y*57.0 + 113.0*p.z;
-
-     return mix(mix(mix( iqhash(n+0.0 ), iqhash(n+1.0 ),f.x),
-     mix( iqhash(n+57.0 ), iqhash(n+58.0 ),f.x),f.y),
-     mix(mix( iqhash(n+113.0), iqhash(n+114.0),f.x),
-     mix( iqhash(n+170.0), iqhash(n+171.0),f.x),f.y),f.z);
-   }
 
    void main()
    {
