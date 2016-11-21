df7b229834ea211ce0e2ca261360c787794f71e0
lasers <lasers@users.noreply.github.com>
2016-11-21 08:34:33 -0600

    Add sublabel for Playlists

--- intl/msg_hash_us.h
+++ intl/msg_hash_us.h
@@ -1626,6 +1626,8 @@ MSG_HASH(MENU_ENUM_SUBLABEL_RECORDING_SETTINGS,
       "Change settings for the recording.")
 MSG_HASH(MENU_ENUM_SUBLABEL_SAVING_SETTINGS,
       "Change settings for the saving.")
+MSG_HASH(MENU_ENUM_SUBLABEL_PLAYLIST_SETTINGS,
+      "Change settings for the playlists.")
 MSG_HASH(MENU_ENUM_SUBLABEL_ADD_CONTENT_LIST,
       "Scan contents and add to the database.")
 MSG_HASH(MENU_ENUM_SUBLABEL_AUDIO_SETTINGS,
