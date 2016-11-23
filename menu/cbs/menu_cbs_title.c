efba5dddf5f83d2b9344d352f1dcc29dda10521e
twinaphex <libretro@gmail.com>
2016-11-23 15:58:52 +0100

    Cleanups

--- menu/cbs/menu_cbs_title.c
+++ menu/cbs/menu_cbs_title.c
@@ -163,17 +163,21 @@ default_fill_title_macro(action_get_title_system_directory,       MENU_ENUM_LABE
 default_fill_title_macro(action_get_title_assets_directory,       MENU_ENUM_LABEL_VALUE_ASSETS_DIRECTORY)
 default_fill_title_macro(action_get_title_extraction_directory,   MENU_ENUM_LABEL_VALUE_CACHE_DIRECTORY)
 default_fill_title_macro(action_get_title_menu,                   MENU_ENUM_LABEL_VALUE_MENU_SETTINGS)
-
-static int action_get_title_font_path(const char *path, const char *label, 
-      unsigned menu_type, char *s, size_t len)
-{
-   return fill_title(s, "Font", path, len);
-}
+default_fill_title_macro(action_get_title_font_path,              MENU_ENUM_LABEL_VALUE_XMB_FONT)
 
 static int action_get_title_help(const char *path, const char *label, 
       unsigned menu_type, char *s, size_t len)
 {
-   strlcpy(s, msg_hash_to_str(MENU_ENUM_LABEL_VALUE_HELP_LIST), len);
+   strlcpy(s,
+         msg_hash_to_str(MENU_ENUM_LABEL_VALUE_HELP_LIST), len);
+   return 0;
+}
+
+static int action_get_title_input_settings(const char *path, const char *label, 
+      unsigned menu_type, char *s, size_t len)
+{
+   strlcpy(s,
+         msg_hash_to_str(MENU_ENUM_LABEL_VALUE_INPUT_SETTINGS), len);
    return 0;
 }
 
@@ -362,15 +366,6 @@ static int action_get_title_group_settings(const char *path, const char *label,
       }
    }
 
-   return 0;
-}
-
-
-static int action_get_title_input_settings(const char *path, const char *label, 
-      unsigned menu_type, char *s, size_t len)
-{
-   strlcpy(s,
-         msg_hash_to_str(MENU_ENUM_LABEL_VALUE_INPUT_SETTINGS), len);
    return 0;
 }
 
