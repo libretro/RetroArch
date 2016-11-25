149522f23c6265c7819924dbb910cd87865e5201
Lioncash <mathew1800@gmail.com>
2016-11-25 17:25:15 -0500

    core_info: Fix a leak in core_info_database_supports_content_path

--- core_info.c
+++ core_info.c
@@ -757,9 +757,14 @@ bool core_info_database_supports_content_path(const char *database_path, const c
          const core_info_t *info = &core_info_curr_list->list[i];
 
          if (string_list_find_elem(info->databases_list, database))
+         {
             if (string_list_find_elem(info->supported_extensions_list, "zip") ||
                 string_list_find_elem(info->supported_extensions_list, "7z"))
-                return false;
+            {
+               free(database);
+               return false;
+            }
+         }
       }
    }
 
