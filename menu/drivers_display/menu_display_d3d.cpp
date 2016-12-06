7a7a4f16321e0d579004f4a158bb007f7c243237
twinaphex <libretro@gmail.com>
2016-12-06 17:44:38 +0100

    (D3D) Buildfix

--- menu/drivers_display/menu_display_d3d.cpp
+++ menu/drivers_display/menu_display_d3d.cpp
@@ -205,7 +205,7 @@ static void menu_display_d3d_draw_pipeline(void *data)
    switch (draw->pipeline.id)
    {
       case VIDEO_SHADER_MENU:
-      case VIDEO_SHADER_MENU_SEC:
+      case VIDEO_SHADER_MENU_2:
       case VIDEO_SHADER_MENU_3:
          shader_info.data                = NULL;
          shader_info.idx                 = draw->pipeline.id;
