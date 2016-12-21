1344e91286f29e53db087fc8ddd7c50f9743c5f6
twinaphex <libretro@gmail.com>
2016-12-21 12:23:19 +0100

    Revert "Locking bug"
    This reverts commit b40c1311b6d5379e58a0065b4b294b59e7733f28.

--- libretro-common/queues/task_queue.c
+++ libretro-common/queues/task_queue.c
@@ -433,10 +433,13 @@ static void threaded_worker(void *userdata)
          continue;
       }
 
-      task_queue_remove(&tasks_running, task);
       slock_unlock(running_lock);
 
       task->handler(task);
+
+      slock_lock(running_lock);
+      task_queue_remove(&tasks_running, task);
+      slock_unlock(running_lock);
 
       /* Update queue */
       if (!task->finished)
