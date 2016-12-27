89bb4f610d100ba0af7e3d277819b5ba9300a275
twinaphex <libretro@gmail.com>
2016-12-28 00:11:56 +0100

    task_http.c - comment this out until we have fixed the threading errors

--- tasks/task_http.c
+++ tasks/task_http.c
@@ -125,7 +125,7 @@ static int task_http_iterate_transfer(retro_task_t *task)
    if (!net_http_update(http->handle, &pos, &tot))
    {
       /* TODO/FIXME - race issue */
-      task->progress = (tot == 0) ? -1 : (signed)(pos * 100 / tot);
+      //task->progress = (tot == 0) ? -1 : (signed)(pos * 100 / tot);
       return -1;
    }
 
@@ -232,7 +232,7 @@ static bool task_http_retriever(retro_task_t *task, void *data)
 
    /* Fill HTTP info link */
    strlcpy(info->url, http->connection.url, sizeof(info->url));
-   info->progress = task->progress;
+   //info->progress = task->progress;
    return true;
 }
 
