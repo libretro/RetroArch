830c01476f7ebdd6b93521485a3eb0c985178646
twinaphex <libretro@gmail.com>
2017-01-08 02:33:06 +0100

    Set fullpath for menu_entries_prepend

--- menu/menu_entries.c
+++ menu/menu_entries.c
@@ -436,20 +436,29 @@ void menu_entries_prepend(file_list_t *list, const char *path, const char *label
 {
    menu_ctx_list_t list_info;
    size_t idx;
+   const char *menu_path           = NULL;
+   const char *menu_label          = NULL;
    menu_file_list_cbs_t *cbs       = NULL;
    if (!list || !label)
       return;
 
    file_list_prepend(list, path, label, type, directory_ptr, entry_idx);
 
+   menu_entries_get_last_stack(&menu_path, &menu_label, NULL, NULL, NULL);
+
    idx              = 0;
 
-   list_info.list   = list;
-   list_info.path   = path;
-   list_info.label  = label;
-   list_info.idx    = idx;
+   if (!string_is_empty(menu_path))
+      list_info.fullpath = strdup(menu_path);
+   list_info.list        = list;
+   list_info.path        = path;
+   list_info.label       = label;
+   list_info.idx         = idx;
 
    menu_driver_ctl(RARCH_MENU_CTL_LIST_INSERT, &list_info);
+
+   if (list_info.fullpath)
+      free(list_info.fullpath);
 
    file_list_free_actiondata(list, idx);
    cbs = (menu_file_list_cbs_t*)
