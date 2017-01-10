b95a817471eb6b25f55d4c550797988d6757bc14
twinaphex <libretro@gmail.com>
2017-01-10 22:53:22 +0100

    (GLSM) Update

--- libretro-common/glsm/glsm.c
+++ libretro-common/glsm/glsm.c
@@ -1908,6 +1908,10 @@ void rglFlushMappedBufferRange(GLenum target, GLintptr offset, GLsizeiptr length
 #endif
 }
 
+#ifndef GL_WAIT_FAILED
+#define GL_WAIT_FAILED                                   0x911D
+#endif
+
 /*
  *
  * Core in:
@@ -1918,6 +1922,8 @@ GLenum rglClientWaitSync(void *sync, GLbitfield flags, uint64_t timeout)
 {
 #if defined(HAVE_OPENGL) || defined(HAVE_OPENGLES) && defined(HAVE_OPENGLES3)
   return glClientWaitSync((GLsync)sync, flags, (GLuint64)timeout);
+#else
+  return GL_WAIT_FAILED;
 #endif
 }
 
