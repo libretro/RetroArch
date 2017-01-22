692bedf7d348e171493ab779cb3edfe9351c0100
radius <andres.430@gmail.com>
2017-01-22 15:42:38 -0500

    just reload the game when connecting to a netplay lobby

--- menu/cbs/menu_cbs_ok.c
+++ menu/cbs/menu_cbs_ok.c
@@ -3140,18 +3140,8 @@ static int action_ok_netplay_connect_room(const char *path,
          netplay_room_list[idx - 1].gamename,
          netplay_room_list[idx - 1].gamecrc);
 
-   /* If we haven't yet started, this will load on its own */
-   if (!content_is_inited())
-   {
-      task_push_netplay_crc_scan(netplay_room_list[idx - 1].gamecrc,
-            tmp_hostname, netplay_room_list[idx - 1].corename);
-   }
-   else
-   {
-      /* Enable Netplay itself */
-      if (!command_event(CMD_EVENT_NETPLAY_INIT, (void *) tmp_hostname))
-         return -1;
-   }
+   task_push_netplay_crc_scan(netplay_room_list[idx - 1].gamecrc,
+      tmp_hostname, netplay_room_list[idx - 1].corename);
 
 #else
    return -1;
