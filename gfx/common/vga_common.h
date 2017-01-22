47ffe944796da86dc73d16ded601009ec439a8b3
Brad Parker <cbparker@gmail.com>
2017-01-22 10:44:59 -0500

    DOS: add vga_common header

---
+++ gfx/common/vga_common.h
@@ -0,0 +1,29 @@
+/*  RetroArch - A frontend for libretro.
+ *  Copyright (C) 2010-2014 - Hans-Kristian Arntzen
+ *  copyright (c) 2011-2015 - Daniel De Matteis
+ *  copyright (c) 2016 - Brad Parker
+ *
+ *  RetroArch is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  RetroArch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with RetroArch.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __VGA_COMMON_H
+#define __VGA_COMMON_H
+
+#define VGA_WIDTH 320
+#define VGA_HEIGHT 200
+
+typedef struct vga
+{
+   bool color;
+} vga_t;
+
+#endif
