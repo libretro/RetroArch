29a1d504d72a8d214aa26a77af728fca22596531
fr500 <andres.430@gmail.com>
2017-01-25 17:48:43 -0500

    allow different server ports again

--- network/netplay/netplay_frontend.c
+++ network/netplay/netplay_frontend.c
@@ -948,8 +948,10 @@ bool init_netplay(void *direct_host, const char *server, unsigned port)
 
    netplay_data = (netplay_t*)netplay_new(
          netplay_is_client ? direct_host : NULL,
-         netplay_is_client ? (!netplay_client_deferred ? server : server_address_deferred) : NULL,
-         netplay_is_client ? (!netplay_client_deferred ? port   : server_port_deferred   ) : RARCH_DEFAULT_PORT,
+         netplay_is_client ? (!netplay_client_deferred ? server 
+            : server_address_deferred) : NULL,
+         netplay_is_client ? (!netplay_client_deferred ? port   
+            : server_port_deferred   ) : (port != 0 ? port : RARCH_DEFAULT_PORT),
          settings->netplay.stateless_mode, settings->netplay.check_frames, &cbs,
          settings->netplay.nat_traversal, settings->username,
          quirks);
