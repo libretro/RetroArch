2b1fbca31861eef200087c10add0ef1e74d302b8
twinaphex <libretro@gmail.com>
2017-02-25 02:49:18 +0100

    Add to the define

--- audio/drivers_resampler/cc_resampler.c
+++ audio/drivers_resampler/cc_resampler.c
@@ -348,7 +348,7 @@ static void resampler_CC_upsample(void *re_, struct resampler_data *data)
 }
 
 
-#elif defined (__ARM_NEON__)
+#elif defined (__ARM_NEON__) && !defined(DONT_WANT_ARM_OPTIMIZATIONS)
 
 #define CC_RESAMPLER_IDENT "NEON"
 
