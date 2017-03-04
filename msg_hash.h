baa666022fb09827f9597739993266fdae268827
losernator <losernator@gmail.com>
2017-03-05 01:42:14 +0900

    add support for Korean

--- msg_hash.h
+++ msg_hash.h
@@ -1836,6 +1836,9 @@ int menu_hash_get_help_it_enum(enum msg_hash_enums msg, char *s, size_t len);
 const char *msg_hash_to_str_jp(enum msg_hash_enums msg);
 int menu_hash_get_help_jp_enum(enum msg_hash_enums msg, char *s, size_t len);
 
+const char *msg_hash_to_str_ko(enum msg_hash_enums msg);
+int menu_hash_get_help_ko_enum(enum msg_hash_enums msg, char *s, size_t len);
+
 const char *msg_hash_to_str_pt(enum msg_hash_enums msg);
 int menu_hash_get_help_pt_enum(enum msg_hash_enums msg, char *s, size_t len);
 
