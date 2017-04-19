841adf04a5938326fab4efbb80169a397c77e5fc
twinaphex <libretro@gmail.com>
2017-04-19 02:14:41 +0200

    Add header include

--- gfx/video_renderchain_driver.c
+++ gfx/video_renderchain_driver.c
@@ -13,6 +13,8 @@
  *  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
+
 #include "video_renderchain_driver.h"
 
 static const renderchain_driver_t *renderchain_drivers[] = {
