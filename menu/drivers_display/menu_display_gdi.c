b303283ac341135a4bb10c7873c0804778b66f17
twinaphex <libretro@gmail.com>
2017-04-29 19:35:13 +0200

    (GDI) Buildfix

--- menu/drivers_display/menu_display_gdi.c
+++ menu/drivers_display/menu_display_gdi.c
@@ -82,11 +82,14 @@ static void menu_display_gdi_clear_color(menu_display_ctx_clearcolor_t *clearcol
 
 static bool menu_display_gdi_font_init_first(
       void **font_handle, void *video_data,
-      const char *font_path, float font_size)
+      const char *font_path, float font_size,
+      bool is_threaded)
 {
    font_data_t **handle = (font_data_t**)font_handle;
    *handle = font_driver_init_first(video_data,
-         font_path, font_size, true, FONT_DRIVER_RENDER_GDI);
+         font_path, font_size, true,
+         is_threaded,
+         FONT_DRIVER_RENDER_GDI);
    return *handle;
 }
 
