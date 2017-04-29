3e5815948037dbbba9ee013918ddcbf9f7349b64
Monroe88 <wmsmart88@gmail.com>
2017-04-28 20:36:52 -0500

    Add WASAPI audio driver settings to the menu

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -5299,6 +5299,17 @@ bool menu_displaylist_ctl(enum menu_displaylist_ctl_state type, void *data)
          menu_displaylist_parse_settings_enum(menu, info,
                MENU_ENUM_LABEL_AUDIO_DSP_PLUGIN,
                PARSE_ONLY_PATH, false);
+#ifdef HAVE_WASAPI
+         menu_displaylist_parse_settings_enum(menu, info,
+               MENU_ENUM_LABEL_AUDIO_WASAPI_EXCLUSIVE_MODE,
+               PARSE_ONLY_BOOL, false);
+         menu_displaylist_parse_settings_enum(menu, info,
+               MENU_ENUM_LABEL_AUDIO_WASAPI_FLOAT_FORMAT,
+               PARSE_ONLY_BOOL, false);
+         menu_displaylist_parse_settings_enum(menu, info,
+               MENU_ENUM_LABEL_AUDIO_WASAPI_SH_BUFFER_LENGTH,
+               PARSE_ONLY_UINT, false);
+#endif
 
          info->need_refresh = true;
          info->need_push    = true;
