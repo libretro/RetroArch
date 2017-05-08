4a7c81b3537394c5a567860771fe27d44e9c537e
twinaphex <libretro@gmail.com>
2017-05-08 18:30:15 +0200

    Cleanup wav2float - add some suggestions for leiradel here

--- libretro-common/audio/audio_mixer.c
+++ libretro-common/audio/audio_mixer.c
@@ -113,9 +113,6 @@ static unsigned s_rate                                       = 0;
 static bool wav2float(const rwav_t* wav, float** pcm, size_t* samples_out)
 {
    size_t i;
-   float sample       = 0.0f;
-   const uint8_t* u8  = NULL;
-   const int16_t* s16 = NULL;
    float* f           = NULL;
 
    /* Allocate on a 16-byte boundary, and pad to a multiple of 16 bytes */
@@ -132,7 +129,8 @@ static bool wav2float(const rwav_t* wav, float** pcm, size_t* samples_out)
    {
       if (wav->bitspersample == 8)
       {
-         u8 = (const uint8_t*)wav->samples;
+         float sample      = 0.0f;
+         const uint8_t *u8 = (const uint8_t*)wav->samples;
          
          for (i = wav->numsamples; i != 0; i--)
          {
@@ -144,7 +142,11 @@ static bool wav2float(const rwav_t* wav, float** pcm, size_t* samples_out)
       }
       else
       {
-         s16 = (const int16_t*)wav->samples;
+         /* TODO/FIXME note to leiradel - can we use audio/conversion/s16_to_float
+          * functions here? */
+
+         float sample       = 0.0f;
+         const int16_t *s16 = (const int16_t*)wav->samples;
          
          for (i = wav->numsamples; i != 0; i--)
          {
@@ -159,7 +161,8 @@ static bool wav2float(const rwav_t* wav, float** pcm, size_t* samples_out)
    {
       if (wav->bitspersample == 8)
       {
-         u8 = (const uint8_t*)wav->samples;
+         float sample      = 0.0f;
+         const uint8_t *u8 = (const uint8_t*)wav->samples;
          
          for (i = wav->numsamples; i != 0; i--)
          {
@@ -173,7 +176,11 @@ static bool wav2float(const rwav_t* wav, float** pcm, size_t* samples_out)
       }
       else
       {
-         s16 = (const int16_t*)wav->samples;
+         /* TODO/FIXME note to leiradel - can we use audio/conversion/s16_to_float
+          * functions here? */
+
+         float sample       = 0.0f;
+         const int16_t *s16 = (const int16_t*)wav->samples;
          
          for (i = wav->numsamples; i != 0; i--)
          {
