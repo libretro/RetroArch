46182e090b78a661c2b6be9fd361d040c951f538
twinaphex <libretro@gmail.com>
2017-05-20 19:46:27 +0200

    Merge keyboard_event_win32.cpp with win32_common.cpp

--- gfx/common/win32_common.cpp
+++ gfx/common/win32_common.cpp
@@ -356,6 +356,70 @@ static int win32_drag_query_file(HWND hwnd, WPARAM wparam)
    return 0;
 }
 
+static LRESULT win32_handle_keyboard_event(HWND hwnd, UINT message,
+		WPARAM wparam, LPARAM lparam)
+{
+   unsigned scancode = (lparam >> 16) & 0xff;
+   unsigned keycode = input_keymaps_translate_keysym_to_rk(scancode);
+   uint16_t mod     = 0;
+   bool keydown     = true;
+
+   if (GetKeyState(VK_SHIFT)   & 0x80)
+      mod |= RETROKMOD_SHIFT;
+   if (GetKeyState(VK_CONTROL) & 0x80)
+      mod |=  RETROKMOD_CTRL;
+   if (GetKeyState(VK_MENU)    & 0x80)
+      mod |=  RETROKMOD_ALT;
+   if (GetKeyState(VK_CAPITAL) & 0x81)
+      mod |= RETROKMOD_CAPSLOCK;
+   if (GetKeyState(VK_SCROLL)  & 0x81)
+      mod |= RETROKMOD_SCROLLOCK;
+   if ((GetKeyState(VK_LWIN) | GetKeyState(VK_RWIN)) & 0x80)
+      mod |= RETROKMOD_META;
+
+   switch (message)
+   {
+      /* Seems to be hard to synchronize 
+       * WM_CHAR and WM_KEYDOWN properly.
+       */
+      case WM_CHAR:
+         input_keyboard_event(keydown, RETROK_UNKNOWN, wparam, mod,
+               RETRO_DEVICE_KEYBOARD);
+         return TRUE;
+
+      case WM_KEYUP:
+      case WM_SYSKEYUP:
+      case WM_KEYDOWN:
+      case WM_SYSKEYDOWN:
+         /* Key released? */
+         if (message == WM_KEYUP || message == WM_SYSKEYUP)
+            keydown = false;
+
+         /* DirectInput uses scancodes directly. */
+         input_keyboard_event(keydown, keycode, 0, mod,
+               RETRO_DEVICE_KEYBOARD);
+
+         if (message == WM_SYSKEYDOWN)
+         {
+            switch (wparam)
+            {
+               case VK_F10:
+               case VK_MENU:
+               case VK_RSHIFT:
+                  return 0;
+               default:
+                  break;
+            }
+         }
+         else
+            return 0;
+
+         break;
+   }
+
+   return DefWindowProc(hwnd, message, wparam, lparam);
+}
+
 static LRESULT CALLBACK WndProcCommon(bool *quit, HWND hwnd, UINT message,
       WPARAM wparam, LPARAM lparam)
 {
