fa0d82b18891ee75d669f0c97ac7a68989d481a0
Maschell <Maschell@gmx.de>
2017-05-20 13:13:52 +0200

    [Wii U] Fixing the .rpx build

--- wiiu/include/wiiu/syshid.h
+++ wiiu/include/wiiu/syshid.h
@@ -1,32 +1,27 @@
 #pragma once
 #include <wiiu/types.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
 typedef struct
 {
-    u32 handle;
-    u32 physical_device_inst;
-    u16 vid;
-    u16 pid;
-    u8 interface_index;
-    u8 sub_class;
-    u8 protocol;
+    uint32_t handle;
+    uint32_t physical_device_inst;
+    uint16_t vid;
+    uint16_t pid;
+    uint8_t interface_index;
+    uint8_t sub_class;
+    uint8_t protocol;
 
-    u16 max_packet_size_rx;
-    u16 max_packet_size_tx;
+    uint16_t max_packet_size_rx;
+    uint16_t max_packet_size_tx;
 
 } HIDDevice;
+
+typedef struct _HIDClient HIDClient;
 
 #define HID_DEVICE_DETACH   0
 #define HID_DEVICE_ATTACH   1
 
-typedef struct _HIDClient HIDClient;
-
-typedef s32 (*HIDAttachCallback)(HIDClient *p_hc,HIDDevice *p_hd,u32 attach);
+typedef int32_t (*HIDAttachCallback)(HIDClient *p_hc,HIDDevice *p_hd,uint32_t attach);
 
 struct _HIDClient
 {
@@ -34,27 +29,97 @@ struct _HIDClient
     HIDAttachCallback attach_cb;
 };
 
-typedef void  HIDCallback(u32 handle,s32 error,u8 *p_buffer,u32 bytes_transferred,void *p_user);
+typedef void (*HIDCallback)(uint32_t handle,int32_t error,uint8_t *p_buffer,uint32_t bytes_transferred,void *p_user);
 
-s32 HIDSetup(void);
-s32 HIDTeardown(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-s32 HIDAddClient(HIDClient *p_client, HIDAttachCallback attach_callback);
-s32 HIDDelClient(HIDClient *p_client);
 
-s32 HIDGetDescriptor(u32 handle,u8 descriptor_type,u8 descriptor_index, u16 language_id, u8 *p_buffer, u32 buffer_length, HIDCallback hc, void *p_user);
-s32 HIDSetDescriptor(u32 handle,u8 descriptor_type,u8 descriptor_index, u16 language_id, u8 *p_buffer, u32 buffer_length, HIDCallback hc, void *p_user);
+int32_t
+HIDSetup(void);
 
-s32 HIDGetReport(u32 handle, u8 report_type, u8 report_id, u8 *p_buffer, u32 buffer_length, HIDCallback hc, void *p_user);
-s32 HIDSetReport(u32 handle, u8 report_type, u8 report_id, u8 *p_buffer, u32 buffer_length, HIDCallback hc, void *p_user);
+int32_t 
+HIDTeardown(void);
 
-s32 HIDSetIdle(u32 handle, u8 interface_index,u8 duration, HIDCallback hc, void *p_user);
+int32_t
+HIDAddClient(HIDClient *p_client,
+             HIDAttachCallback attach_callback);
+int32_t
+HIDDelClient(HIDClient *p_client);
 
-s32 HIDSetProtocol(u32 handle,u8 interface_index,u8 protocol, HIDCallback hc, void *p_user);
-s32 HIDGetProtocol(u32 handle,u8 interface_index,u8 * protocol, HIDCallback hc, void *p_user);
+int32_t
+HIDGetDescriptor(uint32_t handle,
+                 uint8_t descriptor_type,
+                 uint8_t descriptor_index, 
+                 uint16_t language_id, 
+                 uint8_t *p_buffer, 
+                 uint32_t buffer_length, 
+                 HIDCallback hc, 
+                 void *p_user);
+                        
+int32_t
+HIDSetDescriptor(uint32_t handle,
+                 uint8_t descriptor_type,
+                 uint8_t descriptor_index, 
+                 uint16_t language_id, 
+                 uint8_t *p_buffer, 
+                 uint32_t buffer_length, 
+                 HIDCallback hc, 
+                 void *p_user);
 
-s32 HIDRead(u32 handle, u8 *p_buffer, u32 buffer_length, HIDCallback hc, void *p_user);
-s32 HIDWrite(u32 handle, u8 *p_buffer, u32 buffer_length, HIDCallback hc, void *p_user);
+int32_t
+HIDGetReport(uint32_t handle, 
+             uint8_t report_type, 
+             uint8_t report_id, 
+             uint8_t *p_buffer, 
+             uint32_t buffer_length, 
+             HIDCallback hc, 
+             void *p_user);
+
+int32_t
+HIDSetReport(uint32_t handle, 
+             uint8_t report_type, 
+             uint8_t report_id, 
+             uint8_t *p_buffer, 
+             uint32_t buffer_length, 
+             HIDCallback hc, 
+             void *p_user);
+
+int32_t
+HIDSetIdle(uint32_t handle, 
+           uint8_t interface_index,
+           uint8_t duration, 
+           HIDCallback hc, 
+           void *p_user);
+
+int32_t
+HIDSetProtocol(uint32_t handle,
+               uint8_t int32_terface_index,
+               uint8_t protocol, 
+               HIDCallback hc, 
+               void *p_user);
+                    
+int32_t
+HIDGetProtocol(uint32_t handle,
+               uint8_t interface_index,
+               uint8_t * protocol, 
+               HIDCallback hc, 
+               void *p_user);
+
+int32_t
+HIDRead(uint32_t handle, 
+        uint8_t *p_buffer, 
+        uint32_t buffer_length, 
+        HIDCallback hc, 
+        void *p_user);
+                
+int32_t
+HIDWrite(uint32_t handle, 
+         uint8_t *p_buffer, 
+         uint32_t buffer_length, 
+         HIDCallback hc, 
+         void *p_user);
 
 #ifdef __cplusplus
 }
