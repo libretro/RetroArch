bcbcabffe09c198e9b4df1c01621b461ea4650c1
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-05-21 15:28:23 +0200

    Reorder the reboot and shutdown entries

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -4551,10 +4551,10 @@ bool menu_displaylist_ctl(enum menu_displaylist_ctl_state type, void *data)
                   PARSE_ACTION, false);
 #if defined(HAVE_LAKKA)
             menu_displaylist_parse_settings_enum(menu, info,
-                  MENU_ENUM_LABEL_SHUTDOWN,
+                  MENU_ENUM_LABEL_REBOOT,
                   PARSE_ACTION, false);
             menu_displaylist_parse_settings_enum(menu, info,
-                  MENU_ENUM_LABEL_REBOOT,
+                  MENU_ENUM_LABEL_SHUTDOWN,
                   PARSE_ACTION, false);
 #endif
             info->need_push    = true;
