a98c2c38c0b5b1a4cc8f78ff5dda2eb67b02062e
radius <andres.430@gmail.com>
2017-05-22 19:16:13 -0500

    update copyrights

--- frontend/drivers/platform_linux.c
+++ frontend/drivers/platform_linux.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2011-2017 - Daniel De Matteis
  * Copyright (C) 2012-2015 - Jason Fetters
  * Copyright (C) 2012-2015 - Michael Lelli
+ * Copyright (C) 2015-2017 - Andrés Suárez
  *
  * RetroArch is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Found-
