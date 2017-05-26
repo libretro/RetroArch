1d013b49c65356f3fe56f8c3655e883fe02ba65c
twinaphex <libretro@gmail.com>
2017-05-26 16:17:14 +0200

    Update parse_drive_list

--- frontend/drivers/platform_win32.c
+++ frontend/drivers/platform_win32.c
@@ -251,7 +251,7 @@ static int frontend_win32_parse_drive_list(void *data, bool load_content)
                drive,
                msg_hash_to_str(MENU_ENUM_LABEL_FILE_DETECT_CORE_LIST_PUSH_DIR),
                enum_idx,
-               MENU_SETTING_ACTION, 0, 0);
+               FILE_TYPE_DIRECTORY, 0, 0);
    }
 #endif
 
