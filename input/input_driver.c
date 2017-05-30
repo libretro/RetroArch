d849c30f3c5b36978fa2b09690766863723b05a1
radius <andres.430@gmail.com>
2017-05-30 00:46:39 -0500

    this seems to fix playback on windows but now the movie never ends

--- input/input_driver.c
+++ input/input_driver.c
@@ -348,7 +348,6 @@ int16_t input_state(unsigned port, unsigned device,
       int16_t bsv_result;
       if (bsv_movie_get_input(&bsv_result))
          return bsv_result;
-
       bsv_movie_ctl(BSV_MOVIE_CTL_SET_END, NULL);
    }
 
