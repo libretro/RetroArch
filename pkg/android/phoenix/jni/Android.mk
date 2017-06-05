8f55e77afedc01777fa48a1464c816d68c3344ad
radius <andres.430@gmail.com>
2017-06-04 20:55:51 -0500

    disable NEON

--- pkg/android/phoenix/jni/Android.mk
+++ pkg/android/phoenix/jni/Android.mk
@@ -43,16 +43,7 @@ DEFINES += -DANDROID_ARM_V7
 endif
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-
-ifeq ($(HAVE_NEON),1)
-	DEFINES += -D__ARM_NEON__
-   LOCAL_SRC_FILES += $(LIBRETRO_COMM_DIR)/audio/conversion/s16_to_float_neon.S.neon \
-							 $(LIBRETRO_COMM_DIR)/audio/conversion/float_to_s16_neon.S.neon \
-							 $(LIBRETRO_COMM_DIR)/audio/resampler/drivers/sinc_resampler_neon.S.neon \
-							 $(RARCH_DIR)/audio/drivers_resampler/cc_resampler_neon.S.neon
-endif
-DEFINES += -DSINC_LOWER_QUALITY 
-DEFINES += -DANDROID_AARCH64
+   DEFINES += -DANDROID_AARCH64
 endif
 
 ifeq ($(TARGET_ARCH),mips)
