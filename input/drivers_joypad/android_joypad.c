4a5593255fe2597698ab10ef1bd2a3e6b4fbcb33
twinaphex <libretro@gmail.com>
2017-06-07 21:10:06 +0200

    (android_joypad.c) Small cleanup

--- input/drivers_joypad/android_joypad.c
+++ input/drivers_joypad/android_joypad.c
@@ -30,14 +30,12 @@ static bool android_joypad_init(void *data)
 
 static bool android_joypad_button(unsigned port, uint16_t joykey)
 {
-   unsigned hat_dir                = 0;
    uint8_t *buf                    = android_keyboard_state_get(port);
    struct android_app *android_app = (struct android_app*)g_android;
+   unsigned hat_dir                = GET_HAT_DIR(joykey);
 
    if (port >= MAX_PADS)
       return false;
-
-   hat_dir = GET_HAT_DIR(joykey);
 
    if (hat_dir)
    {
