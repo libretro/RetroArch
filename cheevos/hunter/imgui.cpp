436d28f18c609c03567604fcc2ab6f78c4224718
radius <andres.430@gmail.com>
2017-07-02 15:00:26 -0500

    stub this assert for now

--- cheevos/hunter/imgui.cpp
+++ cheevos/hunter/imgui.cpp
@@ -2131,7 +2131,10 @@ void ImGui::NewFrame()
     ImGuiContext& g = *GImGui;
 
     // Check user data
+    /* TODO: fixme 
+       some user data seems to be retained accross content loads so this throws an assert, neeed to figure how to cleanup properly
     IM_ASSERT(g.IO.DeltaTime >= 0.0f);               // Need a positive DeltaTime (zero is tolerated but will cause some timing issues)
+    */
     IM_ASSERT(g.IO.DisplaySize.x >= 0.0f && g.IO.DisplaySize.y >= 0.0f);
     IM_ASSERT(g.IO.Fonts->Fonts.Size > 0);           // Font Atlas not created. Did you call io.Fonts->GetTexDataAsRGBA32 / GetTexDataAsAlpha8 ?
     IM_ASSERT(g.IO.Fonts->Fonts[0]->IsLoaded());     // Font Atlas not created. Did you call io.Fonts->GetTexDataAsRGBA32 / GetTexDataAsAlpha8 ?
