058dc172c7814af42185ff5325c615adb07d5faf
twinaphex <libretro@gmail.com>
2017-07-20 04:37:19 +0100

    Update

--- libretro-common/include/streams/file_stream_transforms.h
+++ libretro-common/include/streams/file_stream_transforms.h
@@ -1,7 +1,7 @@
 /* Copyright  (C) 2010-2017 The RetroArch team
 *
 * ---------------------------------------------------------------------------------------
-* The following license statement only applies to this file (file_stream.h).
+* The following license statement only applies to this file (file_stream_transforms.h).
 * ---------------------------------------------------------------------------------------
 *
 * Permission is hereby granted, free of charge,
