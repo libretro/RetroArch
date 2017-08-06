e3192ad3c511ed88b999604d904022631117c015
twinaphex <libretro@gmail.com>
2017-08-06 13:55:32 +0200

    Get rid of some incompatible implicit declaration warnings

--- libretro-common/file/retro_dirent.c
+++ libretro-common/file/retro_dirent.c
@@ -20,8 +20,9 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <retro_common.h>
 
