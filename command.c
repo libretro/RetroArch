a87fc9cd6d4c1735cb591efbeac3fdb1d9d37006
Daniel Jimenez <daniel.jimenez.gomez@gmail.com>
2017-08-12 03:03:49 -0700

    Fix regression for builds with no network support

--- command.c
+++ command.c
@@ -1865,13 +1865,17 @@ bool command_event(enum event_command cmd, void *data)
             if (settings->bools.cheevos_hardcore_mode_enable)
                return false;
 #endif
+           if (settings->bools.rewind_enable)
+           {
 #ifdef HAVE_NETWORKING
             /* Only enable state manager if netplay is not underway 
                TODO: Add a setting for these tweaks */
-            if (settings->bools.rewind_enable
-               && !netplay_driver_ctl(RARCH_NETPLAY_CTL_IS_ENABLED, NULL))
+            if (!netplay_driver_ctl(RARCH_NETPLAY_CTL_IS_ENABLED, NULL))
 #endif
+            {
                state_manager_event_init((unsigned)settings->rewind_buffer_size);
+            }
+           }
          }
          break;
       case CMD_EVENT_REWIND_TOGGLE:
