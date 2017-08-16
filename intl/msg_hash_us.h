cf7fd7b34bfb843aab3bf0e509b0a5b573bfd85d
twinaphex <libretro@gmail.com>
2017-08-16 03:18:18 +0200

    Shorten sublabel for Information

--- intl/msg_hash_us.h
+++ intl/msg_hash_us.h
@@ -1840,7 +1840,7 @@ MSG_HASH(MENU_ENUM_SUBLABEL_NETPLAY,
 MSG_HASH(MENU_ENUM_SUBLABEL_NETPLAY_LAN_SCAN_SETTINGS,
       "Search for and connect to netplay hosts on the local network.")
 MSG_HASH(MENU_ENUM_SUBLABEL_INFORMATION_LIST_LIST,
-      "Display core, network, and system information.")
+      "Display system information.")
 MSG_HASH(MENU_ENUM_SUBLABEL_ONLINE_UPDATER,
       "Download add-ons, components, and content for RetroArch.")
 MSG_HASH(MENU_ENUM_SUBLABEL_SAMBA_ENABLE,
