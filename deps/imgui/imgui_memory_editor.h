8f6c6ed4f0e765e70d967a5a5a123cba621af8bd
leiradel <andre@leiradella.com>
2017-08-20 23:54:08 +0100

    Added navigation through the highlighted addresses

--- deps/imgui/imgui_memory_editor.h
+++ deps/imgui/imgui_memory_editor.h
@@ -1,5 +1,6 @@
 // Mini memory editor for Dear ImGui (to embed in your game/tools)
 // Animated GIF: https://twitter.com/ocornut/status/894242704317530112
+// Get latest version at http://www.github.com/ocornut/imgui_club
 //
 // You can adjust the keyboard repeat delay/rate in ImGuiIO.
 // The code assume a mono-space font for simplicity! If you don't use the default font, use ImGui::PushFont()/PopFont() to switch to a mono-space font before caling this.
@@ -28,6 +29,11 @@
 // - v0.20: added options menu. added OptShowAscii checkbox. added optional HexII display. split Draw() in DrawWindow()/DrawContents(). fixing glyph width. refactoring/cleaning code.
 // - v0.21: fixes for using DrawContents() in our own window. fixed HexII to actually be useful and not on the wrong side.
 // - v0.22: clicking Ascii view select the byte in the Hex view. Ascii view highlight selection.
+// - v0.23: fixed right-arrow triggering a byte write
+// - v0.24: added buttons to cycle through highlighted addresses, if the highlight callback is provided.
+//
+// Todo/Bugs:
+// - Arrows are being sent to the InputText() about to disappear which for LeftArrow makes the text cursor appear at position 1 for one frame.
 
 #pragma once
 #include <stdio.h>  // sprintf, scanf
@@ -56,7 +62,7 @@ struct MemoryEditor
     bool            DataEditingTakeFocus;
     char            DataInputBuf[32];
     char            AddrInputBuf[32];
-    size_t          GotoAddr;
+    size_t          GotoAddr, HighlightAddr;
     size_t          HighlightMin, HighlightMax;
 
     MemoryEditor()
@@ -83,6 +89,7 @@ struct MemoryEditor
         memset(AddrInputBuf, 0, sizeof(AddrInputBuf));
         GotoAddr = (size_t)-1;
         HighlightMin = HighlightMax = (size_t)-1;
+        HighlightAddr = (size_t)-1;
     }
 
     void GotoAddrAndHighlight(size_t addr_min, size_t addr_max)
@@ -282,6 +289,8 @@ struct MemoryEditor
                     ImGui::PopItemWidth();
                     if (user_data.CursorPos >= 2)
                         data_write = data_next = true;
+                    if (data_editing_addr_next != (size_t)-1)
+                        data_write = data_next = false;
                     int data_input_value;
                     if (data_write && sscanf(DataInputBuf, "%X", &data_input_value) == 1)
                     {
@@ -394,6 +403,77 @@ struct MemoryEditor
             }
         }
         ImGui::PopItemWidth();
+
+        if (HighlightFn)
+        {
+            ImGui::SameLine();
+
+            if (ImGui::Button("<"))
+            {
+                size_t start;
+                bool set = false;
+                size_t addr;
+
+                if (HighlightAddr == 0 || HighlightAddr == (size_t)-1) start = mem_size - 1;
+                else start = HighlightAddr - 1;
+
+                for (addr = start; addr != (size_t)-1; addr--)
+                {
+                    if (HighlightFn(mem_data, addr))
+                    {
+                        HighlightAddr = GotoAddr = addr;
+                        set = true;
+                        break;
+                    }
+                }
+
+                if (!set)
+                {
+                    for (addr = mem_size - 1; addr > start; addr--)
+                    {
+                        if (HighlightFn(mem_data, addr))
+                        {
+                            HighlightAddr = GotoAddr = addr;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            ImGui::SameLine();
+
+            if (ImGui::Button(">"))
+            {
+                size_t start;
+                bool set = false;
+                size_t addr;
+
+                if (HighlightAddr == mem_size - 1 || HighlightAddr == (size_t)-1) start = 0;
+                else start = HighlightAddr + 1;
+
+                for (addr = start; addr < mem_size; addr++)
+                {
+                    if (HighlightFn(mem_data, addr))
+                    {
+                        HighlightAddr = GotoAddr = addr;
+                        set = true;
+                        break;
+                    }
+                }
+
+                if (!set)
+                {
+                    for (addr = 0; addr < start; addr++)
+                    {
+                        if (HighlightFn(mem_data, addr))
+                        {
+                            HighlightAddr = GotoAddr = addr;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
         if (GotoAddr != (size_t)-1)
         {
