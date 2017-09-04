c96b8433306aa2fe3952e1a98fc2cedbcf17a9fe
twinaphex <libretro@gmail.com>
2017-09-04 23:50:13 +0200

    xdk_renderchain.cpp should become the D3D8 renderchain implementation (for Xbox OG/PC); xdk360_renderchain.cpp will become the D3D9 HLSL one (for 360/PC)

--- gfx/drivers_renderchain/xdk360_renderchain.cpp
+++ gfx/drivers_renderchain/xdk360_renderchain.cpp
@@ -34,7 +34,11 @@ typedef struct xdk_renderchain
    LPDIRECT3DVERTEXBUFFER vertex_buf;
    unsigned last_width;
    unsigned last_height;
+#ifdef HAVE_D3D9
+   LPDIRECT3DVERTEXDECLARATION vertex_decl;
+#else
    void *vertex_decl;
+#endif
    unsigned tex_w;
    unsigned tex_h;
    uint64_t frame_count;
@@ -43,10 +47,21 @@ typedef struct xdk_renderchain
 static void renderchain_set_mvp(void *data, unsigned vp_width,
       unsigned vp_height, unsigned rotation)
 {
+#if defined(HAVE_CG) || defined(HAVE_GLSL) || defined(HAVE_HLSL)
+   video_shader_ctx_mvp_t mvp;
+#endif
    d3d_video_t      *d3d = (d3d_video_t*)data;
    LPDIRECT3DDEVICE d3dr = (LPDIRECT3DDEVICE)d3d->dev;
-   D3DXMATRIX p_out, p_rotate, mat;
 
+#if defined(_XBOX360) && defined(HAVE_HLSL)
+   hlsl_set_proj_matrix(XMMatrixRotationZ(rotation * (M_PI / 2.0)));
+
+   mvp.data   = d3d;
+   mvp.matrix = NULL;
+
+   video_shader_driver_set_mvp(mvp);
+#elif defined(HAVE_D3D8)
+   D3DXMATRIX p_out, p_rotate, mat;
    D3DXMatrixOrthoOffCenterLH(&mat, 0, vp_width,  vp_height, 0, 0.0f, 1.0f);
    D3DXMatrixIdentity(&p_out);
    D3DXMatrixRotationZ(&p_rotate, rotation * (M_PI / 2.0));
@@ -54,6 +69,7 @@ static void renderchain_set_mvp(void *data, unsigned vp_width,
    d3d_set_transform(d3dr, D3DTS_WORLD, &p_rotate);
    d3d_set_transform(d3dr, D3DTS_VIEW, &p_out);
    d3d_set_transform(d3dr, D3DTS_PROJECTION, &p_out);
+#endif
 }
 
 static void xdk_renderchain_clear(void *data)
@@ -62,6 +78,30 @@ static void xdk_renderchain_clear(void *data)
 
    d3d_texture_free(chain->tex);
    d3d_vertex_buffer_free(chain->vertex_buf, chain->vertex_decl);
+}
+
+static bool xdk_renderchain_init_shader_fvf(void *data, void *pass_data)
+{
+   d3d_video_t *d3d         = (d3d_video_t*)data;
+   d3d_video_t *pass        = (d3d_video_t*)data;
+   LPDIRECT3DDEVICE d3dr    = (LPDIRECT3DDEVICE)d3d->dev;
+   xdk_renderchain_t *chain = (xdk_renderchain_t*)d3d->renderchain_data;
+
+   (void)pass_data;
+
+#if defined(_XBOX360)
+   static const D3DVERTEXELEMENT VertexElements[] =
+   {
+      { 0, 0 * sizeof(float), D3DDECLTYPE_FLOAT2, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_POSITION, 0 },
+      { 0, 2 * sizeof(float), D3DDECLTYPE_FLOAT2, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_TEXCOORD, 0 },
+      D3DDECL_END()
+   };
+
+   if (FAILED(d3dr->CreateVertexDeclaration(VertexElements, &chain->vertex_decl)))
+      return false;
+#endif
+
+   return true;
 }
 
 static bool renderchain_create_first_pass(void *data,
@@ -94,12 +134,21 @@ static bool renderchain_create_first_pass(void *data,
    d3d_set_render_state(d3dr, D3DRS_CULLMODE, D3DCULL_NONE);
    d3d_set_render_state(d3dr, D3DRS_ZENABLE, FALSE);
 
+   if (!xdk_renderchain_init_shader_fvf(chain, chain))
+      return false;
+
    return true;
 }
 
 static void renderchain_set_vertices(void *data, unsigned pass,
       unsigned vert_width, unsigned vert_height, uint64_t frame_count)
 {
+#if defined(HAVE_CG) || defined(HAVE_GLSL) || defined(HAVE_HLSL)
+#ifdef _XBOX
+   video_shader_ctx_params_t params;
+   video_shader_ctx_info_t shader_info;
+#endif
+#endif
    unsigned width, height;
    d3d_video_t *d3d         = (d3d_video_t*)data;
    xdk_renderchain_t *chain = d3d ? (xdk_renderchain_t*)d3d->renderchain_data : NULL;
@@ -120,6 +169,10 @@ static void renderchain_set_vertices(void *data, unsigned pass,
 
       float tex_w        = vert_width;
       float tex_h        = vert_height;
+#ifdef _XBOX360
+      tex_w             /= ((float)chain->tex_w);
+      tex_h             /= ((float)chain->tex_h);
+#endif
 
       vert[0].x        = -1.0f;
       vert[1].x        =  1.0f;
@@ -130,6 +183,7 @@ static void renderchain_set_vertices(void *data, unsigned pass,
       vert[1].y        = -1.0f;
       vert[2].y        =  1.0f;
       vert[3].y        =  1.0f;
+#if defined(_XBOX1)
       vert[0].z        =  1.0f;
       vert[1].z        =  1.0f;
       vert[2].z        =  1.0f;
@@ -149,6 +203,17 @@ static void renderchain_set_vertices(void *data, unsigned pass,
       vert[1].v        = 0.0f;
       vert[2].v        = 0.0f;
       vert[3].v        = 0.0f;
+#elif defined(_XBOX360)
+      vert[0].u        = 0.0f;
+      vert[1].u        = tex_w;
+      vert[2].u        = 0.0f;
+      vert[3].u        = tex_w;
+
+      vert[0].v        = tex_h;
+      vert[1].v        = tex_h;
+      vert[2].v        = 0.0f;
+      vert[3].v        = 0.0f;
+#endif
 
       /* Align texels and vertices. */
       for (i = 0; i < 4; i++)
@@ -161,6 +226,34 @@ static void renderchain_set_vertices(void *data, unsigned pass,
       memcpy(verts, vert, sizeof(vert));
       d3d_vertex_buffer_unlock(chain->vertex_buf);
    }
+
+#if defined(HAVE_CG) || defined(HAVE_GLSL) || defined(HAVE_HLSL)
+#ifdef _XBOX
+   renderchain_set_mvp(d3d, width, height, d3d->dev_rotation);
+
+   shader_info.data = d3d;
+   shader_info.idx  = pass;
+   shader_info.set_active = true;
+
+   video_shader_driver_use(shader_info);
+
+   params.data          = d3d;
+   params.width         = vert_width;
+   params.height        = vert_height;
+   params.tex_width     = chain->tex_w;
+   params.tex_height    = chain->tex_h;
+   params.out_width     = width;
+   params.out_height    = height;
+   params.frame_counter = (unsigned int)frame_count;
+   params.info          = NULL;
+   params.prev_info     = NULL;
+   params.feedback_info = NULL;
+   params.fbo_info      = NULL;
+   params.fbo_info_cnt  = 0;
+
+   video_shader_driver_set_parameters(params);
+#endif
+#endif
 }
 
 static void renderchain_blit_to_texture(void *data, const void *frame,
@@ -194,6 +287,12 @@ static void xdk_renderchain_deinit(void *data)
       free(renderchain);
 }
 
+static void xdk_renderchain_deinit_shader(void *data)
+{
+   (void)data;
+   /* stub */
+}
+
 static void xdk_renderchain_free(void *data)
 {
    d3d_video_t *chain = (d3d_video_t*)data;
@@ -201,11 +300,14 @@ static void xdk_renderchain_free(void *data)
    if (!chain)
       return;
 
+   xdk_renderchain_deinit_shader(chain);
    xdk_renderchain_deinit(chain->renderchain_data);
    xdk_renderchain_clear(chain->renderchain_data);
 
+#ifndef _XBOX
    if (chain->tracker)
       state_tracker_free(chain->tracker);
+#endif
 }
 
 
@@ -216,6 +318,29 @@ void *xdk_renderchain_new(void)
       return NULL;
 
    return renderchain;
+}
+
+static bool xdk_renderchain_init_shader(void *data, void *renderchain_data)
+{
+   d3d_video_t        *d3d = (d3d_video_t*)data;
+#if defined(HAVE_HLSL)
+   const char *shader_path = NULL;
+   settings_t *settings    = config_get_ptr();
+#endif
+   (void)renderchain_data;
+
+   if (!d3d)
+      return false;
+
+#if defined(HAVE_HLSL)
+   RARCH_LOG("D3D]: Using HLSL shader backend.\n");
+   shader_path = settings->path.shader;
+   const shader_backend_t *shader = &hlsl_backend;
+
+   return video_shader_driver_init(hlsl_backend, d3d, shader_path); 
+#endif
+
+   return true;
 }
 
 static bool xdk_renderchain_init(void *data,
@@ -236,6 +361,9 @@ static bool xdk_renderchain_init(void *data,
    struct video_viewport *custom_vp = video_viewport_get_custom();
    (void)final_viewport_data;
    
+   if (!xdk_renderchain_init_shader(d3d, NULL))
+      return false;
+
    video_driver_get_size(&width, &height);
 
    chain->dev                   = (LPDIRECT3DDEVICE)dev_data;
