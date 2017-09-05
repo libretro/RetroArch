56c66376ff4165610050a77cec39a3a9ed570879
twinaphex <libretro@gmail.com>
2017-09-06 00:47:38 +0200

    Update libco

--- libretro-common/libco/libco.c
+++ libretro-common/libco/libco.c
@@ -4,20 +4,31 @@
   license: public domain
 */
 
-#if defined(__GNUC__) && defined(__i386__) || (defined(_MSC_VER) && defined(_M_IX86))
-  #include "x86.c"
-#elif defined(__GNUC__) && defined(__amd64__) || (defined(_MSC_VER) && defined(_M_AMD64))
-  #include "amd64.c"
-#elif defined(__GNUC__) && defined(_ARCH_PPC)
-  #include "ppc.c"
-#elif defined(__GNUC__) && defined(VITA)
-  #include "scefiber.c"
-#elif defined(__GNUC__) && (defined(__ARM_EABI__) || defined(__arm__))
-  #include "armeabi.c"
-#elif defined(__GNUC__)
-  #include "sjlj.c"
-#elif defined(_MSC_VER)
-  #include "fiber.c"
+#if defined _MSC_VER
+  #include <Windows.h>
+  #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
+    #include "fiber.c"
+  #elif defined _M_IX86
+    #include "x86.c"
+  #elif defined _M_AMD64
+    #include "amd64.c"
+  #else
+    #include "fiber.c"
+  #endif
+#elif defined __GNUC__
+  #if defined __i386__
+    #include "x86.c"
+  #elif defined __amd64__
+    #include "amd64.c"
+  #elif defined _ARCH_PPC
+    #include "ppc.c"
+  #elif defined VITA
+    #include "scefiber.c"
+  #elif defined(__ARM_EABI__) || defined(__arm__)
+    #include "armeabi.c"
+  #else
+    #include "sjlj.c"
+  #endif
 #else
   #error "libco: unsupported processor, compiler or operating system"
 #endif
