971458c0393b9cd4f06df911b5a7937d623be5a4
radius <andres.430@gmail.com>
2017-09-17 12:44:17 -0500

    only set_controller=true for the number of ports the core supports

--- command.c
+++ command.c
@@ -1018,10 +1018,10 @@ static void command_event_init_controllers(void)
             set_controller = true;
             break;
          case RETRO_DEVICE_JOYPAD:
-            /* ideally this check wouldn't be required but if we always
+            /* ideally these checks shouldn't be required but if we always
              *  call core_set_controller_port_device input won't work on 
-             *  cores that don't set port information */
-            if (info->ports.size != 0)
+             *  cores that don't set port information properly */
+            if (info->ports.size != 0 && i < info->ports.size)
                set_controller = true;
             break;
          default:
