94830e80d6d2d75f79d0dcec23f2319a30929970
Brian Koropoff <bkoropoff@gmail.com>
2017-09-17 02:38:54 -0700

    clang supports GCC block expressions

--- deps/libFLAC/include/private/macros.h
+++ deps/libFLAC/include/private/macros.h
@@ -32,7 +32,7 @@
 #ifndef FLAC__PRIVATE__MACROS_H
 #define FLAC__PRIVATE__MACROS_H
 
-#if defined(__GNUC__) && (__GNUC__ > 4 || ( __GNUC__ == 4 && __GNUC_MINOR__ >= 3))
+#if defined(__GNUC__) && (defined(__clang__) || __GNUC__ > 4 || ( __GNUC__ == 4 && __GNUC_MINOR__ >= 3))
 
 #define flac_max(a,b) \
 	({ __typeof__ (a) _a = (a); \
