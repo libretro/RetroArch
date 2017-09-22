d03ce027880513c327d768bc19f91c87a80dd393
twinaphex <libretro@gmail.com>
2017-09-22 04:59:20 +0200

    Update deps/libFLAC

--- deps/libFLAC/md5.c
+++ deps/libFLAC/md5.c
@@ -499,8 +499,8 @@ FLAC__bool FLAC__MD5Accumulate(FLAC__MD5Context *ctx, const FLAC__int32 * const 
 		return false;
 
 	if (ctx->capacity < bytes_needed) {
-		if (0 == (ctx->internal_buf.p8 = safe_realloc_(ctx->internal_buf.p8, bytes_needed))) {
-			if (0 == (ctx->internal_buf.p8 = safe_malloc_(bytes_needed))) {
+		if (0 == (ctx->internal_buf.p8 = (FLAC__byte*)safe_realloc_(ctx->internal_buf.p8, bytes_needed))) {
+			if (0 == (ctx->internal_buf.p8 = (FLAC__byte*)safe_malloc_(bytes_needed))) {
 				ctx->capacity = 0;
 				return false;
 			}
