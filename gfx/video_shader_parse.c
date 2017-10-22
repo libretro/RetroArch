d203e4dc9c0ed55ec46ad6a2539aee224eba0b39
twinaphex <libretro@gmail.com>
2017-10-22 05:21:31 +0200

    video_shader_driver_resolve_parameters - fix crash at startup with MSVC

--- gfx/video_shader_parse.c
+++ gfx/video_shader_parse.c
@@ -538,7 +538,8 @@ bool video_shader_resolve_parameters(config_file_t *conf,
       /* If that doesn't work, fallback to the old path.
        * Ideally, we'd get rid of this path sooner or later. */
 #endif
-      file = filestream_open(path, RFILE_MODE_READ_TEXT, -1);
+	  if (!string_is_empty(path))
+         file = filestream_open(path, RFILE_MODE_READ_TEXT, -1);
 
       if (!file)
       {
