28ec3ac3b55e7d23de3d4dc7c8d4d1056ef6e85a
Brad Parker <cbparker@gmail.com>
2017-10-30 16:48:34 -0400

    temporary fix for startup hang on Windows

--- libretro-common/streams/file_stream.c
+++ libretro-common/streams/file_stream.c
@@ -474,6 +474,14 @@ error:
 
 int filestream_eof(RFILE *stream)
 {
+   return feof(stream->fp);
+
+   /* TODO: FIXME: I can't figure out why this breaks on Windows.
+      The while loop in config_file_new_internal just never exits.
+      The current position seems to jump backwards a few lines,
+      but it doesn't start until somewhere in the middle of the file.
+    */
+   /*
    size_t current_position = filestream_tell(stream);
    size_t end_position;
 
@@ -485,6 +493,7 @@ int filestream_eof(RFILE *stream)
    if (current_position >= end_position)
       return 1;
    return 0;
+   */
 }
 
 ssize_t filestream_tell(RFILE *stream)
