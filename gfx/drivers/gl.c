5eadf021cf51d61a81b6b2e03494ce0a88d402fa
twinaphex <libretro@gmail.com>
2017-11-12 17:16:14 +0100

    early return

--- gfx/drivers/gl.c
+++ gfx/drivers/gl.c
@@ -449,7 +449,7 @@ GLenum min_filter_to_mag(GLenum type)
 static uintptr_t gl_get_current_framebuffer(void *data)
 {
    gl_t *gl = (gl_t*)data;
-   if (!gl->fbo_inited)
+   if (!gl || !gl->fbo_inited)
       return NULL;
    return gl->hw_render_fbo[(gl->tex_index + 1) % gl->textures];
 }
