e4df20656a6c54555eac5384e884b79e1231dffe
David Walters <contact@davidwaltersdevelopment.com>
2017-11-27 21:52:21 +0000

    change prototype for hid_driver :: get_buttons

--- input/input_driver.h
+++ input/input_driver.h
@@ -189,7 +189,7 @@ struct hid_driver
    bool (*query_pad)(void *, unsigned);
    void (*free)(void *);
    bool (*button)(void *, unsigned, uint16_t);
-   uint64_t (*get_buttons)(void *, unsigned);
+   void (*get_buttons)(void *, unsigned, retro_bits *);
    int16_t (*axis)(void *, unsigned, uint32_t);
    void (*poll)(void *);
    bool (*set_rumble)(void *, unsigned, enum retro_rumble_effect, uint16_t);
