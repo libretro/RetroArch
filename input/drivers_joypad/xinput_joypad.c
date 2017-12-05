408878a7e10685a11b7956d821160c68211922fe
twinaphex <libretro@gmail.com>
2017-12-05 20:04:12 +0100

    Start making it possible for xinput_joypad to be used without DirectInput

--- input/drivers_joypad/xinput_joypad.c
+++ input/drivers_joypad/xinput_joypad.c
@@ -40,11 +40,9 @@
 
 #include "../../verbosity.h"
 
-#ifndef HAVE_DINPUT
-#error Cannot compile xinput without dinput.
-#endif
-
+#ifdef HAVE_DINPUT
 #include "dinput_joypad.h"
+#endif
 
 /* Check if the definitions do not already exist.
  * Official and mingw xinput headers have different include guards.
@@ -159,6 +157,8 @@ static const char* const XBOX_ONE_CONTROLLER_NAMES[4] =
 const char *xinput_joypad_name(unsigned pad)
 {
    int xuser = pad_index_to_xuser_index(pad);
+
+#ifdef HAVE_DINPUT
    /* Use the real controller name for XBOX One controllers since
       they are slightly different  */
    if (xuser < 0)
@@ -166,6 +166,7 @@ const char *xinput_joypad_name(unsigned pad)
 
    if (strstr(dinput_joypad.name(pad), "Xbox One For Windows"))
       return XBOX_ONE_CONTROLLER_NAMES[xuser];
+#endif
 
    return XBOX_CONTROLLER_NAMES[xuser];
 }
@@ -262,6 +263,7 @@ static bool xinput_joypad_init(void *data)
 #endif
    g_xinput_block_pads = true;
 
+#ifdef HAVE_DINPUT
    /* We're going to have to be buddies with dinput if we want to be able
     * to use XInput and non-XInput controllers together. */
    if (!dinput_joypad.init(data))
@@ -269,6 +271,7 @@ static bool xinput_joypad_init(void *data)
       g_xinput_block_pads = false;
       return false;
    }
+#endif
 
    for (j = 0; j < MAX_USERS; j++)
    {
@@ -279,13 +282,16 @@ static bool xinput_joypad_init(void *data)
 
       if (pad_index_to_xuser_index(j) > -1)
       {
-         int vid = 0;
-         int pid = 0;
+         int vid          = 0;
+         int pid          = 0;
+#ifdef HAVE_DINPUT
          int dinput_index = 0;
-         bool success = dinput_joypad_get_vidpid_from_xinput_index(j, &vid, &pid, &dinput_index);
+         bool success     = dinput_joypad_get_vidpid_from_xinput_index(j, &vid, &pid, &dinput_index);
 
          if (success)
-            RARCH_LOG("[XInput]: Found VID/PID (%04X/%04X) from DINPUT index %d for \"%s\", user #%u\n", vid, pid, dinput_index, xinput_joypad_name(j), j);
+            RARCH_LOG("[XInput]: Found VID/PID (%04X/%04X) from DINPUT index %d for \"%s\", user #%u\n",
+                  vid, pid, dinput_index, xinput_joypad_name(j), j);
+#endif
 
          if (!input_autoconfigure_connect(
                xinput_joypad_name(j),
@@ -306,7 +312,11 @@ static bool xinput_joypad_query_pad(unsigned pad)
    int xuser = pad_index_to_xuser_index(pad);
    if (xuser > -1)
       return g_xinput_states[xuser].connected;
+#ifdef HAVE_DINPUT
    return dinput_joypad.query_pad(pad);
+#else
+   return false;
+#endif
 }
 
 static void xinput_joypad_destroy(void)
@@ -322,7 +332,9 @@ static void xinput_joypad_destroy(void)
    g_XInputGetStateEx  = NULL;
    g_XInputSetState    = NULL;
 
+#ifdef HAVE_DINPUT
    dinput_joypad.destroy();
+#endif
 
    g_xinput_block_pads = false;
 }
@@ -351,8 +363,10 @@ static bool xinput_joypad_button(unsigned port_num, uint16_t joykey)
    unsigned hat_dir     = 0;
    int xuser            = pad_index_to_xuser_index(port_num);
 
+#ifdef HAVE_DINPUT
    if (xuser == -1)
       return dinput_joypad.button(port_num, joykey);
+#endif
 
    if (!(g_xinput_states[xuser].connected))
       return false;
@@ -400,8 +414,10 @@ static int16_t xinput_joypad_axis (unsigned port_num, uint32_t joyaxis)
 
    xuser = pad_index_to_xuser_index(port_num);
 
+#ifdef HAVE_DINPUT
    if (xuser == -1)
       return dinput_joypad.axis(port_num, joyaxis);
+#endif
 
    if (!(g_xinput_states[xuser].connected))
       return 0;
@@ -469,7 +485,9 @@ static void xinput_joypad_poll(void)
       }
    }
 
+#ifdef HAVE_DINPUT
    dinput_joypad.poll();
+#endif
 }
 
 static bool xinput_joypad_rumble(unsigned pad,
@@ -479,8 +497,10 @@ static bool xinput_joypad_rumble(unsigned pad,
 
    if (xuser == -1)
    {
+#ifdef HAVE_DINPUT
       if (dinput_joypad.set_rumble)
          return dinput_joypad.set_rumble(pad, effect, strength);
+#endif
       return false;
    }
 
