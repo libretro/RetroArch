f282044be2456091f60a570cad300fbf5cd260fa
Alcaro <floating@muncher.se>
2017-12-14 18:10:04 +0100

    this doesn't belong here.

--- libretro-common/vfs/vfs_implementation.c
+++ libretro-common/vfs/vfs_implementation.c
@@ -234,7 +234,7 @@ libretro_vfs_implementation_file *retro_vfs_file_open_impl(const char *path, uns
 
    if ((stream->hints & RFILE_HINT_UNBUFFERED) == 0 && mode_str)
    {
-      stream->fp = fopen(path_utf8, mode_str);
+      stream->fp = fopen_utf8(path, mode_str);
 
       if (!stream->fp)
          goto error;
