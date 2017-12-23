50ad1ab3d762259272fb92cdc81aea5185a10f07
Brad Parker <cbparker@gmail.com>
2017-12-22 23:40:03 -0500

    fix extra directories being made if filename from index has a path in it

--- menu/cbs/menu_cbs_ok.c
+++ menu/cbs/menu_cbs_ok.c
@@ -2823,6 +2823,7 @@ static int action_ok_download_generic(const char *path,
          break;
       case MENU_ENUM_LABEL_CB_CORE_UPDATER_DOWNLOAD:
          append = false;
+         path = path_basename(path);
          net_http_create_url(s3, settings->paths.network_buildbot_url, path, sizeof(s3));
          break;
       default:
