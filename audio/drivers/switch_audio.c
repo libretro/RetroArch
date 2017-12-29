258c8acdad12bac0fccd92922365a5a72fa97796
Twinaphex <libretro@gmail.com>
2017-12-29 02:28:34 +0100

    Update switch_audio.c

--- audio/drivers/switch_audio.c
+++ audio/drivers/switch_audio.c
@@ -1,6 +1,4 @@
 /*  RetroArch - A frontend for libretro.
- *  Copyright (C) 2014-2017 - Ali Bouhlel
- *  Copyright (C) 2016-2017 - FIX94
  *
  *  RetroArch is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU General Public License as published by the Free Software Found-
