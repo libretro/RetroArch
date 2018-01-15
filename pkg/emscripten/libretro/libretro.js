a682ccd8e8956725f1166454cc5dfd4aa2863f85
Toad King <toadking@toadking.com>
2018-01-14 21:02:59 -0600

    fix menu button

--- pkg/emscripten/libretro/libretro.js
+++ pkg/emscripten/libretro/libretro.js
@@ -393,31 +393,7 @@ function keyPress(k)
 }
 
 kp = function(k, event) {
-   var oEvent = document.createEvent('KeyboardEvent');
-
-   // Chromium Hack
-   Object.defineProperty(oEvent, 'keyCode', {
-      get : function() {
-         return this.keyCodeVal;
-      }
-   });
-   Object.defineProperty(oEvent, 'which', {
-      get : function() {
-         return this.keyCodeVal;
-      }
-   });
-
-   if (oEvent.initKeyboardEvent) {
-      oEvent.initKeyboardEvent(event, true, true, document.defaultView, false, false, false, false, k, k);
-   } else {
-      oEvent.initKeyEvent(event, true, true, document.defaultView, false, false, false, false, k, 0);
-   }
-
-   oEvent.keyCodeVal = k;
-
-   if (oEvent.keyCode !== k) {
-      alert("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ")");
-   }
+   var oEvent = new KeyboardEvent(event, { code: k });
 
    document.dispatchEvent(oEvent);
    document.getElementById('canvas').focus();
