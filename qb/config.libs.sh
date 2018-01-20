b9585df325234b9545903112f554b25e4fe39139
kwyxz <kwyxz@kwyxz.org>
2018-01-20 00:19:01 -0800

    Update Haiku port to allow it to build again

--- qb/config.libs.sh
+++ qb/config.libs.sh
@@ -10,10 +10,15 @@ add_define MAKEFILE NOUNUSED_VARIABLE "$HAVE_NOUNUSED_VARIABLE"
 [ -z "$CROSS_COMPILE" ] && [ -d /opt/local/lib ] && add_dirs LIBRARY /opt/local/lib
 
 [ "$GLOBAL_CONFIG_DIR" ] || \
-{	case "$PREFIX" in
-		/usr*) GLOBAL_CONFIG_DIR=/etc ;;
-		*) GLOBAL_CONFIG_DIR="$PREFIX"/etc ;;
-	esac
+{
+	if [ "$OS" = 'Haiku' ]; then
+		GLOBAL_CONFIG_DIR="$PREFIX"/settings
+	else
+		case "$PREFIX" in
+			/usr*) GLOBAL_CONFIG_DIR=/etc ;;
+			*) GLOBAL_CONFIG_DIR="$PREFIX"/etc ;;
+		esac
+	fi
 }
 
 DYLIB=-ldl;
@@ -157,9 +162,16 @@ fi
    add_define MAKEFILE libretro "$LIBRETRO"
 }
 
+
+if [ "$OS" = 'Haiku' ]; then
+add_define MAKEFILE ASSETS_DIR "${ASSETS_DIR:-${PREFIX}/data}"
+add_define MAKEFILE MAN_DIR "${MAN_DIR:-${PREFIX}/documentation/man}"
+else
 add_define MAKEFILE ASSETS_DIR "${ASSETS_DIR:-${PREFIX}/share}"
-add_define MAKEFILE BIN_DIR "${BIN_DIR:-${PREFIX}/bin}"
 add_define MAKEFILE MAN_DIR "${MAN_DIR:-${PREFIX}/share/man}"
+fi
+
+add_define MAKEFILE BIN_DIR "${BIN_DIR:-${PREFIX}/bin}"
 
 if [ "$OS" = 'DOS' ]; then
    HAVE_SHADERPIPELINE=no
