be64bf08e4218a7cc2835e818a30c3730e5b1c43
Lubosz Sarnecki <lubosz@gmail.com>
2018-01-24 07:55:00 +0100

    config.def.h: Toggle menu with start & select (- & +) on Switch.

--- config.def.h
+++ config.def.h
@@ -352,6 +352,8 @@ static bool default_screenshots_in_content_dir = false;
 static unsigned menu_toggle_gamepad_combo    = INPUT_TOGGLE_L3_R3;
 #elif defined(VITA)
 static unsigned menu_toggle_gamepad_combo    = INPUT_TOGGLE_L1_R1_START_SELECT;
+#elif defined(SWITCH)
+static unsigned menu_toggle_gamepad_combo    = INPUT_TOGGLE_START_SELECT;
 #else
 static unsigned menu_toggle_gamepad_combo    = INPUT_TOGGLE_NONE;
 #endif
