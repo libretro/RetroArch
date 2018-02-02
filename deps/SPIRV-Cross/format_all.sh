cd5852d3b0f87869f43d6ccd15291226a036ddfc
aliaspider <aliaspider@gmail.com>
2018-02-02 11:53:23 +0100

    use git subtree for deps/SPIRV-Cross

--- deps/SPIRV-Cross/format_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in spirv_*.{cpp,hpp} include/spirv_cross/*.{hpp,h} samples/cpp/*.cpp main.cpp
-do
-    echo "Formatting file: $file ..."
-    clang-format -style=file -i $file
-done
--- deps/SPIRV-Cross/shaders-hlsl/vert/point-size-compat.vert
+++
@@ -1,8 +0,0 @@
-#version 310 es
-
-void main()
-{
-	gl_Position = vec4(1.0);
-	gl_PointSize = 10.0;
-}
-
