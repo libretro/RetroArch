d8b5fea374666d7a1fe899b69f07d325f63ac18b
radius <andres.430@gmail.com>
2018-04-19 10:07:59 -0500

    fix coverity warnings

--- menu/cbs/menu_cbs_left.c
+++ menu/cbs/menu_cbs_left.c
@@ -403,7 +403,7 @@ static int action_left_input_desc_kbd(unsigned type, const char *label,
    if (key_id > 0)
       key_id--;
    else
-      key_id = RARCH_MAX_KEYS + MENU_SETTINGS_INPUT_DESC_KBD_BEGIN;
+      key_id = (RARCH_MAX_KEYS - 1) + MENU_SETTINGS_INPUT_DESC_KBD_BEGIN;
 
    settings->uints.input_keymapper_ids[offset][id] = key_descriptors[key_id].key;
 
