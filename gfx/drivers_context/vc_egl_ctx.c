6f4fd8859ac6d7b2b96f5a6d1232bb427e49127c
twinaphex <libretro@gmail.com>
2018-04-23 13:12:00 +0200

    Add customizable swapchain images flag to VC EGL context

--- gfx/drivers_context/vc_egl_ctx.c
+++ gfx/drivers_context/vc_egl_ctx.c
@@ -695,7 +695,7 @@ static gfx_ctx_proc_t gfx_ctx_vc_get_proc_address(const char *symbol)
 static uint32_t gfx_ctx_vc_get_flags(void *data)
 {
    uint32_t flags = 0;
-   BIT32_SET(flags, GFX_CTX_FLAGS_NONE);
+   BIT32_SET(flags, GFX_CTX_FLAGS_CUSTOMIZABLE_SWAPCHAIN_IMAGES);
    return flags;
 }
 
