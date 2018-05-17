9adc4d4a2ff5f8fbbb5c3ed38d07495f33e26ae7
twinaphex <libretro@gmail.com>
2018-05-17 19:48:16 +0200

    Another cleanup

--- gfx/drivers_renderchain/d3d9_cg_renderchain.c
+++ gfx/drivers_renderchain/d3d9_cg_renderchain.c
@@ -851,10 +851,11 @@ static bool d3d9_cg_renderchain_add_lut(void *data,
 }
 
 static void d3d9_cg_renderchain_calc_and_set_shader_mvp(
-      CGprogram vprg,
+      void *data, /* stock vertex program */
       unsigned vp_width, unsigned vp_height,
       unsigned rotation)
 {
+
    struct d3d_matrix proj, ortho, rot, matrix;
 
    d3d_matrix_ortho_off_center_lh(&ortho, 0, vp_width, 0, vp_height, 0, 1);
@@ -864,7 +865,7 @@ static void d3d9_cg_renderchain_calc_and_set_shader_mvp(
    d3d_matrix_multiply(&proj, &ortho, &rot);
    d3d_matrix_transpose(&matrix, &proj);
 
-   d3d9_cg_set_param_matrix(vprg, NULL, "modelViewProj", (const void*)&matrix);
+   d3d9_cg_set_param_matrix(data, NULL, "modelViewProj", (const void*)&matrix);
 }
 
 static void cg_d3d9_renderchain_set_vertices(
