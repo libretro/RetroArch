af2f679ff583f8ceafc26b45c7854bc511461ef4
Dwedit <danweiss@gmail.com>
2018-05-17 09:49:45 -0500

    Fix the reset command not closing the menu

--- command.c
+++ command.c
@@ -1862,7 +1862,7 @@ bool command_event(enum event_command cmd, void *data)
 #if HAVE_NETWORKING
          netplay_driver_ctl(RARCH_NETPLAY_CTL_RESET, NULL);
 #endif
-         break;
+         return false;
       case CMD_EVENT_SAVE_STATE:
          {
             settings_t *settings      = config_get_ptr();
