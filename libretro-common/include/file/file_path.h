23b0444afe57e3b56ee1fce26fa36fb4be9c0ea0
twinaphex <libretro@gmail.com>
2018-05-28 17:30:32 +0200

    Update libretro-common

--- libretro-common/include/file/file_path.h
+++ libretro-common/include/file/file_path.h
@@ -100,11 +100,15 @@ const char *path_get_extension(const char *path);
  * path_remove_extension:
  * @path               : path
  *
- * Removes the extension from the path and returns the result.
- * Removes all text after and including the last '.'.
+ * Mutates path by removing its extension. Removes all
+ * text after and including the last '.'.
  * Only '.'s after the last slash are considered.
  *
- * Returns: path with the extension part removed.
+ * Returns: 
+ * 1) If path has an extension, returns path with the
+ *    extension removed.
+ * 2) If there is no extension, returns NULL.
+ * 3) If path is empty or NULL, returns NULL
  */
 char *path_remove_extension(char *path);
 
