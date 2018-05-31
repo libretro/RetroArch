libretro/RetroArch

commit 822e2fd0d49fffff6d1538524678287d4ce97565

Put this around an HW_RVL ifdef

frontend/drivers/platform_gx.c
@@ -309,7 +309,9 @@ static void frontend_gx_init(void *data)
    __exception_setreload(8);
 #endif
 
+#ifdef HW_RVL
    fatInitDefault();
+#endif
 
 #ifdef HAVE_LOGGER
    devoptab_list[STD_OUT] = &dotab_stdout;

commit 3f8235db2869773dafd0ccc70fd766ea9cf2a596

revert commit 822e2fd
commit 822e2fd broke sd gecko mounting on gamecube, making the gamecube build unusable, reverting it makes it run fine again

frontend/drivers/platform_gx.c
@@ -310,9 +310,7 @@ static void frontend_gx_init(void *data)
    __exception_setreload(8);
 #endif
 
-#ifdef HW_RVL
    fatInitDefault();
-#endif
 
 #ifdef HAVE_LOGGER
    devoptab_list[STD_OUT] = &dotab_stdout;