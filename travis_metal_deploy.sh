c64a75c58aece70a0679281d4668bcb87c0b0b75
Brad Parker <cbparker@gmail.com>
2018-09-03 19:50:04 -0400

    travis: clean up OSX Metal image and add assets

--- travis_metal_deploy.sh
+++ travis_metal_deploy.sh
@@ -10,14 +10,28 @@ openssl aes-256-cbc -K $encrypted_e9bb4da59666_key -iv $encrypted_e9bb4da59666_i
 
 chmod 600 ~/.ssh/id_rsa
 
+mkdir ~/dist
+cd ~/dist
+
+echo "Copying binary into dist folder..."
+
+cp -rv ~/libretro-super/retroarch/pkg/apple/build/Release/RetroArch.app .
+
+echo "Downloading assets..."
+
+cd RetroArch.app/Contents/Resources/
+curl -O http://bot.libretro.com/assets/frontend/bundle.zip
+unzip -o bundle.zip
+rm -rf bundle.zip
+
 echo "Creating DMG image..."
 
-cd ${TRAVIS_BUILD_DIR}/pkg/apple/build/Release
+cd ~/dist
 
 FILENAME=$(date +%F)_RetroArch_Metal.dmg
 
-hdiutil create -volname RetroArch -srcfolder ./ -ov -format UDZO ${FILENAME}
+hdiutil create -volname RetroArch -srcfolder ./ -ov -format UDZO ~/${FILENAME}
 
 echo "Uploading to server..."
 
-rsync -avhP -e 'ssh -p 12346 -o StrictHostKeyChecking=no' ${FILENAME} travis@bot.libretro.com:~/nightly/apple/osx/x86_64/
+rsync -avhP -e 'ssh -p 12346 -o StrictHostKeyChecking=no' ~/${FILENAME} travis@bot.libretro.com:~/nightly/apple/osx/x86_64/
