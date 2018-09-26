fd155db9cca5696495440a35cc7fb9fc9b8831ff
M4xw <m4x@m4xw.net>
2018-09-27 01:18:07 +0200

    [LIBNX] Fix font driver

--- gfx/font_driver.c
+++ gfx/font_driver.c
@@ -524,6 +524,37 @@ static bool ctr_font_init_first(
 }
 #endif
 
+#ifdef HAVE_LIBNX 
+static const font_renderer_t *switch_font_backends[] = {
+   &switch_font,
+   NULL
+};
+
+static bool switch_font_init_first(
+      const void **font_driver, void **font_handle,
+      void *video_data, const char *font_path,
+      float font_size, bool is_threaded)
+{
+   unsigned i;
+
+   for (i = 0; switch_font_backends[i]; i++)
+   {
+      void *data = switch_font_backends[i]->init(
+            video_data, font_path, font_size,
+            is_threaded);
+
+      if (!data)
+         continue;
+
+      *font_driver = switch_font_backends[i];
+      *font_handle = data;
+      return true;
+   }
+
+   return false;
+}
+#endif
+
 #ifdef WIIU
 static const font_renderer_t *wiiu_font_backends[] = {
    &wiiu_font,
@@ -628,6 +659,11 @@ static bool font_init_first(
 #ifdef HAVE_SIXEL
       case FONT_DRIVER_RENDER_SIXEL:
          return sixel_font_init_first(font_driver, font_handle,
+               video_data, font_path, font_size, is_threaded);
+#endif
+#ifdef HAVE_LIBNX
+      case FONT_DRIVER_RENDER_SWITCH:
+         return switch_font_init_first(font_driver, font_handle,
                video_data, font_path, font_size, is_threaded);
 #endif
 #if defined(_WIN32) && !defined(_XBOX)
