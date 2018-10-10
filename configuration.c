b61c67b5c68e6a5ecd733c34a2a91e03cb5e1166
twinaphex <libretro@gmail.com>
2018-10-10 17:17:02 +0200

    Don't have GDI fallback to rgui

--- configuration.c
+++ configuration.c
@@ -2357,6 +2357,7 @@ static bool check_menu_driver_compatibility(void)
          string_is_equal(video_driver, "d3d10")  ||
          string_is_equal(video_driver, "d3d11")  ||
          string_is_equal(video_driver, "d3d12")  ||
+         string_is_equal(video_driver, "gdi")    ||
          string_is_equal(video_driver, "gl")     ||
          string_is_equal(video_driver, "gx2")    ||
          string_is_equal(video_driver, "vulkan") ||
