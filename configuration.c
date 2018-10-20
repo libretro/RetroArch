882a25326c6f24fc391854bdf4d42ac9a3bb02dd
twinaphex <libretro@gmail.com>
2018-10-20 03:04:53 +0200

    Add ctr to the list too

--- configuration.c
+++ configuration.c
@@ -2376,6 +2376,7 @@ static bool check_menu_driver_compatibility(void)
          string_is_equal(video_driver, "gx2")    ||
          string_is_equal(video_driver, "vulkan") ||
          string_is_equal(video_driver, "metal")  ||
+         string_is_equal(video_driver, "ctr")    ||
          string_is_equal(video_driver, "vita2d"))
       return true;   
 
