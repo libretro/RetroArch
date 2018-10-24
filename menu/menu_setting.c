49b657fce26543bd96710cb69ec45ef2b87ef758
twinaphex <libretro@gmail.com>
2018-10-24 04:42:58 +0200

    Menu Font Green/Blue Color can now be manipulated with the GUI

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -7784,8 +7784,6 @@ static bool setting_append_list(
                   general_write_handler,
                   general_read_handler);
             (*list)[list_info->index - 1].action_ok = &setting_action_ok_uint;
-            (*list)[list_info->index - 1].get_string_representation =
-               &setting_get_string_representation_float_video_msg_color;
             menu_settings_list_current_add_range(list, list_info, 0, 255, 1, true, true);
             settings_data_list_current_add_flags(list, list_info, SD_FLAG_ADVANCED);
 
@@ -7800,8 +7798,6 @@ static bool setting_append_list(
                   parent_group,
                   general_write_handler,
                   general_read_handler);
-            (*list)[list_info->index - 1].get_string_representation =
-               &setting_get_string_representation_float_video_msg_color;
             (*list)[list_info->index - 1].action_ok = &setting_action_ok_uint;
             menu_settings_list_current_add_range(list, list_info, 0, 255, 1, true, true);
             settings_data_list_current_add_flags(list, list_info, SD_FLAG_ADVANCED);
