6eb0fabbfcd57f74e6d33de2ca338a9ce9b045e7
Christian Murphy <christheromguy@googlemail.com>
2018-10-27 18:52:59 +0100

    whitespace fix

--- gfx/video_driver.c
+++ gfx/video_driver.c
@@ -2464,7 +2464,7 @@ void video_driver_frame(const void *data, unsigned width,
          if (video_info.fps_show)
          {
             snprintf(video_info.fps_text, sizeof(video_info.fps_text),
-                  "|| FPS: %6.1f ", last_fps);
+                  "||  FPS: %6.1f ", last_fps);
             if (video_info.framecount_show)
             {
                snprintf(frames_text,
