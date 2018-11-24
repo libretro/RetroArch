62c6d60c57b9d4ca1af676beae8fb87779c9d0af
twinaphex <libretro@gmail.com>
2018-11-24 01:29:35 +0100

    Revert "Cleanup shaders_common.h"
    This reverts commit c1de2e569770837d42f0099f95b8838c5f33dc27.

--- gfx/drivers/gl_shaders/shaders_common.h
+++ gfx/drivers/gl_shaders/shaders_common.h
@@ -1,27 +1,33 @@
 #ifndef _SHADERS_COMMON
 #define _SHADERS_COMMON
 
-#define GLSL_DERIV_PREAMBLE() "#extension GL_OES_standard_derivatives : enable\n"
-#define GLSL_PREAMBLE() \
-   "#ifdef GL_ES\n" \
-   "  #ifdef GL_FRAGMENT_PRECISION_HIGH\n" \
-   "    precision highp float;\n" \
-   "  #else\n" \
-   "    precision mediump float;\n" \
-   "  #endif\n" \
-   "#else\n" \
-   "  precision mediump float;\n" \
-   "#endif\n" 
-
 #if defined(HAVE_OPENGLES)
-#define CG(src)       "" #src
-#define GLSL(src)     GLSL_DERIV_PREAMBLE() GLSL_PREAMBLE() #src
-#define GLSL_330(src) "#version 330 es\n" GLSL_PREAMBLE() #src
+#define CG(src)   "" #src
+#define GLSL(src) "#extension GL_OES_standard_derivatives : enable\n" \
+                  "#ifdef GL_ES\n" \
+                  "  #ifdef GL_FRAGMENT_PRECISION_HIGH\n" \
+                  "    precision highp float;\n" \
+                  "  #else\n" \
+                  "    precision mediump float;\n" \
+                  "  #endif\n" \
+                  "#else\n" \
+                  "  precision mediump float;\n" \
+                  "#endif\n" #src
+#define GLSL_330(src) "#version 330 es\n" \
+                  "#ifdef GL_ES\n" \
+                  "  #ifdef GL_FRAGMENT_PRECISION_HIGH\n" \
+                  "    precision highp float;\n" \
+                  "  #else\n" \
+                  "    precision mediump float;\n" \
+                  "  #endif\n" \
+                  "#else\n" \
+                  "  precision mediump float;\n" \
+                  "#endif\n" #src
 #else
-#define CG(src)         "" #src
-#define GLSL(src)       "" GLSL_PREAMBLE() #src
-#define GLSL_300(src)   "#version 300 es\n" GLSL_PREAMBLE() #src
-#define GLSL_330(src)   "#version 330 core\n" GLSL_PREAMBLE() #src
+#define CG(src)   "" #src
+#define GLSL(src) "" #src
+#define GLSL_300(src)   "#version 300 es\n"   #src
+#define GLSL_330(src)   "#version 330 core\n"   #src
 #endif
 
 #endif
