418f28b57a954a1ad51dbe833a87fb11adde7d33
twinaphex <libretro@gmail.com>
2018-11-25 17:09:00 +0100

    (dispserv_x11.c) Use snprintf instead of sprintf

--- gfx/display_servers/dispserv_x11.c
+++ gfx/display_servers/dispserv_x11.c
@@ -55,23 +55,28 @@ static void x11_display_server_destroy(void *data)
 
    if (crt_en)
    {
-      sprintf(output, "xrandr -s %dx%d", orig_width, orig_height);
+      snprintf(output, sizeof(output),
+		      "xrandr -s %dx%d", orig_width, orig_height);
       system(output);
 
       for (i = 0; i < 3; i++)
       {
-         sprintf(output, "xrandr --delmode %s%d %s", "VGA", i, old_mode);
+         snprintf(output, sizeof(output),
+			 "xrandr --delmode %s%d %s", "VGA", i, old_mode);
          system(output);
-         sprintf(output, "xrandr --delmode %s-%d %s", "VGA", i, old_mode);
+         snprintf(output, sizeof(output),
+			 "xrandr --delmode %s-%d %s", "VGA", i, old_mode);
          system(output);
 
-         sprintf(output, "xrandr --delmode %s%d %s", "DVI", i, old_mode);
+         snprintf(output, sizeof(output),
+			 "xrandr --delmode %s%d %s", "DVI", i, old_mode);
          system(output);
-         sprintf(output, "xrandr --delmode %s-%d %s", "DVI", i, old_mode);
+         snprintf(output, sizeof(output),
+			 "xrandr --delmode %s-%d %s", "DVI", i, old_mode);
          system(output);
       }
 
-      sprintf(output, "xrandr --rmmode %s", old_mode);
+      snprintf(output, sizeof(output), "xrandr --rmmode %s", old_mode);
       system(output);
    }
 
