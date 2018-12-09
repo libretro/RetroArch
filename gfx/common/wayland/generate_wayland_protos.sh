d1c15efc2486ff102e360b8fd729cec64ee90cef
Sunderland93 <samoilov.lex@gmail.com>
2018-12-09 18:47:47 +0400

    Revert "Add KDE's server-decoration protocol as fallback (for KDE)"
    This reverts commit 058a880a8b1cffecb5be9b53b4846563e57e4b14.

--- gfx/common/wayland/generate_wayland_protos.sh
+++ gfx/common/wayland/generate_wayland_protos.sh
@@ -22,7 +22,3 @@ $WAYSCAN private-code $WAYLAND_PROTOS/unstable/idle-inhibit/idle-inhibit-unstabl
 #Generate xdg-decoration header and .c files
 $WAYSCAN client-header $WAYLAND_PROTOS/unstable/xdg-decoration/xdg-decoration-unstable-v1.xml $OUTPUT/xdg-decoration-unstable-v1.h
 $WAYSCAN private-code $WAYLAND_PROTOS/unstable/xdg-decoration/xdg-decoration-unstable-v1.xml $OUTPUT/xdg-decoration-unstable-v1.c
-
-#Generate server-decorations header and .c files
-$WAYSCAN client-header gfx/common/wayland/server-decoration.xml $OUTPUT/server-decorations.h
-$WAYSCAN private-code gfx/common/wayland/server-decoration.xml $OUTPUT/server-decorations.c
