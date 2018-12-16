e0c634cd76886024887b248f91f629f572e8b11e
orbea <orbea@fredslev.dk>
2018-12-15 19:01:36 -0800

    qb: Fix typo.

--- qb/config.libs.sh
+++ qb/config.libs.sh
@@ -477,7 +477,7 @@ if [ "$HAVE_WAYLAND_PROTOS" = yes ] && [ "$HAVE_WAYLAND" = yes ]; then
     check_pkgconf WAYLAND_SCANNER wayland-scanner 1.15
     ./gfx/common/wayland/generate_wayland_protos.sh
 else
-    die : 'Notice: wayland-egl or wayland-protocols not present. Skiping Wayland code paths.'
+    die : 'Notice: wayland-egl or wayland-protocols not found, disabling wayland support.'
     HAVE_WAYLAND='no'
 fi
 
