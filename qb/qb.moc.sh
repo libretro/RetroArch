9c7affc2cf264f5068292f9d1dbd0822ad0bfa02
orbea <orbea@fredslev.dk>
2019-01-19 08:21:31 -0800

    Travis + qb fixes. (#8040)
    * travis: Enable building qt for linux.
    
    * qb: Set QT_SELECT for moc and qtchooser.
    
    This should have no effect on systems where qtchooser is not used.
    
    Fixes https://github.com/libretro/RetroArch/issues/8028
    
    * qb: Actually disable c++ features if there is no c++ compiler.
    
    Now this warning actually does something.
    
      Warning: A working C++ compiler was not found, C++ features will be disabled.

--- qb/qb.moc.sh
+++ qb/qb.moc.sh
@@ -1,3 +1,5 @@
+. qb/config.moc.sh
+
 TEMP_MOC=.moc.h
 TEMP_CPP=.moc.cpp
 
@@ -16,18 +18,19 @@ HAVE_MOC=no
 if [ "$HAVE_QT" = "yes" ]; then
 	moc_works=0
 	if [ "$MOC" ]; then
+		QT_SELECT="$QT_VERSION" \
 		"$MOC" -o "$TEMP_CPP" "$TEMP_MOC" >/dev/null 2>&1 &&
-		"$CXX" -o "$TEMP_EXE" $(printf %s "$QT5CORE_CFLAGS $QT5CORE_LIBS") \
+		"$CXX" -o "$TEMP_EXE" $(printf %s "$QT_FLAGS") \
 			-fPIC -c "$TEMP_CPP" >/dev/null 2>&1 &&
 		moc_works=1
 	else
-		for moc in moc-qt5 moc; do
+		for moc in "moc-$QT_VERSION" moc; do
 			MOC="$(exists "$moc")" || MOC=""
 			if [ "$MOC" ]; then
+				QT_SELECT="$QT_VERSION" \
 				"$MOC" -o "$TEMP_CPP" "$TEMP_MOC" >/dev/null 2>&1 ||
 					continue
-				"$CXX" -o "$TEMP_EXE" \
-					$(printf %s "$QT5CORE_CFLAGS $QT5CORE_LIBS") \
+				"$CXX" -o "$TEMP_EXE" $(printf %s "$QT_FLAGS") \
 					-fPIC -c "$TEMP_CPP" >/dev/null 2>&1 && {
 					moc_works=1
 					break
