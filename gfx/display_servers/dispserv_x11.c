8ff70ecbbbd5cc29bd54cf4230fa19c537e5e1f5
alphanu1 <37101891+alphanu1@users.noreply.github.com>
2019-02-01 20:34:48 +0000

    Update dispserv_x11.c

--- gfx/display_servers/dispserv_x11.c
+++ gfx/display_servers/dispserv_x11.c
@@ -267,8 +267,6 @@ static bool x11_display_server_set_resolution(void *data,
  
    if (monitor_index == 0)
    {
-       if (res->noutput < 2)
-            snprintf(orig_output, sizeof(orig_output),"%s", outputs->name);
       
       for (int i = 0; i < res->noutput; i++)
       { 
