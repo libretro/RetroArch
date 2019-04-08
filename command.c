b750199db4f0f17a0569a23a1d1201806f398f57
natinusala <natinusala@gmail.com>
2019-04-08 16:44:21 +0200

    menu widgets: reimplement pause widget

--- command.c
+++ command.c
@@ -59,6 +59,9 @@
 #include "menu/menu_content.h"
 #include "menu/menu_shader.h"
 #include "menu/widgets/menu_dialog.h"
+#ifdef HAVE_MENU_WIDGETS
+#include "menu/widgets/menu_widgets.h"
+#endif
 #endif
 
 #ifdef HAVE_NETWORKING
@@ -2488,6 +2491,9 @@ TODO: Add a setting for these tweaks */
                RARCH_LOG("%s\n", msg_hash_to_str(MSG_PAUSED));
                command_event(CMD_EVENT_AUDIO_STOP, NULL);
 
+#if defined(HAVE_MENU) && defined(HAVE_MENU_WIDGETS)
+               if (!video_driver_has_widgets() || !menu_widgets_set_paused(is_paused))
+#endif
                runloop_msg_queue_push(msg_hash_to_str(MSG_PAUSED), 1,
                      1, true,
                      NULL, MESSAGE_QUEUE_ICON_DEFAULT, MESSAGE_QUEUE_CATEGORY_INFO);
@@ -2502,6 +2508,10 @@ TODO: Add a setting for these tweaks */
             }
             else
             {
+#if defined(HAVE_MENU) && defined(HAVE_MENU_WIDGETS)
+               if (video_driver_has_widgets())
+                  menu_widgets_set_paused(is_paused);
+#endif
                RARCH_LOG("%s\n", msg_hash_to_str(MSG_UNPAUSED));
                command_event(CMD_EVENT_AUDIO_START, NULL);
             }
