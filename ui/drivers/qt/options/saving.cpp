4b3ca341c2f83d0b116e6431b54fb5d9f7cf2127
twinaphex <libretro@gmail.com>
2019-04-14 18:58:51 +0200

    (UI/QT) saving - dehardcode more additions

--- ui/drivers/qt/options/saving.cpp
+++ ui/drivers/qt/options/saving.cpp
@@ -23,35 +23,33 @@ SavingPage::SavingPage(QObject *parent) :
 
 QWidget *SavingPage::widget()
 {
-   QWidget *widget = new QWidget;
-
-   FormLayout *layout = new FormLayout;
-
-   SettingsGroup *savesGroup = new SettingsGroup("Saves");
-   SettingsGroup *savestatesGroup = new SettingsGroup("Savestates");
+   QWidget                             *widget = new QWidget;
+   FormLayout                          *layout = new FormLayout;
+   SettingsGroup                   *savesGroup = new SettingsGroup("Saves");
+   SettingsGroup              *savestatesGroup = new SettingsGroup("Savestates");
    CheckableSettingsGroup *autoSavestatesGroup = new CheckableSettingsGroup(MENU_ENUM_LABEL_SAVESTATE_AUTO_SAVE);
-   SettingsGroup *saveRamGroup = new SettingsGroup("SaveRAM");
-   SettingsGroup *systemFilesDirGroup = new SettingsGroup("System Files");
-   SettingsGroup *screenshotsDirGroup = new SettingsGroup("Screenshots");
+   SettingsGroup                 *saveRamGroup = new SettingsGroup("SaveRAM");
+   SettingsGroup          *systemFilesDirGroup = new SettingsGroup("System Files");
+   SettingsGroup          *screenshotsDirGroup = new SettingsGroup("Screenshots");
 
-   savesGroup->addCheckBox(MENU_ENUM_LABEL_SORT_SAVEFILES_ENABLE);
-   savesGroup->addCheckBox(MENU_ENUM_LABEL_SAVEFILES_IN_CONTENT_DIR_ENABLE);
+   savesGroup->add(MENU_ENUM_LABEL_SORT_SAVEFILES_ENABLE);
+   savesGroup->add(MENU_ENUM_LABEL_SAVEFILES_IN_CONTENT_DIR_ENABLE);
 
-   savestatesGroup->addCheckBox(MENU_ENUM_LABEL_SAVESTATE_AUTO_INDEX);
+   savestatesGroup->add(MENU_ENUM_LABEL_SAVESTATE_AUTO_INDEX);
 
-   autoSavestatesGroup->addCheckBox(MENU_ENUM_LABEL_SAVESTATE_AUTO_LOAD);
+   autoSavestatesGroup->add(MENU_ENUM_LABEL_SAVESTATE_AUTO_LOAD);
 
    savestatesGroup->addRow(autoSavestatesGroup);
-   savestatesGroup->addCheckBox(MENU_ENUM_LABEL_SAVESTATE_THUMBNAIL_ENABLE);
-   savestatesGroup->addCheckBox(MENU_ENUM_LABEL_SORT_SAVESTATES_ENABLE);
-   savestatesGroup->addCheckBox(MENU_ENUM_LABEL_SAVESTATES_IN_CONTENT_DIR_ENABLE);
+   savestatesGroup->add(MENU_ENUM_LABEL_SAVESTATE_THUMBNAIL_ENABLE);
+   savestatesGroup->add(MENU_ENUM_LABEL_SORT_SAVESTATES_ENABLE);
+   savestatesGroup->add(MENU_ENUM_LABEL_SAVESTATES_IN_CONTENT_DIR_ENABLE);
 
-   saveRamGroup->addCheckBox(MENU_ENUM_LABEL_BLOCK_SRAM_OVERWRITE);
-   saveRamGroup->addUIntSpinBox(MENU_ENUM_LABEL_AUTOSAVE_INTERVAL);
+   saveRamGroup->add(MENU_ENUM_LABEL_BLOCK_SRAM_OVERWRITE);
+   saveRamGroup->add(MENU_ENUM_LABEL_AUTOSAVE_INTERVAL);
 
-   systemFilesDirGroup->addCheckBox(MENU_ENUM_LABEL_SYSTEMFILES_IN_CONTENT_DIR_ENABLE);
+   systemFilesDirGroup->add(MENU_ENUM_LABEL_SYSTEMFILES_IN_CONTENT_DIR_ENABLE);
 
-   screenshotsDirGroup->addCheckBox(MENU_ENUM_LABEL_SCREENSHOTS_IN_CONTENT_DIR_ENABLE);
+   screenshotsDirGroup->add(MENU_ENUM_LABEL_SCREENSHOTS_IN_CONTENT_DIR_ENABLE);
 
    layout->addRow(savesGroup);
    layout->addRow(savestatesGroup);
