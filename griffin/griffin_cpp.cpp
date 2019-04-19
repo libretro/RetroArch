1fb1b1bdb7855d86672b8e0f8673ee84fac0b12d
twinaphex <libretro@gmail.com>
2019-04-19 19:28:25 +0200

    Add this back

--- griffin/griffin_cpp.cpp
+++ griffin/griffin_cpp.cpp
@@ -78,6 +78,7 @@ UI
 #include "../ui/drivers/qt/options/ui.cpp"
 #include "../ui/drivers/qt/options/achievements.cpp"
 #include "../ui/drivers/qt/options/network.cpp"
+#include "../ui/drivers/qt/moc_settingswidgets.cpp"
 #include "../ui/drivers/qt/options/moc_options.cpp"
 #endif
 #include "../ui/drivers/moc_ui_qt.cpp"
