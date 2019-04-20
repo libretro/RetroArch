0805838050293e0e0bcff2b6b140b92e35c23595
twinaphex <libretro@gmail.com>
2019-04-21 00:54:35 +0200

    (QT/UI) Cleanups

--- ui/drivers/qt/options/ui.cpp
+++ ui/drivers/qt/options/ui.cpp
@@ -160,27 +160,7 @@ QuickMenuPage::QuickMenuPage(QObject *parent) :
 
 QWidget *QuickMenuPage::widget()
 {
-   unsigned i;
-   QWidget            * widget = new QWidget;
-   FormLayout          *layout = new FormLayout;
-   file_list_t           *list = (file_list_t*)calloc(1, sizeof(*list));
-
-   menu_displaylist_build_list(
-         list, DISPLAYLIST_QUICK_MENU_VIEWS_SETTINGS_LIST);
-
-   for (i = 0; i < list->size; i++)
-   {
-      menu_file_list_cbs_t *cbs = (menu_file_list_cbs_t*)
-         file_list_get_actiondata_at_offset(list, i);
-
-      layout->add(cbs->enum_idx);
-   }
-
-   file_list_free(list);
-
-   widget->setLayout(layout);
-
-   return widget;
+   return create_widget(DISPLAYLIST_QUICK_MENU_VIEWS_SETTINGS_LIST);
 }
 
 AppearancePage::AppearancePage(QObject *parent) :
