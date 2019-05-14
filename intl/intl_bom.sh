3647631428af642865a09ef362ab221bdac40e42
orbea <orbea@riseup.net>
2019-05-14 11:02:30 -0700

    intl: Add intl_bom.sh
    This script supports testing for missing BOM in c and header
    files as well as automatically fixing them.
    
    Travis will now test if these files contain a BOM and fail
    when missing. They can be trivially fixed by running this
    script.
    
    To print errors when the BOM is missing.
    
      intl/intl_bom.sh
    
    To automatically fix the files.
    
      intl/intl_bom.sh -f

---
+++ intl/intl_bom.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -eu
+
+cd -- "$(cd -- "${0%/*}/" && pwd -P)"
+
+. ../qb/qb.init.sh
+
+error=
+fix=
+
+usage='intl_bom.sh - Tests files for a UTF-8 BOM at the header.
+
+  Usage: intl_bom.sh [OPTIONS]
+    -f, --fix               Automatically add a BOM when missing.
+    -h, --help              Shows this message.'
+
+while [ $# -gt 0 ]; do
+   option="$1"
+   shift
+   case "$option" in
+      -- ) break ;;
+      -f|--fix ) fix=1 ;;
+      -h|--help ) die 0 "$usage" ;;
+      * ) die 1 "Unrecognized option '$option', use -h for help." ;;
+   esac
+done
+
+for file in *.c *.h; do
+  hex="$(hexdump -n 3 -C "$file" | head -1 | tr -s '[:blank:]')"
+  bom="${hex% *}"
+  if [ "${bom#* }" != 'ef bb bf' ]; then
+    if [ -n "$fix" ]; then
+      sed -i '1s/^/\xef\xbb\xbf/' "$file"
+    else
+      error="$error $file"
+    fi
+  fi
+done
+
+if [ -n "$error" ]; then
+  eval "set -- $error"
+  for err do
+    die : "ERROR: $err is missing an UTF-8 BOM at the header."
+  done
+  die 1 "Run 'intl/intl_bom.sh -f' and commit the results to fix these files."
+fi
+
+exit 0
