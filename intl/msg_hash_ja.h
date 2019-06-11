5b0c8ab2ab798ab3e3c3294eb80ef8c3510f21f1
Toshiyuki Maezawa <toshiyuki.maezawa@gmail.com>
2019-06-11 23:37:13 +0900

    Added BOM

--- intl/msg_hash_ja.h
+++ intl/msg_hash_ja.h
@@ -1,4 +1,4 @@
-#if defined(_MSC_VER) && !defined(_XBOX) && (_MSC_VER >= 1500 && _MSC_VER < 1900)
+﻿#if defined(_MSC_VER) && !defined(_XBOX) && (_MSC_VER >= 1500 && _MSC_VER < 1900)
 #if (_MSC_VER >= 1700)
 /* https://support.microsoft.com/en-us/kb/980263 */
 #pragma execution_character_set("utf-8")
