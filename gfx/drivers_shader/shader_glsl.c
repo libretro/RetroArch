fc62f7afaa02dbe2b362bc19a79c0dfb2d53d2d5
twinaphex <libretro@gmail.com>
2019-06-15 09:58:34 +0200

    Update

--- gfx/drivers_shader/shader_glsl.c
+++ gfx/drivers_shader/shader_glsl.c
@@ -460,9 +460,8 @@ static bool gl_glsl_compile_programs(
       if (!pass)
          continue;
 
-      /* If we load from GLSLP (CGP),
-       * load the file here, and pretend
-       * we were really using XML all along.
+      /* If we load from GLSLP (preset),
+       * load the file here.
        */
       if (     !string_is_empty(pass->source.path)
             && !gl_glsl_load_source_path(pass, pass->source.path))
