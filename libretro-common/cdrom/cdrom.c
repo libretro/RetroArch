5aa2ebe6d58240e9a5d1be2935fe194c04a0106a
Brad Parker <cbparker@gmail.com>
2019-06-30 19:50:58 -0400

    cdrom: I/O always starts at the beginning of the file, and we translate those byte positions to start from the beginning MSF of the track anyway, so index should always be zero, fixes some audio playback issues

--- libretro-common/cdrom/cdrom.c
+++ libretro-common/cdrom/cdrom.c
@@ -560,7 +560,7 @@ int cdrom_write_cue(libretro_vfs_implementation_file *stream, char **out_buf, si
          pos += snprintf(*out_buf + pos, len - pos, "FILE \"cdrom://drive%c-track%02d.bin\" BINARY\n", cdrom_drive, point);
 #endif
          pos += snprintf(*out_buf + pos, len - pos, "  TRACK %02d %s\n", point, track_type);
-         pos += snprintf(*out_buf + pos, len - pos, "    INDEX 01 %02d:%02d:%02d\n", pmin, psec, pframe);
+         pos += snprintf(*out_buf + pos, len - pos, "    INDEX 01 00:00:00\n");
       }
    }
 
