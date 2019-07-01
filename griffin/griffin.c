d3c34f0b06b5e55be7d72d994ea81a9f57074772
Brad Parker <cbparker@gmail.com>
2019-07-01 11:24:57 -0400

    griffin: define VFS_FRONTEND

--- griffin/griffin.c
+++ griffin/griffin.c
@@ -14,6 +14,7 @@
 * You should have received a copy of the GNU General Public License along with RetroArch.
 * If not, see <http://www.gnu.org/licenses/>.
 */
+#define VFS_FRONTEND
 #include <retro_environment.h>
 
 #define CINTERFACE
