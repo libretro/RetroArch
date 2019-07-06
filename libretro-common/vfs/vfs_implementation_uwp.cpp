3ffc5fc3433cbe539a16d0e75be55bef94d2d1f1
twinaphex <libretro@gmail.com>
2019-07-06 07:29:51 +0200

    (MSVC2017 UWP) Buildfix

--- libretro-common/vfs/vfs_implementation_uwp.cpp
+++ libretro-common/vfs/vfs_implementation_uwp.cpp
@@ -20,6 +20,8 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+#include <retro_environment.h>
+
 #include <ppl.h>
 #include <ppltasks.h>
 #include <stdio.h>
