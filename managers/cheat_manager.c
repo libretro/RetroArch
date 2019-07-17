0f574c0b9a2b995585e1298f80cb2585045d332f
twinaphex <libretro@gmail.com>
2019-07-17 20:04:08 +0200

    Cleanups - turn some function callbacks static

--- managers/cheat_manager.c
+++ managers/cheat_manager.c
@@ -301,11 +301,9 @@ static void cheat_manager_new(unsigned size)
       cheat_manager_state.cheats[i].repeat_add_to_value = 0;
       cheat_manager_state.cheats[i].repeat_add_to_address = 1;
    }
-
-   return;
 }
 
-void cheat_manager_load_cb_first_pass(char *key, char *value)
+static void cheat_manager_load_cb_first_pass(char *key, char *value)
 {
    errno = 0;
 
@@ -318,7 +316,7 @@ void cheat_manager_load_cb_first_pass(char *key, char *value)
    }
 }
 
-void cheat_manager_load_cb_second_pass(char *key, char *value)
+static void cheat_manager_load_cb_second_pass(char *key, char *value)
 {
    char cheat_num_str[20];
    unsigned cheat_num;
@@ -389,7 +387,6 @@ void cheat_manager_load_cb_second_pass(char *key, char *value)
       cheat_manager_state.cheats[cheat_idx].rumble_value = (unsigned)strtoul(value, NULL, 0);
    else if (string_is_equal(key, "value"))
       cheat_manager_state.cheats[cheat_idx].value = (unsigned)strtoul(value, NULL, 0);
-
 }
 
 bool cheat_manager_load(const char *path, bool append)
@@ -600,7 +597,6 @@ void cheat_manager_toggle_index(unsigned i)
 
 void cheat_manager_toggle(void)
 {
-
    if (!cheat_manager_state.cheats || cheat_manager_state.size == 0)
       return;
 
@@ -1211,38 +1207,38 @@ void cheat_manager_apply_rumble(struct item_cheat *cheat, unsigned int curr_valu
 
    switch (cheat->rumble_type)
    {
-   case RUMBLE_TYPE_DISABLED:
-      return;
-   case RUMBLE_TYPE_CHANGES:
-      rumble = (curr_value != cheat->rumble_prev_value);
-      break;
-   case RUMBLE_TYPE_DOES_NOT_CHANGE:
-      rumble = (curr_value == cheat->rumble_prev_value);
-      break;
-   case RUMBLE_TYPE_INCREASE:
-      rumble = (curr_value > cheat->rumble_prev_value);
-      break;
-   case RUMBLE_TYPE_DECREASE:
-      rumble = (curr_value < cheat->rumble_prev_value);
-      break;
-   case RUMBLE_TYPE_EQ_VALUE:
-      rumble = (curr_value == cheat->rumble_value);
-      break;
-   case RUMBLE_TYPE_NEQ_VALUE:
-      rumble = (curr_value != cheat->rumble_value);
-      break;
-   case RUMBLE_TYPE_LT_VALUE:
-      rumble = (curr_value < cheat->rumble_value);
-      break;
-   case RUMBLE_TYPE_GT_VALUE:
-      rumble = (curr_value > cheat->rumble_value);
-      break;
-   case RUMBLE_TYPE_INCREASE_BY_VALUE:
-      rumble = (curr_value == cheat->rumble_prev_value + cheat->rumble_value);
-      break;
-   case RUMBLE_TYPE_DECREASE_BY_VALUE:
-      rumble = (curr_value == cheat->rumble_prev_value - cheat->rumble_value);
-      break;
+      case RUMBLE_TYPE_DISABLED:
+         return;
+      case RUMBLE_TYPE_CHANGES:
+         rumble = (curr_value != cheat->rumble_prev_value);
+         break;
+      case RUMBLE_TYPE_DOES_NOT_CHANGE:
+         rumble = (curr_value == cheat->rumble_prev_value);
+         break;
+      case RUMBLE_TYPE_INCREASE:
+         rumble = (curr_value > cheat->rumble_prev_value);
+         break;
+      case RUMBLE_TYPE_DECREASE:
+         rumble = (curr_value < cheat->rumble_prev_value);
+         break;
+      case RUMBLE_TYPE_EQ_VALUE:
+         rumble = (curr_value == cheat->rumble_value);
+         break;
+      case RUMBLE_TYPE_NEQ_VALUE:
+         rumble = (curr_value != cheat->rumble_value);
+         break;
+      case RUMBLE_TYPE_LT_VALUE:
+         rumble = (curr_value < cheat->rumble_value);
+         break;
+      case RUMBLE_TYPE_GT_VALUE:
+         rumble = (curr_value > cheat->rumble_value);
+         break;
+      case RUMBLE_TYPE_INCREASE_BY_VALUE:
+         rumble = (curr_value == cheat->rumble_prev_value + cheat->rumble_value);
+         break;
+      case RUMBLE_TYPE_DECREASE_BY_VALUE:
+         rumble = (curr_value == cheat->rumble_prev_value - cheat->rumble_value);
+         break;
    }
 
    cheat->rumble_prev_value = curr_value;
