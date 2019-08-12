6a4207fe55d0a3d4ed80b1eae1abc885b987fe70
twinaphex <libretro@gmail.com>
2019-08-12 12:12:16 +0200

    (Menu setting) 'Quit Press Twice' - set default value

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -9814,7 +9814,7 @@ static bool setting_append_list(
                   &settings->bools.quit_press_twice,
                   MENU_ENUM_LABEL_QUIT_PRESS_TWICE,
                   MENU_ENUM_LABEL_VALUE_QUIT_PRESS_TWICE,
-                  false,
+                  DEFAULT_QUIT_PRESS_TWICE,
                   MENU_ENUM_LABEL_VALUE_OFF,
                   MENU_ENUM_LABEL_VALUE_ON,
                   &group_info,
