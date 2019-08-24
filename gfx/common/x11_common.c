aa428e4a8236af6aaa8fdd394d253d645f1b7624
twinaphex <libretro@gmail.com>
2019-08-24 20:49:47 +0200

    (X11) Better resizing of menu when window is resized

--- gfx/common/x11_common.c
+++ gfx/common/x11_common.c
@@ -606,9 +606,12 @@ void x11_check_window(void *data, bool *quit,
 
    if (new_width != *width || new_height != *height)
    {
-      *resize = true;
       *width  = new_width;
       *height = new_height;
+      *resize = true;
+#ifdef HAVE_MENU
+      rarch_ctl(RARCH_CTL_SET_DEFERRED_MENU_CONTEXT_RESET, NULL);
+#endif
    }
 
    x11_alive(data);
