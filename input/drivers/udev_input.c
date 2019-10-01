2e480a37ea4d0f25d44bb327b19f863b18680278
SindenLightgun <47393540+SindenLightgun@users.noreply.github.com>
2019-10-01 12:26:13 +0100

    Fixed bug with touchscreen/
    From my analysis I "believe" this is a development bug/typo and is causing issues with mouse and touchscreen input, that would affect touchscreen and lightgun APIs.

--- input/drivers/udev_input.c
+++ input/drivers/udev_input.c
@@ -520,7 +520,7 @@ static bool udev_input_add_device(udev_input_t *udev,
          else
          {
             device->mouse.x_min = absinfo.minimum;
-            device->mouse.x_min = absinfo.maximum;
+            device->mouse.x_max = absinfo.maximum;
          }
       }
 
@@ -534,7 +534,7 @@ static bool udev_input_add_device(udev_input_t *udev,
 	     else
          {
            device->mouse.y_min = absinfo.minimum;
-           device->mouse.y_min = absinfo.maximum;
+           device->mouse.y_max = absinfo.maximum;
          }
       }
    }
