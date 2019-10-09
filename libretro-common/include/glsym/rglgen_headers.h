cb576b1b95f9ffd7fe9d2e10287ae4fe74cc91db
twinaphex <libretro@gmail.com>
2019-10-09 22:12:14 +0200

    Move include headers to rglgen_private_headers.h

--- libretro-common/include/glsym/rglgen_headers.h
+++ libretro-common/include/glsym/rglgen_headers.h
@@ -28,55 +28,7 @@
 #include <EGL/eglext.h>
 #endif
 
-#if defined(IOS)
-
-#if defined(HAVE_OPENGLES3)
-#include <OpenGLES/ES3/gl.h>
-#include <OpenGLES/ES3/glext.h>
-#else
-#include <OpenGLES/ES2/gl.h>
-#include <OpenGLES/ES2/glext.h>
-#endif
-
-#elif defined(__APPLE__)
-#include <compat/apple_compat.h>
-#if MAC_OS_X_VERSION_10_7
-#include <OpenGL/gl3.h>
-#include <OpenGL/gl3ext.h>
-#else
-#include <OpenGL/gl.h>
-#include <OpenGL/glext.h>
-#endif
-#elif defined(HAVE_PSGL)
-#include <PSGL/psgl.h>
-#include <GLES/glext.h>
-#elif defined(HAVE_OPENGL_MODERN)
-#include <GL3/gl3.h>
-#include <GL3/gl3ext.h>
-#elif defined(HAVE_OPENGLES3)
-#include <GLES3/gl3.h>
-#define __gl2_h_
-#include <GLES2/gl2ext.h>
-#elif defined(HAVE_OPENGLES2)
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-#elif defined(HAVE_OPENGLES1)
-#include <GLES/gl.h>
-#include <GLES/glext.h>
-#else
-#if defined(_WIN32) && !defined(_XBOX)
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#endif
-#ifndef HAVE_LIBNX
-#include <GL/gl.h>
-#include <GL/glext.h>
-#else
-/* We need to avoid including <GL/gl.h> on this platform */
-#include "switch/nx_gl.h"
-#include <GL/glext.h>
-#endif /* SWITCH */
-#endif
+#include "rglgen_private_headers.h"
 
 #ifndef GL_MAP_WRITE_BIT
 #define GL_MAP_WRITE_BIT 0x0002
