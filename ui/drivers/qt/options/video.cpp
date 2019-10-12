042783af21256d9fd0dd69bf13d1dd55e8fcabbe
Tatsuya79 <jeanpascalmeunier@hotmail.com>
2019-10-12 14:25:02 +0200

    always show advanced sync options

--- ui/drivers/qt/options/video.cpp
+++ ui/drivers/qt/options/video.cpp
@@ -123,17 +123,15 @@ QWidget *VideoPage::widget()
    vSyncGroup->add(MENU_ENUM_LABEL_VIDEO_FRAME_DELAY);
    syncGroup->addRow(vSyncGroup);
 
+   rarch_setting_t *hardSyncSetting = menu_setting_find_enum(MENU_ENUM_LABEL_VIDEO_HARD_SYNC);
+
+   if (hardSyncSetting)
    {
-      rarch_setting_t *hardSyncSetting = menu_setting_find_enum(MENU_ENUM_LABEL_VIDEO_HARD_SYNC);
+      CheckableSettingsGroup *hardSyncGroup = new CheckableSettingsGroup(hardSyncSetting);
 
-      if (hardSyncSetting)
-      {
-         CheckableSettingsGroup *hardSyncGroup = new CheckableSettingsGroup(hardSyncSetting);
+      hardSyncGroup->add(MENU_ENUM_LABEL_VIDEO_HARD_SYNC_FRAMES);
 
-         hardSyncGroup->add(MENU_ENUM_LABEL_VIDEO_HARD_SYNC_FRAMES);
-
-         syncGroup->addRow(hardSyncGroup);
-      }
+      syncGroup->addRow(hardSyncGroup);
    }
 
    syncGroup->add(MENU_ENUM_LABEL_VIDEO_MAX_SWAPCHAIN_IMAGES);
