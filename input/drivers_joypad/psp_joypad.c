65f406a39e57b29f223497967756da7e6aa66168
ThePhxRises <40254312+ThePhxRises@users.noreply.github.com>
2019-11-21 11:06:39 -0500

    [VITA] MiniVitaTV Support
    Set kernel model manually with sceCtrlIsMultiControllerSupported()

--- input/drivers_joypad/psp_joypad.c
+++ input/drivers_joypad/psp_joypad.c
@@ -86,7 +86,13 @@ static bool psp_joypad_init(void *data)
    (void)data;
 
 #if defined(VITA)
-   psp2_model = sceKernelGetModelForCDialog();
+   if (!sceCtrlIsMultiControllerSupported())
+      {
+      psp2_model = SCE_KERNEL_MODEL_VITA;
+      } else if(sceCtrlIsMultiControllerSupported() > 0)
+      {
+      psp2_model = SCE_KERNEL_MODEL_VITATV;
+      }
    if (psp2_model != SCE_KERNEL_MODEL_VITATV)
    {
       sceTouchSetSamplingState(SCE_TOUCH_PORT_BACK, SCE_TOUCH_SAMPLING_STATE_START);
