36676900271473a6ab0a617456434fe2899eac45
twinaphex <libretro@gmail.com>
2020-01-03 13:55:01 +0100

    Revert "Integer scale fix for windowed mode"
    This reverts commit 4ec9fe5a57ee22558f6faa1f931910ec4a296064.

--- retroarch.c
+++ retroarch.c
@@ -21401,10 +21401,8 @@ void video_viewport_get_scaled_integer(struct video_viewport *vp,
          if (keep_aspect)
          {
             /* X/Y scale must be same. */
-            unsigned max_scale = MIN(
-                  (unsigned)roundf((float)width / base_width),
-                  (unsigned)roundf((height / base_height)));
-
+            unsigned max_scale = MIN(width / base_width,
+                  height / base_height);
             padding_x          = width - base_width * max_scale;
             padding_y          = height - base_height * max_scale;
          }
