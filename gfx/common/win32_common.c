142503d3b54fbb0098244ad2e934ceca3d20d798
twinaphex <libretro@gmail.com>
2020-01-18 09:28:46 +0100

    move win32_menu_loop and win32_browser to win32_common

--- gfx/common/win32_common.c
+++ gfx/common/win32_common.c
@@ -604,6 +604,217 @@ static void win32_save_position(void)
    }
 }
 
+static bool win32_browser(
+      HWND owner,
+      char *filename,
+      size_t filename_size,
+      const char *extensions,
+      const char *title,
+      const char *initial_dir)
+{
+   bool result = false;
+   const ui_browser_window_t *browser =
+      ui_companion_driver_get_browser_window_ptr();
+
+   if (browser)
+   {
+      ui_browser_window_state_t browser_state;
+
+      /* These need to be big enough to hold the
+       * path/name of any file the user may select.
+       * FIXME: We should really handle the
+       * error case when this isn't big enough. */
+      char new_title[PATH_MAX];
+      char new_file[32768];
+
+      new_title[0] = '\0';
+      new_file[0] = '\0';
+
+      if (!string_is_empty(title))
+         strlcpy(new_title, title, sizeof(new_title));
+
+      if (filename && *filename)
+         strlcpy(new_file, filename, sizeof(new_file));
+
+      /* OPENFILENAME.lpstrFilters is actually const,
+       * so this cast should be safe */
+      browser_state.filters  = (char*)extensions;
+      browser_state.title    = new_title;
+      browser_state.startdir = strdup(initial_dir);
+      browser_state.path     = new_file;
+      browser_state.window   = owner;
+
+      result = browser->open(&browser_state);
+
+      if (filename && browser_state.path)
+         strlcpy(filename, browser_state.path, filename_size);
+
+      free(browser_state.startdir);
+   }
+
+   return result;
+}
+
+
+static LRESULT win32_menu_loop(HWND owner, WPARAM wparam)
+{
+   WPARAM mode         = wparam & 0xffff;
+   enum event_command cmd         = CMD_EVENT_NONE;
+   bool do_wm_close     = false;
+   settings_t *settings = config_get_ptr();
+
+   switch (mode)
+   {
+      case ID_M_LOAD_CORE:
+         {
+            char win32_file[PATH_MAX_LENGTH] = {0};
+            char    *title_cp       = NULL;
+            size_t converted        = 0;
+            const char *extensions  = "Libretro core (.dll)\0*.dll\0All Files\0*.*\0\0";
+            const char *title       = msg_hash_to_str(MENU_ENUM_LABEL_VALUE_CORE_LIST);
+            const char *initial_dir = settings->paths.directory_libretro;
+
+            /* Convert UTF8 to UTF16, then back to the
+             * local code page.
+             * This is needed for proper multi-byte
+             * string display until Unicode is
+             * fully supported.
+             */
+            wchar_t *title_wide     = utf8_to_utf16_string_alloc(title);
+
+            if (title_wide)
+               title_cp             = utf16_to_utf8_string_alloc(title_wide);
+
+            if (!win32_browser(owner, win32_file, sizeof(win32_file),
+                     extensions, title_cp, initial_dir))
+            {
+               if (title_wide)
+                  free(title_wide);
+               if (title_cp)
+                  free(title_cp);
+               break;
+            }
+
+            if (title_wide)
+               free(title_wide);
+            if (title_cp)
+               free(title_cp);
+            path_set(RARCH_PATH_CORE, win32_file);
+            cmd         = CMD_EVENT_LOAD_CORE;
+         }
+         break;
+      case ID_M_LOAD_CONTENT:
+         {
+            char win32_file[PATH_MAX_LENGTH] = {0};
+            char *title_cp          = NULL;
+            size_t converted        = 0;
+            const char *extensions  = "All Files (*.*)\0*.*\0\0";
+            const char *title       = msg_hash_to_str(
+                  MENU_ENUM_LABEL_VALUE_LOAD_CONTENT_LIST);
+            const char *initial_dir = settings->paths.directory_menu_content;
+
+            /* Convert UTF8 to UTF16, then back to the
+             * local code page.
+             * This is needed for proper multi-byte
+             * string display until Unicode is
+             * fully supported.
+             */
+            wchar_t *title_wide     = utf8_to_utf16_string_alloc(title);
+
+            if (title_wide)
+               title_cp             = utf16_to_utf8_string_alloc(title_wide);
+
+            if (!win32_browser(owner, win32_file, sizeof(win32_file),
+                     extensions, title_cp, initial_dir))
+            {
+               if (title_wide)
+                  free(title_wide);
+               if (title_cp)
+                  free(title_cp);
+               break;
+            }
+
+            if (title_wide)
+               free(title_wide);
+            if (title_cp)
+               free(title_cp);
+            win32_load_content_from_gui(win32_file);
+         }
+         break;
+      case ID_M_RESET:
+         cmd = CMD_EVENT_RESET;
+         break;
+      case ID_M_MUTE_TOGGLE:
+         cmd = CMD_EVENT_AUDIO_MUTE_TOGGLE;
+         break;
+      case ID_M_MENU_TOGGLE:
+         cmd = CMD_EVENT_MENU_TOGGLE;
+         break;
+      case ID_M_PAUSE_TOGGLE:
+         cmd = CMD_EVENT_PAUSE_TOGGLE;
+         break;
+      case ID_M_LOAD_STATE:
+         cmd = CMD_EVENT_LOAD_STATE;
+         break;
+      case ID_M_SAVE_STATE:
+         cmd = CMD_EVENT_SAVE_STATE;
+         break;
+      case ID_M_DISK_CYCLE:
+         cmd = CMD_EVENT_DISK_EJECT_TOGGLE;
+         break;
+      case ID_M_DISK_NEXT:
+         cmd = CMD_EVENT_DISK_NEXT;
+         break;
+      case ID_M_DISK_PREV:
+         cmd = CMD_EVENT_DISK_PREV;
+         break;
+      case ID_M_FULL_SCREEN:
+         cmd = CMD_EVENT_FULLSCREEN_TOGGLE;
+         break;
+      case ID_M_MOUSE_GRAB:
+         cmd = CMD_EVENT_GRAB_MOUSE_TOGGLE;
+         break;
+      case ID_M_TAKE_SCREENSHOT:
+         cmd = CMD_EVENT_TAKE_SCREENSHOT;
+         break;
+      case ID_M_QUIT:
+         do_wm_close = true;
+         break;
+      case ID_M_TOGGLE_DESKTOP:
+         cmd = CMD_EVENT_UI_COMPANION_TOGGLE;
+         break;
+      default:
+         if (mode >= ID_M_WINDOW_SCALE_1X && mode <= ID_M_WINDOW_SCALE_10X)
+         {
+            unsigned idx = (mode - (ID_M_WINDOW_SCALE_1X-1));
+            rarch_ctl(RARCH_CTL_SET_WINDOWED_SCALE, &idx);
+            cmd = CMD_EVENT_RESIZE_WINDOWED_SCALE;
+         }
+         else if (mode == ID_M_STATE_INDEX_AUTO)
+         {
+            signed idx = -1;
+            configuration_set_int(
+                  settings, settings->ints.state_slot, idx);
+         }
+         else if (mode >= (ID_M_STATE_INDEX_AUTO+1)
+               && mode <= (ID_M_STATE_INDEX_AUTO+10))
+         {
+            signed idx = (mode - (ID_M_STATE_INDEX_AUTO+1));
+            configuration_set_int(
+                  settings, settings->ints.state_slot, idx);
+         }
+         break;
+   }
+
+   if (cmd != CMD_EVENT_NONE)
+      command_event(cmd, NULL);
+
+   if (do_wm_close)
+      PostMessage(owner, WM_CLOSE, 0, 0);
+
+   return 0L;
+}
+
 static LRESULT CALLBACK WndProcCommon(bool *quit, HWND hwnd, UINT message,
       WPARAM wparam, LPARAM lparam)
 {
