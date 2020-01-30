df06a0effe5a4385cdc2f1102de33503bda49bb8
twinaphex <libretro@gmail.com>
2020-01-30 17:14:45 +0100

    Fix names

--- libretro-common/include/playlists/label_sanitization.h
+++ libretro-common/include/playlists/label_sanitization.h
@@ -1,7 +1,7 @@
 /* Copyright  (C) 2010-2019 The RetroArch team
  *
  * ---------------------------------------------------------------------------------------
- * The following license statement only applies to this file (file_path.h).
+ * The following license statement only applies to this file (label_sanitization.h).
  * ---------------------------------------------------------------------------------------
  *
  * Permission is hereby granted, free of charge,
