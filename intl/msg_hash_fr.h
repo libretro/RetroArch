609dde098839ce34a7d09072818cd7b0d237602f
Weedy Weed Smoker <weeedyweedsmoker@yahoo.fr>
2020-02-05 03:15:03 +0700

    Another small change to the french translation

--- intl/msg_hash_fr.h
+++ intl/msg_hash_fr.h
@@ -2421,7 +2421,7 @@ MSG_HASH(
     )
 MSG_HASH(
     MENU_ENUM_LABEL_VALUE_MENU_INSERT_DISK_RESUME,
-    "Reprendre le contenu après le changement de disques"
+    "Reprendre le contenu après le changement du disque"
     )
 MSG_HASH(
     MENU_ENUM_SUBLABEL_MENU_INSERT_DISK_RESUME,
