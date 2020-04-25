c09e4c6cd820f0d29a8ad1c5f8efa554c3c4caff
Weedy Weed Smoker <weeedyweedsmoker@yahoo.fr>
2020-04-25 17:06:46 +0700

    International: Formatting fix for all languages

--- intl/msg_hash_ar.h
+++ intl/msg_hash_ar.h
@@ -7,96 +7,157 @@
 #endif
 
 MSG_HASH(
-      MENU_ENUM_LABEL_VALUE_CONFIGURATIONS_LIST,
-      "ملفات التكوين"
-      )
+   MENU_ENUM_LABEL_VALUE_CONFIGURATIONS_LIST,
+   "ملفات التكوين"
+   )
 MSG_HASH(
-      MENU_ENUM_LABEL_VALUE_AUDIO_DRIVER,
-      "نظام تشغيل الصوت"
-      )
+   MENU_ENUM_LABEL_VALUE_AUDIO_DRIVER,
+   "نظام تشغيل الصوت"
+   )
 MSG_HASH(
-      MENU_ENUM_LABEL_VALUE_AUDIO_SETTINGS,
-      "الصوت"
-      )
+   MENU_ENUM_LABEL_VALUE_AUDIO_SETTINGS,
+   "الصوت"
+   )
 MSG_HASH(
-      MENU_ENUM_LABEL_VALUE_CAMERA_DRIVER,
-      "نظام تشغيل الكاميرا"
-      )
+   MENU_ENUM_LABEL_VALUE_CAMERA_DRIVER,
+   "نظام تشغيل الكاميرا"
+   )
 MSG_HASH(
-      MENU_ENUM_LABEL_VALUE_CONFIGURATIONS,
-      "تحميل ملف التكوين"
-      )
+   MENU_ENUM_LABEL_VALUE_CONFIGURATIONS,
+   "تحميل ملف التكوين"
+   )
 MSG_HASH(
-      MENU_ENUM_LABEL_VALUE_CONFIGURATION_SETTINGS,
-      "ملفات التكوين"
-      )
+   MENU_ENUM_LABEL_VALUE_CONFIGURATION_SETTINGS,
+   "ملفات التكوين"
+   )
 MSG_HASH(
-      MENU_ENUM_LABEL_VALUE_CONFIG_SAVE_ON_EXIT,
-      "Save Configuration on Exit"
-      )
-MSG_HASH(MENU_ENUM_LABEL_VALUE_CONTENT_SETTINGS,
-      "القائمة السريعة")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_CORE_LIST,
-      "تحميل الكور")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_DIRECTORY_SETTINGS,
-      "الدلائل")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_DRIVER_SETTINGS,
-      "أنظمة التشغيل")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_HELP,
-      "مساعدة")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_HELP_LIST,
-      "مساعدة")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_INFORMATION,
-      "معلومات")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_INFORMATION_LIST,
-      "معلومات")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_INPUT_DRIVER,
-      "نظام تشغيل أجهزة الادخال")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_INPUT_SETTINGS,
-      "أجهزة الادخال")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_JOYPAD_DRIVER,
-      "نظام تشغيل الجوي باد")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_LANG_ARABIC,
-      "عربى")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_LOAD_CONTENT_LIST,
-      "تحميل المحتوى")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_MAIN_MENU,
-      "القائمة الرئيسية")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_MENU_DRIVER,
-      "نظام تشغيل القائمة")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_NETWORK_SETTINGS,
-      "الشبكة")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_ONLINE_UPDATER,
-      "التحديث عبر الانترنت")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_ONSCREEN_DISPLAY_SETTINGS,
-      "العرض على الشاشة")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_PLAYLISTS_TAB,
-      "قوائم التشغيل")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_PLAYLIST_DIRECTORY,
-      "قائمة التشغيل")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_PLAYLIST_SETTINGS,
-      "قوائم التشغيل")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_PRIVACY_SETTINGS,
-      "خصوصية")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_QUIT_RETROARCH,
-      "إنهاء البرنامج")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_RECORD_DRIVER,
-      "نظام تشغيل التسجيل")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_RETRO_ACHIEVEMENTS_SETTINGS,
-      "الإنجازات")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_SAVING_SETTINGS,
-      "الحفظ")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_USER_INTERFACE_SETTINGS,
-      "واجهة المستخدم")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_USER_SETTINGS,
-      "المستخدم")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_VIDEO_DRIVER,
-      "نظام تشغيل الفيديو")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_VIDEO_SETTINGS,
-      "الفيديو")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_WIFI_DRIVER,
-      "نظام تشغيل الواي-فاي")
-MSG_HASH(MENU_ENUM_SUBLABEL_DRIVER_SETTINGS,
-      "تغيير أنظمة التشغيل المستخدمة من قبل البرنامج.")
-MSG_HASH(MENU_ENUM_LABEL_VALUE_DELETE_PLAYLIST,
-      "إحذف قائمة التشغيل")
+   MENU_ENUM_LABEL_VALUE_CONTENT_SETTINGS,
+   "القائمة السريعة"
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_CORE_LIST,
+   "تحميل الكور"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_DIRECTORY_SETTINGS,
+   "الدلائل"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_DRIVER_SETTINGS,
+   "أنظمة التشغيل"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_HELP,
+   "مساعدة"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_HELP_LIST,
+   "مساعدة"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_INFORMATION,
+   "معلومات"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_INFORMATION_LIST,
+   "معلومات"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_INPUT_DRIVER,
+   "نظام تشغيل أجهزة الادخال"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_INPUT_SETTINGS,
+   "أجهزة الادخال"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_JOYPAD_DRIVER,
+   "نظام تشغيل الجوي باد"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_LANG_ARABIC,
+   "عربى"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_LOAD_CONTENT_LIST,
+   "تحميل المحتوى"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_MAIN_MENU,
+   "القائمة الرئيسية"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_MENU_DRIVER,
+   "نظام تشغيل القائمة"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_NETWORK_SETTINGS,
+   "الشبكة"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_ONLINE_UPDATER,
+   "التحديث عبر الانترنت"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_ONSCREEN_DISPLAY_SETTINGS,
+   "العرض على الشاشة"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_PLAYLISTS_TAB,
+   "قوائم التشغيل"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_PLAYLIST_DIRECTORY,
+   "قائمة التشغيل"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_PLAYLIST_SETTINGS,
+   "قوائم التشغيل"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_PRIVACY_SETTINGS,
+   "خصوصية"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_QUIT_RETROARCH,
+   "إنهاء البرنامج"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_RECORD_DRIVER,
+   "نظام تشغيل التسجيل"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_RETRO_ACHIEVEMENTS_SETTINGS,
+   "الإنجازات"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_SAVING_SETTINGS,
+   "الحفظ"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_USER_INTERFACE_SETTINGS,
+   "واجهة المستخدم"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_USER_SETTINGS,
+   "المستخدم"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_VIDEO_DRIVER,
+   "نظام تشغيل الفيديو"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_VIDEO_SETTINGS,
+   "الفيديو"
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_WIFI_DRIVER,
+   "نظام تشغيل الواي-فاي"
+   )
+MSG_HASH(
+   MENU_ENUM_SUBLABEL_DRIVER_SETTINGS,
+   "تغيير أنظمة التشغيل المستخدمة من قبل البرنامج."
+   )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_DELETE_PLAYLIST,
+   "إحذف قائمة التشغيل"
+   )
