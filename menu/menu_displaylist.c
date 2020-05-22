eaba575c9ebed13047a94af1b7f7fc0a3e596f1f
twinaphex <libretro@gmail.com>
2020-05-22 21:11:35 +0200

    Replace strstrs where possible

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -10570,7 +10570,7 @@ bool menu_displaylist_ctl(enum menu_displaylist_ctl_state type,
          {
             menu_entries_ctl(MENU_ENTRIES_CTL_CLEAR, info->list);
 
-            if (strstr(info->path, "core_option_"))
+            if (string_starts_with(info->path, "core_option_"))
             {
                struct string_list *tmp_str_list = string_split(info->path, "_");
 
@@ -10988,7 +10988,7 @@ bool menu_displaylist_ctl(enum menu_displaylist_ctl_state type,
       case DISPLAYLIST_DROPDOWN_LIST_SPECIAL:
          menu_entries_ctl(MENU_ENTRIES_CTL_CLEAR, info->list);
 
-         if (strstr(info->path, "core_option_"))
+         if (string_starts_with(info->path, "core_option_"))
          {
             struct string_list *tmp_str_list = string_split(info->path, "_");
 
