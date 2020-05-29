40a4b395d7ca63d73359750aa51368d9c1d5f6b4
twinaphex <libretro@gmail.com>
2020-05-29 10:50:08 +0200

    Localize string

--- menu/cbs/menu_cbs_title.c
+++ menu/cbs/menu_cbs_title.c
@@ -88,7 +88,11 @@ static int action_get_title_remap_port(const char *path, const char *label,
       unsigned menu_type, char *s, size_t len)
 {
    char lbl[128];
-   snprintf(lbl, sizeof(lbl), "Port %d Controls", atoi(path) + 1);
+   snprintf(lbl, sizeof(lbl), "%s %d %s",
+         msg_hash_to_str(MENU_ENUM_LABEL_VALUE_PORT),
+         atoi(path) + 1,
+         msg_hash_to_str(MENU_ENUM_LABEL_VALUE_CORE_INPUT_REMAPPING_OPTIONS)
+         );
    sanitize_to_string(s, lbl, len);
    return 1;
 }
