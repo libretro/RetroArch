4bbc22633596ae22da0e3790e0560e6f8941515b
twinaphex <libretro@gmail.com>
2020-06-06 21:57:22 +0200

    Move netplay_frontend.c to retroarch.c - move global state to retroarch global state

--- griffin/griffin.c
+++ griffin/griffin.c
@@ -1212,7 +1212,6 @@ NETPLAY
 #ifdef HAVE_NETWORKING
 #include "../network/netplay/netplay_delta.c"
 #include "../network/netplay/netplay_handshake.c"
-#include "../network/netplay/netplay_frontend.c"
 #include "../network/netplay/netplay_init.c"
 #include "../network/netplay/netplay_io.c"
 #include "../network/netplay/netplay_keyboard.c"
