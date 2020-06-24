3a4af3149b5d6745bc7ca362c0cea678b8ff9649
twinaphex <libretro@gmail.com>
2020-06-24 17:35:02 +0200

    (libretro-common) Use malloc instead of calloc when possible

--- libretro-common/queues/message_queue.c
+++ libretro-common/queues/message_queue.c
@@ -59,14 +59,15 @@ struct msg_queue
 msg_queue_t *msg_queue_new(size_t size)
 {
    struct queue_elem **elems = NULL;
-   msg_queue_t *queue        = (msg_queue_t*)calloc(1, sizeof(*queue));
+   msg_queue_t *queue        = (msg_queue_t*)malloc(sizeof(*queue));
 
    if (!queue)
       return NULL;
 
-   queue->size = size + 1;
+   queue->size               = size + 1;
+   queue->tmp_msg            = NULL;
 
-   elems = (struct queue_elem**)calloc(queue->size,
+   elems                     = (struct queue_elem**)calloc(queue->size,
          sizeof(struct queue_elem*));
 
    if (!elems)
@@ -75,8 +76,8 @@ msg_queue_t *msg_queue_new(size_t size)
       return NULL;
    }
 
-   queue->elems = elems;
-   queue->ptr   = 1;
+   queue->elems              = elems;
+   queue->ptr                = 1;
 
    return queue;
 }
@@ -119,14 +120,14 @@ void msg_queue_push(msg_queue_t *queue, const char *msg,
    if (!queue || queue->ptr >= queue->size)
       return;
 
-   new_elem                      = (struct queue_elem*)
-      calloc(1, sizeof(struct queue_elem));
+   new_elem                      = (struct queue_elem*)malloc(
+      sizeof(struct queue_elem));
    if (!new_elem)
       return;
 
-   new_elem->prio                = prio;
    new_elem->duration            = duration;
-   new_elem->msg                 = msg ? strdup(msg) : NULL;
+   new_elem->prio                = prio;
+   new_elem->msg                 = msg   ? strdup(msg)   : NULL;
    new_elem->title               = title ? strdup(title) : NULL;
    new_elem->icon                = icon;
    new_elem->category            = category;
