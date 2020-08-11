ee8a2f4268a962bf33018f287354b15365ce86e3
jdgleaver <james@leaver.myzen.co.uk>
2020-08-11 14:07:00 +0100

    Add API extension for cores to query the number of active inputs provided by the frontend

--- libretro-common/include/libretro.h
+++ libretro-common/include/libretro.h
@@ -1321,6 +1321,18 @@ enum retro_mod
                                             * fallback, stderr).
                                             */
 
+#define RETRO_ENVIRONMENT_GET_INPUT_MAX_USERS 61
+                                           /* unsigned * --
+                                            * Unsigned value is the number of active input devices
+                                            * provided by the frontend. This may change at any time.
+                                            * If callback returns true, a core need not poll any
+                                            * input device with an index greater than or equal to
+                                            * the number of active devices.
+                                            * If callback returns false, the number of active input
+                                            * devices is unknown. In this case, all input devices
+                                            * should be considered active.
+                                            */
+
 /* VFS functionality */
 
 /* File paths:
