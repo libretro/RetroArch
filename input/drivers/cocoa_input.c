8b18ae3018a24c3a566ba72240cab32978915a99
twinaphex <libretro@gmail.com>
2020-09-14 19:38:52 +0200

    (Cocoa) Fix keyboard input

--- input/drivers/cocoa_input.c
+++ input/drivers/cocoa_input.c
@@ -398,9 +398,7 @@ static int16_t cocoa_input_state(
             {
                for (i = 0; i < RARCH_FIRST_CUSTOM_BIND; i++)
                {
-                  if (
-                        (binds[port][i].key < RETROK_LAST) &&
-                        apple_key_state[rarch_keysym_lut[binds[port][i].key]] & 0x80)
+                  if (apple_key_state[rarch_keysym_lut[binds[port][i].key]])
                      ret |= (1 << i);
                }
             }
