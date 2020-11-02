2647ce47932485d83ab362cb1ec52a17e83f2ecc
Stephanie Gawroriski <xerthesquirrel@gmail.com>
2020-11-02 00:17:24 -0500

    Attempt at getting Static QT working.

--- qb/qb.moc.sh
+++ qb/qb.moc.sh
@@ -23,21 +23,21 @@ if [ "$HAVE_QT" = "yes" ]; then
 	moc_works=0
 	if [ "$MOC" ]; then
 		QT_SELECT="$QT_VERSION" \
-		"$MOC" -o "$TEMP_CPP" "$TEMP_MOC" >/dev/null 2>&1 &&
+		"$MOC" -o "$TEMP_CPP" "$TEMP_MOC" >>config.log 2>&1 &&
 			$(printf %s "$CXX") -o "$TEMP_EXE" \
 			$(printf %s "$QT_FLAGS") -fPIC -c "$TEMP_CPP" \
-			>/dev/null 2>&1 &&
+			>>config.log 2>&1 &&
 		moc_works=1
 	else
 		for moc in "moc-$QT_VERSION" moc; do
 			MOC="$(exists "$moc")" || MOC=""
 			if [ "$MOC" ]; then
 				QT_SELECT="$QT_VERSION" \
-				"$MOC" -o "$TEMP_CPP" "$TEMP_MOC" >/dev/null 2>&1 ||
+				"$MOC" -o "$TEMP_CPP" "$TEMP_MOC" >>config.log 2>&1 ||
 					continue
 				if $(printf %s "$CXX") -o "$TEMP_EXE" \
 						$(printf %s "$QT_FLAGS") -fPIC -c \
-						"$TEMP_CPP" >/dev/null 2>&1; then
+						"$TEMP_CPP" >>config.log 2>&1; then
 					moc_works=1
 					break
 				fi
