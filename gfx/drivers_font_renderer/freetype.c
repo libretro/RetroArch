07db1bbfdef87e44673e458ab584934be9da2f5b
twinaphex <libretro@gmail.com>
2021-01-08 23:20:34 +0100

    Cut down on code duplication

--- gfx/drivers_font_renderer/freetype.c
+++ gfx/drivers_font_renderer/freetype.c
@@ -238,8 +238,6 @@ static void *font_renderer_ft_init(const char *font_path, float font_size)
          goto error;
 
       err = FT_New_Memory_Face(handle->lib, font_data, font_size, 0, &handle->face);
-      if (err)
-         goto error;
    }
    else
 #endif
@@ -247,9 +245,10 @@ static void *font_renderer_ft_init(const char *font_path, float font_size)
       if (!path_is_valid(font_path))
          goto error;
       err = FT_New_Face(handle->lib, font_path, 0, &handle->face);
-      if (err)
-         goto error;
    }
+
+   if (err)
+      goto error;
 
    err = FT_Select_Charmap(handle->face, FT_ENCODING_UNICODE);
    if (err)
