7d56bf2b0a634cc680f711980e916ca40e925250
Autechre <libretro@gmail.com>
2021-01-21 08:36:10 +0100

    (Apple) Packaging update - will now package it into DMG as well (#11919)

--- retroarch-apple-packaging.sh
+++ retroarch-apple-packaging.sh
@@ -1,12 +1,35 @@
 #!/bin/sh
+# app stuff
+
 rm -rf RetroArch.app
+
 mkdir -p RetroArch.app/Contents/MacOS
 cp -r pkg/apple/OSX/* RetroArch.app/Contents
 cp retroarch RetroArch.app/Contents/MacOS
- 
+
 gsed -i -e 's/\${EXECUTABLE_NAME}/RetroArch/' RetroArch.app/Contents/Info.plist
-gsed -i -e 's/\${PRODUCT_BUNDLE_IDENTIFIER}/com.libretro.RetroArch/' RetroArch.app/Contents/Info.plist
+gsed -i -e 's/\$(PRODUCT_BUNDLE_IDENTIFIER)/com.libretro.RetroArch/' RetroArch.app/Contents/Info.plist
 gsed -i -e 's/\${PRODUCT_NAME}/RetroArch/' RetroArch.app/Contents/Info.plist
 gsed -i -e 's/\${MACOSX_DEPLOYMENT_TARGET}/10.13/' RetroArch.app/Contents/Info.plist
- 
-cp media/retroarch.icns RetroArch.app/Contents/Resources
+
+cp media/retroarch.icns RetroArch.app/Contents/Resources/
+
+# dmg stuff
+
+umount wc
+rm -rf RetroArch.dmg wc empty.dmg
+
+mkdir -p template
+hdiutil create -fs HFSX -layout SPUD -size 200m empty.dmg -srcfolder template -format UDRW -volname RetroArch -quiet
+rmdir template
+
+mkdir -p wc
+hdiutil attach empty.dmg -noautoopen -quiet -mountpoint wc
+rm -rf wc/RetroArch.app
+ditto -rsrc RetroArch.app wc/RetroArch.app
+ln -s /Applications wc/Applications
+WC_DEV=`hdiutil info | grep wc | grep "Apple_HFS" | awk '{print $1}'` && hdiutil detach $WC_DEV -quiet -force
+hdiutil convert empty.dmg -quiet -format UDZO -imagekey zlib-level=9 -o RetroArch.dmg
+
+umount wc
+rm -rf wc empty.dmg
