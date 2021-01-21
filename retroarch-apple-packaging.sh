90c438d37001aa65bb26058f829c45c44e4c087f
Autechre <libretro@gmail.com>
2021-01-21 09:22:44 +0100

    (retroarch-apple-packaging) Remove dependence on gsed

--- retroarch-apple-packaging.sh
+++ retroarch-apple-packaging.sh
@@ -7,10 +7,10 @@ mkdir -p RetroArch.app/Contents/MacOS
 cp -r pkg/apple/OSX/* RetroArch.app/Contents
 cp retroarch RetroArch.app/Contents/MacOS
 
-gsed -i -e 's/\${EXECUTABLE_NAME}/RetroArch/' RetroArch.app/Contents/Info.plist
-gsed -i -e 's/\$(PRODUCT_BUNDLE_IDENTIFIER)/com.libretro.RetroArch/' RetroArch.app/Contents/Info.plist
-gsed -i -e 's/\${PRODUCT_NAME}/RetroArch/' RetroArch.app/Contents/Info.plist
-gsed -i -e 's/\${MACOSX_DEPLOYMENT_TARGET}/10.13/' RetroArch.app/Contents/Info.plist
+sed -i'.bak' 's/\${EXECUTABLE_NAME}/RetroArch/' RetroArch.app/Contents/Info.plist
+sed -i'.bak' 's/\$(PRODUCT_BUNDLE_IDENTIFIER)/com.libretro.RetroArch/' RetroArch.app/Contents/Info.plist
+sed -i'.bak' 's/\${PRODUCT_NAME}/RetroArch/' RetroArch.app/Contents/Info.plist
+sed -i'.bak' 's/\${MACOSX_DEPLOYMENT_TARGET}/10.13/' RetroArch.app/Contents/Info.plist
 
 cp media/retroarch.icns RetroArch.app/Contents/Resources/
 
