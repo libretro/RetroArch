dbbf55795460ecc7e9d6f2b9995c13ceb4529c38
twinaphex <libretro@gmail.com>
2021-01-24 03:11:13 +0100

    (Metal/WindowListener) Fix duplicate symbol errors by moving implementation of WindowListener to cocoa_common.m

--- ui/drivers/cocoa/apple_platform.h
+++ ui/drivers/cocoa/apple_platform.h
@@ -9,18 +9,7 @@
 #if defined(HAVE_COCOA_METAL) && !defined(HAVE_COCOATOUCH)
 @interface WindowListener : NSResponder<NSWindowDelegate>
 @end
-
-@implementation WindowListener
-
-/* Similarly to SDL, we'll respond to key events by doing nothing so we don't beep.
- */
-- (void)flagsChanged:(NSEvent *)event { }
-- (void)keyDown:(NSEvent *)event { }
-- (void)keyUp:(NSEvent *)event { }
-
-@end
 #endif
-
 
 #if defined(HAVE_COCOA_METAL) || defined(HAVE_COCOATOUCH)
 @protocol ApplePlatform
