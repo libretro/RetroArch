ebfe32db3e8ae130cee94554788fa35eb2fb5b28
jdgleaver <james@leaver.myzen.co.uk>
2021-01-31 13:37:34 +0000

    Add 'Upscale_256x-320x240' video filter

--- griffin/griffin.c
+++ griffin/griffin.c
@@ -1020,6 +1020,7 @@ FILTERS
 #include "../gfx/video_filters/dot_matrix_3x.c"
 #include "../gfx/video_filters/dot_matrix_4x.c"
 #include "../gfx/video_filters/upscale_1_5x.c"
+#include "../gfx/video_filters/upscale_256x_320x240.c"
 #endif
 
 #ifdef HAVE_DSP_FILTER
