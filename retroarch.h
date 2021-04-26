c7f3fa2b5d4da01ac268a9f62a3040e8d6bee15c
David Carlier <devnexen@gmail.com>
2021-04-26 20:31:15 +0100

    macOS with vulkan build fix

--- retroarch.h
+++ retroarch.h
@@ -1867,6 +1867,7 @@ extern const gfx_ctx_driver_t gfx_ctx_videocore;
 extern const gfx_ctx_driver_t gfx_ctx_qnx;
 extern const gfx_ctx_driver_t gfx_ctx_cgl;
 extern const gfx_ctx_driver_t gfx_ctx_cocoagl;
+extern const gfx_ctx_driver_t gfx_ctx_cocoavk;
 extern const gfx_ctx_driver_t gfx_ctx_emscripten;
 extern const gfx_ctx_driver_t gfx_ctx_opendingux_fbdev;
 extern const gfx_ctx_driver_t gfx_ctx_khr_display;
