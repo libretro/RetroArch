6e2709d62a4892cd2f19cc0c5da15a241d30db9c
Grant <christie.grant.com>
2021-06-19 08:47:35 +0100

    cleanup

--- input/drivers/udev_input.c
+++ input/drivers/udev_input.c
@@ -1060,14 +1060,13 @@ static int16_t udev_input_state(
                      (binds[port][id].key < RETROK_LAST) && 
                      udev_keyboard_pressed(udev, binds[port][id].key)
                      && ((    id != RARCH_GAME_FOCUS_TOGGLE) 
-                        || !keyboard_mapping_blocked)
+                        && !keyboard_mapping_blocked)
                      )
                   return 1;
                else if ( 
                      (binds[port][id].key < RETROK_LAST) && 
                      udev_keyboard_pressed(udev, binds[port][id].key)
-                     && ((    id == RARCH_GAME_FOCUS_TOGGLE) 
-                        || !keyboard_mapping_blocked)
+                     && (    id == RARCH_GAME_FOCUS_TOGGLE)
                      )
                   return 1;
                else if (udev_mouse_button_pressed(udev, port,
