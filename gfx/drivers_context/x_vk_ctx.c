97ad8cbfd261f46f717055e6f77d69c5ecddc81e
twinaphex <libretro@gmail.com>
2021-06-22 18:26:32 +0200

    Remove this code for Vulkan over X too

--- gfx/drivers_context/x_vk_ctx.c
+++ gfx/drivers_context/x_vk_ctx.c
@@ -111,12 +111,6 @@ static void gfx_ctx_x_vk_destroy_resources(gfx_ctx_x_vk_data_t *x)
          x11_exit_fullscreen(g_x11_dpy);
          x->should_reset_mode = false;
       }
-
-      if (!video_driver_is_video_cache_context())
-      {
-         XCloseDisplay(g_x11_dpy);
-         g_x11_dpy = NULL;
-      }
    }
 }
 
