149beaf06133577cdb552137351ad3b3af5967bb
sonninnos <tony@pihaus.org>
2021-07-29 00:28:26 +0300

    'Automatic Mouse Grab' option

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -6345,6 +6345,10 @@ unsigned menu_displaylist_build_list(
                   PARSE_ONLY_BOOL, false) == 0)
             count++;
          if (MENU_DISPLAYLIST_PARSE_SETTINGS_ENUM(list,
+                  MENU_ENUM_LABEL_INPUT_AUTO_MOUSE_GRAB,
+                  PARSE_ONLY_BOOL, false) == 0)
+            count++;
+         if (MENU_DISPLAYLIST_PARSE_SETTINGS_ENUM(list,
                   MENU_ENUM_LABEL_INPUT_AUTO_GAME_FOCUS,
                   PARSE_ONLY_UINT, false) == 0)
             count++;
--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -12831,6 +12831,22 @@ static bool setting_append_list(
                   SD_FLAG_NONE
                   );
 
+            CONFIG_BOOL(
+                  list, list_info,
+                  &settings->bools.input_auto_mouse_grab,
+                  MENU_ENUM_LABEL_INPUT_AUTO_MOUSE_GRAB,
+                  MENU_ENUM_LABEL_VALUE_INPUT_AUTO_MOUSE_GRAB,
+                  false,
+                  MENU_ENUM_LABEL_VALUE_OFF,
+                  MENU_ENUM_LABEL_VALUE_ON,
+                  &group_info,
+                  &subgroup_info,
+                  parent_group,
+                  general_write_handler,
+                  general_read_handler,
+                  SD_FLAG_NONE
+                  );
+
             CONFIG_UINT(
                   list, list_info,
                   &settings->uints.input_auto_game_focus,
