libretro/RetroArch

commit 39fd4677d9b9601b864fbd71dcd1e6eb3262cd0b

win32: fix non-ASCII text display in window title (#13089)

gfx/common/win32_common.c
@@ -1615,20 +1615,22 @@ bool win32_window_create(void *data, unsigned style,
    bool    window_save_positions = settings->bools.video_window_save_positions;
    unsigned    user_width        = width;
    unsigned    user_height       = height;
+   wchar_t *title_wide     = utf8_to_utf16_string_alloc(msg_hash_to_str(MSG_PROGRAM));
 
    if (window_save_positions && !fullscreen)
    {
       user_width                 = g_win32->pos_width;
       user_height                = g_win32->pos_height;
    }
-   main_window.hwnd              = CreateWindowEx(0,
-         "RetroArch", msg_hash_to_str(MSG_PROGRAM),
+   main_window.hwnd              = CreateWindowExW(0,
+         L"RetroArch", title_wide,
          style,
          fullscreen ? mon_rect->left : g_win32->pos_x,
          fullscreen ? mon_rect->top  : g_win32->pos_y,
          user_width,
          user_height,
          NULL, NULL, NULL, data);
+   free(title_wide);
    if (!main_window.hwnd)
       return false;
 

ui/drivers/ui_win32.c
@@ -116,7 +116,9 @@ static void ui_window_win32_set_visible(void *data,
 static void ui_window_win32_set_title(void *data, char *buf)
 {
    ui_window_win32_t *window = (ui_window_win32_t*)data;
-   SetWindowText(window->hwnd, buf);
+   wchar_t *title_wide = utf8_to_utf16_string_alloc(buf);
+   SetWindowTextW(window->hwnd, title_wide);
+   free(title_wide);
 }
 
 void ui_window_win32_set_droppable(void *data, bool droppable)

commit 451990c0e58417f0740d2a8f7e56f829b5b58e3b

win32: fix broken win9x builds caused by #13089

gfx/common/win32_common.c
@@ -1615,22 +1615,31 @@ bool win32_window_create(void *data, unsigned style,
    bool    window_save_positions = settings->bools.video_window_save_positions;
    unsigned    user_width        = width;
    unsigned    user_height       = height;
-   wchar_t *title_wide     = utf8_to_utf16_string_alloc(msg_hash_to_str(MSG_PROGRAM));
+#ifdef LEGACY_WIN32
+   char *title_local       = utf8_to_local_string_alloc(new_label2);
+#else
+   wchar_t *title_local    = utf8_to_utf16_string_alloc(msg_hash_to_str(MSG_PROGRAM));
+#endif
 
    if (window_save_positions && !fullscreen)
    {
       user_width                 = g_win32->pos_width;
       user_height                = g_win32->pos_height;
    }
+#ifdef LEGACY_WIN32
+   main_window.hwnd              = CreateWindowEx(0,
+         "RetroArch", title_local,
+#else
    main_window.hwnd              = CreateWindowExW(0,
-         L"RetroArch", title_wide,
+         L"RetroArch", title_local,
+#endif
          style,
          fullscreen ? mon_rect->left : g_win32->pos_x,
          fullscreen ? mon_rect->top  : g_win32->pos_y,
          user_width,
          user_height,
          NULL, NULL, NULL, data);
-   free(title_wide);
+   free(title_local);
    if (!main_window.hwnd)
       return false;
 

ui/drivers/ui_win32.c
@@ -116,9 +116,14 @@ static void ui_window_win32_set_visible(void *data,
 static void ui_window_win32_set_title(void *data, char *buf)
 {
    ui_window_win32_t *window = (ui_window_win32_t*)data;
-   wchar_t *title_wide = utf8_to_utf16_string_alloc(buf);
-   SetWindowTextW(window->hwnd, title_wide);
-   free(title_wide);
+#ifdef LEGACY_WIN32
+   char *title_local = utf8_to_local_string_alloc(new_label2);
+   SetWindowText(window->hwnd, title_local);
+#else
+   wchar_t *title_local = utf8_to_utf16_string_alloc(buf);
+   SetWindowTextW(window->hwnd, title_local);
+#endif
+   free(title_local);
 }
 
 void ui_window_win32_set_droppable(void *data, bool droppable)