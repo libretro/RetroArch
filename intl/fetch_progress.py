32873575621059145810cfeaf99916b68d1998d2
Guo Yunhe <i@guoyunhe.me>
2021-10-31 13:02:02 +0800

    Update fetch_progress.py

--- intl/fetch_progress.py
+++ intl/fetch_progress.py
@@ -8,17 +8,14 @@ with open("crowdin.yaml", 'r') as config_file:
    config = yaml.safe_load(config_file)
    headers = { 'Authorization': 'Bearer ' + config['api_token']}
 
-   url1 = 'https://api.crowdin.com/api/v2/projects/' + config['project_id'] + '/branches'
-   res1 =  requests.get(url1, headers=headers)
-   branch_id = res1.json()['data'][0]['data']['id']
-   url2 = url1 + '/' + branch_id + '/languages/progress?limit=100'
-   res2 = requests.get(url2, headers=headers)
+   url1 = 'https://api.crowdin.com/api/v2/projects/' + config['project_id'] + '/languages/progress?limit=100'
+   res1 = requests.get(url1, headers=headers)
    output = ''
-   for lang in res2.json()['data']:
+   for lang in res1.json()['data']:
       lang_id = lang['data']['languageId']
-      url3 = 'https://api.crowdin.com/api/v2/languages/' + lang_id
-      res3 =  requests.get(url3, headers=headers)
-      lang_name = res0.json()['data']['name']
+      url2 = 'https://api.crowdin.com/api/v2/languages/' + lang_id
+      res2 =  requests.get(url2, headers=headers)
+      lang_name = res2.json()['data']['name']
    
       output += '/* ' + lang_name + ' */\n'
       escaped_name = lang_name.replace(', ', '_').replace(' ', '_').upper()
