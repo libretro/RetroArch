318f153012185db8813660fd1da2f160c4170c24
twinaphex <libretro@gmail.com>
2021-11-10 15:49:26 +0100

    Fix default overlay file - overlays were re-arranged and startup overlay was not updated to reflect this

--- file_path_special.h
+++ file_path_special.h
@@ -109,7 +109,7 @@ RETRO_BEGIN_DECLS
 #define FILE_PATH_LOCK_EXTENSION_NO_DOT "lck"
 #define FILE_PATH_BACKUP_EXTENSION ".bak"
 #if defined(RARCH_MOBILE)
-#define FILE_PATH_DEFAULT_OVERLAY "gamepads/neo-retropad/neo-retropad.cfg"
+#define FILE_PATH_DEFAULT_OVERLAY "Gamepads/RetroPad - Neo/neo-retropad.cfg"
 #endif
 #define FILE_PATH_CORE_INFO_CACHE "core_info.cache"
 #define FILE_PATH_CORE_INFO_CACHE_REFRESH "core_info.refresh"
