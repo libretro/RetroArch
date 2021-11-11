e64cd247150de4cadaab62a574d219eef3fdbd84
twinaphex <libretro@gmail.com>
2021-11-11 05:38:45 +0100

    (Cthulhu88) Fix for multi-char arrays

--- network/netplay/netplay.h
+++ network/netplay/netplay.h
@@ -191,7 +191,7 @@ struct netplay_host_list
 struct netplay_mitm_pending
 {
    int          *fds;
-   uint8_t      *ids[NETPLAY_MITM_ID_SIZE];
+   uint8_t      *ids[NETPLAY_MITM_MAX_PENDING];
    retro_time_t *timeouts;
 
    int current;
