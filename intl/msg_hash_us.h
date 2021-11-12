efa16a8fca735bab348abd3ebcb747c004c2ce7b
twinaphex <libretro@gmail.com>
2021-11-12 19:00:42 +0100

    Revert "(Netplay) Cthulhu88 -"
    This reverts commit cb4028fd3d770241100708c8fea66782cfa6b4de.

--- intl/msg_hash_us.h
+++ intl/msg_hash_us.h
@@ -2681,14 +2681,6 @@ MSG_HASH(
    "Switches current netplay session between 'play' and 'spectate' modes."
    )
 MSG_HASH(
-   MENU_ENUM_LABEL_VALUE_INPUT_META_NETPLAY_PLAYER_CHAT,
-   "Netplay Player Chat"
-   )
-MSG_HASH(
-   MENU_ENUM_SUBLABEL_INPUT_META_NETPLAY_PLAYER_CHAT,
-   "Sends a chat message to the current netplay session."
-   )
-MSG_HASH(
    MENU_ENUM_LABEL_VALUE_INPUT_META_ENABLE_HOTKEY,
    "Hotkey Enable"
    )
@@ -10828,10 +10820,6 @@ MSG_HASH(
 MSG_HASH(
    MSG_NETPLAY_ENTER_PASSWORD,
    "Enter netplay server password:"
-   )
-MSG_HASH(
-   MSG_NETPLAY_ENTER_CHAT,
-   "Enter netplay chat message:"
    )
 MSG_HASH(
    MSG_DISCORD_CONNECTION_REQUEST,
