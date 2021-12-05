207c4f707fb9a532f461a1bb5e0a1d5f14705108
twinaphex <libretro@gmail.com>
2021-12-05 05:45:55 +0100

    Remove if 0s

--- deps/miniupnpc/miniupnpc.c
+++ deps/miniupnpc/miniupnpc.c
@@ -324,10 +324,6 @@ upnpDiscover(int delay, const char * multicastif,
              int * error)
 {
 	static const char * const deviceList[] = {
-#if 0
-		"urn:schemas-upnp-org:device:InternetGatewayDevice:2",
-		"urn:schemas-upnp-org:service:WANIPConnection:2",
-#endif
 		"urn:schemas-upnp-org:device:InternetGatewayDevice:1",
 		"urn:schemas-upnp-org:service:WANIPConnection:1",
 		"urn:schemas-upnp-org:service:WANPPPConnection:1",
--- deps/miniupnpc/miniupnpcstrings.h
+++ deps/miniupnpc/miniupnpcstrings.h
@@ -11,13 +11,8 @@
 #define OS_STRING "OS/version"
 #define MINIUPNPC_VERSION_STRING "version"
 
-#if 0
-/* according to "UPnP Device Architecture 1.0" */
-#define UPNP_VERSION_STRING "UPnP/1.0"
-#else
 /* according to "UPnP Device Architecture 1.1" */
 #define UPNP_VERSION_STRING "UPnP/1.1"
-#endif
 
 #endif
 
