b620ad274e80cfa6ffa1d4b713f5b740b561f855
Jamiras <jamirasmt@gmail.com>
2022-01-10 09:36:16 -0700

    reset hardcore default to enabled; show message when trying to load state in hardcore

--- intl/msg_hash_us.h
+++ intl/msg_hash_us.h
@@ -12255,6 +12255,10 @@ MSG_HASH(
    "Failed to receive header from host"
    )
 MSG_HASH(
+   MSG_CHEEVOS_LOAD_STATE_PREVENTED_BY_HARDCORE_MODE,
+   "You must pause or disable Achievements Hardcore Mode to load states."
+   )
+MSG_HASH(
    MSG_CHEEVOS_HARDCORE_MODE_DISABLED,
    "A save state was loaded. Achievements Hardcore Mode disabled for the current session."
    )
