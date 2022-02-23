2770fdef0fa60f76b462e0627dfd3461d9e1be65
= <jc@pop-os.localdomain>
2022-02-23 20:04:54 +1100

    just fixing something

--- menu/cbs/menu_cbs_title.c
+++ menu/cbs/menu_cbs_title.c
@@ -659,6 +659,7 @@ DEFAULT_TITLE_MACRO(action_get_audio_mixer_settings_list,           MENU_ENUM_LA
 DEFAULT_TITLE_MACRO(action_get_input_settings_list,             MENU_ENUM_LABEL_VALUE_INPUT_SETTINGS)
 DEFAULT_TITLE_MACRO(action_get_latency_settings_list,           MENU_ENUM_LABEL_VALUE_LATENCY_SETTINGS)
 DEFAULT_TITLE_MACRO(action_get_load_content_list,               MENU_ENUM_LABEL_VALUE_LOAD_CONTENT_LIST)
+DEFAULT_TITLE_MACRO(action_get_load_dropbox_list,               MENU_ENUM_LABEL_VALUE_LOAD_DROPBOX_LIST)
 DEFAULT_TITLE_MACRO(action_get_load_content_special,            MENU_ENUM_LABEL_VALUE_LOAD_CONTENT_SPECIAL)
 DEFAULT_TITLE_MACRO(action_get_cursor_manager_list,             MENU_ENUM_LABEL_VALUE_CURSOR_MANAGER)
 DEFAULT_TITLE_MACRO(action_get_database_manager_list,           MENU_ENUM_LABEL_VALUE_DATABASE_MANAGER)
@@ -1112,6 +1113,8 @@ static int menu_cbs_init_bind_title_compare_label(menu_file_list_cbs_t *cbs,
          action_get_title_log_dir},
       {MENU_ENUM_LABEL_LOAD_CONTENT_LIST,
          action_get_load_content_list},
+      {MENU_ENUM_LABEL_LOAD_DROPBOX_LIST,
+         action_get_load_dropbox_list},
       {MENU_ENUM_LABEL_NETPLAY,
          action_get_netplay_list},
       {MENU_ENUM_LABEL_INFORMATION_LIST,
@@ -1443,6 +1446,9 @@ static int menu_cbs_init_bind_title_compare_label(menu_file_list_cbs_t *cbs,
             break;
          case MENU_ENUM_LABEL_LOAD_CONTENT_LIST:
             BIND_ACTION_GET_TITLE(cbs, action_get_load_content_list);
+            break;
+         case MENU_ENUM_LABEL_LOAD_DROPBOX_LIST:
+            BIND_ACTION_GET_TITLE(cbs, action_get_load_dropbox_list);
             break;
          case MENU_ENUM_LABEL_NETPLAY:
             BIND_ACTION_GET_TITLE(cbs, action_get_netplay_list);
