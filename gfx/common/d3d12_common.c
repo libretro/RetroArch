ba2203e1e484368f2989439c7a45a49df6e3a679
tunip3 <26260613+tunip3@users.noreply.github.com>
2022-05-06 20:49:21 +0100

    fix uwp debug build (#13918)
    * fix uwp debug build

--- gfx/common/d3d12_common.c
+++ gfx/common/d3d12_common.c
@@ -165,6 +165,10 @@ bool d3d12_init_base(d3d12_video_t* d3d12)
 {
    DXGIAdapter adapter = NULL;
 #ifdef DEBUG
+#ifdef __WINRT__
+   if (SUCCEEDED(D3D12GetDebugInterface_(&d3d12->debugController)))
+      d3d12->debugController->lpVtbl->EnableDebugLayer(&d3d12->debugController);
+#else
    D3D12GetDebugInterface_(&d3d12->debugController);
    D3D12EnableDebugLayer(d3d12->debugController);
 #endif
