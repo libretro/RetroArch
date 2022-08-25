288b2da13ec2c76e13eb05dba8b2801122188871
LibretroAdmin <reallibretroretroarch@gmail.com>
2022-08-25 16:50:12 +0200

    Create separate file compat_strldup.c since compat_strl.c is not getting built-in for Apple targets at all

--- griffin/griffin.c
+++ griffin/griffin.c
@@ -91,6 +91,7 @@ COMPATIBILITY
 #endif
 
 #include "../libretro-common/compat/compat_fnmatch.c"
+#include "../libretro-common/compat/compat_strldup.c"
 #include "../libretro-common/compat/fopen_utf8.c"
 #include "../libretro-common/memmap/memalign.c"
 
--- libretro-common/compat/compat_strldup.c
+++ libretro-common/compat/compat_strldup.c
@@ -25,38 +25,9 @@
 
 #include <compat/strl.h>
 
-/* Implementation of strlcpy()/strlcat() based on OpenBSD. */
-
-#ifndef __MACH__
-
-size_t strlcpy(char *dest, const char *source, size_t size)
+char *strldup(const char *s, size_t n)
 {
-   size_t src_size = 0;
-   size_t        n = size;
-
-   if (n)
-      while (--n && (*dest++ = *source++)) src_size++;
-
-   if (!n)
-   {
-      if (size) *dest = '\0';
-      while (*source++) src_size++;
-   }
-
-   return src_size;
+   char *dst = (char*)malloc(sizeof(char) * (n + 1));
+   strlcpy(dst, s, n);
+   return dst;
 }
-
-size_t strlcat(char *dest, const char *source, size_t size)
-{
-   size_t len = strlen(dest);
-
-   dest += len;
-
-   if (len > size)
-      size = 0;
-   else
-      size -= len;
-
-   return len + strlcpy(dest, source, size);
-}
-#endif
