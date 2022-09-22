cd93bdc0583e28b3627e02903a8c1788ca6fa027
Bobby Smith <33353403+bslenul@users.noreply.github.com>
2022-09-22 12:58:56 +0200

    Fix "Windowed Scale" dropdown menu (#14433)

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -12006,6 +12006,7 @@ static bool setting_append_list(
                      general_write_handler,
                      general_read_handler);
                (*list)[list_info->index - 1].action_ok = &setting_action_ok_uint;
+               (*list)[list_info->index - 1].offset_by = 1;
                menu_settings_list_current_add_range(list, list_info, 1, 10, 1, true, true);
                SETTINGS_DATA_LIST_CURRENT_ADD_FLAGS(list, list_info, SD_FLAG_LAKKA_ADVANCED);
 
