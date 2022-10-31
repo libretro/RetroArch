47070a64e6a17993da73577db181422bf95f8037
redenvelope2000 <114486565+redenvelope2000@users.noreply.github.com>
2022-10-31 18:35:17 +0800

    Fixed macOS keyboard bindings in game focus mode. (#14570)
    In the game focus mode the keyboard should not be remapped for joypads unless it is used for toggling the game focus mode.

--- input/drivers/cocoa_input.c
+++ input/drivers/cocoa_input.c
@@ -424,8 +424,10 @@ static int16_t cocoa_input_state(
          if (binds[port][id].valid)
          {
             if (id < RARCH_BIND_LIST_END)
-               if (apple_key_state[rarch_keysym_lut[binds[port][id].key]])
-                  return 1;
+               if (!keyboard_mapping_blocked || (id == RARCH_GAME_FOCUS_TOGGLE))
+                  if (apple_key_state[rarch_keysym_lut[binds[port][id].key]])
+                     return 1;
+
          }
          break;
       case RETRO_DEVICE_ANALOG:
