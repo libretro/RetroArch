7b241597e5cd0a3344e82bb7d509dd150e0024a0
YY-GitHub <52448621+gitgudyyao@users.noreply.github.com>
2023-01-06 10:15:21 +0800

    updated code based on suggestion

--- location_driver.c
+++ location_driver.c
@@ -107,24 +107,6 @@ bool driver_location_start(void)
 
 void driver_location_stop(void)
 {
-#ifdef __ANDROID__
-   
-   settings_t* settings = config_get_ptr();
-   bool auto_save_state = settings->bools.auto_save_state;
-
-   if (auto_save_state)
-   {
-      /* Make a save state */
-      command_event(CMD_EVENT_SAVE_STATE, NULL);
-
-        /* Flush the auto save state to disk */
-      command_event(CMD_EVENT_AUTOSAVE_DELETE, NULL);
-   }
-
-  /* Flush SRAM to disk */
-   command_event(CMD_EVENT_SAVE_FILES, NULL);
-#endif
-
    location_driver_state_t 
       *location_st              = &location_driver_st;
    if (     location_st
--- location_driver.h
+++ location_driver.h
@@ -92,7 +92,25 @@ void driver_location_set_interval(unsigned interval_msecs,
  *
  * Returns: true (1) if successful, otherwise false (0).
  **/
-void driver_location_stop(void);
+void driver_location_stop(void){
+   #ifdef __ANDROID__
+   
+   settings_t* settings = config_get_ptr();
+   bool auto_save_state = settings->bools.auto_save_state;
+
+   if (auto_save_state)
+   {
+      /* Make a save state */
+      command_event(CMD_EVENT_SAVE_STATE, NULL);
+
+        /* Flush the auto save state to disk */
+      command_event(CMD_EVENT_AUTOSAVE_DELETE, NULL);
+   }
+
+  /* Flush SRAM to disk */
+   command_event(CMD_EVENT_SAVE_FILES, NULL);
+#endif
+}
 
 /**
  * driver_location_start:
