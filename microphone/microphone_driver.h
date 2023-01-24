b8b17fbe9084176b58d32f9f0d00ccd0600da8dd
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-01-24 12:04:55 -0500

    Revise comments for microphone_driver.h

--- microphone/microphone_driver.h
+++ microphone/microphone_driver.h
@@ -93,7 +93,7 @@ typedef struct microphone_driver
     * After this function is called,
     * microphones can be opened with \c open_mic.
     *
-    * @returns A handle to the general microphone driver context,
+    * @returns A handle to the microphone driver context,
     * or \c NULL if there was an error.
     **/
    void *(*init)(void);
@@ -116,19 +116,17 @@ typedef struct microphone_driver
    /**
     * Read samples from the microphone into the provided buffer.
     *
-    * Samples are provided in mono format,
-    * in either signed 16-bit integer or 32-bit floating point.
+    * Samples are provided in mono.
     * Since samples are in mono, a "frame" and a "sample" mean the same thing
     * in the context of microphone input.
     *
     * If \c use_float() returns \c true,
     * samples will be in 32-bit \c float format with a range of [-1.0, 1.0].
-    * format will be used, with range [-1.0, 1.0].
     * Otherwise, samples will be in signed 16-bit integer format.
     * Data will be in native byte order either way.
     *
     * All reads should block until all requested frames are provided,
-    * unless set otherwise wioth set_nonblock_state().
+    * unless set otherwise with set_nonblock_state().
     *
     * @param[in] driver_context Pointer to the driver context.
     * Will be the value that was returned by \c ::init().
@@ -158,12 +156,11 @@ typedef struct microphone_driver
    bool (*stop)(void *driver_context);
 
    /**
-    * Resumes audio driver from the paused state.
-    * Microphones will resume recording \em if they were already active
+    * Resumes the driver from the paused state.
+    * Microphones will resume activity \em if they were already active
     * before the driver was stopped.
     **/
    bool (*start)(void *data, bool is_shutdown);
-
 
    /**
     * Queries whether the driver is active.
@@ -178,11 +175,10 @@ typedef struct microphone_driver
    bool (*alive)(void *driver_context);
 
    /**
-    * Toggles the active state of the provided microphone.
-    * TODO mics are inactive when created
-    * TODO intended for cores that provide a mic button
-    * TODO paused driver won't resume, but will determine if mic resumes when unpaused
-    * TODO active mics retrieve samples
+    * Sets the active state of the provided microphone.
+    * Microphones must be active in order to read samples from them.
+    * Microphones are inactive when created,
+    * so cores will need to activate them explicitly with this function.
     *
     * @param[in] driver_context Pointer to the driver context.
     * Will be the value that was returned by \c ::init().
@@ -196,6 +192,7 @@ typedef struct microphone_driver
    bool (*set_mic_active)(void *driver_context, void *mic_context, bool active);
 
    /**
+    * Returns the active state of the provided microphone.
     * Note that this describes the user-facing state of the microphone;
     * this function can still return \c true if the mic driver is paused
     * or muted.
@@ -340,16 +337,16 @@ typedef struct microphone_driver
    /**
     * Releases the resources used by a particular microphone
     * and stops its activity.
-    * Will do nothing if either \p data or \p microphone_context is \c NULL.
+    * Will do nothing if either \p driver_context or \p microphone_context is \c NULL.
     *
-    * @param data Opaque handle to the audio driver context
+    * @param driver_context Opaque handle to the audio driver context
     * that was used to create the provided microphone.
     * Implementations may use this to help in cleaning up the microphone,
     * but the driver context itself must \em not be released.
     * @param microphone_context Opaque handle to the microphone that will be freed.
     * Implementations should stop any recording activity before freeing resources.
     *
-    * @post \p data will still be valid,
+    * @post \p driver_context will still be valid,
     * while \p microphone_context will not.
     */
    void (*close_mic)(void *driver_context, void *microphone_context);
@@ -360,24 +357,28 @@ typedef struct
    struct string_list *devices_list;
 
    /**
-    * A scratch buffer for audio output to be processed,
-    * up to (but excluding) the point where it's converted to 16-bit audio
-    * to give to the driver.
+    * A scratch buffer for audio input to be received.
     */
    void  *input_samples_buf;
    size_t input_samples_buf_length;
 
    /**
     * A scratch buffer for processed audio output to be converted to 16-bit,
-    * so that it can be sent to the driver.
+    * so that it can be sent to the core.
     */
    int16_t *input_samples_conv_buf;
    size_t input_samples_conv_buf_length;
 
    /**
     * The current microphone driver.
+    * Will be a pointer to one of the elements of \c microphone_drivers.
     */
    const microphone_driver_t *driver;
+
+   /**
+    * Opaque handle to the driver-specific context.
+    */
+   void *driver_context;
 
    /**
     * The handle to the created microphone, if any.
@@ -386,12 +387,6 @@ typedef struct
     * PRs welcome!
     */
    retro_microphone_t microphone;
-
-   /**
-    * Opaque handle to the driver-specific context.
-    */
-   void *driver_context;
-
 
    enum microphone_driver_state_flags flags;
 
@@ -402,27 +397,35 @@ typedef struct
 } microphone_driver_state_t;
 
 /**
- * TODO
- * @param is_shutdown
- * @return
+ * Starts the active microphone driver.
+ * @param is_shutdown TODO
+ * @return \c true if the configured driver was started,
+ * \c false if there was an error.
  */
 bool microphone_driver_start(bool is_shutdown);
 
 /**
- * TODO
- * @return
+ * Stops the active microphone driver.
+ * Microphones will not receive any input
+ * until \c microphone_driver_start is called again.
+ * @return \c true if the driver was stopped,
+ * \c false if there was an error.
  */
 bool microphone_driver_stop(void);
 
 /**
  * Driver function for opening a microphone.
  * Provided to retro_microphone_interface::init_microphone().
- * @return TODO
+ * @return Pointer to the newly-opened microphone,
+ * or \c NULL if there was an error.
  */
 retro_microphone_t *microphone_driver_open_mic(void);
 
 /**
- * TODO
+ * Driver function for closing an open microphone.
+ * Does nothing if the provided microphone is \c NULL.
+ * @param microphone Pointer to the microphone to close.
+ * Will be a value that was returned by \c microphone_driver_open_mic.
  */
 void microphone_driver_close_mic(retro_microphone_t *microphone);
 
@@ -434,47 +437,56 @@ bool microphone_driver_set_mic_state(retro_microphone_t *microphone, bool state)
 /**
  * TODO
  * @param microphone
- * @return
+ * @return The active state of \c microphone.
+ * \c true if the microphone is ready to accept input,
+ * \c false if not.
  */
 bool microphone_driver_get_mic_state(const retro_microphone_t *microphone);
 
 /**
- * TODO
- * @param microphone
- * @param frames
- * @param num_frames
- * @return
+ * Reads a particular number of samples from a microphone
+ * and stores it in a buffer.
+ * This should be called by the core each frame if there's an active microphone.
+ * Will block until the buffer is filled,
+ * so don't ask for more than you'll use in a single frame.
+ *
+ * @param microphone The microphone from which samples will be read.
+ * @param samples The buffer in which incoming samples will be stored.
+ * @param num_samples The available size of the provided buffer,
+ * in samples (\em not bytes).
+ * @return The number of samples that were read, or -1 if there was an error.
  */
-int microphone_driver_read(retro_microphone_t *microphone, int16_t* frames, size_t num_frames);
+int microphone_driver_read(retro_microphone_t *microphone, int16_t* samples, size_t num_samples);
 
 /**
- * TODO
+ * The ALSA-backed microphone driver.
  */
 extern microphone_driver_t microphone_alsa;
 
 /**
- * TODO
+ * The multithreaded ALSA-backed microphone driver.
  */
 extern microphone_driver_t microphone_alsathread;
 
 /**
- * TODO
+ * The SDL-backed microphone driver.
  */
 extern microphone_driver_t microphone_sdl;
 
 /**
- * TODO
+ * The WASAPI-backed microphone driver.
  */
 extern microphone_driver_t microphone_wasapi;
 
 /**
- * TODO
- * @return
+ * @return Pointer to the global microphone driver state.
  */
 microphone_driver_state_t *microphone_state_get_ptr(void);
 
 /**
- * TODO
+ * All microphone drivers available for use in this build.
+ * The contents of this array depend on the build configuration
+ * and target platform.
  */
 extern microphone_driver_t *microphone_drivers[];
 
