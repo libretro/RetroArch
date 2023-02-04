109e9ba8bdb8178cbb6813f21e8660630866418e
zoltanvb <balogh.zoltan.v@gmail.com>
2023-02-04 17:05:48 +0100

    Ozone footer enhancement: Scan button
    Display Scan button in footer when it is applicable.

--- intl/msg_hash_us.h
+++ intl/msg_hash_us.h
@@ -7666,6 +7666,10 @@ MSG_HASH(
    MENU_ENUM_SUBLABEL_MANUAL_CONTENT_SCAN_LIST,
    "Configurable scan based on content file names. Does not require content to match the database."
    )
+MSG_HASH(
+   MENU_ENUM_LABEL_VALUE_SCAN_ENTRY,
+   "Scan"
+   )
 
 /* Import Content > Scan File */
 
