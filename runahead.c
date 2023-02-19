0c1c345541d3f4a645c4a128aeb6c49b337aa6df
reallibretroadmin <reallibretroretroarch@gmail.com>
2023-02-19 10:24:30 +0100

    (Apple) Silence some warnings

--- runahead.c
+++ runahead.c
@@ -1588,7 +1588,7 @@ static INLINE void preempt_input_poll(preempt_t *preempt,
 
             /* Check requested analogs */
             if (     preempt->analog_mask[p]
-                  && preempt_analog_input_dirty(preempt, state_cb, p, mapped_port))
+                  && preempt_analog_input_dirty(preempt, state_cb, (unsigned)p, mapped_port))
                runloop_st->flags |= RUNLOOP_FLAG_INPUT_IS_DIRTY;
             break;
          case RETRO_DEVICE_MOUSE:
@@ -1596,7 +1596,7 @@ static INLINE void preempt_input_poll(preempt_t *preempt,
          case RETRO_DEVICE_POINTER:
             /* Check full device state */
             if (preempt_ptr_input_dirty(
-                  preempt, state_cb, preempt->ptr_dev[p], p, mapped_port))
+                  preempt, state_cb, preempt->ptr_dev[p], (unsigned)p, mapped_port))
                runloop_st->flags |= RUNLOOP_FLAG_INPUT_IS_DIRTY;
             break;
          default:
