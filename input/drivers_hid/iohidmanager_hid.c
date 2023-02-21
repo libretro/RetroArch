6944495b56729f2c7443c6297375b5f021fe5364
Sara Kelley <serakeri@gmail.com>
2023-02-20 20:47:47 -0500

    Various memory access fixes to prevent crashes (#14459)
    Prevent double free and null dereference when the controller is quickly reconnected.
    Handle error when controller device query returns null instead of crashing.

--- input/drivers_hid/iohidmanager_hid.c
+++ input/drivers_hid/iohidmanager_hid.c
@@ -544,6 +544,7 @@ static void iohidmanager_hid_device_remove(IOHIDDeviceRef device, iohidmanager_h
           free(tmp);
       }
       free(adapter);
+      adapter = NULL;
    }
    RARCH_LOG("Device removed from port %d\n", slot);
 }
@@ -702,6 +703,10 @@ static void iohidmanager_hid_device_add(IOHIDDeviceRef device, iohidmanager_hid_
 
    /* scan for buttons, axis, hats */
    elements_raw = IOHIDDeviceCopyMatchingElements(device, NULL, kIOHIDOptionsTypeNone);
+
+   if (!elements_raw)
+      goto error;
+
    count        = (int)CFArrayGetCount(elements_raw);
    elements     = CFArrayCreateMutableCopy(
          kCFAllocatorDefault,(CFIndex)count,elements_raw);
@@ -940,6 +945,7 @@ error:
          free(tmp);
       }
       free(adapter);
+      adapter = NULL;
    }
 }
 
