1a18f369717a7fd90c8d2765ce5b183ddc28ef29
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-06-16 16:51:22 -0400

    Forgot to commit my changes to griffin_cpp.cpp

--- griffin/griffin_cpp.cpp
+++ griffin/griffin_cpp.cpp
@@ -58,20 +58,13 @@ UI
 #include "../ui/drivers/qt/qt_downloads.cpp"
 #ifdef HAVE_MENU
 #include "../ui/drivers/qt/qt_options.cpp"
-#include "../ui/drivers/qt/moc_options.cpp"
-#include "../ui/drivers/qt/moc_settingswidgets.cpp"
+#include "../ui/drivers/qt/moc_qt_options.cpp"
 #endif
 #include "../ui/drivers/moc_ui_qt.cpp"
-#include "../ui/drivers/qt/moc_coreinfodialog.cpp"
-#include "../ui/drivers/qt/moc_coreoptionsdialog.cpp"
-#include "../ui/drivers/qt/moc_filedropwidget.cpp"
 #include "../ui/drivers/qt/moc_gridview.cpp"
-#include "../ui/drivers/qt/moc_playlistentrydialog.cpp"
-#if defined(HAVE_CG) || defined(HAVE_GLSL) || defined(HAVE_SLANG) || defined(HAVE_HLSL)
-#include "../ui/drivers/qt/moc_shaderparamsdialog.cpp"
-#endif
 #include "../ui/drivers/qt/moc_ui_qt_load_core_window.cpp"
-#include "../ui/drivers/qt/moc_viewoptionsdialog.cpp"
+#include "../ui/drivers/qt/moc_qt_dialogs.cpp"
+#include "../ui/drivers/qt/moc_qt_widgets.cpp"
 #endif
 
 /*============================================================
