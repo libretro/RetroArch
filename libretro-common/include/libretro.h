1c11d36a8a6cdfaf407146949abea019a6eed68b
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-09-04 21:49:46 -0400

    Update libretro-common/include/libretro.h
    Co-authored-by: Rob Loach <robloach@gmail.com>

--- libretro-common/include/libretro.h
+++ libretro-common/include/libretro.h
@@ -572,6 +572,7 @@ enum retro_mod
  * @see retro_message
  * @see RETRO_ENVIRONMENT_GET_LOG_INTERFACE
  * @see RETRO_ENVIRONMENT_SET_MESSAGE_EXT
+ * @see RETRO_ENVIRONMENT_SET_MESSAGE_EXT
  * @note The frontend must make its own copy of the message and the underlying string.
  */
 #define RETRO_ENVIRONMENT_SET_MESSAGE   6
