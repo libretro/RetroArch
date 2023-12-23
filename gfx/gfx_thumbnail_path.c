fa9d491116f0db0f7fcaccd5126c341bc6db383e
zoltanvb <balogh.zoltan.v@gmail.com>
2023-12-23 16:13:50 +0100

    Flexible thumbnail matching
    Add logic to handle 3 possible thumbnail names, in following order:
    - most exact name derived from content file (same name, with .png extension)
    - usual name derived from playlist (usually coming from database)
    - shortened name up to first bracket, chopping off region/publisher etc. info
    
    For local file system, names are checked always.
    For thumbnail downloads, names are checked each time the item comes up
    in the playlist, meaning that it may take going back and forth 3 times
    for a thumbnail to appear. However, as a positive change, failed thumbnail
    downloads are not repeated for the same playlist, which was not the case
    earlier.

--- gfx/gfx_thumbnail_path.c
+++ gfx/gfx_thumbnail_path.c
@@ -37,14 +37,23 @@
 
 /* Fills content_img field of path_data using existing
  * content_label field (for internal use only) */
-static void gfx_thumbnail_fill_content_img(char *s, size_t len, const char *src)
+static void gfx_thumbnail_fill_content_img(char *s, size_t len, const char *src, bool shorten)
 {
+   const char* cut = " (";
    char *scrub_char_ptr = NULL;
    /* Copy source label string */
    size_t _len          = strlcpy(s, src, len);
+   int bracketpos       = -1;
+   
+   /* Shortening logic: up to first space + bracket */
+   if (shorten) {
+      bracketpos = string_find_index_substring_string(src, cut);
+      if (bracketpos > 2)
+         _len = bracketpos;
+   }
    /* Scrub characters that are not cross-platform and/or violate the
     * No-Intro filename standard:
-    * http://datomatic.no-intro.org/stuff/The%20Official%20No-Intro%20Convention%20(20071030).zip
+    * https://datomatic.no-intro.org/stuff/The%20Official%20No-Intro%20Convention%20(20071030).pdf
     * Replace these characters in the entry name with underscores */
    while ((scrub_char_ptr = strpbrk(s, "&*/:`\"<>?\\|")))
       *scrub_char_ptr = '_';
@@ -289,7 +298,9 @@ bool gfx_thumbnail_set_content(gfx_thumbnail_path_data_t *path_data, const char 
 
    /* Determine content image name */
    gfx_thumbnail_fill_content_img(path_data->content_img,
-         sizeof(path_data->content_img), path_data->content_label);
+         sizeof(path_data->content_img), path_data->content_label, false);
+   gfx_thumbnail_fill_content_img(path_data->content_img_short,
+         sizeof(path_data->content_img_short), path_data->content_label, true);
 
    /* Have to set content path to *something*...
     * Just use label value (it doesn't matter) */
@@ -444,8 +455,6 @@ bool gfx_thumbnail_set_content_playlist(
             "", sizeof(path_data->content_label));
 
    /* Determine content image name */
-   if (settings->bools.playlist_use_filename ||
-       playlist_thumbnail_match_with_filename(playlist))
    {
       char* content_name_no_ext = NULL;
       char tmp_buf[PATH_MAX_LENGTH];
@@ -457,13 +466,12 @@ bool gfx_thumbnail_set_content_playlist(
       strlcpy(tmp_buf, base_name, sizeof(tmp_buf));
       content_name_no_ext = path_remove_extension(tmp_buf);
       
+      gfx_thumbnail_fill_content_img(path_data->content_img_full,
+         sizeof(path_data->content_img_full), content_name_no_ext,false);
       gfx_thumbnail_fill_content_img(path_data->content_img,
-         sizeof(path_data->content_img), content_name_no_ext);
-   }
-   else
-   {
-      gfx_thumbnail_fill_content_img(path_data->content_img,
-         sizeof(path_data->content_img), path_data->content_label);
+         sizeof(path_data->content_img), path_data->content_label,false);
+      gfx_thumbnail_fill_content_img(path_data->content_img_short,
+         sizeof(path_data->content_img_short), path_data->content_label,true);
    }
 
    /* Store playlist index */
@@ -620,10 +628,28 @@ bool gfx_thumbnail_update_path(
       /* >> Add type */
       fill_pathname_join_special(tmp_buf, thumbnail_path, type, sizeof(tmp_buf));
 
-      /* >> Add content image */
       thumbnail_path[0] = '\0';
-      fill_pathname_join_special(thumbnail_path, tmp_buf,
-            path_data->content_img, PATH_MAX_LENGTH * sizeof(char));
+      /* >> Add content image - first try with full file name */
+      if(path_data->content_img_full[0] != '\0') {
+         fill_pathname_join_special(thumbnail_path, tmp_buf,
+               path_data->content_img_full, PATH_MAX_LENGTH * sizeof(char));
+      }
+      /* >> Add content image - second try with label (database name) */
+      if(!path_is_valid(thumbnail_path) && path_data->content_img[0] != '\0')
+      {
+         thumbnail_path[0] = '\0';
+         fill_pathname_join_special(thumbnail_path, tmp_buf,
+               path_data->content_img, PATH_MAX_LENGTH * sizeof(char));
+      }
+      /* >> Add content image - third try with shortened name (title only) */
+      if(!path_is_valid(thumbnail_path) && path_data->content_img_short[0] != '\0')
+      {
+         thumbnail_path[0] = '\0';
+         fill_pathname_join_special(thumbnail_path, tmp_buf,
+               path_data->content_img_short, PATH_MAX_LENGTH * sizeof(char));
+      }
+      /* This logic is valid for locally stored thumbnails. For optional downloads, 
+       * gfx_thumbnail_get_img_name() is used */
    }
 
    /* Final error check - is cached path empty? */
@@ -715,18 +741,35 @@ bool gfx_thumbnail_get_core_name(
    return true;
 }
 
-/* Fetches current thumbnail image name
+/* Fetches current thumbnail image name according to name flag
  * (name is the same for all thumbnail types).
  * Returns true if image name is valid. */
 bool gfx_thumbnail_get_img_name(
-      gfx_thumbnail_path_data_t *path_data, const char **img_name)
+      gfx_thumbnail_path_data_t *path_data, const char **img_name,
+      enum playlist_thumbnail_name_flags name_flags)
 {
-   if (!path_data || !img_name)
+   if (!path_data || !img_name || name_flags == PLAYLIST_THUMBNAIL_FLAG_NONE)
       return false;
-   if (string_is_empty(path_data->content_img))
-      return false;
+   if (name_flags & PLAYLIST_THUMBNAIL_FLAG_SHORT_NAME) {
+      if (string_is_empty(path_data->content_img_short))
+         return false;
 
-   *img_name = path_data->content_img;
+      *img_name = path_data->content_img_short;
+   }
+   else if (name_flags & PLAYLIST_THUMBNAIL_FLAG_STD_NAME) {
+      if (string_is_empty(path_data->content_img))
+         return false;
+
+      *img_name = path_data->content_img;
+   }
+   else if (name_flags & PLAYLIST_THUMBNAIL_FLAG_FULL_NAME) {
+      if (string_is_empty(path_data->content_img_full))
+         return false;
+
+      *img_name = path_data->content_img_full;
+   }
+   else
+      return false;
 
    return true;
 }
