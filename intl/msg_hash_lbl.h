a5a3d986596938ae2f2efcd46864bbc53c4fa9d1
Jason Breitweiser <jbreitweiser@gmail.com>
2024-04-06 08:13:21 -0400

    This commit is to amend a previous PR where the mesg andd label hashs were in the wrong file.

--- intl/msg_hash_lbl.h
+++ intl/msg_hash_lbl.h
@@ -17,14 +17,6 @@ MSG_HASH(
    MENU_ENUM_LABEL_ADD_ENTRY_TO_PLAYLIST,
    "add_entry_to_playlist"
    )
-MSG_HASH(
-   MSG_ADDED_TO_PLAYLIST,
-   "Added to playlist"
-   )
-MSG_HASH(
-   MSG_ADD_TO_PLAYLIST_FAILED,
-   "Failed to add to playlist: playlist full"
-   )
 
 MSG_HASH(
    MENU_ENUM_LABEL_ACCOUNTS_CHEEVOS_USERNAME,
