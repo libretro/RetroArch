2d3a6b121f683c536bfaaf1d37d999e4a3343767
zoltanvb <balogh.zoltan.v@gmail.com>
2024-04-12 18:22:30 +0200

    Test input driver and sensor test screen
    A new input driver (test_input), similar to test joypad driver.
    Takes its inputs from a json file provided in options. Supports
    keyboard input and sensor input (accelerometer, gyroscope,
    illuminance).
    
    Remote Retropad extended with a sensor test screen.

--- griffin/griffin.c
+++ griffin/griffin.c
@@ -733,6 +733,7 @@ INPUT
 
 #ifdef HAVE_TEST_DRIVERS
 #include "../input/drivers_joypad/test_joypad.c"
+#include "../input/drivers/test_input.c"
 #endif
 
 /*============================================================
