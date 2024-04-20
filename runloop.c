101fa6c23956d1ae89406186f18a2341c6b8fb12
Rob Loach <robloach@gmail.com>
2024-04-20 08:05:26 -0400

    remove-set-rotation-message (#16453)
    Whenever `SET_ROTATION` is called, it outputs an INFO message in the logs. Can get quite busy. I'm assuming this was here during someone's debugging?

--- runloop.c
+++ runloop.c
@@ -1903,7 +1903,6 @@ bool runloop_environment_cb(unsigned cmd, void *data)
          unsigned rotation       = *(const unsigned*)data;
          bool video_allow_rotate = settings->bools.video_allow_rotate;
 
-         RARCH_LOG("[Environ]: SET_ROTATION: %u\n", rotation);
          if (sys_info)
             sys_info->core_requested_rotation = rotation;
 
