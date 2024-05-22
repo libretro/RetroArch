4ca28a868ab7e903699fd2f462abb4e18361df44
Eric Warmenhoven <eric@warmenhoven.org>
2024-05-22 13:40:51 -0400

    macOS/iOS/tvOS: enable text-to-speech using AVSpeechSynthesizer.
    Fixes #16532.

--- ui/drivers/cocoa/cocoa_common.m
+++ ui/drivers/cocoa/cocoa_common.m
@@ -40,6 +40,8 @@
 #include "../../../configuration.h"
 #include "../../../content.h"
 #include "../../../core_info.h"
+#include "../../../defaults.h"
+#include "../../../file_path_special.h"
 #include "../../../menu/menu_cbs.h"
 #include "../../../paths.h"
 #include "../../../retroarch.h"
@@ -48,6 +50,10 @@
 
 #include "../../input/drivers/cocoa_input.h"
 #include "../../input/drivers_keyboard/keyboard_event_apple.h"
+
+#ifdef HAVE_MENU
+#include "../../menu/menu_driver.h"
+#endif
 
 #if defined(HAVE_COCOA_METAL) || defined(HAVE_COCOATOUCH)
 id<ApplePlatform> apple_platform;
