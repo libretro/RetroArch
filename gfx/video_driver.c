1aa0580b0bc6340f033e65d1f050c5b93410197e
Eric Warmenhoven <eric@warmenhoven.org>
2024-08-10 17:16:11 -0400

    macOS: Don't include video/input drivers when compiling with SDL2

--- gfx/video_driver.c
+++ gfx/video_driver.c
@@ -157,7 +157,7 @@ static const gfx_ctx_driver_t *gfx_ctx_gl_drivers[] = {
    &gfx_ctx_cocoagl,
 #endif
 #endif
-#if (defined(HAVE_SDL) || defined(HAVE_SDL2)) && (defined(HAVE_OPENGL) || defined(HAVE_OPENGL1) || defined(HAVE_OPENGL_CORE))
+#if (defined(HAVE_SDL) || defined(HAVE_SDL2)) && (defined(HAVE_OPENGL) || defined(HAVE_OPENGL1) || defined(HAVE_OPENGL_CORE)) && !defined(HAVE_COCOA)
    &gfx_ctx_sdl_gl,
 #endif
 #ifdef HAVE_OSMESA
@@ -406,7 +406,7 @@ const video_driver_t *video_drivers[] = {
 #if defined(HAVE_SDL) && !defined(HAVE_SDL_DINGUX)
    &video_sdl,
 #endif
-#ifdef HAVE_SDL2
+#if defined(HAVE_SDL2) && !(defined(HAVE_COCOA) || defined(HAVE_COCOA_METAL))
    &video_sdl2,
 #endif
 #ifdef HAVE_SDL_DINGUX
