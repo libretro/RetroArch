e15c07a0a4f06edc7d5cf77d6815f9db6f3ae030
Eric Warmenhoven <eric@warmenhoven.org>
2024-12-08 02:58:33 -0500

    Add option to load overlay based on system name (#17233)

--- retroarch.h
+++ retroarch.h
@@ -106,7 +106,8 @@ enum rarch_state_flags
    RARCH_FLAGS_BLOCK_CONFIG_READ            = (1 << 13),
    RARCH_FLAGS_CLI_DATABASE_SCAN            = (1 << 14),
    RARCH_FLAGS_HAS_SET_XDELTA_PREF          = (1 << 15),
-   RARCH_FLAGS_XDELTA_PREF                  = (1 << 16)
+   RARCH_FLAGS_XDELTA_PREF                  = (1 << 16),
+   RARCH_FLAGS_HAS_SET_OVERLAY_PRESET       = (1 << 17)
 };
 
 bool retroarch_ctl(enum rarch_ctl_state state, void *data);
