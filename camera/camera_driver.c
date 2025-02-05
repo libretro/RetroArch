0f7e878af81bbeef951602c4b057002afcdb9d30
Viačasłaŭ Chalikin <viachaslavic@outlook.com>
2025-02-05 21:22:41 +0300

    Buildfix for Debian 12

--- camera/camera_driver.c
+++ camera/camera_driver.c
@@ -49,7 +49,7 @@ const camera_driver_t *camera_drivers[] = {
 #ifdef HAVE_V4L2
    &camera_v4l2,
 #endif
-#ifdef HAVE_PIPEWIRE
+#ifdef HAVE_PIPEWIRE_STABLE
    &camera_pipewire,
 #endif
 #ifdef EMSCRIPTEN
