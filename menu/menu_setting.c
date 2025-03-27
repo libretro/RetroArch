5ef2b5a4423f1cba53551dff09ae3d66d080369b
pstef <3462925+pstef@users.noreply.github.com>
2025-03-27 19:06:28 +0000

    Silence a defined-but-not-used-function warning
    Enclose the static function definitions in an ifdef like the one used
    where they are called.

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -6175,6 +6175,7 @@ static int setting_string_action_right_driver(
    return 0;
 }
 
+#if !defined(RARCH_CONSOLE)
 static int setting_string_action_left_midi_input(
       rarch_setting_t *setting, size_t idx, bool wraparound)
 {
@@ -6266,6 +6267,7 @@ static int setting_string_action_right_midi_output(
    command_event(CMD_EVENT_AUDIO_REINIT, NULL);
    return -1;
 }
+#endif
 
 #ifdef HAVE_CHEATS
 static size_t setting_get_string_representation_uint_cheat_exact(
