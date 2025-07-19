99e7a80314d4aac1a3f2580909afcd8affea3d5b
libretroadmin <reallibretroretroarch@gmail.com>
2025-07-19 21:59:54 +0200

    Inline uwpgdi.h into wgl_ctx.c

--- gfx/drivers_context/wgl_ctx.c
+++ gfx/drivers_context/wgl_ctx.c
@@ -56,7 +56,36 @@
 #endif
 
 #ifdef __WINRT__
-#include "../common/uwpgdi.h"
+#include <GL/gl.h>
+
+#if !defined(_GDI32_)
+#define WINGDIAPI_UWP __declspec(dllimport)
+#else
+#define WINGDIAPI_UWP __declspec(dllexport)
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Undeclared APIs exported by libgallium on UWP
+ */
+
+WINGDIAPI_UWP HGLRC WINAPI wglCreateContext(HDC);
+WINGDIAPI_UWP BOOL  WINAPI wglDeleteContext(HGLRC);
+WINGDIAPI_UWP BOOL  WINAPI wglMakeCurrent(HDC, HGLRC);
+WINGDIAPI_UWP BOOL APIENTRY wglSwapBuffers(HDC hdc);
+WINGDIAPI_UWP PROC APIENTRY wglGetProcAddress(LPCSTR lpszProc);
+WINGDIAPI_UWP BOOL APIENTRY wglShareLists(
+   HGLRC unnamedParam1,
+   HGLRC unnamedParam2
+);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
 
 #if (defined(HAVE_OPENGL) || defined(HAVE_OPENGL1) || defined(HAVE_OPENGL_CORE)) && !defined(HAVE_OPENGLES)
