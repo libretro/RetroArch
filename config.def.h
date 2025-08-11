9c9f5aa8d0316a86f8f2fbeab4b09c411613f893
zoltanvb <101990835+zoltanvb@users.noreply.github.com>
2025-08-11 10:47:44 +0200

    Touchscreen support for PS Vita (#18165)

--- config.def.h
+++ config.def.h
@@ -84,7 +84,7 @@
 
 #define DEFAULT_TOUCH_SCALE 1
 
-#if defined(RARCH_MOBILE) || defined(HAVE_LIBNX) || defined(__WINRT__) || defined(EMSCRIPTEN)
+#if defined(RARCH_MOBILE) || defined(HAVE_LIBNX) || defined(__WINRT__) || defined(EMSCRIPTEN) || defined (VITA)
 #define DEFAULT_POINTER_ENABLE true
 #else
 #define DEFAULT_POINTER_ENABLE false
