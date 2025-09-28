7ce0a62bb227c72575a97d272c33b60060ca2023
zoltanvb <balogh.zoltan.v@gmail.com>
2025-09-28 19:59:54 +0200

    Debug logs for catching #17087

--- tasks/task_core_updater.c
+++ tasks/task_core_updater.c
@@ -1221,6 +1221,8 @@ static void task_update_installed_cores_handler(retro_task_t *task)
             {
                update_installed_handle->list_size =
                      core_updater_list_size(update_installed_handle->core_list);
+               RARCH_DBG("[Core Updater] Updater list size from buildbot: %d\n",
+                          update_installed_handle->list_size);
 
                if (update_installed_handle->list_size < 1)
                   update_installed_handle->status = UPDATE_INSTALLED_CORES_END;
@@ -1255,6 +1257,7 @@ static void task_update_installed_cores_handler(retro_task_t *task)
                         update_installed_handle->list_index;
                   update_installed_handle->status          =
                         UPDATE_INSTALLED_CORES_UPDATE_CORE;
+                  RARCH_DBG("[Core Updater] Updating installed core: \"%s\"\n",list_entry->local_core_path);
                }
             }
 
@@ -1337,6 +1340,7 @@ static void task_update_installed_cores_handler(retro_task_t *task)
             if ((local_crc != 0) && (local_crc == list_entry->crc))
             {
                update_installed_handle->status = UPDATE_INSTALLED_CORES_ITERATE;
+               RARCH_DBG("[Core Updater] Core %s is already at latest version\n",list_entry->display_name);
                break;
             }
 
@@ -1376,6 +1380,7 @@ static void task_update_installed_cores_handler(retro_task_t *task)
 
                /* Wait for download to complete */
                update_installed_handle->status = UPDATE_INSTALLED_CORES_WAIT_DOWNLOAD;
+               RARCH_DBG("[Core Updater] Downloading core %s\n",list_entry->display_name);
             }
          }
          break;
