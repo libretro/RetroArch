b7d6794aca67b44eac51f17de411f8cc8fa28326
Joseph C. Osborn <joseph.osborn@pomona.edu>
2025-10-23 11:38:04 -0700

    Remove an old emscripten hack around atomics
    In talking with @BinBashBanana it became clear this wasn't necessary,
    and it has started to conflict with some code in emscripten's
    libpthread in recent versions.

--- emscripten/pre.js
+++ emscripten/pre.js
@@ -1,6 +1,2 @@
 // To work around a bug in emscripten's polyfills for setImmediate in strict mode
 var setImmediate;
-
-// To work around a deadlock in firefox
-// Use platform_emscripten_has_async_atomics() to determine actual availability
-if (Atomics && !Atomics.waitAsync) Atomics.waitAsync = true;
