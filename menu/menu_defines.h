b38369e9ff7fd49b327fe9e2d0622211e534fdb5
Eric Warmenhoven <eric@warmenhoven.org>
2025-11-06 02:03:26 -0500

    iOS/tvOS: don't use draw observer unless fast forwarding (#18362)

--- menu/menu_defines.h
+++ menu/menu_defines.h
@@ -50,7 +50,8 @@ enum menu_state_flags
    MENU_ST_FLAG_SCREENSAVER_SUPPORTED       = (1 << 10),
    MENU_ST_FLAG_SCREENSAVER_ACTIVE          = (1 << 11),
    MENU_ST_FLAG_PENDING_RELOAD_CORE         = (1 << 12),
-   MENU_ST_FLAG_PENDING_STARTUP_PAGE        = (1 << 13)
+   MENU_ST_FLAG_PENDING_STARTUP_PAGE        = (1 << 13),
+   MENU_ST_FLAG_BLOCK_ALL_INPUT             = (1 << 14)
 };
 
 enum menu_scroll_mode
