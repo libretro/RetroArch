acd3fa11727f7fd9858cb6de35e4d07a18c6a866
zoltanvb <101990835+zoltanvb@users.noreply.github.com>
2025-11-16 20:59:18 +0100

    Change size to 64-bit (#18410)
    Several rom sizes could exceed 32-bit. Database uses uint64_t, so let's align.

--- database_info.c
+++ database_info.c
@@ -718,7 +718,7 @@ static int database_cursor_iterate(libretrodb_cursor_t *cur,
       else if (string_is_equal(str, "analog"))
          db_info->analog_supported        = (int)val->val.uint_;
       else if (string_is_equal(str, "size"))
-         db_info->size                    = (unsigned)val->val.uint_;
+         db_info->size                    = (uint64_t)val->val.uint_;
       else if (string_is_equal(str, "crc"))
       {
          switch (val->val.binary.len)
