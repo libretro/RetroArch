b67035fa7b5debe39522cff19c5cc2fc39c4fe78
David Hedlund <davidhedlund@users.noreply.github.com>
2025-12-10 09:35:06 +0100

    Update ui_qt.cpp
    Qt not QT

--- ui/drivers/ui_qt.cpp
+++ ui/drivers/ui_qt.cpp
@@ -1832,7 +1832,7 @@ void MainWindow::showWelcomeScreen()
    const QString welcome_txt = QStringLiteral(""
       "Welcome to the RetroArch Desktop Menu!<br>\n"
       "<br>\n"
-      "The Desktop Menu provides a WIMP (Windows, Icons, Menus, Pointer) QT interface 
+      "The Desktop Menu provides a WIMP (Windows, Icons, Menus, Pointer) Qt interface 
 	  "that's functional for launching content and managing playlists.<br>\n"
       "<br>\n"
       "But many settings and actions are currently only available in the familiar "
