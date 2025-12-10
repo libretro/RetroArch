4e23d638e65f045662916e3a75b0adf5f2c05e07
David Hedlund <davidhedlund@users.noreply.github.com>
2025-12-10 09:39:14 +0100

    Update ui_qt.cpp
    Wording ("Qt-based WIMP)"

--- ui/drivers/ui_qt.cpp
+++ ui/drivers/ui_qt.cpp
@@ -1832,7 +1832,7 @@ void MainWindow::showWelcomeScreen()
    const QString welcome_txt = QStringLiteral(""
       "Welcome to the RetroArch Desktop Menu!<br>\n"
       "<br>\n"
-      "The Desktop Menu provides a WIMP (Windows, Icons, Menus, Pointer) Qt interface 
+      "The Desktop Menu provides a Qt-based WIMP (Windows, Icons, Menus, Pointer) interface "
 	  "that's functional for launching content and managing playlists.<br>\n"
       "<br>\n"
       "But many settings and actions are currently only available in the familiar "
