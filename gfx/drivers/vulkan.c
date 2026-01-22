ab54fdd0b736c6ed4610cef6bef1fe296d842924
Eric Warmenhoven <eric@warmenhoven.org>
2026-01-21 20:15:05 -0500

    Revert "minor vulkan tweaks"
    This reverts commit 6131f07c4c79a08cbe4ab2c45831116440939bfd. Believed
    to cause blinking on windows/android:
    
    https://www.reddit.com/r/RetroArch/comments/1qj2o6g/nightly_build_20260121_vulkan_has_screen_blinking/

--- gfx/drivers/vulkan.c
+++ gfx/drivers/vulkan.c
@@ -515,47 +515,42 @@ static void vulkan_draw_triangles(vk_t *vk, const struct vk_draw_triangles *call
 
    /* Upload descriptors */
    {
-      VkImageView call_view = call->texture ? call->texture->view : VK_NULL_HANDLE;
+      VkDescriptorSet set;
+      /* Upload UBO */
+      struct vk_buffer_range range;
+      float *mvp_data_ptr          = NULL;
 
-      /* Check if we can skip descriptor work */
-      if (   call_view != vk->tracker.view
-          || call->sampler != vk->tracker.sampler
-          || call->uniform_size > sizeof(vk->tracker.mvp)
-          || memcmp(call->uniform, &vk->tracker.mvp, call->uniform_size) != 0)
-      {
-         VkDescriptorSet set;
-         struct vk_buffer_range range;
+      if (!vulkan_buffer_chain_alloc(vk->context, &vk->chain->ubo,
+               call->uniform_size, &range))
+         return;
 
-         if (!vulkan_buffer_chain_alloc(vk->context, &vk->chain->ubo,
-                  call->uniform_size, &range))
-            return;
+      memcpy(range.data, call->uniform, call->uniform_size);
 
-         memcpy(range.data, call->uniform, call->uniform_size);
+      set = vulkan_descriptor_manager_alloc(
+            vk->context->device,
+            &vk->chain->descriptor_manager);
 
-         set = vulkan_descriptor_manager_alloc(
-               vk->context->device,
-               &vk->chain->descriptor_manager);
+      vulkan_write_quad_descriptors(
+            vk->context->device,
+            set,
+            range.buffer,
+            range.offset,
+            call->uniform_size,
+            call->texture,
+            call->sampler);
 
-         vulkan_write_quad_descriptors(
-               vk->context->device,
-               set,
-               range.buffer,
-               range.offset,
-               call->uniform_size,
-               call->texture,
-               call->sampler);
+      vkCmdBindDescriptorSets(vk->cmd,
+            VK_PIPELINE_BIND_POINT_GRAPHICS,
+            vk->pipelines.layout, 0,
+            1, &set, 0, NULL);
 
-         vkCmdBindDescriptorSets(vk->cmd,
-               VK_PIPELINE_BIND_POINT_GRAPHICS,
-               vk->pipelines.layout, 0,
-               1, &set, 0, NULL);
-
-         /* Update tracker with what we just bound */
-         vk->tracker.view    = call_view;
-         vk->tracker.sampler = call->sampler;
-         if (call->uniform_size <= sizeof(vk->tracker.mvp))
-            memcpy(&vk->tracker.mvp, call->uniform, call->uniform_size);
-      }
+      vk->tracker.view    = VK_NULL_HANDLE;
+      vk->tracker.sampler = VK_NULL_HANDLE;
+      for (
+              mvp_data_ptr = &vk->tracker.mvp.data[0]
+            ; mvp_data_ptr < vk->tracker.mvp.data + 16
+            ; mvp_data_ptr++)
+         *mvp_data_ptr = 0.0f;
    }
 
    /* VBO is already uploaded. */
@@ -4396,12 +4391,19 @@ static void vulkan_draw_quad(vk_t *vk, const struct vk_draw_quad *quad)
       VkDescriptorSet set;
       struct vk_buffer_range range;
 
+      if (!vulkan_buffer_chain_alloc(vk->context, &vk->chain->ubo,
+               sizeof(*quad->mvp), &range))
+         return;
+
       if (
                (memcmp(quad->mvp,
-                  &vk->tracker.mvp, sizeof(*quad->mvp)) != 0)
+                  &vk->tracker.mvp, sizeof(*quad->mvp)) == 0)
             || quad->texture->view != vk->tracker.view
             || quad->sampler != vk->tracker.sampler)
       {
+         /* Upload UBO */
+         struct vk_buffer_range range;
+
          if (!vulkan_buffer_chain_alloc(vk->context, &vk->chain->ubo,
                   sizeof(*quad->mvp), &range))
             return;
@@ -4663,7 +4665,7 @@ static bool vulkan_frame(void *data, const void *frame,
       uint64_t frame_count,
       unsigned pitch, const char *msg, video_frame_info_t *video_info)
 {
-   int j, k;
+   int i, j, k;
    VkSubmitInfo submit_info;
    VkClearValue clear_color;
    VkRenderPassBeginInfo rp_info;
@@ -4757,6 +4759,10 @@ static bool vulkan_frame(void *data, const void *frame,
    vk->tracker.scissor.extent.height = 0;
    vk->flags                        &= ~VK_FLAG_TRACKER_USE_SCISSOR;
    vk->tracker.pipeline              = VK_NULL_HANDLE;
+   vk->tracker.view                  = VK_NULL_HANDLE;
+   vk->tracker.sampler               = VK_NULL_HANDLE;
+   for (i = 0; i < 16; i++)
+      vk->tracker.mvp.data[i]        = 0.0f;
 
    waits_for_semaphores              =
           (vk->flags & VK_FLAG_HW_ENABLE)
