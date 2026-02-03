1bc125686ef0a2c3e8f58a8e90853accf6d5f290
hizzlekizzle <hizzlekizzle@users.noreply.github.com>
2026-02-03 15:16:38 -0600

    revert mipmap changes from 66a7657
    This was causing RGUI to flicker and Ozone menu to randomly crash. Unfortunately, this re-breaks mipmaps on this driver, but the instability is a bigger problem.

--- gfx/drivers/d3d12.c
+++ gfx/drivers/d3d12.c
@@ -720,13 +720,13 @@ static void d3d12_init_texture(D3D12Device device, d3d12_texture_t* texture)
    if (!texture->desc.MipLevels)
       texture->desc.MipLevels          = 1;
 
-   /* Calculate mipmap count */
-   if (texture->desc.Width > 1 || texture->desc.Height > 1)
+   if (   !(texture->desc.Width  >> (texture->desc.MipLevels - 1))
+       && !(texture->desc.Height >> (texture->desc.MipLevels - 1)))
    {
-      unsigned width                   = texture->desc.Width;
-      unsigned height                  = texture->desc.Height;
+      unsigned width                   = texture->desc.Width >> 5;
+      unsigned height                  = texture->desc.Height >> 5;
       texture->desc.MipLevels          = 1;
-      while ((width > 1) || (height > 1))
+      while (width && height)
       {
          width  >>= 1;
          height >>= 1;
@@ -835,11 +835,33 @@ static void d3d12_init_texture(D3D12Device device, d3d12_texture_t* texture)
    texture->size_data.w = 1.0f / texture->desc.Height;
 }
 
-static void d3d12_generate_mipmaps(
-      D3D12GraphicsCommandList cmd,
-      d3d12_texture_t* texture,
-      void *userdata)
+static void d3d12_upload_texture(D3D12GraphicsCommandList cmd,
+      d3d12_texture_t* texture, void *userdata)
 {
+   D3D12_TEXTURE_COPY_LOCATION src, dst;
+
+   src.pResource        = texture->upload_buffer;
+   src.Type             = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT;
+   src.PlacedFootprint  = texture->layout;
+
+   dst.pResource        = texture->handle;
+   dst.Type             = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
+   dst.SubresourceIndex = 0;
+
+   D3D12_RESOURCE_TRANSITION(
+         cmd,
+         texture->handle,
+         D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+         D3D12_RESOURCE_STATE_COPY_DEST);
+
+   cmd->lpVtbl->CopyTextureRegion(cmd, &dst, 0, 0, 0, &src, NULL);
+
+   D3D12_RESOURCE_TRANSITION(
+         cmd,
+         texture->handle,
+         D3D12_RESOURCE_STATE_COPY_DEST,
+         D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+
    if (texture->desc.MipLevels > 1)
    {
       unsigned       i;
@@ -893,37 +915,6 @@ static void d3d12_generate_mipmaps(
          }
       }
    }
-}
-
-static void d3d12_upload_texture(D3D12GraphicsCommandList cmd,
-      d3d12_texture_t* texture, void *userdata)
-{
-   D3D12_TEXTURE_COPY_LOCATION src, dst;
-
-   src.pResource        = texture->upload_buffer;
-   src.Type             = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT;
-   src.PlacedFootprint  = texture->layout;
-
-   dst.pResource        = texture->handle;
-   dst.Type             = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
-   dst.SubresourceIndex = 0;
-
-   D3D12_RESOURCE_TRANSITION(
-         cmd,
-         texture->handle,
-         D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
-         D3D12_RESOURCE_STATE_COPY_DEST);
-
-   cmd->lpVtbl->CopyTextureRegion(cmd, &dst, 0, 0, 0, &src, NULL);
-
-   D3D12_RESOURCE_TRANSITION(
-         cmd,
-         texture->handle,
-         D3D12_RESOURCE_STATE_COPY_DEST,
-         D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
-
-   d3d12_generate_mipmaps(cmd, texture, userdata);
-
    texture->dirty = false;
 }
 
@@ -3715,8 +3706,6 @@ static void d3d12_init_render_targets(d3d12_video_t* d3d12, unsigned width, unsi
          d3d12->pass[i].rt.desc.Flags      = D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET;
          d3d12->pass[i].rt.srv_heap        = &d3d12->desc.srv_heap;
          d3d12->pass[i].rt.desc.Format = glslang_format_to_dxgi(d3d12->pass[i].semantics.format);
-         /* Initialize MipLevels so mipmaps are created for render target */
-         d3d12->pass[i].rt.desc.MipLevels = 0;
          d3d12_release_texture(&d3d12->pass[i].rt);
          d3d12_init_texture(d3d12->device, &d3d12->pass[i].rt);
 
@@ -4365,9 +4354,6 @@ static bool d3d12_gfx_frame(
                   d3d12->pass[i].rt.handle,
                   D3D12_RESOURCE_STATE_RENDER_TARGET,
                   D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
-
-            /* Generate mipmaps for framebuffer if it has multiple mipmap levels */
-            d3d12_generate_mipmaps(cmd, &d3d12->pass[i].rt, d3d12);
             texture = &d3d12->pass[i].rt;
          }
          else
