71ecc6dfb41a5de51c314cdbc13dc3b929f3ff49
sonninnos <tony@pihaus.org>
2026-03-17 23:49:03 +0200

    Increase Bind Timeout range

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -16400,7 +16400,7 @@ static bool setting_append_list(
                   general_read_handler);
             (*list)[list_info->index - 1].action_ok = &setting_action_ok_uint;
             (*list)[list_info->index - 1].offset_by = 1;
-            menu_settings_list_current_add_range(list, list_info, 1, 5, 1, true, true);
+            menu_settings_list_current_add_range(list, list_info, 1, 30, 1, true, true);
             SETTINGS_DATA_LIST_CURRENT_ADD_FLAGS(list, list_info, SD_FLAG_ADVANCED);
 
             CONFIG_UINT(
