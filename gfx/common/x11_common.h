9081fc01b5e2459095c8c4cd43fbc3437afd2a8f
PoloniumRain <poloniumrain@users.noreply.github.com>
2026-03-21 10:01:34 +0000

    GNOME X11: Fix title bar showing in fullscreen

--- gfx/common/x11_common.h
+++ gfx/common/x11_common.h
@@ -30,6 +30,7 @@ extern unsigned g_x11_screen;
 
 void x11_show_mouse(void *data, bool state);
 void x11_set_net_wm_fullscreen(Display *dpy, Window win);
+void x11_set_net_wm_fullscreen_hint(Display *dpy, Window win);
 bool x11_suspend_screensaver(void *data, bool enable);
 
 void x11_move_window(Display *dpy, Window win,
