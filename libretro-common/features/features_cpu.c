675344c4a7458533dff717b4a6aa65020c6bf561
U-DESKTOP-SPFP6AQ\twistedtechre <reallibretroretroarch@gmail.com>
2026-04-08 05:08:21 +0200

    Indent nits

--- libretro-common/features/features_cpu.c
+++ libretro-common/features/features_cpu.c
@@ -522,21 +522,21 @@ unsigned cpu_features_get_core_amount(void)
    CFGU_GetSystemModel(&device_model);/*(0 = O3DS, 1 = O3DSXL, 2 = N3DS, 3 = 2DS, 4 = N3DSXL, 5 = N2DSXL)*/
    switch (device_model)
    {
-		case 0:
-		case 1:
-		case 3:
-			/*Old 3/2DS*/
-			return 2;
+      case 0:
+      case 1:
+      case 3:
+         /*Old 3/2DS*/
+         return 2;
 
-		case 2:
-		case 4:
-		case 5:
-			/*New 3/2DS*/
-			return 4;
+      case 2:
+      case 4:
+      case 5:
+         /*New 3/2DS*/
+         return 4;
 
-		default:
-			/*Unknown Device Or Check Failed*/
-			break;
+      default:
+         /*Unknown Device Or Check Failed*/
+         break;
    }
    return 1;
 #elif defined(WIIU)
