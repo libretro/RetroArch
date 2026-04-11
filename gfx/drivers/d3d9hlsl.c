03f8fd1fc3070fd23c444402624e58de3f51973b
U-DESKTOP-SPFP6AQ\twistedtechre <reallibretroretroarch@gmail.com>
2026-04-11 18:05:02 +0200

    Merge some shader files

--- gfx/drivers/d3d9hlsl.c
+++ gfx/drivers/d3d9hlsl.c
@@ -50,7 +50,6 @@
 #include "../common/d3dcompiler_common.h"
 #include "../common/d3d9_common.h"
 
-#include "d3d_shaders/opaque.hlsl.d3d9.h"
 #include "../video_driver.h"
 
 #ifdef HAVE_CONFIG_H
@@ -83,6 +82,51 @@
 #include "../../core.h"
 #include "../../verbosity.h"
 #include "../../retroarch.h"
+
+#include "d3d_shaders/shaders_common.h"
+
+static const char *stock_hlsl_program = CG(
+      void main_vertex
+      (
+         float3 position : POSITION,
+         float4 color    : COLOR,
+         float2 texCoord : TEXCOORD0,
+
+         uniform float4x4 modelViewProj,
+
+         out float4 oPosition : POSITION,
+         out float4 oColor : COLOR,
+         out float2 otexCoord : TEXCOORD
+      )
+      {
+         oPosition = mul(modelViewProj, float4(position, 1.0f));
+         oColor = color;
+         otexCoord = texCoord;
+      }
+
+      struct output
+      {
+         float4 color: COLOR;
+      };
+
+      struct input
+      {
+         float2 video_size;
+         float2 texture_size;
+         float2 output_size;
+         float frame_count;
+         float frame_direction;
+         float frame_rotation;
+      };
+
+      output main_fragment(float4 color : COLOR, float2 texCoord : TEXCOORD0,
+      uniform sampler2D decal : TEXUNIT0, uniform input IN)
+      {
+         output OUT;
+         OUT.color = color * tex2D(decal, texCoord);
+         return OUT;
+      }
+);
 
 static INLINE void d3d_matrix_identity(void *_pout)
 {
--- gfx/drivers_shader/shader_gl_cg.c
+++ gfx/drivers_shader/shader_gl_cg.c
@@ -52,6 +52,73 @@
 #include "../../state_manager.h"
 #endif
 
+#if defined(HAVE_OPENGLES)
+#define CG(src)   "" #src
+#define GLSL(src) "#extension GL_OES_standard_derivatives : enable\n" \
+                  "#ifdef GL_ES\n" \
+                  "  #ifdef GL_FRAGMENT_PRECISION_HIGH\n" \
+                  "    precision highp float;\n" \
+                  "  #else\n" \
+                  "    precision mediump float;\n" \
+                  "  #endif\n" \
+                  "#else\n" \
+                  "  precision mediump float;\n" \
+                  "#endif\n" #src
+#define GLSL_STANDARD_DERIVATIVES(src) "#version 130\n" \
+                  "#extension GL_OES_standard_derivatives : enable\n" \
+                  "#ifdef GL_ES\n" \
+                  "  #ifdef GL_FRAGMENT_PRECISION_HIGH\n" \
+                  "    precision highp float;\n" \
+                  "  #else\n" \
+                  "    precision mediump float;\n" \
+                  "  #endif\n" \
+                  "#else\n" \
+                  "  precision mediump float;\n" \
+                  "#endif\n" #src
+#else
+#define CG(src)   "" #src
+#define GLSL(src) "" #src
+#define GLSL_STANDARD_DERIVATIVES(src) "" #src
+#endif
+
+#ifndef GLSL_300
+#define GLSL_300(src)   "#version 300 es\n"   #src
+#endif
+
+static const char *stock_cg_gl_program = CG(
+      struct input
+      {
+        float2 tex_coord;
+        float4 color;
+        float4 vertex_coord;
+        uniform float4x4 mvp_matrix;
+        uniform sampler2D texture;
+      };
+
+      struct vertex_data
+      {
+        float2 tex;
+        float4 color;
+      };
+
+      void main_vertex
+      (
+        out float4 oPosition : POSITION,
+        input IN,
+        out vertex_data vert
+      )
+      {
+        oPosition = mul(IN.mvp_matrix, IN.vertex_coord);
+        vert = vertex_data(IN.tex_coord, IN.color);
+      }
+
+      float4 main_fragment(input IN, vertex_data vert, uniform sampler2D s0 : TEXUNIT0) : COLOR
+      {
+        return vert.color * tex2D(s0, vert.tex);
+      }
+);
+
+
 #define PREV_TEXTURES         (GFX_MAX_TEXTURES - 1)
 
 #define set_param_2f(param, x, y)    if (param) cgGLSetParameter2f(param, x, y)
@@ -137,8 +204,6 @@ struct uniform_cg
       cgGLSetTextureParameter(param, texture); \
       cgGLEnableTextureParameter(param); \
    }
-
-#include "../drivers/gl_shaders/opaque.cg.h"
 
 static void gl_cg_set_uniform_parameter(
       void *data,
