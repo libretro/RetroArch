fc4c6c7de4da83f99039309cb3b4810647e8b17b
hunterk <hunter_kaller@yahoo.com>
2026-05-31 19:24:40 -0500

    stop 'viewport override' logspam
    buildfix

--- retroarch.c
+++ retroarch.c
@@ -4155,6 +4155,7 @@ bool command_event(enum event_command cmd, void *data)
 
             ol->index                      = ol->next_index;
             ol->active                     = &ol->overlays[ol->index];
+            ((struct overlay *)ol->active)->viewport_override_logged = false;
 
             input_overlay_opacity          = (ol->flags & INPUT_OVERLAY_IS_OSK)
                   ? settings->floats.input_osk_overlay_opacity
