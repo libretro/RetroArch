018cfbc30ea60d7fee50c4dde7c0bfd233e879eb
Craig <1188869+cscd98@users.noreply.github.com>
2026-09-12 00:51:02 +0100

    webOS: use SDL3 by default on aarch64 builds, disable popup keyboard on startup (#19533)
    * webOS: use SDL3 by default on aarch64 builds
    
    * webOS: don't use SDL_StartTextInput on webOS as causes popup keyboard on startup

--- gfx/common/sdl3_common.c
+++ gfx/common/sdl3_common.c
@@ -275,8 +275,10 @@ static SDL_Window *sdl3_window_create(unsigned width, unsigned height,
    /* SDL_EVENT_TEXT_INPUT is emitted for windows that opted in for
     * it. The SDL3 input driver handles those events for menu
     * text entry and core keyboard callbacks. */
+#ifndef WEBOS
    if (!sdl3_uses_screen_keyboard())
       SDL_StartTextInput(win);
+#endif
 
    return win;
 }
