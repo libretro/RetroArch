f494e600a6d49e2a16a07d74ee280d65e03efa07
sonninnos <tony@pihaus.org>
2026-09-25 02:13:09 +0300

    Use common value label for video_shader_watch_files since it is a normal bool

--- menu/cbs/menu_cbs_get_value.c
+++ menu/cbs/menu_cbs_get_value.c
@@ -231,24 +231,6 @@ static size_t menu_action_setting_disp_set_label_shader_filter_pass(
    return 0;
 }
 
-static size_t menu_action_setting_disp_set_label_shader_watch_for_changes(
-      file_list_t* list,
-      unsigned *w, unsigned type, unsigned i,
-      const char *label,
-      char *s, size_t len,
-      const char *path,
-      char *s2, size_t len2)
-{
-   menu_file_list_cbs_t *cbs = (menu_file_list_cbs_t*)
-      list->list[i].actiondata;
-   *w = 19;
-   if (path && *path)
-      strlcpy(s2, path, len2);
-   if (cbs && cbs->setting && *cbs->setting->value.target.boolean)
-      return strlcpy(s, msg_hash_to_str(MENU_ENUM_LABEL_VALUE_TRUE), len);
-   return strlcpy(s, msg_hash_to_str(MENU_ENUM_LABEL_VALUE_FALSE), len);
-}
-
 static size_t menu_action_setting_disp_set_label_shader_num_passes(
       file_list_t* list,
       unsigned *w, unsigned type, unsigned i,
@@ -2029,7 +2011,7 @@ static int menu_cbs_init_bind_get_string_representation_compare_label(
          case MENU_ENUM_LABEL_SHADER_WATCH_FOR_CHANGES:
 #if defined(HAVE_CG) || defined(HAVE_GLSL) || defined(HAVE_SLANG) || defined(HAVE_HLSL)
             BIND_ACTION_GET_VALUE(cbs,
-                  menu_action_setting_disp_set_label_shader_watch_for_changes);
+                  menu_action_setting_disp_set_label_setting_bool);
 #endif
             break;
          case MENU_ENUM_LABEL_VIDEO_SHADER_PASS:
