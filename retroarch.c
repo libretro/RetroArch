6dc05aa5570c107bfc47232419b3438764d1e0d1
libretroadmin <libretroadmin@users.noreply.github.com>
2026-09-25 01:19:03 +0200

    Remove unused variable

--- retroarch.c
+++ retroarch.c
@@ -3314,7 +3314,6 @@ bool command_event(enum event_command cmd, void *data)
 {
    struct rarch_state *p_rarch     = &rarch_st;
    runloop_state_t *runloop_st     = runloop_state_get_ptr();
-   uico_driver_state_t *uico_st    = uico_state_get_ptr();
 #if defined(HAVE_ACCESSIBILITY) || defined(HAVE_TRANSLATE)
    access_state_t *access_st       = access_state_get_ptr();
 #endif
